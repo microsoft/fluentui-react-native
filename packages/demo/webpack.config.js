55541c442d38312f730c2895b4998631cf60b0a2
Jason Morse <jasonmo@microsoft.com>
2019-07-28 02:03:23 -0700

    add start of a demo app

---
+++ packages/demo/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
