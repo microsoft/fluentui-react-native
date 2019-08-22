eb937a888ca75f6f07440343c257df3362715fd5
Jason Morse <jasonmo@microsoft.com>
2019-08-21 17:07:35 -0700

    Copy over build logic from fabric

--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -1,17 +1,10 @@
-const path = require('path');
-
-/** Jest config for packages within the just monorepo */
-module.exports = {
-  roots: ['<rootDir>/src'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s'],
-  reporters: [path.resolve(__dirname, './jest-reporter.js')],
-  verbose: true,
-  globals: {
-    'ts-jest': {
-      tsConfig: path.resolve(process.cwd(), 'tsconfig.json'),
-      packageJson: path.resolve(process.cwd(), 'package.json')
-    }
-  }
+const config = {
+  moduleNameMapper: {
+    'office-ui-fabric-react/lib/(.*)$': '<rootDir>/src/$1'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(js)$',
+  transform: {},
+  testEnvironment: 'node'
 };
+
+module.exports = config;
