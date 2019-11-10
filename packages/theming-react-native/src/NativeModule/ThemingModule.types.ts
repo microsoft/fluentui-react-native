dac5776ebbd4d67708eabb5771074e1faeef6176
REDMOND\adrum <adrum@microsoft.com>
2019-11-09 16:14:18 -0800

    :sparkles: Add Theming Module helper wrapper

---
+++ packages/theming-react-native/src/NativeModule/ThemingModule.types.ts
@@ -0,0 +1,36 @@
+import { IWindowsPalette, ITypography, ColorValue } from '@uifabricshared/theming-ramp';
+import { INativeTheme, INativeThemeDefinition } from '..';
+
+export type PlatformDefaultsChangedCallback = () => void;
+
+export interface ICxxException {
+  message: string;
+}
+
+export interface INativeColorRamps {
+  FluentGrays: ColorValue[];
+  ClassicGrays: ColorValue[];
+  App: ColorValue[];
+  Sepias: ColorValue[];
+  [key: string]: ColorValue[];
+}
+
+export interface IPaletteIDs {
+  [key: string]: string;
+}
+
+export interface IThemingModule {
+  getPalette(palette?: string): IWindowsPalette | ICxxException;
+  typography: ITypography;
+  ramps: INativeColorRamps;
+  palettes: IPaletteIDs;
+}
+
+export type IPlatformThemeDefinition = (parent: INativeTheme) => INativeThemeDefinition;
+
+export interface IThemingModuleHelper {
+  getPlatformTheme: (palette?: string) => INativeTheme;
+  getPlatformThemeDefinition: (palette?: string) => IPlatformThemeDefinition;
+  getPalettes: () => string[];
+  addListener: (listener: PlatformDefaultsChangedCallback) => void; // TODO: Should probably be able to remove
+}
