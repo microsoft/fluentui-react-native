631a54b7debf3b6826e0ea192306762726abb799
REDMOND\adrum <adrum@microsoft.com>
2019-11-13 16:40:54 -0800

    :white_check_mark: fix jest tests for stylesheets

--- packages/themed-stylesheet/jest.config.js
+++ packages/themed-stylesheet/jest.config.js
@@ -1,1 +1,2 @@
-module.exports = require('../../scripts/jest.config');
+let { createRawConfig } = require('@uifabricshared/build-native/jest/jest-resources');
+module.exports = { preset: 'react-native', testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$' };
