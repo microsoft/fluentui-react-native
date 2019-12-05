c35a09bf630c798c5a79516da5ca5fc9ece09772
Jason Morse <jasonmo360@gmail.com>
2019-12-05 14:44:40 -0800

    fix error on web where __DEV__ is undefined

--- packages/theming-react-native/src/NativeModule/ThemingModuleHelpers.ts
+++ packages/theming-react-native/src/NativeModule/ThemingModuleHelpers.ts
@@ -9,11 +9,19 @@ import {
 import { getBaselinePlatformTheme } from '../BaselinePlatformDefaults';
 import { IOfficePalette, paletteFromOfficeColors } from './office';
 
+function _isRNDevMode(): boolean {
+  try {
+    return __DEV__;
+  } catch {
+    return false;
+  }
+}
+
 /**
  *  If we have a userAgent string, let's assume we're web debugging.  __DEV__ is for developer bundles.  Currently,
  *  react-native only polyfills navigator with { product: 'ReactNative', geolocation: NativeModules.Geolocation }
  */
-export const useFakePalette = __DEV__ && navigator && navigator.userAgent !== undefined;
+export const useFakePalette = _isRNDevMode() && navigator && navigator.userAgent !== undefined;
 
 const createColorRamp = ({ values, index = -1 }: Partial<IColorRamp>) => ({
   values,
