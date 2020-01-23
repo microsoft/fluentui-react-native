31ac0493368ab72bb602512e7806eea0142485a4
Luan Nguyen <kinhln@microsoft.com>
2020-01-23 12:15:38 -0800

    Undo metro config file.

--- scripts/tasks/metro-pack.js
+++ scripts/tasks/metro-pack.js
@@ -1,15 +1,5 @@
 // @ts-check
 
-// const path = require('path');
-// const a = require('just-scripts');
-const Metro = require('metro');
-
-exports.metroPack = async function() {
-  const config = await Metro.loadConfig();
-
-  await Metro.runBuild(config, {
-    platform: 'win32',
-    minify: true,
-    out: 'react-native-uifabric.js'
-  });
+exports.metroPack = function() {
+  console.warn('Metro bundling task is not implemented yet. It is coming soon.');
 };
