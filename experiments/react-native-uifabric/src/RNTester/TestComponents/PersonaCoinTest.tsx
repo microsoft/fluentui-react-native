f1214090055190938f556cf3fb742a06f177ccb6
Luan Nguyen <kinhln@microsoft.com>
2020-01-31 12:06:35 -0800

    add show image option.

--- experiments/react-native-uifabric/src/RNTester/TestComponents/PersonaCoinTest.tsx
+++ experiments/react-native-uifabric/src/RNTester/TestComponents/PersonaCoinTest.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import { PersonaCoin, PersonaSize, PersonaCoinColor } from '../../components/PersonaCoin';
+import { Switch, View, Text } from 'react-native';
+import { Stack } from '../../components/Stack';
 
 export const PersonaCoinTest: React.FunctionComponent<{}> = () => {
-    
+
+    const [showImage, setShowImage] = React.useState(true);
+
     return (
-      <PersonaCoin
-        size={PersonaSize.size100}
-        initials='SN'
-        imageUrl='https://www.microsoft.com/en-us/CMSImages/satya.jpg?version=0881eb71-4942-b627-d602-84c832b8a0b6&amp;CollectionId=1b46ce2d-c90d-421e-94f1-cfb6bc6ef6ec'
-        coinColor={PersonaCoinColor.burgundy} />
+        <Stack gap={2}>
+            <View style={{flexDirection: 'row', marginBottom: 8, alignItems: 'center'}}>
+                <Switch 
+                    value={showImage}
+                    onValueChange={setShowImage}
+                    />
+                <Text>Show image</Text>
+            </View>
+
+            <PersonaCoin
+              size={PersonaSize.size100}
+              initials='SN'
+              imageUrl={showImage ? 'https://www.microsoft.com/en-us/CMSImages/satya.jpg?version=0881eb71-4942-b627-d602-84c832b8a0b6&amp;CollectionId=1b46ce2d-c90d-421e-94f1-cfb6bc6ef6ec' : undefined}
+              coinColor={PersonaCoinColor.burgundy} />
+        </Stack>
     );
   };
   
--- experiments/react-native-uifabric/src/components/PersonaCoin/PersonaCoin.tsx
+++ experiments/react-native-uifabric/src/components/PersonaCoin/PersonaCoin.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Image } from 'react-native';
 import { IPersonaCoinProps, PersonaSize } from './PersonaCoin.types';
+import { ViewWin32, TextWin32 } from '@office-iss/react-native-win32';
 
 function getPhysicalSize(size: PersonaSize): number {
   switch (size) {
@@ -15,13 +16,45 @@ function getPhysicalSize(size: PersonaSize): number {
   }
 }
 
+function getFontSize(size: PersonaSize): number {
+  switch (size) {
+    case PersonaSize.size8: return 8;
+    case PersonaSize.size24: return 10;
+    case PersonaSize.size32: return 12;
+    case PersonaSize.size40: return 14;
+    case PersonaSize.size48: return 16;
+    case PersonaSize.size56: return 18;
+    case PersonaSize.size72: return 20;
+    case PersonaSize.size100: return 36;
+  }
+}
+
+
 export const PersonaCoin: React.FunctionComponent<IPersonaCoinProps> = (props: IPersonaCoinProps) => {
-  const { imageUrl, size } = props;
+  const { imageUrl, size, initials } = props;
   const physicalSize = getPhysicalSize(size || PersonaSize.size40);
-  return (
+  
+  const showImage = !!imageUrl;
+  if (showImage) {
+    return (
       <Image 
         style={{borderRadius: physicalSize / 2, width: physicalSize, height: physicalSize}}
         source={{uri: imageUrl, width: physicalSize, height: physicalSize}}
       />
-  )
+    );
+  } else {
+    const fontSize = getFontSize(size || PersonaSize.size40);
+    return (
+      <ViewWin32
+        style={{
+          borderRadius: physicalSize / 2, 
+          width: physicalSize, 
+          height: physicalSize,
+          backgroundColor: 'green', 
+          justifyContent: 'center', 
+          alignItems: 'center'}}>
+          <TextWin32 style={{fontSize: fontSize, color: 'white'}}>{initials}</TextWin32>
+      </ViewWin32>
+    );
+  }
 };
