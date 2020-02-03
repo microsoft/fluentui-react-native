a83d1b9bbc251d6896109df5ba94d3962a493ebb
Luan Nguyen <kinhln@microsoft.com>
2020-02-03 11:33:49 -0800

    set presence prop

--- experiments/react-native-uifabric/src/RNTester/TestComponents/PersonaCoinTest.tsx
+++ experiments/react-native-uifabric/src/RNTester/TestComponents/PersonaCoinTest.tsx
@@ -82,6 +82,7 @@ export const PersonaCoinTest: React.FunctionComponent<{}> = () => {
           size={imageSize}
           initials="SN"
           imageDescription="Photo of Satya Nadella"
+          presence={presence}
           imageUrl={
             showImage
               ? 'https://www.microsoft.com/en-us/CMSImages/satya.jpg?version=0881eb71-4942-b627-d602-84c832b8a0b6&amp;CollectionId=1b46ce2d-c90d-421e-94f1-cfb6bc6ef6ec'
