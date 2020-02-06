c842a19702b4af7790b9d41841b2cb70ac1c3012
Luan Nguyen <kinhln@microsoft.com>
2020-02-05 17:20:15 -0800

    delete Persona folder

--- experiments/react-native-uifabric/src/components/Persona/Persona.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { IPersonaProps } from './Persona.types';
-import { ViewWin32 } from '@office-iss/react-native-win32';
-
-export const Persona: React.FunctionComponent<IPersonaProps> = (props: IPersonaProps) => {
-  return <ViewWin32 {...props} />;
-};
--- experiments/react-native-uifabric/src/components/index.ts
+++ experiments/react-native-uifabric/src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './Button/index';
 export * from './FocusTrapZone/index';
 export * from './Link/index';
-export * from './Persona/index';
 export * from './PersonaCoin/index';
 export * from './Pressable/index';
 export * from './Separator/index';
