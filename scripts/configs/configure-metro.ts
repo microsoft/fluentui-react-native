9f46ffedcfa33e73d248c227c79189cb41ceed7d
Jason Morse <jasonmo@microsoft.com>
2020-02-27 16:11:56 -0800

    generalize platform overridding for metro

--- scripts/configs/configure-metro.ts
+++ scripts/configs/configure-metro.ts
@@ -1,19 +1,27 @@
+import { getPackagePaths, resolveModule, resolveFile } from '../utils/queryFiles';
+
 export type PlatformValue = 'win32' | 'ios' | 'android' | 'windows' | 'web' | 'macos';
 
 export interface IMetroOptions {
   /**
    * bundle name, potentially used to keep caches distinct
    */
-  bundle: string;
+  bundle?: string;
+
   /**
    * which platforms should this bundle target, either a single string or an array
    */
-  platforms?: PlatformValue | PlatformValue[];
+  platform?: PlatformValue;
 }
 
 function prepareRegex(blacklistPath: string) {
   return new RegExp(`${blacklistPath.replace(/[/\\\\]/g, '[/\\\\]')}.*`);
 }
+
+const _platformFlags: { [K in PlatformValue]?: { rnOverride: string } } = {
+  win32: { rnOverride: '@office-iss/react-native-win32' },
+  windows: { rnOverride: 'react-native-windows' }
+};
 
 /**
  * This configures metro bundling based on the passed in options.
@@ -21,45 +29,45 @@ function prepareRegex(blacklistPath: string) {
  * @param options - metro configuration options
  */
 export function configureMetro(options: IMetroOptions): any {
-  const { bundle = 'anonymous', platforms = [] } = options;
-  const _win32 = Array.isArray(platforms) ? platforms.find(p => p === 'win32') : platforms === 'win32';
+  const { platform = 'iOS' } = options;
+  const rnOverride: string = platform && _platformFlags[platform] && _platformFlags[platform].rnOverride;
 
   const path = require('path');
-  const { getPackagePaths, resolveModule, resolveFile } = require('../utils/queryFiles');
   const blacklist = require('metro-config/src/defaults/blacklist');
-  const rnWin32Path = resolveModule('@office-iss/react-native-win32');
   const rnPath = resolveModule('react-native');
+  const rnName = rnOverride || 'react-native';
+  const rnPlatformPath = (rnOverride && resolveModule(rnOverride)) || rnPath;
 
   return {
     // WatchFolders is only needed due to the yarn workspace layout of node_modules, we need to watch the symlinked locations separately
     watchFolders: [
       // Include hoisted modules
       path.resolve(__dirname, '../..', 'node_modules'),
-      rnWin32Path,
+      rnPlatformPath,
       ...getPackagePaths()
     ],
     serializer: {
       getPolyfills: () => {
         return [
-          resolveFile('@office-iss/react-native-win32/Libraries/polyfills/console.js'),
-          resolveFile('@office-iss/react-native-win32/Libraries/polyfills/error-guard.js'),
-          resolveFile('@office-iss/react-native-win32/Libraries/polyfills/Object.es7.js')
+          resolveFile(rnName + '/Libraries/polyfills/console.js'),
+          resolveFile(rnName + '/Libraries/polyfills/error-guard.js'),
+          resolveFile(rnName + '/Libraries/polyfills/Object.es7.js')
         ];
       }
     },
     resolver: {
-      extraNodeModules: { 'react-native': rnWin32Path },
+      extraNodeModules: { 'react-native': rnPlatformPath },
       blacklistRE: blacklist([
         /node_modules\/react-native\/.*/,
         /node_modules\/.*\/node_modules\/react-native\/.*/,
         prepareRegex(rnPath),
-        prepareRegex(rnWin32Path + '/node_modules/react-native'),
+        prepareRegex(rnPlatformPath + '/node_modules/react-native'),
         prepareRegex(path.resolve('.', 'node_modules/react-native')),
         prepareRegex(path.resolve('.', 'node_modules/@office-iss/react-native-win32'))
       ]),
-      hasteImplModulePath: resolveFile('@office-iss/react-native-win32/jest/hasteImpl.js'),
+      hasteImplModulePath: resolveFile(rnName + '/jest/hasteImpl.js'),
       platforms: ['win32', 'ios', 'android', 'windows', 'web', 'macos'],
-      providesModuleNodeModules: ['@office-iss/react-native-win32']
+      providesModuleNodeModules: [rnName]
     },
     transformer: {
       getTransformOptions: async () => ({
