c6cf010175e69bfaa16e33b98a8d2f67dad74cad
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2020-03-10 08:35:36 -0700

    Split components (#123)
    * move components to individual packages
    
    * fix component dependency build breaks
    
    * export PersonaCoin style utility

---
+++ packages/components/PersonaCoin/src/PersonaCoin.tokens.initials.ts
@@ -0,0 +1,19 @@
+import { styleFunction } from '@uifabricshared/foundation-tokens';
+import { TextProps } from 'react-native';
+import { IPersonaCoinTokens } from './PersonaCoin.types';
+import { ITheme } from '@uifabricshared/theming-ramp';
+import { calculateEffectiveSizes } from './PersonaCoin.helpers';
+
+const _initialsKeyProps: (keyof IPersonaCoinTokens)[] = ['coinSize', 'initialsSize', 'size'];
+
+function _buildInitialsStyles(tokenProps: IPersonaCoinTokens /*, theme: ITheme */): TextProps {
+  const { initialsSize } = calculateEffectiveSizes(tokenProps);
+
+  return {
+    style: {
+      fontSize: initialsSize
+    }
+  };
+}
+
+export const buildInitialsStyles = styleFunction<TextProps, IPersonaCoinTokens, ITheme>(_buildInitialsStyles, _initialsKeyProps);
