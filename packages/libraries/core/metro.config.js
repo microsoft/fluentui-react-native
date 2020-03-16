fb9e1df0358341ad9c0d0c6aa902f7d863d6176b
Jason Morse <jasonmo@microsoft.com>
2020-03-16 13:52:59 -0700

    fix bundle names from package rename

--- packages/libraries/core/metro.config.js
+++ packages/libraries/core/metro.config.js
@@ -2,6 +2,6 @@
 
 const { configureMetro } = require('@uifabricshared/build-native');
 module.exports = configureMetro({
-  bundle: '@fluentui/react-native',
+  bundle: 'fluentui-react-native',
   platform: 'win32'
 });
