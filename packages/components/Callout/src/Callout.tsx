baa4977546afb3b47bc78800e5dd7c70637e05e5
Pat Boyd <ppatboyd@outlook.com>
2020-03-18 17:07:55 -0700

    Add FluentUI Callout component

---
+++ packages/components/Callout/src/Callout.tsx
@@ -0,0 +1,30 @@
+import { requireNativeComponent } from 'react-native';
+import { IUseComposeStyling } from '@uifabricshared/foundation-compose';
+import { calloutName, ICalloutProps, ICalloutSlotProps, ICalloutType } from './Callout.types';
+import { useViewCommandFocus } from '@fluentui-react-native/interactive-hooks';
+import { mergeSettings } from '@uifabricshared/foundation-settings';
+import { composable } from '@uifabricshared/foundation-composable';
+
+const RCTCallout = requireNativeComponent('RCTCallout');
+
+export const Callout = composable<ICalloutType>({
+  displayName: calloutName,
+  usePrepareProps: (props: ICalloutProps, useStyling: IUseComposeStyling<ICalloutType>) => {
+    const { componentRef, ...rest } = props;
+    const buttonRef = useViewCommandFocus(componentRef);
+
+    const slotProps = mergeSettings<ICalloutSlotProps>(useStyling(props), {
+      root: {
+        ref: buttonRef,
+        ...rest
+      }
+    });
+
+    return { slotProps };
+  },
+  slots: {
+    root: RCTCallout
+  }
+});
+
+export default Callout;
