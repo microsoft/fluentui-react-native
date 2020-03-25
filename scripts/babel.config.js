cf44285075aecfae7c274a6c7632eac1d038e4f9
Jason Morse <jasonmo@microsoft.com>
2020-03-25 13:29:19 -0700

    fix build/bundling errors after syncing master

--- scripts/babel.config.js
+++ scripts/babel.config.js
@@ -6,44 +6,5 @@ module.exports = {
     '@babel/preset-react',
     '@babel/preset-typescript',
     'module:metro-react-native-babel-preset'
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          react: require.resolve('react', {
-            paths: [path.join(__dirname, './')]
-          }),
-          '^react-native$': require.resolve(`react-native`, {
-            paths: [path.join(__dirname, './')]
-          }),
-          '^react-native/(.+)': ([, name]) =>
-            require.resolve(`react-native/${name}`, {
-              paths: [path.join(__dirname, './')]
-            })
-        },
-        extensions: [
-          '.ios.js',
-          '.ios.ts',
-          '.ios.tsx',
-          '.android.js',
-          '.android.ts',
-          '.android.tsx',
-          '.native.js',
-          '.native.ts',
-          '.native.tsx',
-          '.win32.js',
-          '.win32.ts',
-          '.win32.tsx',
-          '.windows.js',
-          '.windows.ts',
-          '.windows.tsx',
-          '.js',
-          '.ts',
-          '.tsx'
-        ]
-      }
-    ]
   ]
 };
