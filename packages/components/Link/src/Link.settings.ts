db17f8a281c6aae8b72d0b016e172bd4ffc56ea2
REDMOND\krsiler <krsiler@microsoft.com>
2020-03-25 12:43:47 -0700

    Use fontVariant instead of fontFamily, fontSize, and fontWeight

--- packages/components/Link/src/Link.settings.ts
+++ packages/components/Link/src/Link.settings.ts
@@ -6,9 +6,7 @@ import { IViewProps } from '@fluentui-react-native/adapters';
 export const settings: IComposeSettings<ILinkType> = [
   {
     tokens: {
-      fontFamily: 'primary',
-      fontSize: 'secondary',
-      fontWeight: 'regular',
+      fontVariant: 'secondaryStandard',
       color: 'link'
     },
     root: {
