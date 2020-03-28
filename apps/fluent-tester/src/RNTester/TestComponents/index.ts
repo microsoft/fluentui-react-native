348acddba4424567647e7e8e43bf0004e0acca12
Jason Morse <jasonmo@microsoft.com>
2020-03-27 22:40:58 -0700

    move checkbox test to match pattern from other components

--- apps/fluent-tester/src/RNTester/TestComponents/index.ts
+++ apps/fluent-tester/src/RNTester/TestComponents/index.ts
@@ -1,18 +1,7 @@
 import * as React from 'react';
-<<<<<<< HEAD:apps/fluent-tester/src/RNTester/TestComponents/index.ts
-import { ButtonFocusTest } from './ButtonFocusTest';
-import { PressableTest } from './PressableTest';
-import { LinkTest } from './LinkTest';
-import { SeparatorTest } from './SeparatorTest';
-import { ThemeTest } from './ThemeTest';
-import { FocusTrapTest } from './FocusTrapZoneTest';
-import { PersonaCoinTest } from './PersonaCoin/PersonaCoinTest';
-import { RadioGroupTest } from './RadioGroupTest';
-import { PersonaTest } from './Persona/PersonaTest';
-=======
 import { ButtonFocusTest } from './Button';
 import { CalloutTest } from './Callout';
-import { CheckboxTest } from './CheckboxTest';
+import { CheckboxTest } from './Checkbox';
 import { FocusTrapTest } from './FocusTrapZone';
 import { LinkTest } from './Link';
 import { PersonaTest } from './Persona';
@@ -21,7 +10,6 @@ import { PressableTest } from './Pressable';
 import { RadioGroupTest } from './RadioGroup';
 import { SeparatorTest } from './Separator';
 import { ThemeTest } from './Theme';
->>>>>>> upstream/master:experiments/tester/src/RNTester/TestComponents/index.ts
 
 export type TestDescription = {
   name: string;
