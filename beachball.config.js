51eb45381516089916b07d055539738bd340d379
Jason Morse <jasonmo@microsoft.com>
2020-04-15 20:01:35 -0700

    publish with correct main/module references

--- beachball.config.js
+++ beachball.config.js
@@ -2,13 +2,12 @@ module.exports = {
   disallowedChangeTypes: ['major'],
   hooks: {
     prepublish: bumpInfo => {
+      // iterate through the packages that are being published as part of the bump info
       for (const pkg in bumpInfo.packageInfos) {
         const packageJson = bumpInfo.packageInfos[pkg];
-        if (packageJson.main && packageJson.main.endsWith('.ts')) {
-          packageJson.main = packageJson.main.replace('.ts', '.js').replace('src/', 'lib-commonjs');
-        }
-        if (packageJson.module && packageJson.module.endsWith('.ts')) {
-          packageJson.module = packageJson.module.replace('.ts', '.js').replace('src/', 'lib');
+        if (packageJson && packageJson.onPublish && typeof packageJson.onPublish === 'object') {
+          Object.assign(packageJson, packageJson.onPublish);
+          delete packageJson.onPublish;
         }
       }
     }
