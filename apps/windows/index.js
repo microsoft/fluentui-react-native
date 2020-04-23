3d19cf47b60bf0bdf8643f0484dceb3c970aa513
Jason Morse <jasonmo@microsoft.com>
2020-04-22 17:29:48 -0700

    fix windows bundle to point to tester

--- apps/windows/index.js
+++ apps/windows/index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
+'use strict';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {ThemeProvider} from '@uifabricshared/theming-react-native';
+import {FabricTester, customRegistry} from '@fluentui-react-native/tester';
 
-AppRegistry.registerComponent(appName, () => App);
+const RNTesterApp = (props) => {
+  return (
+    <ThemeProvider registry={customRegistry}>
+      <FabricTester {...props} />
+    </ThemeProvider>
+  );
+};
+
+AppRegistry.registerComponent('RNTesterApp', () => RNTesterApp);
+
+export default RNTesterApp;
