4095ef556d845b68e2cf0b6e70c1fe1eb6f02faa
samuelfreiberg <safreibe@microsoft.com>
2020-05-14 13:32:33 -0700

    Small Link test page update

--- apps/fluent-tester/src/RNTester/TestComponents/Link/LinkTest.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/Link/LinkTest.tsx
@@ -18,11 +18,8 @@ export const LinkTest: React.FunctionComponent<{}> = () => {
         Link Test Page
       </Text>
       <Stack style={stackStyle}>
-        <Link url="https://www.bing.com/" content="Click to find yourself." />
-        <Link onPress={doPress} content="Click to alert yourself." />
-        <Link url="https://www.google.com/" disabled content="Click to advertise yourself.">
-          <Square />
-        </Link>
+        <Link url="https://www.bing.com/" content="Click to navigate." />
+        <Link onPress={doPress} content="Click to alert." />
       </Stack>
     </View>
   );
