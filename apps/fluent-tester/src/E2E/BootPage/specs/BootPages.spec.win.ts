c984af734d7f68ce9ef6eef2a6df4e1afa2301af
samuelfreiberg <safreibe@microsoft.com>
2020-05-28 14:44:24 -0700

    d

--- apps/fluent-tester/src/E2E/BootPage/specs/BootPages.spec.win.ts
+++ apps/fluent-tester/src/E2E/BootPage/specs/BootPages.spec.win.ts
@@ -11,6 +11,7 @@ import RadioGroupTestPage from '../../RadioGroup/pages/RadioGroupTestPage.win';
 import SeparatorTestPage from '../../Separator/pages/SeparatorTestPage.win';
 import TextTestPage from '../../Text/pages/TextTestPage.win';
 import ThemeTestPage from '../../Theme/pages/ThemeTestPage.win';
+// test
 
 describe('Click on each test page and check if it renders', function() {
   it('Button Test Page', () => {
