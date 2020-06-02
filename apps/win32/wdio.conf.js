0ea85196f5c0585746b0438640368f0cfe0a25cb
samuelfreiberg <safreibe@microsoft.com>
2020-06-02 11:05:18 -0700

    Changing screenshot reporting

--- apps/win32/wdio.conf.js
+++ apps/win32/wdio.conf.js
@@ -141,10 +141,20 @@ exports.config = {
    * Function to be executed after a test (in Mocha/Jasmine).
    */
   afterTest: function(test) {
-    if (test.error !== undefined) {
-      const name = 'ERROR-' + Date.now();
-      browser.saveScreenshot('./reports/errorShots/' + name + '.png');
+    // if test passed, ignore, else take and save screenshot.
+    if(test.passed) {
+      return;
     }
+
+    // get current test title and clean it, to use it as file name
+    const fileName = encodeURIComponent(test.title.replace(/\s+/g, '-'));
+
+    // build file path
+    const filePath = this.screenshotPath + fileName + '.png';
+
+    // save screenshot
+    browser.saveScreenshot(filePath);
+    console.log('\n\tScreenshot location:', filePath, '\n');
   },
 
   /**
