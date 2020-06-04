aa6cda3a8b090bbbe1c3bc45a6660726fcfa0597
Saad Najmi <saadnajmi2@gmail.com>
2020-06-03 20:13:20 -0700

    Get run-ios to work with the src/ directory

--- apps/ios/react-native.config.js
+++ apps/ios/react-native.config.js
@@ -18,16 +18,25 @@
 const macSwitch = '--use-react-native-macos';
 const windowsSwitch = '--use-react-native-windows';
 
+var reactNativePlatformPath = 'node_modules/react-native';
+
 if (process.argv.includes(macSwitch)) {
   process.argv = process.argv.filter((arg) => arg !== macSwitch);
   process.argv.push('--config=metro.config.macos.js');
-  module.exports = {
-    reactNativePath: require.resolve('react-native-macos'),
-  };
+  reactNativePlatformPath = 'node_modules/react-native-macos';
+
 } else if (process.argv.includes(windowsSwitch)) {
   process.argv = process.argv.filter((arg) => arg !== windowsSwitch);
   process.argv.push('--config=metro.config.windows.js');
+  reactNativePlatformPath = 'node_modules/react-native-windows';
+
+} else {
   module.exports = {
-    reactNativePath: 'node_modules/react-native-windows',
-  };
+    reactNativePath: reactNativePlatformPath,
+    project: {
+      ios: {
+        project: "apps/ios/src/FluentUITester.xcworkspace"
+      }
+    }
+  }
 }
