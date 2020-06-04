fb659219fcc84bb437515c97d816e82eb58e3fa9
Krystal Siler <krsiler@microsoft.com>
2020-06-04 15:05:24 -0700

    Update fontSize prop to use CSS pixels instead of typographic points (#222)
    * Change files
    
    * Correct text fontWeights in customize usage examples
    
    * Remove temporary fix for undefined typography object
    
    * Change fontSize type from pt to px
    
    * Update snapshots to reflect fontSize in CSS pixels
    
    * Use commonTestStyles in text examples
    
    * Remove root style from text examples
    
    * Update RNTester styles to use CSS pixels for fontSize
    
    * Update control settings to use CSS pixels for fontSize
    
    * Update controls to use fluentui-react-native text and noTest style
    
    * Change files
    
    * Switch back to react-native text for Persona
    
    * Add stack to text standard usage page

--- packages/components/Checkbox/src/Checkbox.tsx
+++ packages/components/Checkbox/src/Checkbox.tsx
@@ -1,6 +1,7 @@
 /** @jsx withSlots */
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '@fluentui-react-native/text';
 import { ICheckboxState, ICheckboxProps, ICheckboxSlotProps, ICheckboxRenderData, ICheckboxType, checkboxName } from './Checkbox.types';
 import { compose, IUseComposeStyling } from '@uifabricshared/foundation-compose';
 import { ISlots, withSlots } from '@uifabricshared/foundation-composable';
