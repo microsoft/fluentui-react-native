e34b83b2be8a9ad16375dc2bd9bf4978c347f50e
samuelfreiberg <safreibe@microsoft.com>
2020-06-03 17:24:04 -0700

    Testing CI Failure

--- apps/fluent-tester/src/E2E/BootPage/specs/BootTestPage.spec.uwp.ts
+++ apps/fluent-tester/src/E2E/BootPage/specs/BootTestPage.spec.uwp.ts
@@ -9,6 +9,7 @@ import RadioGroupTestPage from '../../RadioGroup/pages/RadioGroupTestPage.win';
 import SeparatorTestPage from '../../Separator/pages/SeparatorTestPage.win';
 import TextTestPage from '../../Text/pages/TextTestPage.win';
 import ThemeTestPage from '../../Theme/pages/ThemeTestPage.win';
+import SvgTestPage from '../../Svg/pages/SvgTestPage.win';
 
 // Before testing begins, allow 45 seconds for bundle to load (WebDriverIO)
 beforeAll(() => {
@@ -44,6 +45,11 @@ describe('Click on each test page and check if it renders', function() {
   it('Pressable Test Page', () => {
     BootTestPage.clickAndGoToPressablePage();
     expect(PressableTestPage.isPageLoaded()).toBeTruthy();
+  });
+
+  it('Svg Test Page', () => {
+    BootTestPage.clickAndGoToSvgPage();
+    expect(SvgTestPage.isPageLoaded()).toBeTruthy();
   });
 
   it('RadioGroup Test Page', () => {
