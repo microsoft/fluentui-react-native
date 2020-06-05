2e488268cc33218749b1152dfbcdd90adf69f848
Saad Najmi <saadnajmi2@gmail.com>
2020-06-05 13:36:31 -0700

    Have yarn ios not run react-native start in the background

--- apps/ios/react-native.config.js
+++ apps/ios/react-native.config.js
@@ -15,23 +15,7 @@
  */
 'use strict';
 
-const macSwitch = '--use-react-native-macos';
-const windowsSwitch = '--use-react-native-windows';
-
-var reactNativePlatformPath = 'node_modules/react-native';
-
-if (process.argv.includes(macSwitch)) {
-  process.argv = process.argv.filter((arg) => arg !== macSwitch);
-  process.argv.push('--config=metro.config.macos.js');
-  reactNativePlatformPath = 'node_modules/react-native-macos';
-} else if (process.argv.includes(windowsSwitch)) {
-  process.argv = process.argv.filter((arg) => arg !== windowsSwitch);
-  process.argv.push('--config=metro.config.windows.js');
-  reactNativePlatformPath = 'node_modules/react-native-windows';
-}
-
 module.exports = {
-  reactNativePath: reactNativePlatformPath,
   project: {
     ios: {
       project: "apps/ios/src/FluentUITester.xcworkspace"
