730f6b30d14d2adba051cfe0aa588adf486b3a19
Tariq <taamireh@microsoft.com>
2020-06-22 17:04:01 -0700

    update package versions for fast refresh

--- apps/win32/src/index.tsx
+++ apps/win32/src/index.tsx
@@ -1,13 +1,15 @@
 'use strict';
+require('react-native').unstable_enableLogBox();
 
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text } from 'react-native';
 import { ThemeProvider } from '@uifabricshared/theming-react-native';
 import { FabricTester, IFabricTesterProps, customRegistry } from '@fluentui-react-native/tester';
 
-const FluentTester: React.FunctionComponent<IFabricTesterProps> = props => {
+const FluentTester: React.FunctionComponent<IFabricTesterProps> = (props) => {
   return (
     <ThemeProvider registry={customRegistry}>
+      <Text>Watch carefully</Text>
       <FabricTester {...props} />
     </ThemeProvider>
   );
