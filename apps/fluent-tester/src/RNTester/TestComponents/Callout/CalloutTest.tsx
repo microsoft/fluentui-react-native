467e250d2df187f9df16beac8e55cad26fb4736f
Patrick Boyd <patboyd@microsoft.com>
2020-06-26 11:48:41 -0700

    Callout isbeakvisible (#292)
    * Clarify supported Callout border tokens
    
    * Import for clarified supported border tokens
    
    * Expose isBeakVisible property
    
    * Enhance Callout test page w/ isBeakVisible, backgroundColor, borderColor, and borderWidth selectors
    
    * remove unnecessary type exports
    
    * Change files

--- apps/fluent-tester/src/RNTester/TestComponents/Callout/CalloutTest.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/Callout/CalloutTest.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ScreenRect, Text, View, Switch } from 'react-native';
+import { ScreenRect, Text, View, Switch, Picker } from 'react-native';
 import { Button, Callout, Separator, IFocusable, RestoreFocusEvent } from '@fluentui/react-native';
 import { fabricTesterStyles } from '../Common/styles';
 import { CALLOUT_TESTPAGE } from './consts';
@@ -16,11 +16,15 @@ export const CalloutTest: React.FunctionComponent<{}> = () => {
   const [customRestoreFocus, setCustomRestoreFocus] = React.useState(false);
   const onRestoreFocusChange = React.useCallback((value) => setCustomRestoreFocus(value), []);
 
-  const stdBtnRef = React.useRef<View>(null);
+  const [isBeakVisible, setIsBeakVisible] = React.useState(false);
+  const onIsBeakVisibleChange = React.useCallback((value) => setIsBeakVisible(value), []);
+
+  const redTargetRef = React.useRef<View>(null);
+  const blueTargetRef = React.useRef<View>(null);
+  const greenTargetRef = React.useRef<View>(null);
   const decoyBtn1Ref = React.useRef<IFocusable>(null);
   const decoyBtn2Ref = React.useRef<IFocusable>(null);
-  const custBtnRef = React.useRef<View>(null);
-  const [anchorRef, setAnchorRef] = React.useState(stdBtnRef);
+  const [anchorRef, setAnchorRef] = React.useState(redTargetRef);
 
   const toggleShowStandardCallout = React.useCallback(() => {
     setShowStandardCallout(!showStandardCallout);
@@ -41,7 +45,8 @@ export const CalloutTest: React.FunctionComponent<{}> = () => {
   }, [showCustomizedCallout, setIsCustomizedCalloutVisible, setShowCustomizedCallout]);
 
   const toggleCalloutRef = React.useCallback(() => {
-    setAnchorRef(anchorRef === stdBtnRef ? custBtnRef : stdBtnRef);
+    // Cycle the target ref between the RGB target views
+    setAnchorRef(anchorRef === redTargetRef ? greenTargetRef : anchorRef === greenTargetRef ? blueTargetRef : redTargetRef);
   }, [anchorRef, setAnchorRef]);
 
   const onShowStandardCallout = React.useCallback(() => {
@@ -71,23 +76,35 @@ export const CalloutTest: React.FunctionComponent<{}> = () => {
   const onRestoreFocusStandardCallout = React.useCallback(
     (restoreFocusEvent: RestoreFocusEvent) => {
       if (restoreFocusEvent?.nativeEvent?.containsFocus) {
-        decoyBtn1Ref?.current?.focus();
+        decoyBtn1Ref?.current?.focus?.();
       } else {
-        decoyBtn2Ref?.current?.focus();
+        decoyBtn2Ref?.current?.focus?.();
       }
     },
     [decoyBtn1Ref, decoyBtn2Ref]
   );
 
+  const colorDefault: string = 'default';
+  const colorSelections: string[] = [colorDefault, 'red', 'green', 'blue'];
+
+  const [selectedBackgroundColor, setSelectedBackgroundColor] = React.useState<string | undefined>(undefined);
+  const [selectedBorderColor, setSelectedBorderColor] = React.useState<string | undefined>(undefined);
+
+  const borderWidthDefault: string = 'default (1)';
+  const borderWidthSelections: (number | string)[] = ['default (1)', 2, 4, 10];
+
+  const [selectedBorderWidth, setSelectedBorderWidth] = React.useState<string | undefined>(undefined);
+
   const myRect: ScreenRect = { screenX: 10, screenY: 10, width: 100, height: 100 };
+
   return (
     <View>
       <Text style={fabricTesterStyles.testSection} testID={CALLOUT_TESTPAGE}>
         Standard Usage
       </Text>
       <Separator />
-      <View ref={stdBtnRef} style={{ flexDirection: 'row' }}>
-        <View style={{ flexDirection: 'column' }}>
+      <View style={{ flexDirection: 'row', paddingVertical: 5 }}>
+        <View style={{ flexDirection: 'column', paddingHorizontal: 5 }}>
           <View style={{ flexDirection: 'row' }}>
             <Switch value={shouldSetInitialFocus} onValueChange={onInitialFocusChange} />
             <Text>Set Initial Focus</Text>
@@ -97,34 +114,84 @@ export const CalloutTest: React.FunctionComponent<{}> = () => {
             <Switch value={customRestoreFocus} onValueChange={onRestoreFocusChange} />
             <Text>Customize Restore Focus</Text>
           </View>
+
+          <View style={{ flexDirection: 'row' }}>
+            <Switch value={isBeakVisible} onValueChange={onIsBeakVisibleChange} />
+            <Text>Beak Visible</Text>
+          </View>
+          <Picker
+            prompt="Background Color"
+            selectedValue={selectedBackgroundColor || colorDefault}
+            onValueChange={(color) => setSelectedBackgroundColor(color === colorDefault ? undefined : color)}
+          >
+            {colorSelections.map((color, index) => (
+              <Picker.Item label={color} key={index} value={color} />
+            ))}
+          </Picker>
+
+          <Picker
+            prompt="Border Color"
+            selectedValue={selectedBorderColor || colorDefault}
+            onValueChange={(color) => setSelectedBorderColor(color === colorDefault ? undefined : color)}
+          >
+            {colorSelections.map((color, index) => (
+              <Picker.Item label={color} key={index} value={color} />
+            ))}
+          </Picker>
+
+          <Picker
+            prompt="Border Width"
+            selectedValue={selectedBorderWidth || borderWidthDefault}
+            onValueChange={(width) => setSelectedBorderWidth(width === borderWidthDefault ? undefined : width)}
+          >
+            {borderWidthSelections.map((width, index) => (
+              <Picker.Item label={width.toString()} key={index} value={width} />
+            ))}
+          </Picker>
         </View>
 
         <Separator vertical />
 
-        <View style={{ flexDirection: 'column' }}>
+        <View style={{ flexDirection: 'column', paddingHorizontal: 5 }}>
           <Button content="Press for Callout" onClick={toggleShowStandardCallout} />
           <Text>
             <Text>Visibility: </Text>
             {isStandardCalloutVisible ? <Text style={{ color: 'green' }}>Visible</Text> : <Text style={{ color: 'red' }}>Not Visible</Text>}
           </Text>
         </View>
+
+        <Separator vertical />
+
+        <View style={{ flexDirection: 'column', paddingHorizontal: 5 }}>
+          <View ref={redTargetRef} style={{ height: 20, width: 20, backgroundColor: 'red', padding: 5 }} />
+          <View ref={greenTargetRef} style={{ height: 20, width: 20, backgroundColor: 'green', padding: 5 }} />
+          <View ref={blueTargetRef} style={{ height: 20, width: 20, backgroundColor: 'blue', padding: 5 }} />
+        </View>
       </View>
 
       <Separator />
 
-      <Button componentRef={decoyBtn1Ref} content="Custom reFocus w/ focus in Callout" />
-      <Button componentRef={decoyBtn2Ref} content="Custom reFocus w/o focus in Callout" />
+      <View style={{ paddingVertical: 5 }}>
+        <Button componentRef={decoyBtn1Ref} content="Custom reFocus w/ focus in Callout" />
+        <Button componentRef={decoyBtn2Ref} content="Custom reFocus w/o focus in Callout" />
+      </View>
 
       {showStandardCallout && (
         <Callout
-          target={anchorRef}
-          onDismiss={onDismissStandardCallout}
-          onShow={onShowStandardCallout}
-          onRestoreFocus={customRestoreFocus ? onRestoreFocusStandardCallout : undefined}
-          accessibilityLabel="Standard Callout"
-          setInitialFocus={shouldSetInitialFocus}
+          {...{
+            target: anchorRef,
+            onDismiss: onDismissStandardCallout,
+            onShow: onShowStandardCallout,
+            ...(customRestoreFocus && { onRestoreFocus: onRestoreFocusStandardCallout }),
+            accessibilityLabel: 'Standard Callout',
+            setInitialFocus: shouldSetInitialFocus,
+            isBeakVisible: isBeakVisible,
+            ...(selectedBorderColor && { borderColor: selectedBorderColor }),
+            ...(selectedBackgroundColor && { backgroundColor: selectedBackgroundColor }),
+            ...(selectedBorderWidth && { borderWidth: selectedBorderWidth }),
+          }}
         >
-          <View style={{ padding: 20, borderWidth: 2, borderColor: 'black' }}>
+          <View style={{ padding: 20 }}>
             <Button content="click to change anchor" onClick={toggleCalloutRef} />
           </View>
         </Callout>
@@ -133,7 +200,7 @@ export const CalloutTest: React.FunctionComponent<{}> = () => {
       <Text style={fabricTesterStyles.testSection}>Customized Usage</Text>
       <Separator />
 
-      <View ref={custBtnRef} style={{ flexDirection: 'row' }}>
+      <View style={{ flexDirection: 'column', paddingVertical: 5 }}>
         <Button content="Press for Callout" onClick={toggleShowCustomizedCallout} />
         <Text selectable={true}>
           <Text>Visibility: </Text>
