7827b7fa2cf8d8e339ba837f402e1a802fa3bb83
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-06-30 22:51:56 +0200

    Fix iOS test app rendering into the notch (#303)

--- apps/ios/src/index.tsx
+++ apps/ios/src/index.tsx
@@ -1,15 +1,17 @@
 'use strict';
 
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, SafeAreaView } from 'react-native';
 import { ThemeProvider } from '@uifabricshared/theming-react-native';
 import { FabricTester, IFabricTesterProps, customRegistry } from '@fluentui-react-native/tester';
 
 const FluentTester: React.FunctionComponent<IFabricTesterProps> = props => {
   return (
-    <ThemeProvider registry={customRegistry}>
-      <FabricTester {...props} />
-    </ThemeProvider>
+    <SafeAreaView>
+      <ThemeProvider registry={customRegistry}>
+        <FabricTester {...props} />
+      </ThemeProvider>
+    </SafeAreaView>
   );
 };
 
