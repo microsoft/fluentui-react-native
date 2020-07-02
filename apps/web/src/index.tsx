ee00b2c2dfd7f5c51a7a3ac86d389163e77d4639
Gulnaz Sayed <t-gusaye@microsoft.com>
2020-07-02 12:22:14 -0400

    fixed readme

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,22 +1,3 @@
-// 'use strict';
-
-// import { customRegistry, FabricTester, IFabricTesterProps } from '@fluentui-react-native/tester';
-// import { ThemeProvider } from '@uifabricshared/theming-react-native';
-// import * as React from 'react';
-// import { AppRegistry } from 'react-native';
-
-// const FluentTester: React.FunctionComponent<IFabricTesterProps> = props => {
-//   return (
-//     <ThemeProvider registry={customRegistry}>
-//       <FabricTester {...props} />
-//     </ThemeProvider>
-//   );
-// };
-
-// AppRegistry.registerComponent('FluentTester', () => FluentTester);
-
-// export default FluentTester;
-
 import { AppRegistry } from 'react-native';
 import App from './App';
 
