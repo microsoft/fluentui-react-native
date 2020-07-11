f0a9e559aaf631313b3b225fa7f76e2bd6fa8cda
Gulnaz Sayed <t-gusaye@microsoft.com>
2020-07-10 21:09:12 -0400

    windows test

--- apps/windows/index.js
+++ apps/windows/index.js
@@ -4,12 +4,12 @@ import { customRegistry, FabricTester } from '@fluentui-react-native/tester';
 import { ThemeProvider } from '@uifabricshared/theming-react-native';
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import { allPlatformTests } from '../tests/AllPlatformTests';
+import { windowsTests } from './WindowsTests';
 
 const FluentTester = props => {
   return (
     <ThemeProvider registry={customRegistry}>
-      <FabricTester enabledTests={allPlatformTests} {...props} />
+      <FabricTester enabledTests={windowsTests} {...props} />
     </ThemeProvider>
   );
 };
