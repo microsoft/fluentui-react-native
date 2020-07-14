e2f856da25d265563f35ee0913a0cf56f2686ce9
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-07-14 19:30:44 +0200

    Migrate macOS test app to react-native-test-app (#331)

--- apps/macos/src/FluentUITester-macOS/FRNAppDelegate.m
+++
@@ -1,5 +0,0 @@
-#import "FRNAppDelegate.h"
-
-@implementation FRNAppDelegate
-
-@end
--- apps/macos/src/FluentUITester-macOS/main.m
+++
@@ -1,5 +0,0 @@
-#import <AppKit/AppKit.h>
-
-int main(int argc, const char *argv[]) {
-  return NSApplicationMain(argc, argv);
-}
