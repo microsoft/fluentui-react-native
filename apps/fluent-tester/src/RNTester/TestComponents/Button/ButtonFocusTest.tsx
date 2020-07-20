ff5eb70841022f82c39e24745be6715864dea6d4
Gulnaz Sayed <t-gusaye@microsoft.com>
2020-07-20 15:58:05 -0400

    added button test

--- apps/fluent-tester/src/RNTester/TestComponents/Button/ButtonFocusTest.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/Button/ButtonFocusTest.tsx
@@ -1,13 +1,52 @@
+// import { Button } from '@fluentui-react-native/button';
+// import { IFocusable } from '@fluentui-react-native/interactive-hooks';
+// import { Stack } from '@fluentui-react-native/stack';
+// import * as React from 'react';
+// import { View, findNodeHandle, Text } from 'react-native';
+// import { Separator } from '@fluentui-react-native/separator';
+// import { stackStyle, commonTestStyles as commonStyles } from '../Common/styles';
+// import { BUTTON_TESTPAGE } from './consts';
+
+// export const ButtonFocusTest: React.FunctionComponent<{}> = () => {
+//   const [state, setState] = React.useState({
+//     focused: false
+//   });
+//   const buttonRef = React.useRef<IFocusable>(null);
+
+//   const onFocus = React.useCallback(() => {
+//     setState({ focused: !state.focused });
+//     if (buttonRef.current && !state.focused) {
+//       const node = findNodeHandle(buttonRef.current);
+//       console.log(node);
+//       buttonRef.current.focus();
+
+//     }
+//   }, [state, setState]);
+
+//   return (
+//     <View>
+//       <Text style={commonStyles.section} testID={BUTTON_TESTPAGE}>
+//         Basic Buttons
+//       </Text>
+//       <Separator />
+//       <Stack style={stackStyle}>
+//         <Button content={state.focused ? 'Focused' : 'Not Focused'} componentRef={buttonRef} accessibilityLabel="overridden button name" />
+//         <Button content="Click to focus" onClick={onFocus} tooltip="button tooltip" />
+//       </Stack>
+//     </View>
+//   );
+// };
+
 import { Button } from '@fluentui-react-native/button';
 import { IFocusable } from '@fluentui-react-native/interactive-hooks';
 import { Stack } from '@fluentui-react-native/stack';
 import * as React from 'react';
-import { View, findNodeHandle, Text } from 'react-native';
-import { Separator } from '@fluentui-react-native/separator';
-import { stackStyle, commonTestStyles as commonStyles } from '../Common/styles';
+import { findNodeHandle } from 'react-native';
+import { stackStyle } from '../Common/styles';
 import { BUTTON_TESTPAGE } from './consts';
+import { Test, TestSection } from '../Test';
 
-export const ButtonFocusTest: React.FunctionComponent<{}> = () => {
+const basicButton: React.FunctionComponent<{}> = () => {
   const [state, setState] = React.useState({
     focused: false
   });
@@ -24,15 +63,24 @@ export const ButtonFocusTest: React.FunctionComponent<{}> = () => {
   }, [state, setState]);
 
   return (
-    <View>
-      <Text style={commonStyles.section} testID={BUTTON_TESTPAGE}>
-        Basic Buttons
-      </Text>
-      <Separator />
-      <Stack style={stackStyle}>
-        <Button content={state.focused ? 'Focused' : 'Not Focused'} componentRef={buttonRef} accessibilityLabel="overridden button name" />
-        <Button content="Click to focus" onClick={onFocus} tooltip="button tooltip" />
-      </Stack>
-    </View>
+    <Stack style={stackStyle}>
+      <Button content={state.focused ? 'Focused' : 'Not Focused'} componentRef={buttonRef} accessibilityLabel="overridden button name" />
+      <Button content="Click to focus" onClick={onFocus} tooltip="button tooltip" />
+    </Stack>
+  );
+}
+
+const buttonSections: TestSection[] = [
+  {
+    name: 'Basic Button',
+    status: 'TBD',
+    testID: BUTTON_TESTPAGE,
+    component: basicButton
+  }
+];
+
+export const ButtonFocusTest: React.FunctionComponent<{}> = () => {
+  return (
+    <Test name="Button Test" description="Enter description." sections={buttonSections}></Test>
   );
 };
--- apps/fluent-tester/src/RNTester/TestComponents/Test.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/Test.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export type TestSection = {
   name: string;
+  status: string; // shipped, beta, experimental
+  testID: string;
   component: React.FunctionComponent<{}>;
 };
 
@@ -15,7 +17,12 @@ export interface TestProps {
 const styles = StyleSheet.create({
   name: {},
   description: {},
-  section: {}
+  section: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#0B6A0B',
+    marginTop: 12
+  }
 });
 
 export const Test = (props: TestProps) => {
@@ -23,10 +30,13 @@ export const Test = (props: TestProps) => {
     <View>
       <Text style={styles.name}>{props.name}</Text>
       <Text style={styles.description}>{props.description}</Text>
-      {props.sections.map((section) => {
+      {props.sections.map((section, index) => {
         return (
-          <Text style={styles.section}>{section.name}</Text>
-          // {section.component}
+          <View>
+            <Text key={index} style={styles.section}>{section.name}</Text>
+            <Text key={index}>{section.status}</Text>
+            {section.component}
+          </View>
         );
       })}
     </View>
