d335e1ae2da9dad403a647736cc35b4f20b97a31
warleu <warleu@microsoft.com>
2020-07-21 15:14:56 -0700

    Add transformer for svgs to metro config

--- apps/win32/metro.config.js
+++ apps/win32/metro.config.js
@@ -6,30 +6,55 @@
  */
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
-const { getWatchFolders } = require('@uifabricshared/build-native');
+const {getWatchFolders} = require('@uifabricshared/build-native');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  watchFolders: getWatchFolders(),
-  resolver: {
-    resolveRequest: require('@office-iss/react-native-win32/metro-react-native-platform').reactNativePlatformResolver({
-      win32: '@office-iss/react-native-win32'
-    }),
-    blacklistRE: blacklist([
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(`${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`),
-      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
-      new RegExp(`${path.resolve(__dirname, 'msbuild.ProjectImports.zip').replace(/[/\\]/g, '/')}.*`)
-    ])
-  },
-  transformer: {
-    // The cli defaults this to a full path to react-native, which bypasses the reactNativePlatformResolver above
-    // Hopefully we can fix the default in the future
-    assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
-  }
-};
+
+module.exports = (async () => {
+    const {
+        resolver: {
+            sourceExts,
+            assetExts
+        }
+    } = await getDefaultConfig();
+    return {
+        watchFolders: getWatchFolders(),
+        transformer: {
+            // The cli defaults this to a full path to react-native, which bypasses the reactNativePlatformResolver above
+            // Hopefully we can fix the default in the future
+            assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
+            // This transformer selects between the regular transformer and svg transformer depending on the file type
+            babelTransformerPath: require.resolve('react-native-svg-transformer'),
+            getTransformOptions: async () => (
+                {
+                    transform: {
+                        experimentalImportSupport: false,
+                        inlineRequires: false
+                    }
+                }
+            )
+        },
+        resolver: {
+            assetExts: assetExts.filter(ext => ext !== 'svg'),
+            sourceExts: [
+                ...sourceExts,
+                'svg'
+            ],
+            resolveRequest: require('@office-iss/react-native-win32/metro-react-native-platform').reactNativePlatformResolver(
+                {win32: '@office-iss/react-native-win32'}
+            ),
+            blacklistRE: blacklist(
+                [
+                    // This stops "react-native run-windows" from causing the metro server to crash if its already running
+                    new RegExp(`${
+                        path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')
+                    }.*`),
+                    // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+                    new RegExp(`${
+                        path.resolve(__dirname, 'msbuild.ProjectImports.zip').replace(/[/\\]/g, '/')
+                    }.*`)
+                ]
+            )
+        }
+    };
+})();
