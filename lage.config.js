8383690dc93423fcfe6a15e23bac2b58e2d14226
Jason Morse <jasonmo@microsoft.com>
2020-07-28 12:27:08 -0700

    add full build support

--- lage.config.js
+++ lage.config.js
@@ -3,7 +3,8 @@ module.exports = {
     ['build-tools']: ['^build-tools'],
     build: ['build-tools', '^build'],
     bundle: ['build-tools'],
-    test: ['build-tools', 'lint', 'build'],
-    lint: ['build-tools']
+    ['full-build']: ['build', 'test', 'bundle'],
+    lint: ['build-tools'],
+    test: ['build-tools', 'lint', 'build']
   }
 };
