7395d30bf05aa7ddb7b8bb1382d4ead54c52621e
Jason Morse <jasonmo@microsoft.com>
2020-07-29 13:24:24 -0700

    remove lerna dependency, leaving config file

--- lage.config.js
+++ lage.config.js
@@ -4,7 +4,10 @@ module.exports = {
     build: ['build-tools', '^build'],
     buildci: ['build', 'test', 'bundle'],
     bundle: ['build-tools'],
+    clean: [],
     lint: ['build-tools'],
+    ['verify-api']: [],
+    ['update-api']: [],
     test: ['build-tools', 'lint', 'build']
   }
 };
