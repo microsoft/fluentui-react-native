af1b8d26688e0a88e80bd28b7f1d93cb8c87a026
REDMOND\lehon <lehon@microsoft.com>
2020-08-07 08:32:51 -0700

    Add item click indicator in ContextualMenuTest

--- apps/fluent-tester/src/RNTester/TestComponents/ContextualMenu/ContextualMenuTest.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/ContextualMenu/ContextualMenuTest.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, Alert } from 'react-native';
+import { Text, View } from 'react-native';
 import { Button } from '@fluentui-react-native/button';
 import { Separator } from '@fluentui-react-native/separator';
 import { ContextualMenu, ContextualMenuItem } from '@fluentui-react-native/contextual-menu';
@@ -10,61 +10,65 @@ export const ContextualMenuTest: React.FunctionComponent<{}> = () => {
   const stdBtnRef = React.useRef(null);
 
   const [showContextualMenu, setShowContextualMenu] = React.useState(false);
+  const [isContextualMenuVisible, setIsContextualMenuVisible] = React.useState(false);
+
+  const [lastMenuItemClicked, setLastMenuItemClicked] = React.useState(null);
 
   const toggleShowContextualMenu = React.useCallback(() => {
     setShowContextualMenu(!showContextualMenu);
-  }, [showContextualMenu, setShowContextualMenu]);
+    setIsContextualMenuVisible(false);
+  }, [showContextualMenu, setShowContextualMenu, setIsContextualMenuVisible]);
+
+  const onShowContextualMenu = React.useCallback(() => {
+    setIsContextualMenuVisible(true);
+    console.log('ContextualMenu shown');
+  }, [setIsContextualMenuVisible]);
 
   const onDismissContextualMenu = React.useCallback(() => {
     setShowContextualMenu(false);
+    setIsContextualMenuVisible(false);
+    console.log('ContextualMenu dismissed by unmounting');
   }, [setShowContextualMenu]);
 
-  const onMenuShow = () => {
-    console.log('ContextualMenu shown');
-  };
+  const onItemClick = React.useCallback((key) => {
+    console.log('ContextualMenu dismissed by item click');
+    console.log('This is Menu\'s onItemClick event. Menu item ' + key + ' was clicked.');
+    setLastMenuItemClicked(key);
+  }, [setLastMenuItemClicked]);
 
-  const onItemClick1 = () => {
-    console.log('Item Click', 'This is meuuItem\'s onClick event')
-  }
-
-  const onItemClick2 = (key: string) => {
-    console.log('Menu Click', 'Menu item ' + key + ' was clicked.');
-  }
   return (
     <View>
       <Text style={fabricTesterStyles.testSection} testID={CONTEXTUALMENU_TESTPAGE}>
-        ContextualMenu Test
+        Standard ContextualMenu
       </Text>
       <Separator />
-      <Text>
-        Menu Item Click Handled by Menu
-      </Text>
-      <Separator />
-      <Button content="Press for ContextualMenu" onClick={toggleShowContextualMenu} componentRef={stdBtnRef} />
+      <View style={{ flexDirection: 'column', paddingVertical: 5 }}>
+        <Text selectable={true}>
+          <Text>Menu Visibility: </Text>
+          {isContextualMenuVisible ? <Text style={{ color: 'green' }}>Visible</Text> : <Text style={{ color: 'red' }}>Not Visible</Text>}
+        </Text>
+        <Text selectable={true}>
+          <Text>Last Menu Item Clicked: </Text>
+          {lastMenuItemClicked > 0 ? <Text style={{ color: 'blue' }}>{lastMenuItemClicked}</Text> : <Text style={{ color: 'blue' }}>none</Text>}
+        </Text>
+        <Button content="Press for ContextualMenu" onClick={toggleShowContextualMenu} componentRef={stdBtnRef} />
+      </View>
       {showContextualMenu && (
         <ContextualMenu
           target={stdBtnRef}
           onDismiss={onDismissContextualMenu}
-          onShow={onMenuShow}
+          onShow={onShowContextualMenu}
           accessibilityLabel="Standard ContextualMenu"
-          onItemClick={onItemClick2}
+          onItemClick={onItemClick}
           setShowMenu={setShowContextualMenu}
         >
-          <ContextualMenuItem text="MenuItem 1" itemKey="1" onClick={onItemClick1} accessibilityLabel="First Menu Item" />
+          <ContextualMenuItem text="MenuItem 1" itemKey="1" accessibilityLabel="First Menu Item" />
           <ContextualMenuItem text="MenuItem 2" itemKey="2" />
           <ContextualMenuItem text="Disabled Menu Item" itemKey="3" disabled />
           <ContextualMenuItem text="MenuItem 4" itemKey="4" />
-          <ContextualMenuItem text="MenuItem 5" itemKey="5" onClick={onItemClick1} />
+          <ContextualMenuItem text="MenuItem 5" itemKey="5" />
         </ContextualMenu>
       )}
-      <Text>
-        Menu Item Click Handled by each Item
-      </Text>
-      <Separator />
     </View>
   );
 };
-
-/* Test 1: onClick handled by each item
-** Text 2: onClick handled by Menu
-*/
