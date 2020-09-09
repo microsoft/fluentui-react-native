6042d1bbb538eb130b3fb4ecae360e467379e6ed
Saad Najmi <saadnajmi2@gmail.com>
2020-09-09 12:19:36 -0700

    Removed apps/mobile folder, update eslint dependencies (#421)
    * Removed mobile folder
    
    * format ios test file
    
    * Update eslint dependencies on apple platforms
    
    * Disabled prop-types validation rule
    
    * Change files

--- packages/framework/eslint-config-rules/eslintrc.js
+++ packages/framework/eslint-config-rules/eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "@typescript-eslint/consistent-type-assertions": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-array-constructor": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "no-undef": "off",
-    "no-prototype-builtins": "off",
-    "react/display-name": "off"
-  }
-}
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-array-constructor': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-undef': 'off',
+    'no-prototype-builtins': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 0,
+  },
+};
