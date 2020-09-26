02c8a380e8c9a2c6632c2730b6cbf8e5d081b81e
Alicia D <aliciakds88@gmail.com>
2020-09-25 20:59:25 -0700

    🎨 Map brand colors into host settings, fix up fluent-tester theming

--- apps/fluent-tester/src/RNTester/TestComponents/Test.tsx
+++ apps/fluent-tester/src/RNTester/TestComponents/Test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Separator } from '@fluentui/react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, Separator } from '@fluentui/react-native';
 import { Stack } from '@fluentui-react-native/stack';
 import { stackStyle } from './Common/styles';
-import { app } from '../FabricTester';
+import { getHostSettingsWin32, useTheme } from '@uifabricshared/theming-react-native';
 
 export type TestSection = {
   name: string;
@@ -29,9 +29,6 @@ export interface TestProps {
 
 const styles = StyleSheet.create({
   name: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#0B6A0B',
     marginTop: 4,
   },
   description: {
@@ -39,9 +36,6 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   section: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#0B6A0B',
     marginTop: 12,
   },
   statusView: {
@@ -50,40 +44,24 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   statusHeader: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#0B6A0B',
     marginBottom: 6,
   },
   statusLabel: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: '#0B6A0B',
     marginTop: 2,
     marginStart: 4,
   },
   status: {
-    color: 'black',
     fontWeight: 'normal',
   },
 });
 
 export const Test = (props: TestProps) => {
-  const themeColor = () => {
-    let color = 'black'; // default: office (black)
 
-    if (app == 'office') color = 'black';
-    if (app == 'word') color = '#2B579A';
-    if (app == 'excel') color = '#217346';
-    if (app == 'powerpoint') color = '#B7472A';
-    if (app == 'outlook') color = '#106EBE';
-
-    return { color: color };
-  };
+  const hostColors = getHostSettingsWin32(useTheme())?.palette;
 
   return (
     <View>
-      <Text style={[styles.name, themeColor()]}>{props.name}</Text>
+      <Text style={[styles.name]} variant="heroLargeSemibold" color={hostColors?.TextEmphasis}>{props.name}</Text>
       <Separator />
       <Stack style={stackStyle}>
         <Text style={styles.description}>{props.description}</Text>
@@ -91,43 +69,43 @@ export const Test = (props: TestProps) => {
       <Stack style={stackStyle}>
         <View style={styles.statusView}>
           <Stack>
-            <Text style={[styles.statusHeader, themeColor()]}>Platform Status</Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusHeader]} variant="headerSemibold">Platform Status</Text>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Win32: <Text style={styles.status}>{props.status.win32Status}</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               UWP: <Text style={styles.status}>{props.status.uwpStatus}</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               iOS: <Text style={styles.status}>{props.status.iosStatus}</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               macOS: <Text style={styles.status}>{props.status.macosStatus}</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Android: <Text style={styles.status}>{props.status.androidStatus}</Text>
             </Text>
           </Stack>
 
           <Stack style={stackStyle}>
-            <Text style={[styles.statusHeader, themeColor()]}>Status Definitions</Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusHeader]} variant="headerSemibold">Status Definitions</Text>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Production:{' '}
               <Text style={styles.status}>Control is ready for broad partner use and to be used in production-ready scenarios.</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Beta:{' '}
               <Text style={styles.status}>
                 Control is ready for partner consumption, but not ready for production release (e.g. fixing bugs).
               </Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Experimental: <Text style={styles.status}>Control code checked into repo, but not ready for partner use.</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               Backlog: <Text style={styles.status}>Control is in plan and on our backlog to deliver.</Text>
             </Text>
-            <Text style={[styles.statusLabel, themeColor()]}>
+            <Text style={[styles.statusLabel]} variant="bodySemibold">
               N/A: <Text style={styles.status}>Control is not in current plan.</Text>
             </Text>
           </Stack>
@@ -137,7 +115,7 @@ export const Test = (props: TestProps) => {
         const TestComponent = section.component;
         return (
           <View key={index}>
-            <Text style={[styles.section, themeColor()]} testID={section.testID}>
+            <Text style={[styles.section]} variant="heroStandard" testID={section.testID}>
               {section.name}
             </Text>
             <Separator />
