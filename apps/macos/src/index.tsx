157b19680985b2a83747e207bb04d093ba33363d
Saad Najmi <saadnajmi2@gmail.com>
2020-10-05 17:26:57 -0700

    Removed SafeAreaView from macOS, simplifying index.tsx

--- apps/macos/src/index.tsx
+++ apps/macos/src/index.tsx
@@ -1,17 +1,8 @@
 'use strict';
 
 import {FluentTesterApp} from '@fluentui-react-native/tester';
-import * as React from 'react';
-import {AppRegistry, SafeAreaView} from 'react-native-macos';
+import {AppRegistry} from 'react-native-macos';
 
-const FluentTester: React.FunctionComponent = () => {
-  return (
-    <SafeAreaView>
-      <FluentTesterApp />
-    </SafeAreaView>
-  );
-};
+AppRegistry.registerComponent('FluentTester', () => FluentTesterApp);
 
-AppRegistry.registerComponent('FluentTester', () => FluentTester);
-
-export default FluentTester;
+export default FluentTesterApp;
