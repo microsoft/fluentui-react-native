9f96329eb1fd781de0c177d72f85e90589893f1d
Saad Najmi <saadnajmi2@gmail.com>
2021-02-02 15:35:22 -0600

    iOS, macOS, and android test apps build

--- packages/utils/adapters/src/adapters.win32.ts
+++ packages/utils/adapters/src/adapters.win32.ts
@@ -14,6 +14,7 @@ const _viewMask: IFilterMask<IViewProps> = {
   accessibilityActions: true,
   accessibilityAnnotation: true,
   accessibilityDescribedBy: true,
+  accessibilityDescription: true,
   accessibilityLabel: true,
   accessibilityLabeledBy: true,
   accessibilityLevel: true,
