4c41ed1efb14b92645cec539251f6cbcf7ef2fef
warleu <warleu@microsoft.com>
2021-02-25 20:11:42 -0800

    Update test
    SVGs arent supported in uwp so this part of the test won't work

--- apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonIconTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonIconTest.tsx
@@ -4,11 +4,9 @@ import { Stack } from '@fluentui-react-native/stack';
 import * as React from 'react';
 import { View } from 'react-native';
 import { stackStyle } from '../Common/styles';
-
-import { SvgIconProps, FontIconProps } from '@fluentui-react-native/icon';
+import { FontIconProps } from '@fluentui-react-native/icon';
 
 const testImage = require('./icon_24x24.png');
-import TestSvg from './test.svg';
 const testTtf = require('./Font Awesome 5 Free-Solid-900.otf');
 
 const fontProps: FontIconProps = {
@@ -18,32 +16,12 @@ const fontProps: FontIconProps = {
   fontSize: 16,
 };
 
-const svgProps: SvgIconProps = {
-  src: TestSvg,
-  viewBox: '0 0 500 500',
-};
-
 export const ButtonIconTest: React.FunctionComponent<{}> = () => {
-  const CustomizedIconButton = Button.customize({
-    tokens: { iconColor: 'red' },
-    content: { style: { marginStart: 5 } },
-  });
-
   return (
     <View>
       <Stack style={stackStyle}>
         <Button icon={testImage} content="Button with png Icon" tooltip="button tooltip" />
         <Button icon={{ fontSource: fontProps, color: 'blue' }} content="Button with font Icon" tooltip="button tooltip" />
-        <Button
-          icon={{ svgSource: svgProps, width: 20, height: 20, color: 'red' }}
-          content="Button with svg Icon"
-          tooltip="button tooltip"
-        />
-        <CustomizedIconButton
-          icon={{ svgSource: svgProps, width: 20, height: 20 }}
-          content="Button with Customized Icon"
-          tooltip="button tooltip"
-        />
       </Stack>
     </View>
   );
