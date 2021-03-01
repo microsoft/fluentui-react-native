23cd3d437e63e105f5feee16a1b8ef8e769b61c1
warren-ms <63264034+warren-ms@users.noreply.github.com>
2021-03-01 14:39:21 -0800

    Add support for Icons on Buttons (#574)
    * Checking in wip
    
    * Switch from GetAssetByID to Image.ResolveAssetSource
    
    * Update test
    
    * Update formatting
    
    * Update formatting
    
    * update formatting
    
    * Update deps and add svg type for web
    
    * Update deps
    
    * Update depts and add module definitions for png since linter is complaining about require() syntax
    
    * Change require to import to make linter happy.
    
    * Go back to using reqiure on assets otherwise bundler doesn't pack images
    
    * Change files
    
    * Narrow scope of acceptable types for icon property in IButtonSlotProps
    
    * Update deps
    
    * update deps
    
    * Add IconSourcesType since its definition is used in multiple spots
    
    * retrigger checks
    
    * Update rex version
    
    * Update test
    
    SVGs arent supported in uwp so this part of the test won't work

--- packages/experimental/Icon/src/Icon.tsx
+++ packages/experimental/Icon/src/Icon.tsx
@@ -4,7 +4,6 @@ import { Image, ImageStyle } from 'react-native';
 import { Text } from '@fluentui-react-native/text';
 import { SvgUri } from 'react-native-svg';
 import { mergeStyles } from '@fluentui-react-native/framework';
-import * as assetRegistry from 'react-native/Libraries/Image/AssetRegistry';
 import { stagedComponent, mergeProps, getMemoCache } from '@fluentui-react-native/framework';
 import { useTheme } from '@fluentui-react-native/theme-types';
 
@@ -18,8 +17,8 @@ function renderRasterImage(iconProps: IconProps) {
 }
 
 function fontFamilyFromFontSrcFile(fontSrcFile: string, fontFamily: string): string {
-  const asset = assetRegistry.getAssetByID(fontSrcFile);
-  return `${fontFamily}#${asset.httpServerLocation}/${asset.name}.${asset.type}`;
+  const asset = Image.resolveAssetSource(+fontSrcFile);
+  return `${fontFamily}#${asset.uri}`;
 }
 
 const fontStyleMemoCache = getMemoCache();
