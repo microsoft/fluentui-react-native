ac7c08c78ff272f0741b6af6258bb7de642987a7
Saad Najmi <saadnajmi2@gmail.com>
2021-03-01 23:52:14 -0600

    Revert "Do't use colors from native module"
    This reverts commit 4df53867854b93388f916428932f4b8c7f134297.

--- packages/theming/apple-theme/src/appleTheme.colors.macos.ts
+++ packages/theming/apple-theme/src/appleTheme.colors.macos.ts
@@ -54,7 +54,7 @@ export function fallbackApplePalette(): ThemeColorDefinition {
     buttonBackgroundHovered: fluentUIApple.communicationBlue,
     buttonBackgroundCheckedHovered: fluentUIApple.communicationBlueShade20,
     buttonBackgroundPressed: fluentUIApple.communicationBlueShade20,
-    buttonBackgroundDisabled: fluentUIApple.brandBackgroundDisabled,
+    buttonBackgroundDisabled: fluentUIApple.gray100,
     buttonText: fluentUIApple.neutralInverted,
     buttonTextHovered: fluentUIApple.neutralInverted,
     buttonTextChecked: fluentUIApple.neutralInverted,
--- packages/theming/apple-theme/src/createAppleTheme.macos.ts
+++ packages/theming/apple-theme/src/createAppleTheme.macos.ts
@@ -1,26 +1,41 @@
 import { ThemeReference } from '@fluentui-react-native/theme';
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 import { BaseAppleThemeMacOS } from './appleTheme.macos';
+
+// TODO: There are some unresolved bugs with the native module, so let's use the fallback JS theme instead
+const useNativeThemingModule = false;
 
 export function createAppleTheme(): ThemeReference {
   const appleThemeReference: ThemeReference = new ThemeReference(BaseAppleThemeMacOS);
 
-  const module = NativeModules && NativeModules.MSFAppleThemeModule;
-  if (module) {
-    module.getApplePartialThemeWithCallback((error, applePartialTheme) => {
-      if (error) {
-        console.error(`Error retrieving apple theming module! ${error}`);
-      }
-      // Layer the native apple theming module values on top of the base Apple theme
+  if (useNativeThemingModule) {
+    const module = NativeModules && NativeModules.MSFAppleThemeModule;
+    if (module) {
+      module.getApplePartialThemeWithCallback((error, applePartialTheme) => {
+        if (error) {
+          console.error(`Error retrieving apple theming module! ${error}`);
+        }
+        // Layer the native apple theming module values on top of the base Apple theme
 
-      // TODO this doesn't work. Why?
-      // appleThemeReference.update(applePartialTheme);
-      // Set the individual properties instead
-      appleThemeReference.theme.typography = applePartialTheme.typography;
-      appleThemeReference.invalidate();
-    });
-  } else {
-    console.warn('Apple native theming module not found');
+        // TODO this doesn't work. Why?
+        // appleThemeReference.update(applePartialTheme);
+        // Set the individual properties instead
+        appleThemeReference.theme.colors = applePartialTheme.colors;
+        appleThemeReference.theme.typography = applePartialTheme.typography;
+        appleThemeReference.invalidate();
+      });
+
+      const emitter = new NativeEventEmitter(module);
+
+      const onAppleThemeChanged = () => {
+        appleThemeReference.invalidate();
+      };
+
+      emitter.addListener('AppleInterfaceThemeChanged', onAppleThemeChanged);
+      emitter.addListener('AppleColorPreferencesChanged', onAppleThemeChanged);
+    } else {
+      console.warn('Apple native theming module not found');
+    }
   }
 
   return appleThemeReference;
