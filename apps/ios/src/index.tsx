5ff906fcdcdeec78fc8ee7e30207b6ad9139654e
Saad Najmi <saadnajmi2@gmail.com>
2021-03-03 00:41:13 -0600

    Fix up FluentTester View to have a full background

--- apps/ios/src/index.tsx
+++ apps/ios/src/index.tsx
@@ -2,11 +2,18 @@
 
 import {FluentTesterApp} from '@fluentui-react-native/tester';
 import * as React from 'react';
-import {AppRegistry, SafeAreaView} from 'react-native';
+import {AppRegistry, SafeAreaView, Text, useColorScheme} from 'react-native';
 
 const FluentTester: React.FunctionComponent = () => {
+  const isDarkMode = useColorScheme && useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? 'black' : 'white',
+    flexGrow: 1,
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={backgroundStyle}>
       <FluentTesterApp />
     </SafeAreaView>
   );
