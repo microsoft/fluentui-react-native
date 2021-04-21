e00badb9def0a39c04e573a73e876c9ce4739b5a
Tushar Masane <tamasane@gmail.com>
2021-04-21 06:52:27 +0530

    updated themepicker for android

--- apps/fluent-tester/src/FluentTester/theme/ThemePickers.tsx
+++ apps/fluent-tester/src/FluentTester/theme/ThemePickers.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { View, Picker, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import { Text } from '@fluentui-react-native/experimental-text';
 import { lightnessOptions, testerTheme } from './CustomThemes';
 import { themeChoices, ThemeNames } from './applyTheme';
 import { brandOptions, OfficeBrand } from './applyBrand';
+import { Theme, useTheme } from '@fluentui-react-native/framework';
+import { themedStyleSheet } from '@fluentui-react-native/themed-stylesheet';
+
+const isAndroid = Platform.OS == 'android';
+
+// To be replaced with single import when Windows support comes in.
+const Picker = isAndroid ? require('@react-native-picker/picker').Picker : require('react-native').Picker;
 
 export const themePickerStyles = StyleSheet.create({
   pickerRoot: {
@@ -11,16 +18,21 @@ export const themePickerStyles = StyleSheet.create({
   },
 
   picker: {
-    flexDirection: 'row',
+    flexDirection: isAndroid ? 'column' : 'row',
     alignItems: 'center',
     padding: 4,
   },
+});
 
-  dropdown: {
-    height: 30,
-    width: 120,
-    fontSize: 12,
-  },
+export const getThemedDropdownStyles = themedStyleSheet((t: Theme) => {
+  return {
+    dropdown: {
+      height: 30,
+      width: 120,
+      fontSize: 12,
+      color: t.colors.bodyText,
+    },
+  };
 });
 
 type PartPickerEntry = { label: string; value: string };
@@ -41,8 +53,24 @@ export const PartPicker: React.FunctionComponent<PartPickerProps> = (props: Part
     },
     [setValue, onChange],
   );
-  return (
-    <Picker selectedValue={value} style={themePickerStyles.dropdown} onValueChange={onValueChange}>
+
+  const themedDropdownStyles = getThemedDropdownStyles(useTheme());
+  const dropdownIconColor = useTheme().colors.buttonIcon;
+
+  return isAndroid ? (
+    <Picker
+      selectedValue={value}
+      style={themedDropdownStyles.dropdown}
+      onValueChange={onValueChange}
+      dropdownIconColor={dropdownIconColor}
+      mode="dropdown"
+    >
+      {contents.map((entry: PartPickerEntry, index: number) => (
+        <Picker.Item label={entry.label} value={entry.value} key={`entry${index}`} />
+      ))}
+    </Picker>
+  ) : (
+    <Picker selectedValue={value} style={themedDropdownStyles.dropdown} onValueChange={onValueChange}>
       {contents.map((entry: PartPickerEntry, index: number) => (
         <Picker.Item label={entry.label} value={entry.value} key={`entry${index}`} />
       ))}
