1874f74f04fad4c6d6f47fe6f10e3d6a54527bd6
chiuam <67026167+chiuam@users.noreply.github.com>
2021-04-28 18:05:10 -0400

    componentize shimmer

---
+++ packages/components/Shimmer/src/Shimmer.styling.ts
@@ -0,0 +1,33 @@
+import { Theme, UseStylingOptions, buildProps } from '@fluentui-react-native/framework';
+import { shimmerName, ShimmerProps, ShimmerSlotProps, ShimmerTokens } from './Shimmer.types';
+
+
+export const stylingSettings: UseStylingOptions<ShimmerProps, ShimmerSlotProps, ShimmerTokens> = {
+  tokens: [
+    {
+      toValue: 30,
+      duration: 7000,
+      delay: 0,
+      gradientTintColor: 'white',
+      shimmerTintColor:"#E1E1E1",
+    },
+    shimmerName,
+  ],
+  slotProps: {
+    root: buildProps(
+      (tokens: ShimmerTokens, _theme: Theme) => ({
+        style: {
+          viewbox: '0 0 100 100',
+          width:200,
+          height:300,
+          gradientTintColor:'white',
+          toValue:tokens.toValue,
+          duration: tokens.duration,
+          delay: tokens.delay,
+          shimmerTintColor:tokens.shimmerTintColor,
+        },
+      }),
+    ),
+  },
+
+};
--- packages/components/Shimmer/src/Shimmer.tsx
+++ packages/components/Shimmer/src/Shimmer.tsx
@@ -1,60 +1,97 @@
-import React, {useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  Animated,
-} from "react-native";
-import { LinearGradient, Svg, Defs, Stop, Rect, Circle, ClipPath} from '@microsoft/react-native-svg-desktop';
+/** @jsx withSlots */
+import { useState, useEffect, useMemo } from "react";
+import { Defs, LinearGradient, Rect, Stop, Svg, Image, Circle, ClipPath} from 'react-native-svg';
+import { shimmerName, ShimmerProps, ShimmerType } from './Shimmer.types';
+import { mergeProps } from '../../../framework/merge-props/lib/mergeProps';
+import { buildUseStyling, compose, UseSlots, withSlots } from '../../../experimental/framework/lib';
+import { Animated } from 'react-native';
+import { stylingSettings } from './Shimmer.styling';
 
-const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
-export function Shimmer() {
-  const leftValue = useState(new Animated.Value(0))[0]
+export function renderAnimations(memoData: any) {
+  let startValue = useState(new Animated.Value(0))[0]
   useEffect(() => {
-    Animated.loop(
-      Animated.sequence([
-      Animated.timing(leftValue, {
-     toValue: 30,
-     duration: 9000,
-     delay:0,
-     useNativeDriver:false
-   })]),
+  Animated.loop(
+    Animated.sequence([
+    Animated.timing(startValue, {
+      toValue: memoData.toValue,
+      duration: memoData.duration,
+      delay: memoData.delay,
+      useNativeDriver:false
+    })]),
   ).start()
   })
-    return (
-      <View style={styles.container}>
-          <View>
-            <Svg height="500" width="850" >
-              <Defs>
-                <AnimatedLinearGradient id="gradient" x1={leftValue} y1="0" x2="-1" y2="-1" >
-                  <Stop offset="10%" stopColor="#E1E1E1" stopOpacity="1" />
-                  <Stop offset="20%" stopColor="white" stopOpacity="1" />
-                  <Stop offset="30%" stopColor="#E1E1E1" stopOpacity="1" />
-                </AnimatedLinearGradient>
-                <ClipPath id="shimmerView">
-                <Circle cx="80" cy="80" r="55"/>
-                <Rect x="150" y="30" width="450" height="25" rx="3" ry="3" />
-                <Rect x="150" y="68" width="300" height="25" rx="3" ry="3"/>
-                <Rect x="150" y="105" width="250" height="25" rx="3" ry="3"/>
-                </ClipPath>
-              </Defs>
+  return startValue;
+}
+
+const useStyling = buildUseStyling(stylingSettings);
+export const Shimmer = compose<ShimmerType>({
+  displayName: shimmerName,
+  ...stylingSettings,
+  tokensThatAreAlsoProps: ['duration', 'shimmerTintColor', 'gradientTintColor', 'toValue'],
+  /** Settings for the useSlots that will be passed on */
+  slots: { root: Svg },
+  render: (props: ShimmerProps, useSlots: UseSlots<ShimmerType>) => {
+    const Root = useSlots(props).root;
+    let AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+    const tokens = useStyling(props).root.style;
+    const memoizedShimmerData = useMemo(
+      () => ({
+        gradientTintColor: props.gradientTintColor ? props.gradientTintColor : tokens['gradientTintColor'],
+        shimmerTintColor: props.shimmerTintColor ? props.shimmerTintColor : tokens['shimmerTintColor'],
+        width: props.width ? props.width : tokens['width'],
+        height: props.height ? props.height : tokens['height'],
+        toValue: props.toValue ? props.toValue : tokens['toValue'],
+        delay: props.delay ? props.delay : tokens['delay'],
+        duration: props.duration ? props.duration : tokens['duration'],
+      }),
+      [props.gradientTintColor, props.shimmerTintColor],
+    );
+
+    let startValue = renderAnimations(memoizedShimmerData);
+
+    if(props.element == 'lines') {
+      return (rest: ShimmerProps) => <Root {...mergeProps(props, rest)}>
+      <Defs>
+          <AnimatedLinearGradient id="gradient" x1={startValue} y1="0" x2="-1" y2="-1" >
+          <Stop offset="10%" stopColor={memoizedShimmerData.shimmerTintColor} stopOpacity="1" />
+           <Stop offset="20%" stopColor={memoizedShimmerData.gradientTintColor} stopOpacity="1" />
+           <Stop offset="30%" stopColor={memoizedShimmerData.shimmerTintColor} stopOpacity="1" />
+          </AnimatedLinearGradient>
+         <ClipPath id="shimmerView">
+         <Circle cx="80" cy="80" r="55"/>
+             <Rect x="150" y="30" width="450" height="25" rx="3" ry="3" />
+           <Rect x="150" y="68" width="300" height="25" rx="3" ry="3"/>
+             <Rect x="150" y="105" width="250" height="25" rx="3" ry="3"/>
+             </ClipPath>
+          </Defs>
               <Rect
                 x="0"
                 y="0"
-                width="700"
-                height="500"
+                width="500"
+                height="200"
                 fill="url(#gradient)"
                 clipPath="url(#shimmerView)"
               />
-            </Svg>
-          </View>
-      </View>
-    );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+     </Root>
+    }
+    else {
+      return (rest: ShimmerProps) => <Root {...mergeProps(props, rest)}>
+      <Defs>
+      <AnimatedLinearGradient id="grad" x1={startValue} y1="0" x2="-1" y2="-1" >
+       <Stop offset="10%" stopColor={memoizedShimmerData.gradientTintColor} stopOpacity="0" />
+       <Stop offset="20%" stopColor={memoizedShimmerData.gradientTintColor} stopOpacity=".7"/>
+       <Stop offset="30%" stopColor={memoizedShimmerData.gradientTintColor} stopOpacity="0" />
+     </AnimatedLinearGradient>
+     </Defs>
+     <Image href={props.uri} />
+     <Rect
+         x="0"
+         y="0"
+         width={memoizedShimmerData.width}
+         height={memoizedShimmerData.height}
+         fill="url(#grad)"
+     />
+     </Root>
+    }
   },
 });
-
-export default Shimmer;
