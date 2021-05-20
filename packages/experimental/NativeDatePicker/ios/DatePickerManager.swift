836382eb14cb55a0551fa26cc700dde9251cb912
Chris Pearson <chripear@microsoft.com>
2021-05-20 12:02:09 -0700

    Try to sort out threading issues

--- packages/experimental/NativeDatePicker/ios/DatePickerManager.swift
+++ packages/experimental/NativeDatePicker/ios/DatePickerManager.swift
@@ -2,17 +2,21 @@ import FluentUI
 
 @objc(MSFDatePickerManager)
 class DatePickerManager: NSObject {
+    @objc public func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+
     //     @objc public func present(from presentingController: UIViewController, with mode: DateTimePickerMode, startDate: Date = Date(), endDate: Date? = nil, datePickerType: DatePickerType = .calendar, dateRangePresentation: DateRangePresentation = .paged, titles: Titles? = nil) {
     // @nonobjc public static func with(startTitle: String? = nil, startSubtitle: String? = nil, startTab: String? = nil, endTitle: String? = nil, endSubtitle: String? = nil, endTab: String? = nil, dateTitle: String? = nil, dateSubtitle: String? = nil, dateTimeTitle: String? = nil, dateTimeSubtitle: String? = nil) -> Titles {
 
     let datePicker = DateTimePicker()
 
-    @objc public func present()
-    {
-        guard let viewController = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.rootViewController else {
-            fatalError("Unable to get a UIViewController from current shared application context.")
+    @objc public func present() {
+        DispatchQueue.main.async {
+            guard let viewController = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.rootViewController else {
+                fatalError("Unable to get a UIViewController from current shared application context.")
+            }
+            self.datePicker.present(from: viewController, with: DateTimePickerMode.date)
         }
-        datePicker.present(from: viewController, with: DateTimePickerMode.date)
     }
-
 }
