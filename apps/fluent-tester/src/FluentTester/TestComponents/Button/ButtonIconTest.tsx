22e54aed2b34b208450d36cae409711cdc28587f
Adam Gleitman <adam.gleitman@gmail.com>
2021-05-19 18:05:01 -0700

    Remove SVG icons from Windows test page

--- apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonIconTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonIconTest.tsx
@@ -2,7 +2,7 @@
 import { Button } from '@fluentui/react-native';
 import { Stack } from '@fluentui-react-native/stack';
 import * as React from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { stackStyle } from '../Common/styles';
 import { SvgIconProps } from '@fluentui-react-native/icon';
 import TestSvg from './test.svg';
@@ -20,20 +20,27 @@ export const ButtonIconTest: React.FunctionComponent<{}> = () => {
     viewBox: '0 0 500 500',
   };
 
+  // SVG-based icons are not available on all platforms yet
+  const svgIconsEnabled = ['ios', 'macos', 'win32'].includes(Platform.OS as string);
+
   return (
     <View>
       <Stack style={stackStyle}>
         <Button icon={testImage} content="Button with png Icon" tooltip="button tooltip" />
-        <Button
-          icon={{ svgSource: svgProps, width: 20, height: 20, color: 'red' }}
-          content="Button with svg Icon"
-          tooltip="button tooltip"
-        />
-        <CustomizedIconButton
-          icon={{ svgSource: svgProps, width: 20, height: 20 }}
-          content="Button with Customized Icon"
-          tooltip="button tooltip"
-        />
+        {svgIconsEnabled ? (
+          <Button
+            icon={{ svgSource: svgProps, width: 20, height: 20, color: 'red' }}
+            content="Button with svg Icon"
+            tooltip="button tooltip"
+          />
+        ) : null}
+        {svgIconsEnabled ? (
+          <CustomizedIconButton
+            icon={{ svgSource: svgProps, width: 20, height: 20 }}
+            content="Button with Customized Icon"
+            tooltip="button tooltip"
+          />
+        ) : null}
       </Stack>
     </View>
   );
