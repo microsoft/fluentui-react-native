58ee26c1f6e9beb59dbf8514b69325c52af2f52a
Chris Pearson <chripear@microsoft.com>
2021-05-27 12:36:38 -0700

    Fixing Xcode build errors

--- packages/experimental/NativeDatePicker/ios/DatePickerWrapper.swift
+++ packages/experimental/NativeDatePicker/ios/DatePickerWrapper.swift
@@ -8,8 +8,8 @@ class DatePickerWrapper: UIControl {
     public var dateRangePresentation: DateTimePicker.DateRangePresentation = .paged
     public let titles = DateTimePicker.Titles()
 
-    public override init() {
-        self = super.init(frame: .zero)
+    public init() {
+        super.init(frame: .zero)
         self.addTarget(self, action: #selector(present), for: .touchUpInside)
     }
 
@@ -20,14 +20,19 @@ class DatePickerWrapper: UIControl {
     let datePicker = DateTimePicker()
 
     @objc func present() {
-        self.datePicker.present(
-            from: viewController,
-            with: self.mode,
-            startDate: self.startDate ?? Date(),
-            endDate: self.endDate,
-            datePickerType: self.type,
-            dateRangePresentation: self.dateRangePresentation,
-            titles: self.titles
-        )
+        DispatchQueue.main.async {
+            guard let viewController = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.rootViewController else {
+                fatalError("Unable to get a UIViewController from current shared application context.")
+            }
+            self.datePicker.present(
+                from: viewController,
+                with: self.mode,
+                startDate: self.startDate ?? Date(),
+                endDate: self.endDate,
+                datePickerType: self.type,
+                dateRangePresentation: self.dateRangePresentation,
+                titles: self.titles
+            )
+        }
     }
 }
