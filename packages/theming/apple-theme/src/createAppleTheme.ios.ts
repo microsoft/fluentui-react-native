0bd7e7c7b6b2a4581265300a01ad6c054c2674b7
Saad Najmi <saadnajmi2@gmail.com>
2021-06-15 21:07:44 -0700

    Remove apple theme module from the apple theme (#727)
    * Remove apple theme module
    
    * Get it working
    
    * Update the all important nuspec
    
    * wip
    
    * Undo changes to icon
    
    * revert iconTest changes
    
    * revert one more icon change
    
    * Podfile locks
    
    * Remove one more icon change, add bug number.
    
    * Change files
    
    * Fix typo
    
    * Use API properly
    
    * update deps
    
    * revert changes to button

--- packages/theming/apple-theme/src/createAppleTheme.ios.ts
+++ packages/theming/apple-theme/src/createAppleTheme.ios.ts
@@ -1,10 +1,7 @@
 import { ThemeReference } from '@fluentui-react-native/theme';
-import { PartialTheme, Theme } from '@fluentui-react-native/theme-types';
-import { Appearance, NativeModules } from 'react-native';
+import { Theme } from '@fluentui-react-native/theme-types';
+import { Appearance } from 'react-native';
 import { BaseAppleDarkThemeIOS, BaseAppleLightThemeIOS } from './appleTheme.ios';
-
-// Save this value between calls
-let partialThemeFromNativeModule: PartialTheme;
 
 export function createAppleTheme(): ThemeReference {
   const baseAppleTheme = () => {
@@ -12,26 +9,10 @@ export function createAppleTheme(): ThemeReference {
     return current === 'light' ? BaseAppleLightThemeIOS : BaseAppleDarkThemeIOS;
   };
 
-  const loadNativeTheme = (appleThemeReference: ThemeReference) => {
-    const module = NativeModules && NativeModules.MSFAppleThemeModule;
-    if (module) {
-      module.getApplePartialThemeWithCallback((error, applePartialTheme) => {
-        if (error) {
-          console.error(`Error retrieving apple theming module! ${error}`);
-        }
-        partialThemeFromNativeModule = applePartialTheme;
-        appleThemeReference.update(baseAppleTheme, partialThemeFromNativeModule);
-      });
-    } else {
-      console.warn('Apple native theming module not found');
-    }
-  };
-
-  const appleThemeReference = new ThemeReference({} as Theme, baseAppleTheme, partialThemeFromNativeModule);
-  loadNativeTheme(appleThemeReference);
+  const appleThemeReference = new ThemeReference({} as Theme, baseAppleTheme);
 
   Appearance.addChangeListener(() => {
-    loadNativeTheme(appleThemeReference);
+    appleThemeReference.invalidate();
   });
 
   return appleThemeReference;
