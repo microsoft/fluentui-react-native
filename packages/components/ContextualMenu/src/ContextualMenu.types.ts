983bd2fcedca07469b6d402f2f9dd4fbcfea44cd
Ruriko Araki <ruaraki@microsoft.com>
2021-06-16 17:23:26 -0700

    Apply prettier to components, run

--- packages/components/ContextualMenu/src/ContextualMenu.types.ts
+++ packages/components/ContextualMenu/src/ContextualMenu.types.ts
@@ -20,11 +20,11 @@ export interface ContextualMenuContext {
   onDismissMenu?: () => void;
   /*
    ** Checks if any child menus are open
-  */
+   */
   isSubmenuOpen?: boolean;
   /*
    ** ContextualMenuItems will call this submenu dismissal when they are hovered
-  */
+   */
   dismissSubmenu?: () => void;
 }
 
@@ -39,13 +39,13 @@ export type ContextualMenuTokens = ICalloutTokens;
 
 export interface ContextualMenuProps extends Omit<ICalloutProps, 'setInitialFocus'> {
   /*
-  * Whether to set initial focus on the contextual menu container, as opposed to the first menu item.
-  */
+   * Whether to set initial focus on the contextual menu container, as opposed to the first menu item.
+   */
   shouldFocusOnContainer?: boolean;
 
   /*
-  * Whether to focus on the menu when mounted
-  */
+   * Whether to focus on the menu when mounted
+   */
   shouldFocusOnMount?: boolean;
   /*
    ** Callback for when menu item is clicked
--- packages/components/Stack/src/Stack.settings.ts
+++ packages/components/Stack/src/Stack.settings.ts
@@ -5,57 +5,57 @@ export const settings: IComposeSettings<IStackType> = [
   {
     root: {
       style: {
-        display: 'flex'
-      }
+        display: 'flex',
+      },
     },
     inner: {
       style: {
         display: 'flex',
         flexWrap: 'wrap',
-        overflow: 'visible'
-      }
+        overflow: 'visible',
+      },
     },
     _overrides: {
       horizontal: {
         root: {
           style: {
-            flexDirection: 'row'
-          }
+            flexDirection: 'row',
+          },
         },
         _overrides: {
           reversed: {
             root: {
               style: {
-                flexDirection: 'column-reverse'
-              }
-            }
-          }
-        }
+                flexDirection: 'column-reverse',
+              },
+            },
+          },
+        },
       },
       reversed: {
         root: {
           style: {
-            flexDirection: 'row-reverse'
-          }
-        }
+            flexDirection: 'row-reverse',
+          },
+        },
       },
       verticalFill: {
         root: {
           style: {
-            height: '100%'
-          }
-        }
+            height: '100%',
+          },
+        },
       },
       wrap: {
         root: {
           style: {
             flexWrap: 'wrap',
             height: '100%',
-            overflow: 'visible'
-          }
-        }
-      }
-    }
+            overflow: 'visible',
+          },
+        },
+      },
+    },
   },
-  stackName
+  stackName,
 ];
