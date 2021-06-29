e67cda5aaddb6773bfa7cadeb58d14aef4aec52a
Saad Najmi <sanajmi@microsoft.com>
2021-06-29 14:29:54 -0500

    other fixes and cleanup

--- packages/experimental/Shimmer/src/Shimmer.styling.ts
+++ packages/experimental/Shimmer/src/Shimmer.styling.ts
@@ -4,7 +4,7 @@ import { shimmerName, ShimmerProps, ShimmerSlotProps, ShimmerTokens } from './Sh
 
 export const stylingSettings: UseStylingOptions<ShimmerProps, ShimmerSlotProps, ShimmerTokens> = {
   tokens: [
-    (t) => ({
+    () => ({
       toValue: 30,
       duration: 7000,
       delay: 0,
@@ -39,44 +39,3 @@ export const stylingSettings: UseStylingOptions<ShimmerProps, ShimmerSlotProps, 
     })),
   },
 };
-
-// export function getShimmerStylingSettings(): UseStylingOptions<ShimmerProps, ShimmerSlotProps, ShimmerTokens> {
-//   const currentAppearance = getCurrentAppearance(t.host?.appearance, 'light');
-//   return {
-//     tokens: [
-//       (t) => ({
-//         toValue: 30,
-//         duration: 7000,
-//         delay: 0,
-//         gradientTintColor: currentAppearance === 'light' ? 'white' : 'black',
-//         shimmerTintColor: currentAppearance === 'light' ? '#E1E1E1' : '#404040',
-//         width: 200,
-//         height: 100,
-//         angle: 0,
-//         gradientOpacity: 0.7,
-//       }),
-//       shimmerName,
-//     ],
-//     slotProps: {
-//       root: buildProps(
-//         (tokens: ShimmerTokens) => ({
-//           gradientTintColor: tokens.gradientTintColor,
-//           toValue: tokens.toValue,
-//           duration: tokens.duration,
-//           delay: tokens.delay,
-//           shimmerTintColor: tokens.shimmerTintColor,
-//           width: tokens.width,
-//           height: tokens.height,
-//           angle: tokens.angle,
-//           gradientOpacity: tokens.gradientOpacity,
-//           accessibilityRole: 'progressbar',
-//           accessible: true,
-//         }),
-//         ['gradientTintColor', 'toValue', 'duration', 'delay', 'shimmerTintColor', 'width', 'height', 'angle', 'gradientOpacity'],
-//       ),
-//       image: buildProps((_tokens: ShimmerTokens) => ({
-//         href: null,
-//       })),
-//     },
-//   };
-// }
