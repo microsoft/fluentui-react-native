3dea1f27788c5b9a0f487472649493b06d76278a
REDMOND\krsiler <krsiler@microsoft.com>
2021-06-30 13:09:56 -0400

    Allow override of aria props if defined

--- apps/fluent-tester/src/FluentTester/TestComponents/RadioGroup/RadioGroupTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/RadioGroup/RadioGroupTest.tsx
@@ -35,8 +35,8 @@ const basicRadioGroup: React.FunctionComponent<{}> = () => {
       <Separator />
       <RadioGroup label="Controlled RadioGroup" selectedKey={selectedKey} onChange={onChange2}>
         <RadioButton content="Option A" buttonKey="A" ariaLabel="Test Aria Label" />
-        <RadioButton content="Option B" buttonKey="B" />
-        <RadioButton content="Option C" buttonKey="C" />
+        <RadioButton content="Option B" buttonKey="B" ariaPosInSet={57} />
+        <RadioButton content="Option C" buttonKey="C" ariaSetSize={18} />
         <RadioButton content="Option D" buttonKey="D" />
       </RadioGroup>
     </View>
