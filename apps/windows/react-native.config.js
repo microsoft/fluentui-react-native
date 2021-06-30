650f86bb5d6931cef9175b23371ab80ea316a548
Saad Najmi <sanajmi@microsoft.com>
2021-06-30 12:00:48 -0500

    remove windows metro config

--- apps/windows/react-native.config.js
+++ apps/windows/react-native.config.js
@@ -1,21 +1,22 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const windowsProjectFile = path.relative(
-  path.join(__dirname, "windows"),
+  path.join(__dirname, 'windows'),
   path.join(
-    "node_modules",
-    ".generated",
-    "windows",
-    "ReactTestApp",
-    "ReactTestApp.vcxproj"
-  )
+    'node_modules',
+    '.generated',
+    'windows',
+    'ReactTestApp',
+    'ReactTestApp.vcxproj',
+  ),
 );
 
 module.exports = {
+  project: {
     windows: fs.existsSync(windowsProjectFile) && {
-      sourceDir: "windows",
-      solutionFile: "FluentTester.sln",
+      sourceDir: 'windows',
+      solutionFile: 'FluentTester.sln',
       project: {
         projectFile: windowsProjectFile,
       },
