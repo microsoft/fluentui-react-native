315a2070b79a6a849fc39497c9c9e4387abef7fe
REDMOND\krsiler <krsiler@microsoft.com>
2021-06-30 15:05:37 -0400

    Add comment in RadioGroupTest

--- apps/fluent-tester/src/FluentTester/TestComponents/RadioGroup/RadioGroupTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/RadioGroup/RadioGroupTest.tsx
@@ -35,6 +35,7 @@ const basicRadioGroup: React.FunctionComponent<{}> = () => {
       <Separator />
       <RadioGroup label="Controlled RadioGroup" selectedKey={selectedKey} onChange={onChange2}>
         <RadioButton content="Option A" buttonKey="A" ariaLabel="Test Aria Label" />
+        {/* The values for ariaPosInSet and ariaSetSize show someone can override the auto-generated values */}
         <RadioButton content="Option B" buttonKey="B" ariaPosInSet={57} />
         <RadioButton content="Option C" buttonKey="C" ariaSetSize={18} />
         <RadioButton content="Option D" buttonKey="D" />
