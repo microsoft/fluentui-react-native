dbda6b0c98041bd28cea1e9e243a5f1087b88953
Chris Pearson <chripear@microsoft.com>
2021-07-16 13:17:02 -0700

    Extract macOS FocusZone implementation from devmain (#783)

---
+++ packages/components/FocusZone/macos/KeyCodes.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// from <HIToolbox/Events.h>
+enum {
+	kVK_Tab                       = 0x30,
+	kVK_LeftArrow                 = 0x7B,
+	kVK_RightArrow                = 0x7C,
+	kVK_DownArrow                 = 0x7D,
+	kVK_UpArrow                   = 0x7E
+};
