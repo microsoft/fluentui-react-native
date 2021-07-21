6ff34f45c8a951266293794797700292ba549067
Tommy Nguyen <tonguye@microsoft.com>
2021-07-21 20:35:55 +0200

    fix Metro watching Visual Studio generated files

--- apps/windows/metro.config.js
+++ apps/windows/metro.config.js
@@ -1,5 +1,14 @@
-const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const { exclusionList, makeMetroConfig } = require('@rnx-kit/metro-config');
+
+const blockList = exclusionList([
+  // Prevent Metro from crashing when closing Visual Studio
+  /.*\/.vs\/.*/,
+]);
 
 module.exports = makeMetroConfig({
   projectRoot: __dirname,
+  resolver: {
+    blacklistRE: blockList,
+    blockList,
+  },
 });
