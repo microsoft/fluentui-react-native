0e8c9e310167e1c9ceac5daca2a03818936c4042
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-07-22 08:42:51 +0200

    don't delete non-existent logs

--- apps/windows/wdio.conf.js
+++ apps/windows/wdio.conf.js
@@ -98,6 +98,10 @@ exports.config = {
    * @param {Array.<String>} specs List of spec file paths that are to be run
    */
   beforeSession: function (/* config, capabilities, specs */) {
+    if (process.env["TF_BUILD"]) {
+      return;
+    }
+
     // Delete old screenshots and create empty directory
     const rmdirOptions = {
       maxRetries: 3,
