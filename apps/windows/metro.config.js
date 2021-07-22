a02ef32f635ba6601d141030565da281ffb74471
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-07-22 12:44:59 +0200

    don't watch other test apps and build output

--- apps/windows/metro.config.js
+++ apps/windows/metro.config.js
@@ -1,5 +1,16 @@
-const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const { exclusionList, makeMetroConfig } = require('@rnx-kit/metro-config');
+
+const blockList = exclusionList([
+  // Exclude other test apps
+  /.*\/apps\/(?:android|ios|macos|web|win32)\/.*/,
+  // Exclude build output directory
+  /.*\/apps\/windows\/dist\/.*/,
+]);
 
 module.exports = makeMetroConfig({
   projectRoot: __dirname,
+  resolver: {
+    blacklistRE: blockList,
+    blockList,
+  },
 });
