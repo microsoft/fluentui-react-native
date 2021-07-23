d294d3eb2bccc5e08d0223bcbda4ed2031dafd7a
Keven Arroyo <dake.3601@gmail.com>
2021-07-23 16:01:35 -0500

    Added a better example and tokens

--- packages/components/Tabs/src/TabsItem.settings.ts
+++ packages/components/Tabs/src/TabsItem.settings.ts
@@ -6,24 +6,16 @@ export const tabsItemSelectActionLabel = 'Select a TabsItem';
 export const settings: IComposeSettings<TabsItemType> = [
   {
     tokens: {
-      borderColor: 'menuItemText',
-      color: 'menuItemText',
-      backgroundColor: 'menuItemText',
+      borderColor: 'buttonBorder',
+      color: 'red',
+      backgroundColor: 'transparent',
       textBorderColor: 'transparent',
+      borderWidth: 2,
     },
     root: {
       accessible: true,
       focusable: true,
       accessibilityRole: 'tab',
-      style: {},
-    },
-    button: {
-      style: {
-        borderColor: 'blue',
-      },
-    },
-    content: {
-      variant: 'subheaderStandard',
       style: {},
     },
     _precedence: ['disabled', 'hovered', 'focused', 'selected'],
