f4f6032c5bdf76802ae63df46aa8fae112b7fe73
chiuam <67026167+chiuam@users.noreply.github.com>
2021-07-22 20:33:22 -0400

    fix focus

--- packages/components/NativeRadioButton/macos/RadioButtonView.swift
+++ packages/components/NativeRadioButton/macos/RadioButtonView.swift
@@ -1,24 +1,11 @@
-import Foundation
+import AppKit
 
 class RadioButtonView: NSButton {
 
 	@objc public var onChange:RCTBubblingEventBlock?
 
-	public init() {
-		super.init(frame: .zero)
-		self.title = ""
-		self.setButtonType(NSButton.ButtonType.radio)
-		self.action = #selector(callOnChange)
-		self.target = self
-	}
-
-	required public init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	@objc private func callOnChange() {
-		if onChange != nil {
-			onChange!(nil)
-		}
+	override func becomeFirstResponder() -> Bool {
+		self.state = NSButton.StateValue.on
+		return super.becomeFirstResponder()
 	}
 }
--- packages/components/NativeRadioButton/macos/RadioButtonViewManager.swift
+++ packages/components/NativeRadioButton/macos/RadioButtonViewManager.swift
@@ -2,10 +2,16 @@ import Foundation
 
 @objc(MSFRadioButtonViewManager)
 class RadioButtonViewManager: RCTViewManager {
+	var radioButton :RadioButtonView?
 	
 	override func view()->NSView! {
-		let radioButton = RadioButtonView()
+		radioButton = RadioButtonView.init(radioButtonWithTitle: "", target: self, action: #selector(callOnChange))
 		return radioButton
+	}
+	@objc public func callOnChange() {
+		if let onChange = radioButton?.onChange {
+			onChange(nil)
+		}
 	}
 	override class func requiresMainQueueSetup() -> Bool {
 		return true
