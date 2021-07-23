d3cd1d6ff7d8c6f6ef22138699fe91dfcd796753
Ruriko Araki <ruaraki@microsoft.com>
2021-07-22 17:44:57 -0700

    Import assert-never

--- packages/experimental/Shimmer/src/Shimmer.tsx
+++ packages/experimental/Shimmer/src/Shimmer.tsx
@@ -5,6 +5,7 @@ import { shimmerName, ShimmerProps, ShimmerType } from './Shimmer.types';
 import { compose, mergeProps, withSlots, UseSlots, buildUseStyling } from '@fluentui-react-native/framework';
 import { Animated } from 'react-native';
 import { stylingSettings } from './Shimmer.styling';
+import { assertNever } from 'assert-never';
 
 export function useShimmerAnimation(memoData: any) {
   const startValue = useRef(new Animated.Value(0)).current;
@@ -84,6 +85,8 @@ export const Shimmer = compose<ShimmerType>({
             );
           } else if (element.type == 'circle') {
             rows.push(<Circle key={i} r={element.height / 2} cx={element.xPos} cy={element.yPos} />);
+          } else {
+            assertNever(element.type);
           }
         }
       }
