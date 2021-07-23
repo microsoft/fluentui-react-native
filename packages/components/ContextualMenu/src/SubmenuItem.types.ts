6b1ab23cbb14eb56247fb99f7228215f36e01fea
REDMOND\lehon <lehon@microsoft.com>
2021-07-23 10:12:37 -0700

    Add submenu chevron inline

--- packages/components/ContextualMenu/src/SubmenuItem.types.ts
+++ packages/components/ContextualMenu/src/SubmenuItem.types.ts
@@ -4,9 +4,12 @@ import { IRenderData } from '@uifabricshared/foundation-composable';
 import { ITextProps } from '@fluentui-react-native/text';
 import { ContextualMenuItemProps, ContextualMenuItemTokens, ContextualMenuItemState } from './ContextualMenuItem.types';
 import { IconProps } from '@fluentui-react-native/icon';
+import { XmlProps } from 'react-native-svg';
 
 export const submenuItemName = 'submenuItem';
-export type SubmenuItemTokens = ContextualMenuItemTokens;
+export interface SubmenuItemTokens extends ContextualMenuItemTokens {
+  chevronColor?: string;
+}
 export type SubmenuItemProps = ContextualMenuItemProps;
 
 export interface SubmenuItemState extends ContextualMenuItemState {
@@ -19,7 +22,8 @@ export interface SubmenuItemSlotProps {
   icon: IconProps;
   content: ITextProps;
   rightstack: ViewProps;
-  chevron: IconProps;
+  chevron: XmlProps;
+
 }
 
 export type SubmenuItemRenderData = IRenderData<SubmenuItemSlotProps, SubmenuItemState>;
