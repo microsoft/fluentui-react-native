20ff8dc1893c5eddddf64461d3f6843fd264550c
chiuam <67026167+chiuam@users.noreply.github.com>
2021-07-28 23:40:52 -0400

    update focus on mouse click

--- packages/components/NativeRadioButton/macos/RadioButton.swift
+++ packages/components/NativeRadioButton/macos/RadioButton.swift
@@ -3,6 +3,7 @@ class RadioButton: NSButton {
 	@objc public var onPress:RCTBubblingEventBlock?
 	
 	@objc public func callOnPress() {
+		self.window?.makeFirstResponder(self)
 		if (onPress != nil) {
 			onPress!(nil)
 		}
