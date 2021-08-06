2d277493f17859da70ee946691566c644681e226
Patrick Boyd <patboyd@microsoft.com>
2021-08-06 11:57:14 -0700

    remove commented consts from debug

--- apps/fluent-tester/src/FluentTester/TestComponents/Shimmer/consts.ts
+++ apps/fluent-tester/src/FluentTester/TestComponents/Shimmer/consts.ts
@@ -1,5 +1,2 @@
 export const HOMEPAGE_SHIMMER_BUTTON = 'Homepage_Shimmer_Button';
 export const SHIMMER_TESTPAGE = 'Shimmer_TestPage';
-
-// import { ensureNativeComponent } from '@fluentui-react-native/component-cache';
-// export const RCTNativeAnimatedShimmer = ensureNativeComponent('RCTNativeAnimatedShimmer');
