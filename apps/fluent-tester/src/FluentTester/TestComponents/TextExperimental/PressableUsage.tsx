8850e2c2c06f2019c24bcf8d4234da059cf96866
Jason Morse <jasonmo@microsoft.com>
2021-08-20 11:49:05 -0700

    fix use of old text in experimental text test pages

--- apps/fluent-tester/src/FluentTester/TestComponents/TextExperimental/PressableUsage.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/TextExperimental/PressableUsage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Alert, Linking, View } from 'react-native';
 import { Stack } from '@fluentui-react-native/stack';
-import { Text } from '@fluentui/react-native';
+import { Text } from '@fluentui-react-native/experimental-text';
 import { stackStyle } from '../Common/styles';
 import type { IKeyboardEvent, IHandledKeyboardEvent } from '@office-iss/react-native-win32';
 
