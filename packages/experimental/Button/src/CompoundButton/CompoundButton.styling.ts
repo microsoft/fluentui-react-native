60a4f043f2de7ba1590463774a0275a0afd52d41
Ruriko Araki <ruaraki@microsoft.com>
2021-08-23 18:12:17 -0700

    Fix typo

--- packages/experimental/Button/src/CompoundButton/CompoundButton.styling.ts
+++ packages/experimental/Button/src/CompoundButton/CompoundButton.styling.ts
@@ -13,26 +13,26 @@ export const stylingSettings: UseStylingOptions<CompoundButtonProps, CompoundBut
         secondaryContentFont: {
           variant: 'secondaryStandard',
         },
-        secondaryContentColor: t.colors.ghostSecondaryContent,
+        secondaryContentColor: t.colors.defaultSecondaryContent,
         hovered: {
-          secondaryContentColor: t.colors.ghostHoveredSecondaryContent,
+          secondaryContentColor: t.colors.defaultHoveredSecondaryContent,
         },
         focused: {
-          secondaryContentColor: t.colors.ghostFocusedSecondaryContent,
+          secondaryContentColor: t.colors.defaultFocusedSecondaryContent,
         },
         pressed: {
-          secondaryContentColor: t.colors.ghostPressedSecondaryContent,
+          secondaryContentColor: t.colors.defaultPressedSecondaryContent,
         },
         primary: {
-          secondaryContentColor: t.colors.ghostSecondaryContent,
+          secondaryContentColor: t.colors.brandedSecondaryContent,
           hovered: {
-            secondaryContentColor: t.colors.ghostHoveredSecondaryContent,
+            secondaryContentColor: t.colors.brandedHoveredSecondaryContent,
           },
           focused: {
-            secondaryContentColor: t.colors.ghostFocusedSecondaryContent,
+            secondaryContentColor: t.colors.brandedFocusedSecondaryContent,
           },
           pressed: {
-            secondaryContentColor: t.colors.ghostPressedSecondaryContent,
+            secondaryContentColor: t.colors.brandedPressedSecondaryContent,
           },
         },
         ghost: {
