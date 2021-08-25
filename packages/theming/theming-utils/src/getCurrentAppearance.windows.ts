edba2bfbffc4036a901f198b9fe2727abfe9ed1a
Ruriko Araki <ruaraki@microsoft.com>
2021-08-24 17:49:30 -0700

    Fix some issues

--- packages/theming/theming-utils/src/getCurrentAppearance.windows.ts
+++ packages/theming/theming-utils/src/getCurrentAppearance.windows.ts
@@ -1,18 +1,23 @@
+import { Appearance } from 'react-native';
 import { AppTheme } from 'react-native-windows';
 import { AppearanceOptions, ThemeOptions } from '@fluentui-react-native/theme-types';
 
 export function getCurrentAppearance(appearance: ThemeOptions['appearance'], fallback: AppearanceOptions): AppearanceOptions {
+  if (appearance === undefined) {
+    return fallback;
+  }
+
   const isDynamic = appearance === 'dynamic';
 
   if (isDynamic) {
-    if (!AppTheme || !AppTheme.isAvailable) {
+    if (!AppTheme || !AppTheme.isAvailable || !Appearance) {
       return fallback;
     }
 
     if (AppTheme.isHighContrast) {
       return 'highContrast' as AppearanceOptions;
     } else {
-      return AppTheme.currentTheme as AppearanceOptions; // light or dark
+      return Appearance.getColorScheme(); // light or dark
     }
   } else {
     return appearance as AppearanceOptions;
