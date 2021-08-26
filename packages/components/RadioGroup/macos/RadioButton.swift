b8c866644b2c5b64530990e59153424b0d6f5dcc
chiuam <67026167+chiuam@users.noreply.github.com>
2021-08-25 21:31:57 -0700

    Fix text truncation in radio button on macOS (#948)

--- packages/components/RadioGroup/macos/RadioButton.swift
+++ packages/components/RadioGroup/macos/RadioButton.swift
@@ -2,10 +2,24 @@ class RadioButton: NSButton {
 
 	@objc public var onPress: RCTBubblingEventBlock?
 
+	public override init(frame:NSRect) {
+		super.init(frame: frame)
+		translatesAutoresizingMaskIntoConstraints = false;
+	}
+	
+	required init?(coder: NSCoder) {
+		preconditionFailure("init(coder:) has not been implemented")
+	}
+	
 	@objc public func sendCallback() {
 		self.window?.makeFirstResponder(self)
 		if (onPress != nil) {
 			onPress!(nil)
 		}
+	}
+	
+	override func reactSetFrame(_ frame: CGRect) {
+		super.reactSetFrame(frame)
+		self.frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: intrinsicContentSize.width, height: intrinsicContentSize.height)
 	}
 }
