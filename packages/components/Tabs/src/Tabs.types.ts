948f5ea7744505423239b58e5a902d6cff130f97
REDMOND\krsiler <krsiler@microsoft.com>
2021-08-27 10:37:07 -0400

    Fix comment format in Tabs and TabsItem types

--- packages/components/Tabs/src/Tabs.types.ts
+++ packages/components/Tabs/src/Tabs.types.ts
@@ -9,33 +9,33 @@ import { View, ViewProps } from 'react-native';
 export const tabsName = 'Tabs';
 
 export interface TabsContextData {
-  /*
-   ** The currently selected TabsItem's key
+  /**
+   * The currently selected TabsItem's key
    */
   selectedKey: string | null;
 
-  /*
-   ** Index of currently selected key
+  /**
+   * Index of currently selected key
    */
   getTabId?: (key: string, index: number) => string | null;
 
-  /*
-   ** Updates the selected tabsItem and calls the client’s onTabsClick callback
+  /**
+   * Updates the selected tabsItem and calls the client’s onTabsClick callback
    */
   onTabsClick?: (key: string) => void;
 
-  /*
-   ** Updates the selected tabsItem's ref to set as the default tabbable element
+  /**
+   * Updates the selected tabsItem's ref to set as the default tabbable element
    */
   updateSelectedTabsItemRef?: (ref: React.RefObject<any>) => void;
 
-  /*
-   ** Array of tabsItem keys in the group
+  /**
+   * Array of tabsItem keys in the group
    */
   tabsItemKeys?: string[];
 
-  /*
-   ** A Map to for a TabItems corresponding view
+  /**
+   * A Map to for a TabItems corresponding view
    */
   views?: Map<string, React.ReactNode[]> | null;
 }
@@ -51,46 +51,46 @@ export interface TabsState {
 }
 
 export interface TabsProps extends Pick<FocusZoneProps, 'isCircularNavigation'> {
-  /*
-   ** Descriptive label for the Tabs. This will be displayed as the title of the Tabs to the user
+  /**
+   * Descriptive label for the Tabs. This will be displayed as the title of the Tabs to the user
    */
   label?: string;
 
-  /*
-   ** The key of the TabsItem that will initially be selected
+  /**
+   * The key of the TabsItem that will initially be selected
    */
   defaultSelectedKey?: string;
 
-  /*
-   ** An accessibility label for screen readers. If not provided, it will be set to the label of the Tabs.
+  /**
+   * An accessibility label for screen readers. If not provided, it will be set to the label of the Tabs.
    */
   accessibilityLabel?: string;
 
-  /*
-   ** The key of the selected option. If you provide this, you must maintain selection state by observing
-   ** onTabsClick events and passing a new value in when changed. This overrides defaultSelectedKey
-   ** and makes the Tabs a controlled component. This prop is mutually exclusive to defaultSelectedKey.
+  /**
+   * The key of the selected option. If you provide this, you must maintain selection state by observing
+   * onTabsClick events and passing a new value in when changed. This overrides defaultSelectedKey
+   * and makes the Tabs a controlled component. This prop is mutually exclusive to defaultSelectedKey.
    */
   selectedKey?: string;
 
-  /*
-   ** Callback for receiving a notification when the choice has been changed
+  /**
+   * Callback for receiving a notification when the choice has been changed
    */
   onTabsClick?: (key: string) => void;
 
-  /*
-   ** Callback to customize how IDs are generated for each tab header.
-   ** Useful if you're rendering content outside and need to connect accessibility-labelledby.
+  /**
+   * Callback to customize how IDs are generated for each tab header.
+   * Useful if you're rendering content outside and need to connect accessibility-labelledby.
    */
   getTabId?: (key: string, index: number) => string;
 
-  /*
-   ** Sets whether to only render the header
+  /**
+   * Sets whether to only render the header
    */
   headersOnly?: boolean;
 
-  /*
-   ** A RefObject to access Tabs.
+  /**
+   * A RefObject to access Tabs.
    */
   componentRef?: React.RefObject<View>;
 
