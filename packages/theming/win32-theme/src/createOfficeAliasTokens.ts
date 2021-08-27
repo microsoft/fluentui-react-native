c327c073c26c89edfc0e27b9025eb2ed8d1b641b
Ruriko Araki <ruaraki@microsoft.com>
2021-08-26 17:52:19 -0700

    Integrate updated themes

---
+++ packages/theming/win32-theme/src/createOfficeAliasTokens.ts
@@ -0,0 +1,11 @@
+import { getOfficeAliasTokens } from '@fluentui-react-native/theme-tokens';
+import { AliasColorTokens } from '@fluentui-react-native/theme-types';
+import { memoize } from '@fluentui-react-native/memo-cache';
+import { mapPipelineToTheme } from '@fluentui-react-native/theming-utils';
+
+function createOfficeAliasTokensWorker(officeTheme: string): AliasColorTokens {
+  const aliasTokens = getOfficeAliasTokens(officeTheme);
+  return mapPipelineToTheme(aliasTokens);
+}
+
+export const createOfficeAliasTokens = memoize(createOfficeAliasTokensWorker);
