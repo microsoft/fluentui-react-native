2ebde56a778d05c31a2619a621ea86691925bac7
nabil-k <nkhalil942@gmail.com>
2021-08-30 13:32:52 -0700

    added extra * to the start of multi line comments

--- packages/experimental/Tabs/src/Tabs.types.ts
+++ packages/experimental/Tabs/src/Tabs.types.ts
@@ -8,32 +8,32 @@ import type { IViewWin32Props } from '@office-iss/react-native-win32';
 export const tabsName = 'Tabs';
 
 export interface TabsContextData {
-  /*
+  /**
    * The currently selected TabsItem's key
    */
   selectedKey: string | null;
 
-  /*
+  /**
    * Index of currently selected key
    */
   getTabId?: (key: string, index: number) => string | null;
 
-  /*
+  /**
    * Updates the selected tabsItem and calls the client’s onTabsClick callback
    */
   onTabsClick?: (key: string) => void;
 
-  /*
+  /**
    * Updates the selected tabsItem's ref to set as the default tabbable element
    */
   updateSelectedTabsItemRef?: (ref: React.RefObject<any>) => void;
 
-  /*
+  /**
    * Array of tabsItem keys in the group
    */
   tabsItemKeys?: string[];
 
-  /*
+  /**
    * A Map to for a TabItems corresponding view
    */
   views?: Map<string, React.ReactNode[]> | null;
@@ -46,12 +46,12 @@ export interface TabsTokens extends IForegroundColorTokens, FontTokens, IBackgro
 
 
 export interface TabsProps extends  Pick<FocusZoneProps, 'isCircularNavigation'>, Pick<ViewProps, 'accessibilityRole'> {
-  /*
+  /**
    * Descriptive label for the Tabs. This will be displayed as the title of the Tabs to the user
    */
   label?: string;
 
-  /*
+  /**
    * The key of the TabsItem that will initially be selected
    */
   defaultSelectedKey?: string;
@@ -60,31 +60,31 @@ export interface TabsProps extends  Pick<FocusZoneProps, 'isCircularNavigation'>
 
   focusable?: boolean;
 
-  /*
+  /**
    * The key of the selected option. If you provide this, you must maintain selection state by observing
    * onTabsClick events and passing a new value in when changed. This overrides defaultSelectedKey
    * and makes the Tabs a controlled component. This prop is mutually exclusive to defaultSelectedKey.
    */
   selectedKey?: string;
 
-  /*
+  /**
    * Callback for receiving a notification when the choice has been changed
    */
   onTabsClick?: (key: string) => void;
 
-  /*
+  /**
    * Callback to customize how IDs are generated for each tab header.
    * Useful if you're rendering content outside and need to connect accessibility-labelledby.
    */
   getTabId?: (key: string, index: number) => string;
 
-  /*
+  /**
    * Sets whether to only render the header
    */
   headersOnly?: boolean;
 
 
-  /*
+  /**
    * A RefObject to access Tabs.
    */
   componentRef?: React.RefObject<View>;
--- packages/experimental/Tabs/src/TabsItem.types.ts
+++ packages/experimental/Tabs/src/TabsItem.types.ts
@@ -10,71 +10,71 @@ export const tabsItemName = 'TabsItem';
 type IconSourcesType = number | string | IconProps;
 
 export interface TabsItemTokens extends FontTokens, IBorderTokens, IShadowTokens {
-  /*
+  /**
    * The indicator color.
    */
    indicatorColor?: string;
 
-   /*
+   /**
     * The icon color.
     */
    iconColor?: string;
 
-   /*
+   /**
     * Source URL or name of the icon to show on the TabsItem.
     */
    icon?: IconSourcesType;
 
-   /*
+   /**
     * Text to show on the TabsItem.
     */
    headerText?: string;
 
-   /*
+   /**
     * The amount of padding between the border and the headerText.
     */
    headerTextPadding?: number | string;
 
-   /*
+   /**
     * The amount of padding between the border and the headerText when the TabsItem has focus.
     */
    headerTextPaddingFocused?: number | string;
-  /*
+  /**
    * Background color for the button
    */
    backgroundColor?: ColorValue;
 
-   /*
+   /**
     * Foreground color for the text and/or icon of the button
     */
    color?: ColorValue;
 
-   /*
+   /**
     * The amount of padding between the border and the contents.
     */
    contentPadding?: number | string;
 
-   /*
+   /**
     * The amount of padding between the border and the contents when the Button has focus.
     */
    contentPaddingFocused?: number | string;
 
-   /*
+   /**
     * The icon color when hovering over the Button.
     */
    iconColorHovered?: ColorValue;
 
-   /*
+   /**
     * The icon color when the Button is being pressed.
     */
    iconColorPressed?: ColorValue;
 
-   /*
+   /**
     * The size of the icon.
     */
    iconSize?: number | string;
 
-   /*
+   /**
     * The weight of the lines used when drawing the icon.
     */
    iconWeight?: number;
@@ -83,7 +83,7 @@ export interface TabsItemTokens extends FontTokens, IBorderTokens, IShadowTokens
    minHeight?: ViewStyle['minHeight'];
    minWidth?: ViewStyle['minWidth'];
 
-   /*
+   /**
     * States that can be applied to a button
     */
    hovered?: TabsItemTokens;
@@ -94,48 +94,49 @@ export interface TabsItemTokens extends FontTokens, IBorderTokens, IShadowTokens
 }
 
 export interface TabsItemProps extends Omit<IWithPressableOptions<ViewProps>, 'onPress'> {
-  /*
+  /**
    * The text string for the option
    */
    headerText?: string;
 
-   /*
+   /**
     * The number for the TabsItem count
     */
    itemCount?: number;
 
-   /*
+   /**
     * A unique key-identifier for each option
     */
    itemKey: string;
 
-   /*
+   /**
     * Whether or not the tabs item is selectable
     */
    disabled?: boolean;
 
-   /*
+   /**
     * Defines the current tabs item's position in tabs for accessibility purposes. It's recommended to set this value if
     * tabs item are not direct children of tabs. This value is auto-generated if tabs item are direct children of tabs.
     */
    accessibilityPosInSet?: number;
 
-   /*
+   /**
     * Defines the number of tabs items in the group for accessibility purposes. It's recommended to set this value if tabs
     * items are not direct children of tabs. This value is auto-generated if tabs items are direct children of tabs.
     */
    accessibilitySetSize?: number;
 
-   /*
+   /**
     * An accessibility label for narrator.
     */
    accessibilityLabel?: string;
 
-  /*
+  /**
    * Source URL or name of the icon to show on the Button.
    */
   icon?: IconSourcesType;
-  /*
+
+  /**
    * A RefObject to access the IButton interface. Use this to access the public methods and properties of the component.
    */
   componentRef?: React.RefObject<IFocusable>;
