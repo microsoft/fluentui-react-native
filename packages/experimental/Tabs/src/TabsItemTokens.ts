a3e001998223fd8b99c46a0185c24603e4289e90
sriya112 <sriyaaluru@gmail.com>
2021-09-02 16:41:39 -0700

    updated colors and removed unnecessary tokens

--- packages/experimental/Tabs/src/TabsItemTokens.ts
+++ packages/experimental/Tabs/src/TabsItemTokens.ts
@@ -6,44 +6,32 @@ export const tabsItemStates: (keyof TabsItemTokens)[] = ['hovered', 'selected', 
 
 export const defaultTabsItemTokens: TokenSettings<TabsItemTokens, Theme> = (t: Theme) =>
   ({
-    backgroundColor: 'transparent',
-    color: t.colors.neutralForeground3Brand,
+    color: t.colors.neutralStrokeAccessible,
     borderColor: 'transparent',
-    iconColor: t.colors.buttonIcon,
     indicatorColor: 'transparent',
     variant: 'bodyStandard',
     disabled: {
-      backgroundColor: t.colors.buttonDisabledBackground,
       color: t.colors.neutralForegroundDisabled,
       borderColor: 'transparent',
-      iconColor: t.colors.buttonDisabledIcon,
       indicatorColor: 'transparent',
     },
     hovered: {
-      backgroundColor: t.colors.neutralForeground2Hover,
-      iconColor: t.colors.buttonHoveredIcon,
-      color: t.colors.neutralForeground2Hover,
+      color: t.colors.neutralForeground1,
       indicatorColor: t.colors.neutralStroke1,
     },
     pressed: {
       color: t.colors.neutralForeground2Pressed,
-      borderColor: 'transparent',
-      iconColor: t.colors.buttonPressedIcon,
       indicatorColor: t.colors.brandStroke1,
     },
     focused: {
       color: t.colors.neutralForeground1,
       borderColor: t.colors.neutralForeground1,
       icon: t.colors.buttonFocusedIcon,
-      borderWidth: 2,
-      borderRadius: 4,
     },
     selected: {
       color: t.colors.neutralForeground1,
       icon: t.colors.buttonFocusedIcon,
       indicatorColor: t.colors.brandStroke1,
-      borderColor: 'transparent',
       variant: 'bodySemibold',
-      iconColor: t.colors.buttonDisabledIcon,
     },
   } as TabsItemTokens);
