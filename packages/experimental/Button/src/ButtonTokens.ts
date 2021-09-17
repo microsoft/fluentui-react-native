94f89479bac4f0ac520d7e750431eb140537e556
Ruriko Araki <ruaraki@microsoft.com>
2021-09-17 14:36:27 -0700

    Use alias tokens for win32 and windows to style experimental button

--- packages/experimental/Button/src/ButtonTokens.ts
+++ packages/experimental/Button/src/ButtonTokens.ts
@@ -1,26 +1,7 @@
 import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
-import { ButtonTokens } from '.';
-import { globalTokens } from '@fluentui-react-native/theme-tokens';
-
-export const buttonStates: (keyof ButtonTokens)[] = ['fab', 'fluid', 'primary', 'ghost', 'hovered', 'focused', 'pressed', 'disabled'];
-
-const shadowStyleFromGlobalToken = (shadowToken: number) => {
-  // Grab the second shadow from the global token, as we only support displaying one shadow
-  const shadow = globalTokens.shadow[shadowToken][1];
-  return {
-    // iOS Shadow props
-    shadowColor: shadow.color.substr(0, 7), // split out the color
-    shadowOpacity: Number('0x' + shadow.color.substr(7, 2)) / 255.0, // split out the opacity
-    shadowRadius: shadow.blur,
-    shadowOffset: {
-      width: shadow.x,
-      height: shadow.y,
-    },
-    // Android shadow props
-    elevation: shadowToken,
-  };
-};
+import { ButtonTokens } from './Button.types';
+import { shadowStyleFromGlobalToken } from './shadowStyle';
 
 export const defaultButtonTokens: TokenSettings<ButtonTokens, Theme> = (t: Theme) =>
   ({
