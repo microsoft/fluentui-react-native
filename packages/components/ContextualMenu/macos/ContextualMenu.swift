54b7ac014300ebd40c902379f19d1d58df45c258
Saad Najmi <sanajmi@microsoft.com>
2021-10-01 15:27:52 -0700

    wip

--- packages/components/ContextualMenu/macos/ContextualMenu.swift
+++ packages/components/ContextualMenu/macos/ContextualMenu.swift
@@ -30,6 +30,7 @@ class ContextualMenu: RCTView, NSMenuDelegate {
 
 	private init() {
 		super.init(frame: .zero)
+		menu = NSMenu()
 		menu?.delegate = self
 	}
 
@@ -41,31 +42,32 @@ class ContextualMenu: RCTView, NSMenuDelegate {
 		self.init()
 		self.bridge = bridge
 	}
-
-//	override func viewDidMoveToWindow() {
-//		if (reactSuperview() != nil) {
-//			showContextualMenu()
-//		}
-//	}
+	
+	override func viewWillDraw() {
+		showContextualMenu()
+	}
 
 	// MARK: - RCTComponent
+	
+	override func reactSetFrame(_ frame: CGRect) {
+		super.reactSetFrame(frame)
+		NSLog(NSStringFromCGRect(frame))
+		// Seems to not get called
+	}
 
 	override func insertReactSubview(_ subview: NSView!, at atIndex: Int) {
 		super.insertReactSubview(subview, at: atIndex)
-		NSLog("Inserting Subview at " + String(atIndex))
-		NSLog(NSStringFromCGRect(subview.frame))
-
+		// Adding menu items here means they have zero frame
 	}
 
 	override func didUpdateReactSubviews() {
 		super.didUpdateReactSubviews()
-		NSLog(String(reactSubviews().count))
-		showContextualMenu()
+		// They still have zero frame if I call showContextualMenu
 	}
 
 	// MARK: - NSMenuDelegate
 
-	// TODO: These seem to be blocked by the menu lifecycle
+	// TODO: These seem to be blocked by the menu modal event loop
 //	func menuWillOpen(_ menu: NSMenu) {
 //		sendOnShowEvent()
 //	}
@@ -74,14 +76,16 @@ class ContextualMenu: RCTView, NSMenuDelegate {
 //		sendOnDismissEvent()
 //	}
 	
-	func numberOfItems(in menu: NSMenu) -> Int {
-		return reactSubviews().count
-	}
+	// TODO: Investigate these methods
 	
-	func menu(_ menu: NSMenu, update item: NSMenuItem, at index: Int, shouldCancel: Bool) -> Bool {
-		item.view = reactSubviews()[index]
-		return shouldCancel
-	}
+//	func numberOfItems(in menu: NSMenu) -> Int {
+//		return reactSubviews().count
+//	}
+//
+//	func menu(_ menu: NSMenu, update item: NSMenuItem, at index: Int, shouldCancel: Bool) -> Bool {
+//		item.view = reactSubviews()[index]
+//		return shouldCancel
+//	}
 
 	// MARK: - Private Methods
 
@@ -89,7 +93,7 @@ class ContextualMenu: RCTView, NSMenuDelegate {
 		guard let menu = menu else {
 			return
 		}
-
+		
 		for menuItem in menu.items {
 			menuItem.target = self
 			menuItem.action = #selector(sendOnItemClickEvent)
@@ -120,12 +124,19 @@ class ContextualMenu: RCTView, NSMenuDelegate {
 	private func showContextualMenu() {
 		updateMenu()
 
+		// Tried adding subviews as menu items here, but they were either blank or had zero frame
+		NSLog("Number of Subviews: " + String(subviews.count))
 		for view in reactSubviews() {
 			NSLog(NSStringFromCGRect(view.frame))
+			view.frame = NSMakeRect(0, 0, 100, 20)
+			let menuItem = NSMenuItem()
+			menuItem.view = view
+			menu?.addItem(menuItem)
 		}
 
 		// Popping up an NSMenu is a blocking event, so let's be sure the onShow/onDismiss events are processed correctly
 		sendOnShowEvent()
+		
 		DispatchQueue.main.async {
 			let view = self.targetView ?? self
 			self.menu?.popUp(positioning: nil, at: NSPoint(x: 0, y: view.frame.height), in: view)
