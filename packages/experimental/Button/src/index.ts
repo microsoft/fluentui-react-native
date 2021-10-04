e16da82535dba92e4cd391efd8593d23e38186f3
ValentinaKozlova <v.kozlova13@gmail.com>
2021-10-04 14:22:00 +0300

    Extracted only outer props

--- packages/experimental/Button/src/index.ts
+++ packages/experimental/Button/src/index.ts
@@ -2,3 +2,5 @@ export * from './Button.types';
 export * from './Button';
 export * from './CompoundButton';
 export * from './ToggleButton';
+export * from './Button.styling';
+export * from './ButtonTokens';
--- packages/experimental/MenuButton/src/MenuButton.styling.ts
+++ packages/experimental/MenuButton/src/MenuButton.styling.ts
@@ -1,25 +1,30 @@
 import { UseStylingOptions, buildProps, Theme } from '@fluentui-react-native/framework';
 import { borderStyles, layoutStyles, shadowStyles, fontStyles } from '@fluentui-react-native/tokens';
 import { menuButtonName, MenuButtonProps, MenuButtonSlotProps, MenuButtonTokens } from './MenuButton.types';
-import { defaultButtonTokens, buttonStates } from '@fluentui-react-native/experimental-button/src/ButtonTokens';
+import { defaultButtonTokens, buttonStates } from '@fluentui-react-native/experimental-button';
 
 export const stylingSettings: UseStylingOptions<MenuButtonProps, MenuButtonSlotProps, MenuButtonTokens> = {
   tokens: [menuButtonName, defaultButtonTokens],
   states: buttonStates,
   slotProps: {
     button: buildProps(
-      (tokens: MenuButtonTokens, theme: Theme, props: MenuButtonProps) => ({
-        style: {
-          backgroundColor: tokens.backgroundColor,
-          color: tokens.color,
-          iconColor: tokens.iconColor,
-          ...borderStyles.from(tokens, theme),
-          ...layoutStyles.from(tokens, theme),
-          ...shadowStyles.from(tokens, theme),
-          ...fontStyles.from(tokens, theme),
-        },
-        ...props,
-      }),
+      (tokens: MenuButtonTokens, theme: Theme, props: MenuButtonProps) => {
+        const { onClick, primary, componentRef } = props;
+        return {
+          style: {
+            backgroundColor: tokens.backgroundColor,
+            color: tokens.color,
+            iconColor: tokens.iconColor,
+            ...borderStyles.from(tokens, theme),
+            ...layoutStyles.from(tokens, theme),
+            ...shadowStyles.from(tokens, theme),
+            ...fontStyles.from(tokens, theme),
+          },
+          onClick,
+          primary,
+          componentRef,
+        };
+      },
       ['backgroundColor', 'color', 'iconColor', ...borderStyles.keys, ...layoutStyles.keys, ...shadowStyles.keys, ...fontStyles.keys],
     ),
   },
