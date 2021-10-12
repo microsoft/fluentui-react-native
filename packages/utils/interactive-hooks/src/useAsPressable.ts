2ca24e4d704057353218b3f2f1a78fa09b6f3fa8
Saad Najmi <sanajmi@microsoft.com>
2021-10-11 20:02:45 -0500

    remove unused import

--- packages/utils/interactive-hooks/src/useAsPressable.ts
+++ packages/utils/interactive-hooks/src/useAsPressable.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PressableProps, Platform } from 'react-native';
+import { PressableProps } from 'react-native';
 import { PressableFocusProps, PressableHoverProps, PressablePressProps } from './Pressability/Pressability.types';
 import {
   IPressableHooks,
