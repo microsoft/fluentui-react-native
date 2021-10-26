5c81dfe93eb0d54f4108874cd380889c73a255a8
Saad Najmi <sanajmi@microsoft.com>
2021-10-26 15:29:51 -0400

    Delete iOS PLY code

--- packages/components/Callout/ios/PLYCalloutViewLocalData_ios.m
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2018 Microsoft Corporation. All rights reserved.
-
-#import "PLYCalloutViewLocalData_ios.h"
-
-@implementation PLYCalloutViewLocalData
-
-@end
--- packages/components/Callout/ios/PLYCalloutViewManager.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2018 Microsoft Corporation. All rights reserved.
-
-#import <React/RCTViewManager.h>
-
-@interface PLYCalloutViewManager : RCTViewManager
-
-@end
