02ec7bee1762c289afa0ef60f02be42695cb05e3
Saad Najmi <sanajmi@microsoft.com>
2021-10-29 14:48:18 -0400

    Fix some indentation

--- packages/components/Callout/macos/CalloutWindow.swift
+++ packages/components/Callout/macos/CalloutWindow.swift
@@ -18,25 +18,25 @@ class CalloutWindow: NSWindow {
 	public var windowLifeCycleDelegate: CalloutWindowLifeCycleDelegate?
 
 	override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-			super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
-			
-			NotificationCenter.default.addObserver(self, selector: #selector(appDidChangeActive(notification:)), name: NSApplication.didResignActiveNotification, object: nil)
-			
-			mouseEventMonitor = NSEvent.addLocalMonitorForEvents(matching: .leftMouseUp, handler: { (event) -> NSEvent? in
-					if (event.window != self) {
-							self.windowLifeCycleDelegate?.didDetectHitOutsideCallout(calloutWindow: self)
-					}
-					return event
-			})
+		super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(appDidChangeActive(notification:)), name: NSApplication.didResignActiveNotification, object: nil)
+		
+		mouseEventMonitor = NSEvent.addLocalMonitorForEvents(matching: .leftMouseUp, handler: { (event) -> NSEvent? in
+			if (event.window != self) {
+				self.windowLifeCycleDelegate?.didDetectHitOutsideCallout(calloutWindow: self)
+			}
+			return event
+		})
 	}
 	
 	deinit {
-			NotificationCenter.default.removeObserver(self)
+		NotificationCenter.default.removeObserver(self)
 	}
 	
 	
 	@objc func appDidChangeActive(notification: NSNotification) {
-			windowLifeCycleDelegate?.applicationDidResignActiveForCalloutWindow(calloutWindow: self)
+		windowLifeCycleDelegate?.applicationDidResignActiveForCalloutWindow(calloutWindow: self)
 	}
 	
 	private var mouseEventMonitor: Any?
