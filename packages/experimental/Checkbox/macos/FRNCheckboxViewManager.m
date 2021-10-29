0b6ded5d4fa86b5232dd46c951a8a4d19f02731d
chiuam <67026167+chiuam@users.noreply.github.com>
2021-10-28 21:54:34 -0400

    use enum converter for 'checked'

--- packages/experimental/Checkbox/macos/FRNCheckboxViewManager.m
+++ packages/experimental/Checkbox/macos/FRNCheckboxViewManager.m
@@ -7,13 +7,14 @@ RCT_REMAP_VIEW_PROPERTY(label, title, NSString);
 RCT_CUSTOM_VIEW_PROPERTY(disabled, BOOL, NSButton) {
   [view setEnabled:![RCTConvert BOOL:json]];
 }
-RCT_CUSTOM_VIEW_PROPERTY(checked, BOOL, NSButton) {
-	if ([[RCTConvert NSNumber:json] isEqualToNumber:[NSNumber numberWithBool:YES]]) {
-		[view setState:NSControlStateValueOn];
-	} else {
-		[view setState:NSControlStateValueOff];
-	}
-}
+RCT_REMAP_VIEW_PROPERTY(checked, state, NSControlStateValue);
 RCT_REMAP_VIEW_PROPERTY(onChange, onPress, RCTBubblingEventBlock);
 RCT_REMAP_VIEW_PROPERTY(tooltip, toolTip, NSString)
+@end
+@implementation RCTConvert (FRNCheckboxAdditions)
+
+RCT_ENUM_CONVERTER(NSControlStateValue, (@{
+	@"true": @(NSControlStateValueOn),
+	@"false": @(NSControlStateValueOff),
+}), NSControlStateValueOff, boolValue);
 @end
