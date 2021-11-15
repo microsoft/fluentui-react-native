9aa32d8f33dbdd6edc68244e1568fd94ee0a4cd3
Adam Foxman <afoxman@microsoft.com>
2021-11-15 14:54:44 -0800

    Apply RNTA's published workaround for ios/android issues.

--- apps/ios/react-native.config.js
+++ apps/ios/react-native.config.js
@@ -17,8 +17,41 @@
 
 module.exports = {
   project: {
+    android: {
+      sourceDir: 'android',
+      manifestPath: path.relative(
+        path.join(__dirname, 'android'),
+        path.join(
+          path.dirname(require.resolve('react-native-test-app/package.json')),
+          'android',
+          'app',
+          'src',
+          'main',
+          'AndroidManifest.xml',
+        ),
+      ),
+    },
     ios: {
-      project: 'src/FluentTester.xcworkspace',
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require('react-native-test-app/scripts/configure');
+        if (
+          packageSatisfiesVersionRange(
+            '@react-native-community/cli-platform-ios',
+            '<5.0.2',
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // `project` was only used to infer `sourceDir` and `podfile`.
+          return 'ios/ReactTestApp-Dummy.xcodeproj';
+        }
+
+        // `sourceDir` and `podfile` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return 'node_modules/.generated/ios/ReactTestApp.xcodeproj';
+      })(),
     },
   },
 };
