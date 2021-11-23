b58da5f2b241a7c9c9cdc65977024353b41248a4
Lena Hong (SHE/HER) <lehon@microsoft.com>
2021-11-23 13:01:11 -0800

    autolink modules

---
+++ apps/fluent-tester/src/FluentTester/TestComponents/Drawer/DrawerTest.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import { stackStyle } from '../Common/styles';
+import { Stack } from '@fluentui-react-native/stack';
+import { DRAWER_TESTPAGE } from './consts';
+import { Test, TestSection, PlatformStatus } from '../Test';
+import { NativeModules } from 'react-native';
+
+const basicDrawer: React.FunctionComponent = () => {
+  return (
+    <Stack style={stackStyle} gap={5}>
+      <Text>{NativeModules.DrawerModule}</Text>
+    </Stack>
+  );
+};
+
+const drawerSections: TestSection[] = [
+  {
+    name: 'Basic Drawer',
+    testID: DRAWER_TESTPAGE,
+    component: basicDrawer,
+  },
+];
+
+export const DrawerTest: React.FunctionComponent = () => {
+  const status: PlatformStatus = {
+    win32Status: 'N/A',
+    uwpStatus: 'N/A',
+    iosStatus: 'Backlog',
+    macosStatus: 'N/A',
+    androidStatus: 'Experimental',
+  };
+
+  const description =
+    " A Drawer component using the Fluent Design System.  Currently only implemented on Android.";
+
+  return <Test name="Drawer Test" description={description} sections={drawerSections} status={status} />;
+};
