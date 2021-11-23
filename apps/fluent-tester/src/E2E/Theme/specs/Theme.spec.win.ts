821bd432f99d219766d41317aa6bacc502e03d7b
Samuel Freiberg <32719011+samuelfreiberg@users.noreply.github.com>
2021-11-23 10:29:09 -0700

    Phase 2: Win32 E2E Testing - Implementation Changes + Accessibility Testing Additions (#1165)
    * Yarn
    
    * Merge
    
    * Fixing repo
    
    * Update package
    
    * Merge
    
    * Removing change file
    
    * Adding ContextualMenu E2E
    
    * Adding E2E functional testing for rest of phase 2 components
    
    * Adding MenuButton A11y Tests
    
    * Change files
    
    * Adding testID prop to PersonaCoin
    
    * Removing uneeded PersonaCoin tests. Not yet implemented
    
    * Removing unneeded file
    
    * Nit changes
    
    * Improving constant names

---
+++ apps/fluent-tester/src/E2E/Theme/specs/Theme.spec.win.ts
@@ -0,0 +1,23 @@
+import NavigateAppPage from '../../common/NavigateAppPage';
+import ThemePageObject from '../pages/ThemePageObject.win';
+import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT } from '../../common/consts';
+
+// Before testing begins, allow up to 60 seconds for app to open
+describe('Theme Testing Initialization', function () {
+  it('Wait for app load', () => {
+    NavigateAppPage.waitForPageDisplayed(BOOT_APP_TIMEOUT);
+    expect(NavigateAppPage.isPageLoaded()).toBeTruthy();
+  });
+
+  it('Click and navigate to Theme test page', () => {
+    /* Scroll to component test page button in scrollview if not already visible*/
+    ThemePageObject.scrollToComponentButton();
+    ThemePageObject.waitForButtonDisplayed(PAGE_TIMEOUT);
+
+    /* Click on component button to navigate to test page */
+    NavigateAppPage.clickAndGoToThemePage();
+    ThemePageObject.waitForPageDisplayed(PAGE_TIMEOUT);
+
+    expect(ThemePageObject.isPageLoaded()).toBeTruthy();
+  });
+});
