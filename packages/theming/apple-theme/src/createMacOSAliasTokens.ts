b0aa4517334d4780e2c24a1236028f4730394a74
chiuam <67026167+chiuam@users.noreply.github.com>
2021-12-02 16:57:28 -0500

    remove getMacOSAliasTokens export and create getAliasTokens.macos.ts instead

--- packages/theming/apple-theme/src/createMacOSAliasTokens.ts
+++ packages/theming/apple-theme/src/createMacOSAliasTokens.ts
@@ -1,10 +1,10 @@
-import { getMacOSAliasTokens } from '@fluentui-react-native/theme-tokens';
+import { getAliasTokens } from '@fluentui-react-native/theme-tokens';
 import { AliasColorTokens, AppearanceOptions } from '@fluentui-react-native/theme-types';
 import { memoize } from '@fluentui-react-native/memo-cache';
 import { mapPipelineToTheme } from '@fluentui-react-native/theming-utils';
 
 function createMacOSAliasTokensWorker(mode: AppearanceOptions): AliasColorTokens {
-  const aliasTokens = getMacOSAliasTokens(mode);
+  const aliasTokens = getAliasTokens(mode);
   return mapPipelineToTheme(aliasTokens);
 }
 
--- packages/theming/theme-tokens/src/getAliasTokens.macos.ts
+++ packages/theming/theme-tokens/src/getAliasTokens.macos.ts
@@ -2,14 +2,16 @@ import * as macOSLightAliasTokens from './light-macos/tokens-alias';
 import * as macOSDarkAliasTokens from './dark-macos/tokens-alias';
 import * as hcAliasTokens from './highContrast/tokens-alias';
 import { AppearanceOptions } from '@fluentui-react-native/theme-types';
+import { assertNever } from 'assert-never';
 
-export function getMacOSAliasTokens(mode: AppearanceOptions) {
+export function getAliasTokens(mode: AppearanceOptions) {
   if (mode === 'light') {
     return macOSLightAliasTokens.default;
   } else if (mode === 'dark') {
     return macOSDarkAliasTokens.default;
   } else if (mode === 'highContrast') {
     return hcAliasTokens.default;
+  } else {
+    assertNever(mode);
   }
-  return macOSLightAliasTokens.default;
 }
