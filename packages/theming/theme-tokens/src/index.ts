fb9a5bfdb6bbe3294f29d0a675c67e756fd22da1
chiuam <67026167+chiuam@users.noreply.github.com>
2021-12-03 12:46:09 -0500

    remove export

--- packages/theming/theme-tokens/src/index.ts
+++ packages/theming/theme-tokens/src/index.ts
@@ -1,4 +1,3 @@
 export { default as globalTokens } from './tokens-global';
 export { default as aliasTokens } from './light-macos/tokens-alias';
-export { getAliasTokens } from './getAliasTokens';
 export { getOfficeAliasTokens } from './getOfficeAliasTokens';
