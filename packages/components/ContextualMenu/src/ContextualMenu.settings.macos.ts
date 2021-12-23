e19493c6c474ce520c42b40da52e288d7ed89dce
Saad Najmi <saadnajmi2@gmail.com>
2021-12-22 19:32:13 -0600

    Fix RTL for ContextualMenu on macOS (#1288)
    * Fix RTL for ContextualMenu
    
    * Change files

--- packages/components/ContextualMenu/src/ContextualMenu.settings.macos.ts
+++ packages/components/ContextualMenu/src/ContextualMenu.settings.macos.ts
@@ -1,8 +1,12 @@
 import { contextualMenuName, ContextualMenuType } from './ContextualMenu.types';
 import { IComposeSettings } from '@uifabricshared/foundation-compose';
+import { I18nManager } from 'react-native';
 
 export const settings: IComposeSettings<ContextualMenuType> = [
   {
+    tokens: {
+      directionalHint: I18nManager.isRTL ? 'bottomRightEdge' : 'bottonLeftEdge',
+    },
     container: {
       style: {
         padding: 5,
