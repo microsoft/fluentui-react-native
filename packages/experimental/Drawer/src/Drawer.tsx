18d9eb420f2818250c320084b7d39af2ac74e95d
Ruriko Araki <ruaraki@microsoft.com>
2022-01-10 12:52:14 -0800

    Rerun prettier

--- packages/experimental/Drawer/src/Drawer.tsx
+++ packages/experimental/Drawer/src/Drawer.tsx
@@ -1,8 +1,8 @@
 /** @jsx withSlots */
 import * as React from 'react';
-import { drawerName, DrawerTokens, DrawerProps, NativeDrawerProps, DrawerType} from './Drawer.types';
+import { drawerName, DrawerTokens, DrawerProps, NativeDrawerProps, DrawerType } from './Drawer.types';
 import { compose, UseSlots, buildProps, mergeProps, withSlots } from '@fluentui-react-native/framework';
-import { findNodeHandle} from 'react-native';
+import { findNodeHandle } from 'react-native';
 import { ensureNativeComponent } from '@fluentui-react-native/component-cache';
 
 const FRNDrawer = ensureNativeComponent('FRNDrawer');
@@ -13,8 +13,7 @@ export const Drawer = compose<DrawerType>({
   slots: { root: FRNDrawer },
   slotProps: {
     root: buildProps<NativeDrawerProps, DrawerTokens>(() => ({
-      style: {
-      },
+      style: {},
     })),
   },
   render: (props: DrawerProps, useSlots: UseSlots<DrawerType>) => {
