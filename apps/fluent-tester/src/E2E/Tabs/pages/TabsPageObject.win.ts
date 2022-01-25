83e5f1b177fd0a318efff96d44c4367904ca0f4e
samuelfreiberg <safreibe@microsoft.com>
2022-01-25 10:37:04 -0700

    Adding Tabs E2E Functional Tests

--- apps/fluent-tester/src/E2E/Tabs/pages/TabsPageObject.win.ts
+++ apps/fluent-tester/src/E2E/Tabs/pages/TabsPageObject.win.ts
@@ -1,17 +1,78 @@
 import {
   TABS_TESTPAGE,
   TABS_TEST_COMPONENT,
-  TABS_ITEM_TEST_COMPONENT,
   HOMEPAGE_TABS_BUTTON,
+  SECOND_TABS_ITEM_CONTENT,
+  FIRST_TABS_ITEM,
+  FIRST_TABS_ITEM_CONTENT,
+  SECOND_TABS_ITEM,
+  THIRD_TABS_ITEM,
+  THIRD_TABS_ITEM_CONTENT,
 } from '../../../FluentTester/TestComponents/Tabs/consts';
 import { BasePage, By } from '../../common/BasePage.win';
+
+/* The main Tab group we are testing has THREE tab items. The spec file will
+ * import this enum to easily write tests using these 3 tab items */
+export const enum TabItemSelector {
+  First = 0, // this._firstTabItem
+  Second, // this._secondTabItem
+  Third, // this._thirdTabItem
+}
 
 class TabsPageObject extends BasePage {
   /******************************************************************/
   /**************** UI Element Interaction Methods ******************/
   /******************************************************************/
-  getTabItemAccesibilityRole(): string {
-    return this._tabItem.getAttribute('ControlType');
+  waitForTabsItemsToOpen(tabItemSelector: TabItemSelector, timeout?: number): void {
+    browser.waitUntil(
+      () => {
+        return this.didTabItemContentLoad(tabItemSelector);
+      },
+      {
+        timeout: timeout ?? this.waitForPageTimeout,
+        timeoutMsg: 'The Tab Items content did not open.',
+        interval: 1000,
+      },
+    );
+  }
+
+  getTabItemAccesibilityRole(tabItemSelector: TabItemSelector): string {
+    return this.getTabItem(tabItemSelector).getAttribute('ControlType');
+  }
+
+  clickOnTabItem(tabItemSelector: TabItemSelector): void {
+    this.getTabItem(tabItemSelector).click();
+  }
+
+  didTabItemContentLoad(tabItemSelector: TabItemSelector): boolean {
+    return this.getTabItemContent(tabItemSelector).isDisplayed();
+  }
+
+  /* Returns the correct WebDriverIO element from the TabItem Selector */
+  getTabItem(tabItemSelector: TabItemSelector): WebdriverIO.Element {
+    if (tabItemSelector == TabItemSelector.First) {
+      return this._firstTabItem;
+    } else if (tabItemSelector == TabItemSelector.Second) {
+      return this._secondTabItem;
+    } else {
+      return this._thirdTabItem;
+    }
+  }
+
+  getTabItemContent(tabItemSelector: TabItemSelector): WebdriverIO.Element {
+    if (tabItemSelector == TabItemSelector.First) {
+      return this._firstTabItemContent;
+    } else if (tabItemSelector == TabItemSelector.Second) {
+      return this._secondTabItemContent;
+    } else {
+      return this._thirdTabItemContent;
+    }
+  }
+
+  // Overridden function
+  sendKey(componentID?: string, key?: string, tabItemSelector?: TabItemSelector): void {
+    if (!componentID) this.getTabItem(tabItemSelector).addValue(key);
+    else this.sendKey(componentID, key);
   }
 
   /*****************************************/
@@ -33,11 +94,30 @@ class TabsPageObject extends BasePage {
     return By(HOMEPAGE_TABS_BUTTON);
   }
 
-  /***********/
-  /* TabItem *
-  /***********/
-  get _tabItem() {
-    return By(TABS_ITEM_TEST_COMPONENT);
+  /* The first tab group has 3 Tab item headers, all listed below */
+  get _firstTabItem() {
+    return By(FIRST_TABS_ITEM);
+  }
+
+  get _secondTabItem() {
+    return By(SECOND_TABS_ITEM);
+  }
+
+  get _thirdTabItem() {
+    return By(THIRD_TABS_ITEM);
+  }
+
+  /* Content shown when tab item is clicked */
+  get _firstTabItemContent() {
+    return By(FIRST_TABS_ITEM_CONTENT);
+  }
+
+  get _secondTabItemContent() {
+    return By(SECOND_TABS_ITEM_CONTENT);
+  }
+
+  get _thirdTabItemContent() {
+    return By(THIRD_TABS_ITEM_CONTENT);
   }
 }
 
