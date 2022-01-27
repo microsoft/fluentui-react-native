58ae233da04317fa5c98367af0ba6ae52bb16f11
Ruriko Araki <ruaraki@microsoft.com>
2022-01-26 17:41:04 -0800

    Fix index file

--- packages/components/Button/src/Button.tsx
+++ packages/components/Button/src/Button.tsx
@@ -83,5 +83,3 @@ export const Button = compose<ButtonType>({
     };
   },
 });
-
-export default Button;
--- packages/components/Button/src/index.ts
+++ packages/components/Button/src/index.ts
@@ -2,3 +2,8 @@ export * from './deprecated/Button.types';
 export * from './deprecated/Button';
 export * from './deprecated/PrimaryButton';
 export * from './deprecated/StealthButton';
+export { buttonName as buttonNameV2 } from './Button.types';
+export { Button as ButtonV2 } from './Button';
+export * from './CompoundButton';
+export * from './FAB';
+export * from './ToggleButton';
