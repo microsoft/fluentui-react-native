2d24f069a3290704116b5ee3171069d15b4f3a98
Amanda Chiu <amchiu@microsoft.com>
2022-01-31 23:08:27 -0500

    update getAliasTokens logic

--- packages/theming/theme-tokens/src/getAliasTokens.macos.ts
+++ packages/theming/theme-tokens/src/getAliasTokens.macos.ts
@@ -1,14 +1,25 @@
 import * as macOSLightAliasTokens from './light-macos/tokens-alias';
 import * as macOSDarkAliasTokens from './dark-macos/tokens-alias';
+import * as macOSLightHCAliasTokens from './light-high-contrast-macos/tokens-alias';
+import * as macOSDarkHCAliasTokens from './dark-high-contrast-macos/tokens-alias';
 import * as hcAliasTokens from './highContrast/tokens-alias';
 import { AppearanceOptions } from '@fluentui-react-native/theme-types';
 import { assertNever } from 'assert-never';
+import { isHighContrastMode } from '../../apple-theme/src/createAppleTheme.macos';
 
 export function getAliasTokens(mode: AppearanceOptions) {
   if (mode === 'light') {
-    return macOSLightAliasTokens.default;
+    if (isHighContrastMode) {
+      return macOSLightHCAliasTokens.default;
+    } else {
+      return macOSLightAliasTokens.default;
+    }
   } else if (mode === 'dark') {
-    return macOSDarkAliasTokens.default;
+    if (isHighContrastMode) {
+      return macOSDarkHCAliasTokens.default;
+    } else {
+      return macOSDarkAliasTokens.default;
+    }
   } else if (mode === 'highContrast') {
     return hcAliasTokens.default;
   } else {
