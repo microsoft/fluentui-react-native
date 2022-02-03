148ce7f954a37ba84b0e36b6bed6ec1b964b341d
samuelfreiberg <safreibe@microsoft.com>
2022-02-03 09:56:48 -0700

    Removing unwanted script

--- .ado/scripts/assert_tracking_e2e_testing.ps1
+++
@@ -1,7 +0,0 @@
-$ProcessActive = Get-Process ReactTest -ErrorAction SilentlyContinue
-if($ProcessActive -ne $null)
-{
-  if ( (Get-Process -Name "ReactTest").MainWindowTitle -like "Assertion Failure*" ) {
-    Write-Error "An Assert dialogue popped up. Asserts should not happen. This is a failure. Please review screenshots and logs to debug."
-  }
-}
