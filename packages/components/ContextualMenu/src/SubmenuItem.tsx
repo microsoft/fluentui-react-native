195bd564f5343f0d8b7ecbec10bd1cb38c6e343e
Saad Najmi <sanajmi@microsoft.com>
2022-02-04 13:57:18 -0600

    Update other comment

--- packages/components/ContextualMenu/src/SubmenuItem.tsx
+++ packages/components/ContextualMenu/src/SubmenuItem.tsx
@@ -112,7 +112,7 @@ export const SubmenuItem = compose<SubmenuItemType>({
     };
 
     /**
-     * SubmenuItem launches the submenu onMouseEnter event. submenu should be launched with Spacebar, Enter, or right arrow.
+     * SubmenuItem launches the submenu onMouseEnter event. Submenu should be launched with Spacebar, Enter, or right arrow.
      * Explicitly override onKeyDown to override the native windows behavior of moving focus with arrow keys.
      */
     const onKeyDownProps = useKeyDownProps(onItemHoverIn, ' ', 'Enter', 'ArrowRight');
