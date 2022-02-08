e25ab5d6ab36f46f9fbe1c367d2b8ee2bfaa69d6
Amanda Chiu <amchiu@microsoft.com>
2022-02-07 23:25:53 -0500

    clean up imports

--- packages/theming/theme-tokens/src/getMacOSAliasTokens.ts
+++ packages/theming/theme-tokens/src/getMacOSAliasTokens.ts
@@ -1,22 +1,22 @@
-import * as macOSLightAliasTokens from './light-macos/tokens-alias';
-import * as macOSDarkAliasTokens from './dark-macos/tokens-alias';
-import * as macOSLightHCAliasTokens from './light-high-contrast-macos/tokens-alias';
-import * as macOSDarkHCAliasTokens from './dark-high-contrast-macos/tokens-alias';
+import macOSLightAliasTokens from './light-macos/tokens-alias';
+import macOSDarkAliasTokens from './dark-macos/tokens-alias';
+import macOSLightHCAliasTokens from './light-high-contrast-macos/tokens-alias';
+import macOSDarkHCAliasTokens from './dark-high-contrast-macos/tokens-alias';
 import { AppearanceOptions } from '@fluentui-react-native/theme-types';
 import { assertNever } from 'assert-never';
 
 export function getMacOSAliasTokens(mode: AppearanceOptions, isHighContrast: boolean) {
   if (mode === 'light') {
     if (isHighContrast) {
-      return macOSLightHCAliasTokens.default;
+      return macOSLightHCAliasTokens;
     } else {
-      return macOSLightAliasTokens.default;
+      return macOSLightAliasTokens;
     }
   } else if (mode === 'dark') {
     if (isHighContrast) {
-      return macOSDarkHCAliasTokens.default;
+      return macOSDarkHCAliasTokens;
     } else {
-      return macOSDarkAliasTokens.default;
+      return macOSDarkAliasTokens;
     }
   } else if (mode === 'highContrast') {
     throw new Error('highContrast is not a valid AppearanceOptions on macOS');
