cc7288e3339e2b0b33d232c650ec3d9ce53dd8e0
samuelfreiberg <safreibe@microsoft.com>
2022-02-14 17:22:29 -0700

    Fixing Experimental Button constant to match pattern

--- apps/fluent-tester/src/FluentTester/TestComponents/ButtonExperimental/consts.ts
+++ apps/fluent-tester/src/FluentTester/TestComponents/ButtonExperimental/consts.ts
@@ -1,4 +1,4 @@
-export const HOMEPAGE_BUTTON_BUTTONEXPERIMENTAL = 'Homepage_Button_ButtonExperimental'; // Button on the homepage that navigates to the selected test page
+export const HOMEPAGE_BUTTON_BUTTONEXPERIMENTAL = 'Homepage_ButtonExperimental_Button'; // Button on the homepage that navigates to the selected test page
 export const EXPERIMENTAL_BUTTON_TEST_PAGE = 'ButtonExperimental_TestPage'; // A component on each specific test page.
 
 /* Test Button 1 */
