6c7c29f49abe6a8e11d2ed4093398259c517e275
Ruriko Araki <ruaraki@microsoft.com>
2022-02-23 12:30:41 -0800

    Add functional tests

--- apps/fluent-tester/src/FluentTester/TestComponents/CheckboxExperimental/consts.ts
+++ apps/fluent-tester/src/FluentTester/TestComponents/CheckboxExperimental/consts.ts
@@ -8,3 +8,5 @@ export const EXPERIMENTAL_CHECKBOX_ACCESSIBILITY_LABEL = 'E2E testing Experiment
 /* Test Experimental Checkbox 2 */
 export const EXPERIMENTAL_CHECKBOX_NO_A11Y_LABEL_COMPONENT = 'Experimental Checkbox_No_A11y_label_Component';
 export const EXPERIMENTAL_CHECKBOX_TEST_COMPONENT_LABEL = 'Test Experimental Checkbox2 - No Accessibility Label'; // A component on each specific test page
+
+export const EXPERIMENTAL_CHECKBOX_ON_PRESS = 'Experimental_Checkbox_On_Press'; // For testing the press functionality on a Checkbox
