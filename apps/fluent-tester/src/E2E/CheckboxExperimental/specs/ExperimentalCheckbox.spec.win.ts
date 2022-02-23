97bb6ebe9c984a3a36816eaf57f6eb2f01f40420
Ruriko Araki <ruaraki@microsoft.com>
2022-02-23 14:08:12 -0800

    Add tests for checkbox v1 (#1469)
    * Add snapshot tests
    
    * Add more tokens to token test
    
    * Fix snapshot, remove ariaLabel support
    
    * Fix rerender test
    
    * Fix test
    
    * Add test case to test page
    
    * Add functional tests
    
    * Change files
    
    * undo change
    
    * undo change
    
    * Change files
    
    * Fix depcheck
    
    * fix test

--- apps/fluent-tester/src/E2E/CheckboxExperimental/specs/ExperimentalCheckbox.spec.win.ts
+++ apps/fluent-tester/src/E2E/CheckboxExperimental/specs/ExperimentalCheckbox.spec.win.ts
@@ -1,11 +1,11 @@
 import NavigateAppPage from '../../common/NavigateAppPage.win';
-import ExperimentalCheckboxPageObject from '../pages/ExperimentalCheckboxPageObject';
+import ExperimentalCheckboxPageObject, { ExperimentalCheckboxSelector } from '../pages/ExperimentalCheckboxPageObject';
 import { ComponentSelector } from '../../common/BasePage.win';
 import {
   EXPERIMENTAL_CHECKBOX_TEST_COMPONENT_LABEL,
   EXPERIMENTAL_CHECKBOX_ACCESSIBILITY_LABEL,
 } from '../../../FluentTester/TestComponents/CheckboxExperimental/consts';
-import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT, CHECKBOX_A11Y_ROLE } from '../../common/consts';
+import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT, CHECKBOX_A11Y_ROLE, Keys } from '../../common/consts';
 
 describe('Experimental Checkbox Testing Initialization', () => {
   it('Wait for app load', () => {
@@ -28,6 +28,12 @@ describe('Experimental Checkbox Testing Initialization', () => {
 });
 
 describe('Experimental Checkbox Accessibility Testing', () => {
+  /* Scrolls and waits for the Checkbox to be visible on the Test Page */
+  beforeEach(() => {
+    ExperimentalCheckboxPageObject.scrollToTestElement();
+    ExperimentalCheckboxPageObject.waitForPrimaryElementDisplayed(PAGE_TIMEOUT);
+  });
+
   it('Experimental Checkbox - Validate accessibilityRole is correct', () => {
     ExperimentalCheckboxPageObject.scrollToTestElement();
     ExperimentalCheckboxPageObject.waitForPrimaryElementDisplayed(PAGE_TIMEOUT);
@@ -53,6 +59,46 @@ describe('Experimental Checkbox Accessibility Testing', () => {
     expect(ExperimentalCheckboxPageObject.getAccessibilityLabel(ComponentSelector.Secondary)).toEqual(
       EXPERIMENTAL_CHECKBOX_TEST_COMPONENT_LABEL,
     );
+    expect(ExperimentalCheckboxPageObject.didAssertPopup()).toBeFalsy(ExperimentalCheckboxPageObject.ERRORMESSAGE_ASSERT);
+  });
+});
+
+describe('Checkbox Functional Testing', () => {
+  /* Scrolls and waits for the Checkbox to be visible on the Test Page AND un-checks the Checkbox */
+  beforeEach(() => {
+    ExperimentalCheckboxPageObject.scrollToTestElement();
+    ExperimentalCheckboxPageObject.waitForPrimaryElementDisplayed(PAGE_TIMEOUT);
+
+    ExperimentalCheckboxPageObject.toggleCheckboxToUnchecked();
+  });
+
+  it("Click on a Checkbox -> Validate it toggles correctly AND calls the user's onChange", () => {
+    /* Validate the Checkbox is initially toggled OFF */
+    expect(ExperimentalCheckboxPageObject.isCheckboxChecked()).toBeFalsy();
+
+    /* Click on the Checkbox to toggle on */
+    ExperimentalCheckboxPageObject.clickComponent();
+    ExperimentalCheckboxPageObject.waitForCheckboxChecked(PAGE_TIMEOUT);
+
+    expect(ExperimentalCheckboxPageObject.didOnChangeCallbackFire()).toBeTruthy();
+
+    /* Validate the Checkbox is toggled ON */
+    expect(ExperimentalCheckboxPageObject.isCheckboxChecked()).toBeTruthy();
+
+    ExperimentalCheckboxPageObject.clickComponent();
+
+    /* Validate the Checkbox is toggled OFF */
+    expect(ExperimentalCheckboxPageObject.isCheckboxChecked()).toBeFalsy();
+    expect(ExperimentalCheckboxPageObject.didAssertPopup()).toBeFalsy(ExperimentalCheckboxPageObject.ERRORMESSAGE_ASSERT);
+  });
+
+  it('Click the "Spacebar" on a Checkbox and verify it toggles', () => {
+    /* Presses the "space bar" to select the Checkbox */
+    ExperimentalCheckboxPageObject.sendKey(ExperimentalCheckboxSelector.Primary, Keys.Spacebar);
+    ExperimentalCheckboxPageObject.waitForCheckboxChecked(PAGE_TIMEOUT);
+
+    /* Validate the Checkbox is selected */
+    expect(ExperimentalCheckboxPageObject.isCheckboxChecked()).toBeTruthy();
     expect(ExperimentalCheckboxPageObject.didAssertPopup()).toBeFalsy(ExperimentalCheckboxPageObject.ERRORMESSAGE_ASSERT);
   });
 });
--- packages/experimental/Checkbox/src/__tests__/Checkbox.test.tsx
+++ packages/experimental/Checkbox/src/__tests__/Checkbox.test.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Checkbox } from '../Checkbox';
 import * as renderer from 'react-test-renderer';
+import { checkRenderConsistency, checkReRender } from '@fluentui-react-native/test-tools';
+import { AccessibilityActionName, Text, View } from 'react-native';
+import { Svg } from 'react-native-svg';
 
 function onChange(isChecked: boolean) {
   console.log(isChecked);
@@ -38,16 +41,55 @@ describe('Checkbox component tests', () => {
       checkboxBorderColor: 'red',
       checkboxBorderRadius: 5,
       checkboxBorderWidth: 2,
+      checkboxSize: 11,
       checkmarkColor: 'pink',
       checkmarkOpacity: 0.7,
+      checkmarkSize: 7,
       backgroundColor: 'purple',
       color: 'yellow',
       spacingLabelAfter: 7,
+      spacingLabelBefore: 9,
       borderColor: 'green',
       borderRadius: 4,
-      borderWidth: 1,
+      borderWidth: 2,
     });
     const tree = renderer.create(<BoldCheckbox label="All Tokens Checkbox" onChange={onChange} defaultChecked={false} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Checkbox composed', () => {
+    const ComposedCheckbox = Checkbox.compose({
+      slots: {
+        root: View,
+        checkbox: View,
+        checkmark: Svg,
+        label: Text,
+      },
+    });
+    const tree = renderer.create(<ComposedCheckbox>Composed Button with RNText</ComposedCheckbox>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Checkbox simple rendering does not invalidate styling', () => {
+    checkRenderConsistency(() => <Checkbox>Default button</Checkbox>, 2);
+  });
+
+  it('Checkbox re-renders correctly', () => {
+    checkReRender(() => <Checkbox>Render twice</Checkbox>, 2);
+  });
+
+  it('Checkbox shares produced styles across multiple renders', () => {
+    const style = { backgroundColor: 'black' };
+    checkRenderConsistency(() => <Checkbox style={style}>Shared styles</Checkbox>, 2);
+  });
+
+  it('Checkbox re-renders correctly with style', () => {
+    const style = { borderColor: 'blue' };
+    checkReRender(() => <Checkbox style={style}>Shared Style Render</Checkbox>, 2);
+  });
+
+  it('Checkbox re-renders correctly with accessibilityAction', () => {
+    const action = [{ name: 'Expand' as AccessibilityActionName }];
+    checkReRender(() => <Checkbox accessibilityActions={action}>Shared Style Render</Checkbox>, 2);
   });
 });
