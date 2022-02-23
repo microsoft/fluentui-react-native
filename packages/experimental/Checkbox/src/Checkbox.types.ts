33ed3132687dd45aeb8910aa15916b7b90ad19ac
Ruriko Araki <ruaraki@microsoft.com>
2022-02-23 13:19:26 -0800

    CheckboxV1 documentation (#1467)
    * Initial files
    
    * Spec edits
    
    * Spec edits
    
    * Checkbox spec
    
    * Checkbox update comments
    
    * Migration edits
    
    * Migration edits
    
    * Change files
    
    * Add checkmarkSize to spec
    
    * Address feedback

--- packages/experimental/Checkbox/src/Checkbox.types.ts
+++ packages/experimental/Checkbox/src/Checkbox.types.ts
@@ -9,19 +9,60 @@ export const checkboxName = 'Checkbox';
 export type CheckboxSize = 'medium' | 'large';
 
 export interface CheckboxTokens extends FontTokens, IForegroundColorTokens, IBackgroundColorTokens, IBorderTokens, LayoutTokens {
+  /**
+   * Color of the background of the box containing the checkmark.
+   */
   checkboxBackgroundColor?: ColorValue;
+
+  /**
+   * Color of the border of the box containing the checkmark.
+   */
   checkboxBorderColor?: ColorValue;
+
+  /**
+   * Border radius of the box containing the checkmark.
+   */
   checkboxBorderRadius?: number;
+
+  /**
+   * Width of the border around the box containing the checkmark.
+   */
   checkboxBorderWidth?: number;
+
+  /**
+   * Height and width of the box containing the checkmark.
+   */
   checkboxSize?: number;
+
+  /**
+   * Color of the checkmark icon.
+   * Note: Due to upstream limitations this currently does not support PlatformColors.
+   */
   checkmarkColor?: ColorValue;
+
+  /**
+   * The opacity of checkmark as a number between 0 and 1.
+   */
   checkmarkOpacity?: number;
+
+  /**
+   * Height and width of the checkmark icon.
+   */
   checkmarkSize?: number;
+
+  /**
+   * The amount of spacing between an icon and the content when iconPosition is set to 'before', in pixels
+   */
   spacingLabelAfter?: number;
+
+  /**
+   * The amount of spacing between an icon and the content when iconPosition is set to 'after', in pixels
+   */
   spacingLabelBefore?: number;
 
   /**
    * States that can be applied to a checkbox
+   * These can be used to modify styles of the Checkbox when under the specified state.
    */
   disabled?: CheckboxTokens;
   labelIsBefore?: CheckboxTokens;
@@ -49,17 +90,15 @@ export interface CheckboxProps extends Omit<IViewProps, 'onPress'> {
   circular?: boolean;
 
   /**
+   * A RefObject to access the IFocusable interface. Use this to access the public methods and properties of the component.
+   */
+  componentRef?: React.RefObject<IFocusable>;
+
+  /**
    * Default checked state. Mutually exclusive to ‘checked’. Use this if you want an uncontrolled component, and
    * want the Checkbox instance to maintain its own state.
    */
   defaultChecked?: boolean;
-
-  /**
-   * Allows you to set the checkbox to be at the before (start) or after (end) the label
-   *
-   * @default after
-   */
-  labelPosition?: 'before' | 'after';
 
   /**
    * Disabled state of the checkbox.
@@ -72,9 +111,11 @@ export interface CheckboxProps extends Omit<IViewProps, 'onPress'> {
   label?: string;
 
   /**
-   * A RefObject to access the IFocusable interface. Use this to access the public methods and properties of the component.
+   * Allows you to set the checkbox to be at the before (start) or after (end) the label
+   *
+   * @default after
    */
-  componentRef?: React.RefObject<IFocusable>;
+  labelPosition?: 'before' | 'after';
 
   /**
    * Callback that is called when the checked value has changed.
