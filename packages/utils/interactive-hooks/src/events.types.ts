9e7bde679ba75f96c5b5e477349d5e011d168f67
Ruriko Araki <ruaraki@microsoft.com>
2022-02-25 17:17:06 -0800

    Add tests

--- packages/utils/interactive-hooks/src/events.types.ts
+++ packages/utils/interactive-hooks/src/events.types.ts
@@ -1,4 +1,28 @@
 import { AccessibilityActionEvent, GestureResponderEvent } from 'react-native';
-import { KeyPressEvent } from '.';
+import { KeyPressEvent } from './Pressability/CoreEventTypes';
 
 export type InteractionEvent = GestureResponderEvent | KeyPressEvent | AccessibilityActionEvent;
+
+export const isGestureResponderEvent = (e: InteractionEvent): e is GestureResponderEvent => {
+  if ('touches' in e.nativeEvent) {
+    return true;
+  }
+
+  return false;
+};
+
+export const isKeyPressEvent = (e: InteractionEvent): e is KeyPressEvent => {
+  if ('key' in e.nativeEvent) {
+    return true;
+  }
+
+  return false;
+};
+
+export const isAccessibilityActionEvent = (e: InteractionEvent): e is AccessibilityActionEvent => {
+  if ('actionName' in e.nativeEvent) {
+    return true;
+  }
+
+  return false;
+};
