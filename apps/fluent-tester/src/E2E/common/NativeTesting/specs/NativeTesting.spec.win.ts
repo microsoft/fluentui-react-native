0f290d49138c59c1eb508a5d5e56a3ee34c9d517
Samuel Freiberg <32719011+samuelfreiberg@users.noreply.github.com>
2022-02-25 17:34:26 -0700

    Adding E2E Tests that safeguard our native testing implementation (#1474)
    * Yarn
    
    * Merge
    
    * Fixing repo
    
    * Update package
    
    * Merge
    
    * Removing change file
    
    * Moving all test assets to the same folder in /Common/
    
    * Revert "Moving all test assets to the same folder in /Common/"
    
    This reverts commit cb3ab143254551f81d3080c2a7d42acc6b0f71dd.
    
    * Testing Native changes
    
    * Change files
    
    * Removing import
    
    * Adding additional testing to ensure the children of the scrollview are the navigation buttons.
    
    * Removing unwanted constant
    
    * Reverting unneeded change
    
    * Nit comment/error message changes
    
    * Nit changes - comments
    
    * Changing some logic in validateScrollViewChildren()
    
    * Add additional validation check to ensure all test IDs are in the correct format

---
+++ apps/fluent-tester/src/E2E/common/NativeTesting/specs/NativeTesting.spec.win.ts
@@ -0,0 +1,28 @@
+import NavigateAppPage from '../../../common/NavigateAppPage.win';
+import NativeTestingPageObject from '../pages/NativeTestingPageObject.win';
+import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT } from '../../../common/consts';
+
+describe('Native Safety Check Testing Initialization', () => {
+  it('Wait for app load', () => {
+    NavigateAppPage.waitForPageDisplayed(BOOT_APP_TIMEOUT);
+    expect(NavigateAppPage.isPageLoaded()).toBeTruthy(NavigateAppPage.ERRORMESSAGE_APPLOAD);
+  });
+
+  // The ScrollView in testing is the one that contains all the buttons that navigate to each component's
+  // test page. This is vital for our native testing, as we grab this ScrollView on the native side and
+  // to climb down the UI tree and find all of FURN's testable components.
+  // We need to make sure this ScrollView:
+  //    1) Continues to parent all the test buttons
+  //    2) Keeps testID='SCROLLVIEW_TEST_ID'
+  it('Validate the ScrollView containing all the navigational buttons exists', () => {
+    NativeTestingPageObject.waitForScrollViewDisplayed(PAGE_TIMEOUT);
+    expect(NativeTestingPageObject.doesScrollViewParentExist()).toBeTruthy();
+  });
+
+  // In addition to existing, we want to ensure the Children of the ScrollView stay intact. The children are the buttons that
+  // navigate to each test page. Validate these children exist with the proper testId format.
+  it('Validate the children of ScrollView are the buttons that navigate to each test page with proper automationId', () => {
+    NativeTestingPageObject.waitForScrollViewDisplayed(PAGE_TIMEOUT);
+    expect(NativeTestingPageObject.validateScrollViewChildren()).toBeTruthy();
+  });
+});
