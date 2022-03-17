0dd79e4e094c2b6842287d02f2db6c4bc8cb8df1
Lena Hong (SHE/HER) <lehon@microsoft.com>
2022-03-17 00:42:06 -0700

    remove unused imports

--- packages/experimental/Drawer/src/Drawer.tsx
+++ packages/experimental/Drawer/src/Drawer.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { drawerName, DrawerTokens, DrawerProps, DrawerType} from './Drawer.types';
 import { compose, UseSlots, buildProps, mergeProps, withSlots } from '@fluentui-react-native/framework';
-import { findNodeHandle } from 'react-native';
 import { ensureNativeComponent } from '@fluentui-react-native/component-cache';
 
 const FRNDrawer = ensureNativeComponent('FRNDrawer');
