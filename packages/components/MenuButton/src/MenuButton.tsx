2fd03bdc9b165196649bad06ed19d74921aff57f
Lena Hong (SHE/HER) <lehon@microsoft.com>
2022-03-28 03:48:59 -0700

    restore primary button for menuButton

--- packages/components/MenuButton/src/MenuButton.tsx
+++ packages/components/MenuButton/src/MenuButton.tsx
@@ -1,6 +1,5 @@
 /** @jsx withSlots */
 import React, { useRef, useState, useCallback } from 'react';
-// import { PrimaryButton, Button } from '@fluentui-react-native/button';
 import { ButtonV1 as Button } from '@fluentui-react-native/button';
 import { ContextualMenu, ContextualMenuItem, SubmenuItem, Submenu } from '@fluentui/react-native';
 import { IUseComposeStyling, compose } from '@uifabricshared/foundation-compose';
@@ -74,7 +73,7 @@ export const MenuButton = compose<MenuButtonType>({
   slots: {
     root: React.Fragment,
     button: { slotType: Button as React.ComponentType },
-    // primaryButton: { slotType: PrimaryButton as React.ComponentType },
+    primaryButton: { slotType: Button as React.ComponentType },
     contextualMenu: { slotType: ContextualMenu as React.ComponentType },
     contextualMenuItems: React.Fragment,
     chevronSvg: SvgXml,
