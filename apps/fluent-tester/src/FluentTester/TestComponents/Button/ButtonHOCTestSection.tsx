016d2e87910f862f36713ac14ff0f6c6d4a81338
Ruriko Araki <ruaraki@microsoft.com>
2022-03-29 15:56:16 -0700

    Switch tests to use core package for Button

--- apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonHOCTestSection.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Button/ButtonHOCTestSection.tsx
@@ -1,4 +1,4 @@
-import { ButtonV1 as Button } from '@fluentui-react-native/button';
+import { ButtonV1 as Button } from '@fluentui/react-native';
 import { Text } from '@fluentui-react-native/experimental-text';
 import { Icon, SvgIconProps } from '@fluentui-react-native/icon';
 import * as React from 'react';
