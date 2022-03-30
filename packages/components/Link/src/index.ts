0fd96baf569e9993568e1bf08748a5696d034244
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-03-30 16:34:37 +0200

    chore: enable no-export-all on index files
    Some bundlers, such as esbuild, are not able to tree-shake nested
    `export *` statements. Fixed them using the `no-export-all` ESLint rule.

--- packages/components/Link/src/index.ts
+++ packages/components/Link/src/index.ts
@@ -1,2 +1,14 @@
-export * from './Link.types';
-export * from './Link';
+export { linkName } from './Link.types';
+export type {
+  ILinkInfo,
+  ILinkOptions,
+  ILinkProps,
+  ILinkRenderData,
+  ILinkSlotProps,
+  ILinkState,
+  ILinkTokens,
+  ILinkType,
+  IWithLinkOptions,
+} from './Link.types';
+export { Link, useAsLink } from './Link';
+export type { ILinkHooks } from './Link';
