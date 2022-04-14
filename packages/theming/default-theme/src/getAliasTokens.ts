649142e1425add54960902464d76e0cc4051ad7d
Ruriko Araki <ruaraki@microsoft.com>
2022-04-14 13:25:16 -0700

    Move alias tokens to default theme

--- packages/theming/default-theme/src/getAliasTokens.ts
+++ packages/theming/default-theme/src/getAliasTokens.ts
@@ -1,5 +1,5 @@
-import * as lightAliasTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-aliases.json';
-import * as darkAliasTokens from '@fluentui-react-native/design-tokens-windows/dark/tokens-aliases.json';
+import lightAliasTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-aliases.json';
+import darkAliasTokens from '@fluentui-react-native/design-tokens-windows/dark/tokens-aliases.json';
 import { hcAliasTokens } from './highContrast/tokens-alias';
 import { AppearanceOptions } from '@fluentui-react-native/theme-types';
 import { assertNever } from 'assert-never';
--- packages/theming/theming-utils/src/index.ts
+++ packages/theming/theming-utils/src/index.ts
@@ -1,3 +1,2 @@
-export { createAliasTokens } from './createAliasTokens';
 export { getCurrentAppearance } from './getCurrentAppearance';
 export { mapPipelineToTheme } from './mapPipelineToTheme';
