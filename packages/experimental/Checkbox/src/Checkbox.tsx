c0ac2e8d2c2b9c4955aac5c402ab8cbf5cbb1293
Ruriko Araki <ruaraki@microsoft.com>
2022-04-14 11:50:27 -0700

    Cleanup

--- packages/experimental/Checkbox/src/Checkbox.tsx
+++ packages/experimental/Checkbox/src/Checkbox.tsx
@@ -6,6 +6,7 @@ if (__DEV__) {
     'The @fluentui-react-native/exprimental-checkbox package is deprecated for win32. The contents of this package have been moved to @fluentui-react-native/checkbox. If you need to use the Button component from this package, please use CheckboxV1 from @fluentui-react-native/checkbox.',
   );
 }
+
 export const Checkbox = CheckboxV1;
 
 export default Checkbox;
