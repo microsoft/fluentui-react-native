e8de05de0bcdbbdc576b56545b2057ee3ea4e8a2
ValentinaKozlova <v.kozova13@gmail.com>
2022-04-19 21:49:48 +0200

    Fixed prop for presenceBadge

--- apps/fluent-tester/src/FluentTester/TestComponents/Badge/BadgeTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Badge/BadgeTest.tsx
@@ -70,7 +70,7 @@ export const BasicBadge: React.FunctionComponent = () => {
           <Text>Presence Badge</Text>
           <PresenceBadge presence="available" size="largest" />
           <PresenceBadge presence="available" isOutOfOffice={true} size="large" />
-          <PresenceBadge presence="doNotDisturb" text="doNotDisturb" isOutOfOffice={true} />
+          <PresenceBadge presence="doNotDisturb" isOutOfOffice={true} />
           <PresenceBadge presence="away" size="small" />
           <PresenceBadge presence="busy" size="smallest" />
           <PresenceBadge presence="offline" />
