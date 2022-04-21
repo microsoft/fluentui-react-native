400344d4d699378975a83391c193639cb828774e
Ruriko Araki <ruaraki@microsoft.com>
2022-04-21 11:23:12 -0700

    Initial skeletal implementation of Menu

---
+++ packages/experimental/Menu/src/Menu/Menu.tsx
@@ -0,0 +1,30 @@
+/** @jsx withSlots */
+import { menuName, MenuProps } from './Menu.types';
+import { stagedComponent } from '@fluentui-react-native/framework';
+import React from 'react';
+
+export const Menu = stagedComponent((props: MenuProps) => {
+  const state = useMenu(props);
+  const contextValue = useMenuContextValue(state);
+
+  return (_rest: MenuProps, children: React.ReactNode) => {
+    const childrenArray = React.Children.toArray(props.children) as React.ReactElement[];
+
+    if (__DEV__) {
+      if (childrenArray.length === 0) {
+        // eslint-disable-next-line no-console
+        console.warn('Menu must contain at least one child');
+      }
+
+      if (childrenArray.length > 2) {
+        // eslint-disable-next-line no-console
+        console.warn('Menu must contain at most two children');
+      }
+    }
+
+    return <MenuProvider value={contextValue}>{children}</MenuProvider>;
+  };
+});
+Menu.displayName = menuName;
+
+export default Menu;
--- packages/experimental/Menu/src/index.ts
+++ packages/experimental/Menu/src/index.ts
@@ -1,1 +1,1 @@
-export { Menu } from './Menu';
+export { Menu } from './Menu/Menu';
