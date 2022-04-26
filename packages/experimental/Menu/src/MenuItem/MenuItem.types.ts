85496719e73bd22a9e67cc387efa079e60710915
Ruriko Araki <ruaraki@microsoft.com>
2022-04-26 14:44:38 -0700

    Implement MenuItem proto

--- packages/experimental/Menu/src/MenuItem/MenuItem.types.ts
+++ packages/experimental/Menu/src/MenuItem/MenuItem.types.ts
@@ -1,18 +1,47 @@
 import * as React from 'react';
+import { ViewProps } from 'react-native';
+import { SvgProps, XmlProps } from 'react-native-svg';
 import type { IViewProps } from '@fluentui-react-native/adapters';
+import { TextProps } from '@fluentui-react-native/experimental-text';
+import { IFocusable, InteractionEvent, IPressableHooks, IWithPressableOptions } from '@fluentui-react-native/interactive-hooks';
 import { FontTokens, IBorderTokens, IColorTokens, IShadowTokens, LayoutTokens } from '@fluentui-react-native/tokens';
 
 export const menuItemName = 'MenuItem';
 
 export interface MenuItemTokens extends LayoutTokens, FontTokens, IBorderTokens, IShadowTokens, IColorTokens {}
 
-export interface MenuItemProps extends Omit<IViewProps, 'onPress'> {}
+export interface MenuItemProps extends Omit<IWithPressableOptions<ViewProps>, 'onPress'> {
+  content: string;
 
-export interface MenuItemState {}
+  /**
+   * Applies disabled styles to menu item but remains focusable
+   */
+  disabled?: boolean;
+
+  /**
+   * A RefObject to access the IButton interface. Use this to access the public methods and properties of the component.
+   */
+  componentRef?: React.RefObject<IFocusable>;
+
+  /**
+   * If the menu item is a trigger for a submenu
+   */
+  hasSubmenu?: boolean;
+
+  /**
+   * A callback to call on button click event
+   */
+  onClick?: (e: InteractionEvent) => void;
+}
+
+export type MenuItemState = IPressableHooks<MenuItemProps & React.ComponentPropsWithRef<any>>;
 
 export interface MenuItemSlotProps {
   root: React.PropsWithRef<IViewProps>;
+  content?: TextProps;
+  submenuIndicator?: SvgProps | XmlProps;
 }
+
 export interface MenuItemType {
   props: MenuItemProps;
   tokens: MenuItemTokens;
--- packages/experimental/Menu/src/MenuItem/useMenuItem.ts
+++ packages/experimental/Menu/src/MenuItem/useMenuItem.ts
@@ -1,5 +1,40 @@
+import * as React from 'react';
+import { AccessibilityState } from 'react-native';
 import { MenuItemProps, MenuItemState } from './MenuItem.types';
+import { memoize } from '@fluentui-react-native/framework';
+import { useAsPressable, useKeyProps } from '@fluentui-react-native/interactive-hooks';
+import { useMenuContext } from '../context/menuContext';
 
-export const useMenuItem = (_props: MenuItemProps): MenuItemState => {
-  return {};
+export const useMenuItem = (props: MenuItemProps): MenuItemState => {
+  // attach the pressable state handlers
+  const defaultComponentRef = React.useRef(null);
+  const { onClick, accessibilityState, componentRef = defaultComponentRef, disabled, ...rest } = props;
+  const pressable = useAsPressable({ ...rest, disabled, onPress: onClick });
+  const onKeyUpProps = useKeyProps(onClick, ' ', 'Enter');
+  const hasSubmenu = useMenuContext().isSubmenu;
+
+  return {
+    props: {
+      ...pressable.props,
+      accessible: true,
+      accessibilityRole: 'button',
+      onAccessibilityTap: props.onAccessibilityTap || props.onClick,
+      accessibilityLabel: props.accessibilityLabel,
+      accessibilityState: getAccessibilityState(disabled, accessibilityState),
+      enableFocusRing: true,
+      focusable: !disabled,
+      hasSubmenu,
+      ref: componentRef,
+      ...onKeyUpProps,
+    },
+    state: pressable.state,
+  };
 };
+
+const getAccessibilityState = memoize(getAccessibilityStateWorker);
+function getAccessibilityStateWorker(disabled: boolean, accessibilityState?: AccessibilityState) {
+  if (accessibilityState) {
+    return { disabled: disabled, ...accessibilityState };
+  }
+  return { disabled: disabled };
+}
