dd71d34339535d6b1722ce88def83cf22110a86e
Ruriko Araki <ruaraki@microsoft.com>
2022-04-28 16:03:48 -0700

    Convert to compose

--- packages/experimental/Menu/src/MenuList/MenuList.tsx
+++ packages/experimental/Menu/src/MenuList/MenuList.tsx
@@ -1,13 +1,25 @@
+/** @jsx withSlots */
 import React from 'react';
 import { View } from 'react-native';
-import { stagedComponent } from '@fluentui-react-native/framework';
-import { menuListName, MenuListProps } from './MenuList.types';
+import { compose, UseSlots, withSlots } from '@fluentui-react-native/framework';
+import { menuListName, MenuListProps, MenuListType } from './MenuList.types';
 
-export const MenuList = stagedComponent((_props: MenuListProps) => {
-  return (_rest: MenuListProps, children: React.ReactNode) => {
-    return <View>{children}</View>;
-  };
+export const MenuList = compose<MenuListType>({
+  displayName: menuListName,
+  tokens: [],
+  slotProps: {
+    root: {},
+  },
+  slots: {
+    root: View,
+  },
+  useRender: (userProps: MenuListProps, useSlots: UseSlots<MenuListType>) => {
+    const Slots = useSlots(userProps);
+
+    return (_rest: MenuListProps, children: React.ReactNode) => {
+      return <Slots.root>{children}</Slots.root>;
+    };
+  },
 });
-MenuList.displayName = menuListName;
 
 export default MenuList;
--- packages/experimental/Menu/src/MenuList/MenuList.types.ts
+++ packages/experimental/Menu/src/MenuList/MenuList.types.ts
@@ -2,6 +2,15 @@ import type { IViewProps } from '@fluentui-react-native/adapters';
 
 export const menuListName = 'MenuList';
 
+export interface MenuListTokens {}
+
 export interface MenuListProps extends Omit<IViewProps, 'onPress'> {}
 
-export interface MenuListState {}
+export interface MenuListSlotProps {
+  root: React.PropsWithRef<IViewProps>;
+}
+export interface MenuListType {
+  props: MenuListProps;
+  tokens: MenuListTokens;
+  slotProps: MenuListSlotProps;
+}
