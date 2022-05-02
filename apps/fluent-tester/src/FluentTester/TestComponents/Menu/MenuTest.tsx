bca261a9003512f213abaea80fe3f18a3368157a
Ruriko Araki <ruaraki@microsoft.com>
2022-05-02 12:28:47 -0700

    Prototyping Submenu (#1616)
    ### Platforms Impacted
    - [ ] iOS
    - [x] macOS
    - [x] win32 (Office)
    - [ ] windows
    - [ ] android
    
    ### Description of changes
    
    Prototype of submenu with the current Menu implementation
    Implemented a MenuItem component which is something like a button with a built in chevron
    Added isSUubmenu context state which checks whether certain state is filled out to determine if the current Menu is under a different Menu context. This information is used to determine if the chevron in the MenuItem should be shown.
    
    This PR does not intend to do any sort of good positioning or smart sizing, this is to prove that nested menus will work.
    
    ### Verification
    
    ![menu_submenu](https://user-images.githubusercontent.com/4602628/165398521-e27b3aa2-0fe1-4ce5-ba75-6dbe119bcecc.gif)
    
    ### Pull request checklist
    
    This PR has considered (when applicable):
    - [ ] Automated Tests
    - [ ] Documentation and examples
    - [ ] Keyboard Accessibility
    - [ ] Voiceover
    - [ ] Internationalization and Right-to-left Layouts

--- apps/fluent-tester/src/FluentTester/TestComponents/Menu/MenuTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Menu/MenuTest.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import { ButtonV1 as Button } from '@fluentui/react-native';
-import { Menu, MenuTrigger, MenuPopover } from '@fluentui-react-native/menu';
+import { Menu, MenuItem, MenuTrigger, MenuPopover } from '@fluentui-react-native/menu';
 import { Stack } from '@fluentui-react-native/stack';
 import { Text } from '@fluentui-react-native/experimental-text';
 import { stackStyle } from '../Common/styles';
@@ -25,11 +25,48 @@ const MenuDefault: React.FunctionComponent = () => {
   );
 };
 
+const Submenu: React.FunctionComponent = () => {
+  return (
+    <Menu>
+      <MenuTrigger>
+        <MenuItem content="A second MenuItem" />
+      </MenuTrigger>
+      <MenuPopover>
+        <View style={{ backgroundColor: 'blue', height: 50, width: 200 }}>
+          <Text>Hello world!!!</Text>
+        </View>
+      </MenuPopover>
+    </Menu>
+  );
+};
+
+const MenuSubMenu: React.FunctionComponent = () => {
+  return (
+    <Stack style={stackStyle}>
+      <Menu>
+        <MenuTrigger>
+          <Button>Test</Button>
+        </MenuTrigger>
+        <MenuPopover>
+          <View style={{ backgroundColor: 'red', width: 200, alignItems: 'center' }}>
+            <MenuItem content="A MenuItem" />
+            <Submenu />
+          </View>
+        </MenuPopover>
+      </Menu>
+    </Stack>
+  );
+};
+
 const menuSections: TestSection[] = [
   {
     name: 'Menu Default',
     testID: MENU_TESTPAGE,
     component: MenuDefault,
+  },
+  {
+    name: 'Menu Submenu',
+    component: MenuSubMenu,
   },
 ];
 
--- packages/experimental/Menu/src/index.ts
+++ packages/experimental/Menu/src/index.ts
@@ -1,3 +1,4 @@
 export { Menu } from './Menu/Menu';
 export { MenuTrigger } from './MenuTrigger/MenuTrigger';
 export { MenuPopover } from './MenuPopover/MenuPopover';
+export { MenuItem } from './MenuItem/MenuItem';
