1d14b7a7b450636d804587217af643259158638f
ValentinaKozlova <v.kozova13@gmail.com>
2022-05-04 12:46:36 +0200

    Added Avatar to Windows platform

--- apps/fluent-tester/src/FluentTester/TestComponents/Avatar/CustomizedAvatar.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Avatar/CustomizedAvatar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { JSAvatar, IconAlignment } from '@fluentui-react-native/experimental-avatar';
-import { Switch, View, Text, TextInput, TextStyle } from 'react-native';
+import { Switch, View, Text, TextInput, TextStyle, Platform } from 'react-native';
 import { Slider } from '../Common/Slider';
 import { steveBallmerPhotoUrl } from './../PersonaCoin/styles';
 import { useTheme } from '@fluentui-react-native/theme-types';
@@ -40,6 +40,8 @@ export const CustomizeUsage: React.FunctionComponent = () => {
     };
     return JSAvatar.customize(tokens);
   }, [coinColor, textColor, horizontalAlignment, verticalAlignment, iconSize, initialsSize, size]);
+
+  const svgIconsEnabled = ['ios', 'macos', 'win32', 'android'].includes(Platform.OS as string);
 
   return (
     <View style={commonStyles.root}>
@@ -112,7 +114,7 @@ export const CustomizeUsage: React.FunctionComponent = () => {
         activeAppearance="ring"
         initials="SB"
         accessibilityLabel="Former CEO of Microsoft"
-        badge={{ status: 'blocked' }}
+        badge={{ status: svgIconsEnabled ? 'blocked' : undefined }}
         src={showImage ? steveBallmerPhotoUrl : undefined}
         ring={
           showRing
