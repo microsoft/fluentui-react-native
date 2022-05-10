75be16e5b38b67a02e3bdee32b40a42932489819
Ruriko Araki <ruaraki@microsoft.com>
2022-05-10 16:48:57 -0700

    More cleanup

--- packages/experimental/Menu/src/MenuItemCheckbox/MenuItemCheckbox.types.ts
+++ packages/experimental/Menu/src/MenuItemCheckbox/MenuItemCheckbox.types.ts
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { ViewProps } from 'react-native';
-import { SvgProps, XmlProps } from 'react-native-svg';
 import type { IViewProps } from '@fluentui-react-native/adapters';
 import { TextProps } from '@fluentui-react-native/experimental-text';
 import { IFocusable, InteractionEvent, IPressableHooks, IWithPressableOptions } from '@fluentui-react-native/interactive-hooks';
