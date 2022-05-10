e7298d1741b694797ee6ef3d4cdee5fb43c7f66d
Ruriko Araki <ruaraki@microsoft.com>
2022-05-10 12:35:03 -0700

    Clean up code

--- packages/experimental/Menu/src/MenuPopover/MenuPopover.tsx
+++ packages/experimental/Menu/src/MenuPopover/MenuPopover.tsx
@@ -3,21 +3,19 @@ import { stagedComponent, useFluentTheme } from '@fluentui-react-native/framewor
 import { Callout } from '@fluentui-react-native/callout';
 import { menuPopoverName, MenuPopoverProps } from './MenuPopover.types';
 import { useMenuPopover } from './useMenuPopover';
-import { useMenuContext } from '../context/menuContext';
 
 export const MenuPopover = stagedComponent((props: MenuPopoverProps) => {
   const state = useMenuPopover(props);
   const theme = useFluentTheme();
-  const context = useMenuContext();
 
   return (_rest: MenuPopoverProps, children: React.ReactNode) => {
     return (
       <Callout
-        target={state.triggerRef}
         borderWidth={1}
-        onDismiss={() => context.setOpen(undefined, false /* isOpen */)}
         borderColor={theme.colors.neutralStrokeAccessible}
-        dismissBehaviors={context.isControlled ? ['preventDismissOnKeyDown', 'preventDismissOnClickOutside'] : undefined}
+        target={state.triggerRef}
+        onDismiss={state.onDismiss}
+        dismissBehaviors={state.dismissBehaviors}
       >
         {children}
       </Callout>
--- packages/experimental/Menu/src/MenuPopover/useMenuPopover.ts
+++ packages/experimental/Menu/src/MenuPopover/useMenuPopover.ts
@@ -1,3 +1,4 @@
+import { DismissBehaviors } from '@fluentui-react-native/callout';
 import { useMenuContext } from '../context/menuContext';
 import { MenuPopoverProps, MenuPopoverState } from './MenuPopover.types';
 
@@ -5,6 +6,10 @@ export const useMenuPopover = (_props: MenuPopoverProps): MenuPopoverState => {
   const context = useMenuContext();
 
   const triggerRef = context.triggerRef;
+  const onDismiss = context.setOpen(undefined, false /* isOpen */);
+  const dismissBehaviors = context.isControlled
+    ? (['preventDismissOnKeyDown', 'preventDismissOnClickOutside'] as DismissBehaviors[])
+    : undefined;
 
-  return { triggerRef };
+  return { triggerRef, onDismiss, dismissBehaviors };
 };
