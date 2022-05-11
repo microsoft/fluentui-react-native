0a383e3d6a736a89e628c14d27d82388cf154f2b
Ruriko Araki <ruaraki@microsoft.com>
2022-05-11 15:11:47 -0700

    Implement dismiss and open behaviors on Menu (#1651)
    * Initial open work
    
    * Dismiss and openOnHover
    
    * Fix up controlled behavior
    
    * Clean up code
    
    * Change files
    
    * Cleanup
    
    * Address feedback

--- packages/experimental/Menu/src/Menu/useMenu.ts
+++ packages/experimental/Menu/src/Menu/useMenu.ts
@@ -1,21 +1,44 @@
+import { InteractionEvent } from '@fluentui-react-native/interactive-hooks';
 import React from 'react';
 import { useMenuContext } from '../context/menuContext';
 import { MenuProps, MenuState } from './Menu.types';
 
 export const useMenu = (props: MenuProps): MenuState => {
-  const [open, setOpen] = useMenuOpenState(props);
   const triggerRef = React.useRef(null);
   const context = useMenuContext();
   const isSubmenu = context.triggerRef !== null;
+  const isControlled = typeof props.open !== 'undefined';
+  const [open, setOpen] = useMenuOpenState(isControlled, props);
 
   return {
+    ...props,
     open,
     setOpen,
     triggerRef,
     isSubmenu,
+    isControlled,
   };
 };
 
-const useMenuOpenState = (props: MenuProps) => {
-  return React.useState<boolean>(props.open);
+const useMenuOpenState = (isControlled: boolean, props: MenuProps): [boolean, (e: InteractionEvent, isOpen: boolean) => void] => {
+  const { defaultOpen, onOpenChange, open } = props;
+  const initialState = typeof defaultOpen !== 'undefined' ? defaultOpen : !!open;
+  const [openInternal, setOpenInternal] = React.useState<boolean>(initialState);
+
+  const state = isControlled ? open : openInternal;
+
+  const setOpen = React.useCallback(
+    (e: InteractionEvent, isOpen: boolean) => {
+      const openPrev = state;
+      if (!isControlled) {
+        setOpenInternal(isOpen);
+      }
+      if (onOpenChange && openPrev !== isOpen) {
+        onOpenChange(e, isOpen);
+      }
+    },
+    [isControlled, state, onOpenChange, setOpenInternal],
+  );
+
+  return [state, setOpen];
 };
--- packages/experimental/Menu/src/MenuPopover/MenuPopover.types.ts
+++ packages/experimental/Menu/src/MenuPopover/MenuPopover.types.ts
@@ -1,9 +1,12 @@
 import type { IViewProps } from '@fluentui-react-native/adapters';
+import { DismissBehaviors } from '@fluentui-react-native/callout';
 
 export const menuPopoverName = 'MenuPopover';
 
 export interface MenuPopoverProps extends Omit<IViewProps, 'onPress'> {}
 
 export interface MenuPopoverState {
+  dismissBehaviors: DismissBehaviors[];
+  onDismiss: () => void;
   triggerRef: React.RefObject<React.Component>;
 }
