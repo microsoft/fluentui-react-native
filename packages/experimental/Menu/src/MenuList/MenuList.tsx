236fee41ec8ddacd640101090a184ba553865fe8
Ruriko Araki <ruaraki@microsoft.com>
2022-05-20 18:18:20 -0700

    Implement gap token

--- packages/experimental/Menu/src/MenuList/MenuList.tsx
+++ packages/experimental/Menu/src/MenuList/MenuList.tsx
@@ -1,18 +1,45 @@
 /** @jsx withSlots */
 import React from 'react';
 import { View } from 'react-native';
-import { compose, UseSlots, withSlots } from '@fluentui-react-native/framework';
+import { compose, mergeProps, stagedComponent, UseSlots, withSlots } from '@fluentui-react-native/framework';
 import { menuListName, MenuListProps, MenuListType } from './MenuList.types';
 import { stylingSettings } from './MenuList.styling';
 import { MenuListProvider } from '../context/menuListContext';
 import { useMenuList } from './useMenuList';
-import { useMenuListContextValue } from './useMenuContextValue';
+import { useMenuListContextValue } from './useMenuListContextValue';
+import { IViewProps } from '@fluentui-react-native/adapters';
+
+export const MenuStack = stagedComponent((props: React.PropsWithRef<IViewProps> & { gap?: number }) => {
+  const { gap, ...rest } = props;
+  return (final: React.PropsWithRef<IViewProps> & { gap?: number }, children: React.ReactNode) => {
+    if (gap && gap > 0 && children) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore - TODO, fix typing error
+      children = React.Children.map(children, (child: React.ReactChild, index: number) => {
+        if (React.isValidElement(child) && index > 0) {
+          return React.cloneElement(
+            child,
+            mergeProps(child.props, {
+              style: {
+                ...{ marginTop: gap },
+              },
+            }),
+          );
+        }
+        return child;
+      });
+    }
+
+    return <View {...mergeProps(rest, final)}>{children}</View>;
+  };
+});
+MenuStack.displayName = 'MenuStack';
 
 export const MenuList = compose<MenuListType>({
   displayName: menuListName,
   ...stylingSettings,
   slots: {
-    root: View,
+    root: MenuStack,
   },
   useRender: (userProps: MenuListProps, useSlots: UseSlots<MenuListType>) => {
     const menuList = useMenuList(userProps);
@@ -28,5 +55,3 @@ export const MenuList = compose<MenuListType>({
     };
   },
 });
-
-export default MenuList;
