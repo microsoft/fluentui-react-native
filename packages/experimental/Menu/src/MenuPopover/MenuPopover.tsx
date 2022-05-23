4bca0db3c69056074a38eee6cc8b29ff2f425740
Ruriko Araki <ruaraki@microsoft.com>
2022-05-23 13:13:48 -0700

    Move into useMenuPopover

--- packages/experimental/Menu/src/MenuPopover/MenuPopover.tsx
+++ packages/experimental/Menu/src/MenuPopover/MenuPopover.tsx
@@ -3,16 +3,12 @@ import { stagedComponent, useFluentTheme } from '@fluentui-react-native/framewor
 import { Callout } from '@fluentui-react-native/callout';
 import { menuPopoverName, MenuPopoverProps } from './MenuPopover.types';
 import { useMenuPopover } from './useMenuPopover';
-import { Platform } from 'react-native';
 
 export const MenuPopover = stagedComponent((props: MenuPopoverProps) => {
   const state = useMenuPopover(props);
   const theme = useFluentTheme();
 
   return (_rest: MenuPopoverProps, children: React.ReactNode) => {
-    // Initial focus behavior differs per platform, Windows platforms move focus
-    // automatically onto first element of Callout
-    const setInitialFocus = Platform.OS === ('win32' as any) || Platform.OS === 'windows';
     return (
       <Callout
         borderWidth={1}
@@ -20,7 +16,7 @@ export const MenuPopover = stagedComponent((props: MenuPopoverProps) => {
         target={state.triggerRef}
         onDismiss={state.onDismiss}
         dismissBehaviors={state.dismissBehaviors}
-        setInitialFocus={setInitialFocus}
+        setInitialFocus={state.setInitialFocus}
       >
         {children}
       </Callout>
