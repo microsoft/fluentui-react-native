f43aad06b529335428f62c727d90c86da8383a7a
Ruriko Araki <ruaraki@microsoft.com>
2022-05-23 13:44:13 -0700

    Update comment

--- packages/experimental/Menu/src/MenuList/MenuList.tsx
+++ packages/experimental/Menu/src/MenuList/MenuList.tsx
@@ -14,7 +14,7 @@ const MenuStack = stagedComponent((props: React.PropsWithRef<IViewProps> & { gap
   return (final: React.PropsWithRef<IViewProps> & { gap?: number }, children: React.ReactNode) => {
     if (gap && gap > 0 && children) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore - TODO, fix typing error
+      // @ts-ignore - GH:1684, fix typing error
       children = React.Children.map(children, (child: React.ReactChild, index: number) => {
         if (React.isValidElement(child) && index > 0) {
           return React.cloneElement(child, mergeProps(child.props, { style: { marginTop: gap } }));
