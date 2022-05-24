f5005feecf6ac9cf5c22877c0151ca637720fd97
lenahong <lehon@microsoft.com>
2022-05-24 14:30:34 -0700

    Change Submenu chevron to use an embedded svg that can flip rtl and recolor (#1686)
    * change chevron to svg file
    
    * change to using Svg component
    
    * Change to use an embedded svg that can flip rtl and recolor
    
    * remove test color
    
    * Change files
    
    * delete unused chevron.svg
    
    * delete unnecessary .vs files
    
    * delete unused files
    
    Co-authored-by: warleu <warleu@microsoft.com>

--- packages/components/ContextualMenu/src/SubmenuItem.types.ts
+++ packages/components/ContextualMenu/src/SubmenuItem.types.ts
@@ -4,7 +4,6 @@ import { IRenderData } from '@uifabricshared/foundation-composable';
 import { ITextProps } from '@fluentui-react-native/text';
 import { ContextualMenuItemProps, ContextualMenuItemTokens, ContextualMenuItemState } from './ContextualMenuItem.types';
 import { IconProps } from '@fluentui-react-native/icon';
-import { XmlProps } from 'react-native-svg';
 
 export const submenuItemName = 'SubmenuItem';
 export interface SubmenuItemTokens extends ContextualMenuItemTokens {
@@ -20,7 +19,7 @@ export interface SubmenuItemSlotProps {
   icon: IconProps;
   content: ITextProps;
   endstack: IViewProps;
-  chevron: XmlProps;
+  chevron: IconProps;
 }
 
 export type SubmenuItemRenderData = IRenderData<SubmenuItemSlotProps, SubmenuItemState>;
