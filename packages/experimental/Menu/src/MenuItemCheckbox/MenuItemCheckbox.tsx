6de49b559a9148b6b5c47e18eb81564a50a208b9
Ruriko Araki <ruaraki@microsoft.com>
2022-05-24 12:36:21 -0700

    Fix bad name

--- packages/experimental/Menu/src/MenuItemCheckbox/MenuItemCheckbox.tsx
+++ packages/experimental/Menu/src/MenuItemCheckbox/MenuItemCheckbox.tsx
@@ -35,14 +35,14 @@ export const menuItemFinalRender = (
 ): React.FunctionComponent<MenuItemCheckboxProps> => {
   return (final: MenuItemCheckboxProps) => {
     const mergedProps = mergeProps(menuItem.props, final);
-    const chevronXml = `
+    const checkmarkXml = `
     <svg>
       <path fill='currentColor' d='M9.85355 3.14645C10.0488 3.34171 10.0488 3.65829 9.85355 3.85355L5.35355 8.35355C5.15829 8.54882 4.84171 8.54882 4.64645 8.35355L2.64645 6.35355C2.45118 6.15829 2.45118 5.84171 2.64645 5.64645C2.84171 5.45118 3.15829 5.45118 3.35355 5.64645L5 7.29289L9.14645 3.14645C9.34171 2.95118 9.65829 2.95118 9.85355 3.14645Z' />
     </svg>`;
 
     return (
       <Slots.root {...mergedProps}>
-        <Slots.checkmark xml={chevronXml} />
+        <Slots.checkmark xml={checkmarkXml} />
         {mergedProps.content && <Slots.content>{mergedProps.content}</Slots.content>}
       </Slots.root>
     );
