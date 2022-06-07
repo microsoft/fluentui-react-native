ab4552de824e662d5fddf578ae083d3b3a8113fe
Ruriko Araki <ruaraki@microsoft.com>
2022-06-07 09:33:23 -0700

    Add Menu E2E tests & Accessibility E2E tests (#1720)
    * Fix description
    
    * Add e2e tests
    
    * Change files
    
    * Fix depcheck

--- apps/fluent-tester/src/FluentTester/TestComponents/Menu/consts.ts
+++ apps/fluent-tester/src/FluentTester/TestComponents/Menu/consts.ts
@@ -1,2 +1,16 @@
 export const HOMEPAGE_MENU_BUTTON = 'Homepage_Menu_Button';
 export const MENU_TESTPAGE = 'Menu_TestPage';
+
+export const MENUTRIGGER_TEST_COMPONENT = 'MenuTrigger_Test_Component';
+export const MENUPOPOVER_TEST_COMPONENT = 'MenuPopover_Test_Component';
+
+/* Test MenuItem 1 */
+export const MENUITEM_TEST_COMPONENT = 'MenuItem_Test_Component'; // A component on each specific test page
+export const MENUITEM_ACCESSIBILITY_LABEL = 'E2E testing MenuItem accessibility label';
+
+/* Test MenuItem 2 */
+export const MENUITEM_NO_A11Y_LABEL_COMPONENT = 'MenuItem_No_A11y_label_Component';
+export const MENUITEM_TEST_LABEL = 'Test MenuItem2 - No Accessibility Label'; // A component on each specific test page
+
+export const MENU_ON_OPEN = 'Menu_On_Open'; // For testing the open functionality of Menu
+export const MENU_ON_CLOSE = 'Menu_On_Close'; // For testing the open functionality of Menu
