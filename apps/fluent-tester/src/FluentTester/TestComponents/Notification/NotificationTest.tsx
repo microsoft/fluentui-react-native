448b4fb01e62c21245b95a4dc25394d8ad027c1d
Joanna Qu <joannaquu@gmail.com>
2022-06-13 16:06:31 -0700

    removed unnecessary styling properties

--- apps/fluent-tester/src/FluentTester/TestComponents/Notification/NotificationTest.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Notification/NotificationTest.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-// import { Notification } from
 import { View, Text } from 'react-native';
 import { Test, TestSection, PlatformStatus } from '../Test';
-// import { ColorValue } from 'react-native';
-
 
 const PrimaryToastTest: React.FunctionComponent = () => {
 
@@ -15,22 +12,17 @@ const PrimaryToastTest: React.FunctionComponent = () => {
 
       flexDirection: 'row',
       justifyContent: 'space-between',
-      alignItems: 'center',
 
       padding: 16,
     },
     left: {
       fontSize: 16,
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
     },
     right: {
       fontSize: 16,
       fontWeight: '500',
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
     },
-  }
+  };
 
   return (
     <View style={styles.container as any}>
@@ -65,7 +57,7 @@ const NeutralToastTest: React.FunctionComponent = () => {
       
       marginLeft: 34,
     },
-  }
+  };
 
   return (
     <View style={styles.container as any}>
@@ -99,10 +91,8 @@ const DangerToastTest: React.FunctionComponent = () => {
       color: 'maroon',
       fontSize: 16,
       fontWeight: '500',
-      
-      marginLeft: 34,
     },
-  }
+  };
 
   return (
     <View style={styles.container as any}>
@@ -121,25 +111,20 @@ const WarningToastTest: React.FunctionComponent = () => {
       borderRadius: 12,
 
       flexDirection: 'row',
-      alignItems: 'center',
+      justifyContent: 'space-between',
 
       padding: 16,
     },
     left: {
       color: 'brown',
       fontSize: 16,
-      width: 0,
-      flexGrow: 1,
-      flex: 1,
     },
     right: {
       color: 'brown',
       fontSize: 16,
       fontWeight: '500',
-      
-      marginLeft: 34,
     },
-  }
+  };
 
   return (
     <View style={styles.container as any}>
