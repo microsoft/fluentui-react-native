7c5c2341dfa4faf9f72ced717e488a76d64581af
Ruriko Araki <ruaraki@microsoft.com>
2022-06-13 14:23:43 -0700

    Close all menus when submenu item clicked (#1733)
    * Close all menus when submenu item clicked
    
    * Change files
    
    * Fix flickering behavior

--- packages/experimental/Menu/src/Menu/useMenu.ts
+++ packages/experimental/Menu/src/Menu/useMenu.ts
@@ -3,12 +3,19 @@ import React from 'react';
 import { useMenuContext } from '../context/menuContext';
 import { MenuProps, MenuState } from './Menu.types';
 
+// Due to how events get fired we get double notifications
+// for the same event causing us to immediately reopen
+// a menu when we close it. Adding in a delay to prevent
+// this behavior.
+const delayOpen = 150;
+let lastCloseTimestamp = -1;
+
 export const useMenu = (props: MenuProps): MenuState => {
   const triggerRef = React.useRef();
   const context = useMenuContext();
   const isSubmenu = context.triggerRef !== null;
   const isControlled = typeof props.open !== 'undefined';
-  const [open, setOpen] = useMenuOpenState(isControlled, props);
+  const [open, setOpen] = useMenuOpenState(isControlled, props, context.setOpen);
 
   // Default behavior for submenu is to open on hover
   // the ...props line below will override this behavior for a submenu
@@ -33,7 +40,11 @@ export const useMenu = (props: MenuProps): MenuState => {
   };
 };
 
-const useMenuOpenState = (isControlled: boolean, props: MenuProps): [boolean, (e: InteractionEvent, isOpen: boolean) => void] => {
+const useMenuOpenState = (
+  isControlled: boolean,
+  props: MenuProps,
+  parentSetOpen: (e: InteractionEvent, isOpen: boolean, bubble?: boolean) => void,
+): [boolean, (e: InteractionEvent, isOpen: boolean, bubble?: boolean) => void] => {
   const { defaultOpen, onOpenChange, open } = props;
   const initialState = typeof defaultOpen !== 'undefined' ? defaultOpen : !!open;
   const [openInternal, setOpenInternal] = React.useState<boolean>(initialState);
@@ -41,16 +52,26 @@ const useMenuOpenState = (isControlled: boolean, props: MenuProps): [boolean, (e
   const state = isControlled ? open : openInternal;
 
   const setOpen = React.useCallback(
-    (e: InteractionEvent, isOpen: boolean) => {
+    (e: InteractionEvent, isOpen: boolean, bubble?: boolean) => {
       const openPrev = state;
       if (!isControlled) {
-        setOpenInternal(isOpen);
+        if (!isOpen || lastCloseTimestamp + delayOpen <= Date.now()) {
+          setOpenInternal(isOpen);
+        }
+
+        if (!isOpen) {
+          lastCloseTimestamp = Date.now();
+        }
       }
       if (onOpenChange && openPrev !== isOpen) {
         onOpenChange(e, isOpen);
       }
+
+      if (bubble && parentSetOpen) {
+        parentSetOpen(e, isOpen, bubble);
+      }
     },
-    [isControlled, state, onOpenChange, setOpenInternal],
+    [isControlled, state, onOpenChange, setOpenInternal, parentSetOpen],
   );
 
   return [state, setOpen];
