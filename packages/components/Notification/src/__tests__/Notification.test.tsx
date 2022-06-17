33c4295ed6011bf55022338b8e6d59577b245373
Joanna Qu <joannaquu@gmail.com>
2022-06-17 16:55:18 -0700

    update yarn

--- packages/components/Notification/src/__tests__/Notification.test.tsx
+++ packages/components/Notification/src/__tests__/Notification.test.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { Notification } from '../Notification';
 import * as renderer from 'react-test-renderer';
 
-it('Notification default', () => {
-  const tree = renderer.create(<Notification variant={'primary'} startText="Mail Archived" endText="Undo"></Notification>).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Notification component tests', () => {
+  it('Notification default', () => {
+    const tree = renderer.create(<Notification variant={'primary'} startText="Mail Archived" endText="Undo"></Notification>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
