f4362eb4ef8dd0af83ea806641302595c777e55d
nabil-k <nkhalil942@gmail.com>
2022-06-17 11:43:46 -0700

    replaced picker on AlignmentPicker with picker for win32

---
+++ apps/fluent-tester/src/FluentTester/TestComponents/Common/AlignmentPicker.win32.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { undefinedText } from '../PersonaCoin/styles';
+import { IconAlignment } from '@fluentui/react-native';
+import { StyleProp, ViewStyle } from 'react-native';
+import { MenuPicker } from '../../utilityComponents/MenuPicker';
+
+const alignmentValues: Array<typeof undefinedText | IconAlignment> = [undefinedText, 'start', 'center', 'end'];
+
+interface IAlignmentPickerProps {
+  style?: StyleProp<ViewStyle>;
+  label: string;
+  onSelectionChange: (value: IconAlignment | undefined) => void;
+}
+
+export const AlignmentPicker: React.FunctionComponent<IAlignmentPickerProps> = (props: IAlignmentPickerProps) => {
+  const { label, onSelectionChange, style } = props;
+  return (
+    <MenuPicker
+      style={style}
+      prompt={label}
+      selectedValue={undefinedText}
+      onChange={(value: IconAlignment, index) => onSelectionChange(index == 0 ? undefined : value)}
+      collection={alignmentValues}
+    />
+  );
+};
