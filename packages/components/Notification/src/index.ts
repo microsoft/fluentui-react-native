cb94c004609c3cf96b9e5af57abb71893b8c3753
Joanna Qu <joannaquu@gmail.com>
2022-06-17 13:43:37 -0700

    run prettier again

--- packages/components/Notification/src/index.ts
+++ packages/components/Notification/src/index.ts
@@ -1,7 +1,2 @@
 export { Notification } from './Notification';
-export type {
-  NotificationTokens,
-  NotificationProps,
-  NotificationSlotProps,
-  NotificationType
-} from './Notification.types';
+export type { NotificationTokens, NotificationProps, NotificationSlotProps, NotificationType } from './Notification.types';
