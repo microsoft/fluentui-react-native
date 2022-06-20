235211a5658f435dd1474509e094cfb3cf29a139
Joanna Qu <joannaquu@gmail.com>
2022-06-20 14:22:55 -0700

    update package.jsons

--- packages/components/Notification/src/__tests__/Notification.test.tsx
+++ packages/components/Notification/src/__tests__/Notification.test.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { Notification } from '../Notification';
 import * as renderer from 'react-test-renderer';
+import { checkRenderConsistency, checkReRender } from '@fluentui-react-native/test-tools';
+
 
 describe('Notification component tests', () => {
   it('Notification default', () => {
     const tree = renderer.create(<Notification variant={'primary'} startText="Mail Archived" endText="Undo"></Notification>).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Notification simple rendering does not invalidate styling', () => {
+    checkRenderConsistency(() => <Notification variant={'primary'} startText="Mail Archived" endText="Undo"></Notification>, 2);
+  });
+
+  it('Notification re-renders correctly', () => {
+    checkReRender(() => <Notification variant={'primary'} startText="Mail Archived" endText="Undo"></Notification>, 2);
   });
 });
