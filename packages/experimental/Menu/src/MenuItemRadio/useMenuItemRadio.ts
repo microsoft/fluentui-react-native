86c51d03d67dbcff82f6ce99e24428a968649e62
Ruriko Araki <ruaraki@microsoft.com>
2022-06-23 16:49:58 -0700

    Radio closes menu

--- packages/experimental/Menu/src/MenuItemRadio/useMenuItemRadio.ts
+++ packages/experimental/Menu/src/MenuItemRadio/useMenuItemRadio.ts
@@ -3,27 +3,31 @@ import { InteractionEvent } from '@fluentui-react-native/interactive-hooks';
 import { useMenuListContext } from '../context/menuListContext';
 import { MenuItemCheckboxProps, MenuItemCheckboxState } from '../MenuItemCheckbox/MenuItemCheckbox.types';
 import { useMenuCheckboxInteraction } from '../MenuItemCheckbox/useMenuItemCheckbox';
+import { useMenuContext } from '../context/menuContext';
 
 export const useMenuItemRadio = (props: MenuItemCheckboxProps): MenuItemCheckboxState => {
   const { disabled, name } = props;
-  const context = useMenuListContext();
-  const selectRadio = context.selectRadio;
+  const context = useMenuContext();
+  const listContext = useMenuListContext();
+  const selectRadio = listContext.selectRadio;
+  const setOpen = context.setOpen;
 
   const toggleChecked = React.useCallback(
     (e: InteractionEvent) => {
       if (!disabled) {
         selectRadio(e, name);
+        setOpen(e, false /*isOpen*/, true /*bubble*/);
       }
     },
-    [disabled, name, selectRadio],
+    [disabled, name, selectRadio, setOpen],
   );
 
   // Explicitly only run on mount and unmount
   React.useEffect(() => {
-    context.addRadioItem(name);
+    listContext.addRadioItem(name);
 
     return () => {
-      context.removeRadioItem(name);
+      listContext.removeRadioItem(name);
     };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
