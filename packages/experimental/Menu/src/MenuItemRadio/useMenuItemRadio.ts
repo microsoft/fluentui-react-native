f898e0a3872ecc670cf10189346984e9fef8ed9f
Ruriko Araki <ruaraki@microsoft.com>
2022-06-24 11:02:47 -0700

    Revert "Radio closes menu"
    This reverts commit 3692f2ab275d345de90e6a639803ea2421868fbe.

--- packages/experimental/Menu/src/MenuItemRadio/useMenuItemRadio.ts
+++ packages/experimental/Menu/src/MenuItemRadio/useMenuItemRadio.ts
@@ -3,31 +3,27 @@ import { InteractionEvent } from '@fluentui-react-native/interactive-hooks';
 import { useMenuListContext } from '../context/menuListContext';
 import { MenuItemCheckboxProps, MenuItemCheckboxState } from '../MenuItemCheckbox/MenuItemCheckbox.types';
 import { useMenuCheckboxInteraction } from '../MenuItemCheckbox/useMenuItemCheckbox';
-import { useMenuContext } from '../context/menuContext';
 
 export const useMenuItemRadio = (props: MenuItemCheckboxProps): MenuItemCheckboxState => {
   const { disabled, name } = props;
-  const context = useMenuContext();
-  const listContext = useMenuListContext();
-  const selectRadio = listContext.selectRadio;
-  const setOpen = context.setOpen;
+  const context = useMenuListContext();
+  const selectRadio = context.selectRadio;
 
   const toggleChecked = React.useCallback(
     (e: InteractionEvent) => {
       if (!disabled) {
         selectRadio(e, name);
-        setOpen(e, false /*isOpen*/, true /*bubble*/);
       }
     },
-    [disabled, name, selectRadio, setOpen],
+    [disabled, name, selectRadio],
   );
 
   // Explicitly only run on mount and unmount
   React.useEffect(() => {
-    listContext.addRadioItem(name);
+    context.addRadioItem(name);
 
     return () => {
-      listContext.removeRadioItem(name);
+      context.removeRadioItem(name);
     };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
