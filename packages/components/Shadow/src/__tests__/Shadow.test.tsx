5c071a5e3d9eb06b524ed95e9fcb25453acc0356
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-06-27 15:49:57 -0700

    Fix Shadow.test.tsx

--- packages/components/Shadow/src/__tests__/Shadow.test.tsx
+++ packages/components/Shadow/src/__tests__/Shadow.test.tsx
@@ -1,21 +1,40 @@
 import * as React from 'react';
+import { Text } from '@fluentui/react-native';
 import { Shadow } from '../Shadow';
 import * as renderer from 'react-test-renderer';
 import { checkRenderConsistency, checkReRender } from '@fluentui-react-native/test-tools';
 
 describe('Shadow component tests', () => {
   it('Shadow default', () => {
-    const tree = renderer.create(<Shadow>Your component</Shadow>).toJSON();
+    const tree = renderer
+      .create(
+        <Shadow>
+          <Text>Shadow default test</Text>
+        </Shadow>,
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('Shadow simple rendering does not invalidate styling', () => {
-    checkRenderConsistency(() => <Shadow>Default Shadow</Shadow>, 2);
+    checkRenderConsistency(
+      () => (
+        <Shadow>
+          <Text>Shadow render test</Text>
+        </Shadow>
+      ),
+      2,
+    );
   });
 
   it('Shadow re-renders correctly', () => {
-    checkReRender(() => <Shadow>Render twice</Shadow>, 2);
+    checkReRender(
+      () => (
+        <Shadow>
+          <Text>Shadow render twice test</Text>
+        </Shadow>
+      ),
+      2,
+    );
   });
-
-  // Feel free to add more tests here
 });
