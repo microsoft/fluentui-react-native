7c7a50560c4d7584ea33ed55e64d703b9a722276
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-06-30 14:29:50 -0700

    Fix comment

--- packages/components/Button/src/FAB/FABColorTokens.ts
+++ packages/components/Button/src/FAB/FABColorTokens.ts
@@ -4,7 +4,7 @@ import { ButtonCoreTokens } from '../Button.types';
 import { shadowStyleFromTheme } from '@fluentui-react-native/experimental-shadow';
 
 export const defaultFABColorTokens: TokenSettings<ButtonCoreTokens, Theme> = (t: Theme): ButtonCoreTokens => ({
-  // // Grab the second shadow from the global token, as we only support displaying one shadow
+  // Grab the second shadow from the global token, as we only support displaying one shadow
   ...shadowStyleFromTheme(t, 'shadow8').key,
 
   // coloring same as primary
