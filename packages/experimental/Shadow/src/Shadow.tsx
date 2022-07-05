41d890c365738a690a3749752618beffdb6b8f3e
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-07-05 15:31:35 -0700

    Add cache for shadow token styles (which fixed tests)

--- packages/experimental/Shadow/src/Shadow.tsx
+++ packages/experimental/Shadow/src/Shadow.tsx
@@ -1,22 +1,43 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import { ShadowProps, ShadowTokens, shadowName } from './Shadow.types';
-import { compressible, buildUseTokens, useFluentTheme, mergeProps } from '@fluentui-react-native/framework';
+import { compressible, buildUseTokens, useFluentTheme, mergeProps, UseTokens } from '@fluentui-react-native/framework';
 import { shadowStyleFromTheme } from './shadowStyle';
 
 const useShadowTokens = buildUseTokens<ShadowTokens>(() => ({}), shadowName);
 
-export const Shadow = compressible<ShadowProps, ShadowTokens>((props: ShadowProps) => {
+export const Shadow = compressible<ShadowProps, ShadowTokens>((props: ShadowProps, useTokens: UseTokens<ShadowTokens>) => {
   const { depth } = props;
   const theme = useFluentTheme();
 
+  const [tokens, cache] = useTokens(theme);
   const shadowStyle = shadowStyleFromTheme(theme, depth);
 
+  const [keyShadowTokenStyle] = cache(
+    () => ({
+      shadowColor: shadowStyle.key.shadowColor,
+      shadowOpacity: shadowStyle.key.shadowOpacity,
+      shadowRadius: shadowStyle.key.shadowRadius,
+      shadowOffset: shadowStyle.key.shadowOffset,
+    }),
+    ['shadowColor', 'shadowOpacity', 'shadowRadius', 'shadowOffset'],
+  );
+
+  const [ambientShadowTokenStyle] = cache(
+    () => ({
+      shadowColor: shadowStyle.ambient.shadowColor,
+      shadowOpacity: shadowStyle.ambient.shadowOpacity,
+      shadowRadius: shadowStyle.ambient.shadowRadius,
+      shadowOffset: shadowStyle.ambient.shadowOffset,
+    }),
+    ['shadowColor', 'shadowOpacity', 'shadowRadius', 'shadowOffset'],
+  );
+
   return (extra: ShadowProps, children: React.ReactNode) => {
-    const mergedProps = mergeProps({ style: shadowStyle.key }, extra);
+    const mergedProps = mergeProps(tokens, { style: keyShadowTokenStyle }, extra);
 
     return (
-      <View style={shadowStyle.ambient}>
+      <View style={ambientShadowTokenStyle}>
         <View {...mergedProps}>{children}</View>
       </View>
     );
