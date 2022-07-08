ef2a18b53bbc59178c30d1250ff89e64bb2ca5be
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-07-08 11:11:45 -0700

    Shadow: get tokens from theme and use stagedcomponent (#1812)
    * Copy over and update shadowStyleFromTheme method
    
    - move shadowStyle.ts from Button to Shadow
    - make shadowStyleFromTheme return key and ambient
    - make helper methods in shadowStyleFromTheme
    - replace deprecated substr method with substring method
    - round shadowOpacity to two decimal places
    
    * Use shadow tokens from theme + base compressible framework
    
    * Clean up Shadow (remove comments, unused imports/vars)
    
    * Update shadow package.json
    
    * Respond to comments
    
    - Update shadowStyleFromTheme to take in a ShadowDepth param instead of a string
    - Split out android method for shadowStyleFromTheme
    - use merged props
    
    * Partially fix tests (render consistency and rerender tests still failing)
    
    * Change shadowDepth type from '2' to 'shadow2'
    
    Update tester with new brand types
    
    * Change files
    
    * Revert shadowStyle changes in Button
    
    * Add cache for shadow token styles (which fixed tests)
    
    * Fix the tester for shadows, also make margins more similar to Figma
    
    * Change files
    
    * Revert changes in Button package
    
    * Move shadowStyleFromTheme call into cache code
    
    * Some fixes to cache
    
    Known issue: tests will fail due to typing issues
    
    * Switch to staged component
    
    * Update snapshot
    
    * Add tests for different shadows
    
    * Add shadow description
    
    * Replace shadowStyleFromTheme with getShadowTokenStyleSet
    
    * Update framework version
    
    * Accidentally updated wrong package

--- apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowDepthTestSection.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowDepthTestSection.tsx
@@ -1,53 +1,84 @@
 import * as React from 'react';
 import { commonTestStyles } from '../Common/styles';
-import { View } from 'react-native';
+import { ColorValue, View } from 'react-native';
 import { Text } from '@fluentui/react-native';
-import { Shadow, ShadowDepth } from '@fluentui-react-native/experimental-shadow';
+import { Shadow, ShadowDepth, getShadowTokenStyleSet } from '@fluentui-react-native/experimental-shadow';
 import { Theme, useTheme } from '@fluentui-react-native/theme-types';
 import { themedStyleSheet } from '@fluentui-react-native/themed-stylesheet';
+import { useFluentTheme } from '@fluentui-react-native/framework';
 
-const getThemedStyles = themedStyleSheet((t: Theme) => {
+const getThemedStyles = themedStyleSheet(() => {
   return {
     effectBox: {
       width: 366,
       minHeight: 64,
-      backgroundColor: t.colors.background,
     },
     padding: {
       padding: 20,
       paddingHorizontal: 24,
     },
     vmargin: {
-      marginVertical: 32,
+      marginVertical: 16,
+      marginHorizontal: 32,
     },
   };
 });
 
 interface ShadowTestBoxProps {
   depth: ShadowDepth;
+  backgroundColor: ColorValue;
 }
 
 const ShadowTestBox: React.FunctionComponent<ShadowTestBoxProps> = (props: ShadowTestBoxProps) => {
   const theme = useTheme();
   const themedStyles = getThemedStyles(theme);
+
   return (
     <Shadow depth={props.depth}>
-      <Text style={[commonTestStyles.view, themedStyles.effectBox, themedStyles.vmargin, themedStyles.padding]} variant="bodySemibold">
-        Shadow Depth: {props.depth}
-      </Text>
+      <View
+        style={[
+          commonTestStyles.view,
+          themedStyles.effectBox,
+          themedStyles.vmargin,
+          themedStyles.padding,
+          { backgroundColor: props.backgroundColor },
+        ]}
+      >
+        <Text variant="bodySemibold">{props.depth}</Text>
+        <Text>{getShadowDescription(props.depth, theme)}</Text>
+      </View>
     </Shadow>
   );
 };
 
 export const ShadowDepthTestSection: React.FunctionComponent = () => {
+  const theme = useFluentTheme();
+
   return (
-    <View style={{ padding: 20 }}>
-      <ShadowTestBox depth="2" />
-      <ShadowTestBox depth="4" />
-      <ShadowTestBox depth="8" />
-      <ShadowTestBox depth="16" />
-      <ShadowTestBox depth="28" />
-      <ShadowTestBox depth="64" />
+    <View>
+      <ShadowTestBox depth="shadow2" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow4" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow8" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow16" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow28" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow64" backgroundColor={theme.colors.background} />
+      <ShadowTestBox depth="shadow2brand" backgroundColor={theme.colors.brandBackground} />
+      <ShadowTestBox depth="shadow4brand" backgroundColor={theme.colors.brandBackground} />
+      <ShadowTestBox depth="shadow8brand" backgroundColor={theme.colors.brandBackground} />
+      <ShadowTestBox depth="shadow16brand" backgroundColor={theme.colors.brandBackground} />
+      <ShadowTestBox depth="shadow28brand" backgroundColor={theme.colors.brandBackground} />
+      <ShadowTestBox depth="shadow64brand" backgroundColor={theme.colors.brandBackground} />
     </View>
   );
 };
+
+function getShadowDescription(depth: ShadowDepth, t: Theme): string {
+  const shadowStyle = getShadowTokenStyleSet(t, depth);
+
+  return (
+    '\nAmbient: ' +
+    JSON.stringify(shadowStyle.ambient, undefined, ' ').split('\n').join('').replace(/['"]+/g, '') +
+    '\n\nKey: ' +
+    JSON.stringify(shadowStyle.key, undefined, ' ').split('\n').join('').replace(/['"]+/g, '')
+  );
+}
