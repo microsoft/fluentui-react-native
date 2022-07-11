05dc5b22adf41007b49ba4a8ef67bb169233e4d7
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-07-11 14:57:00 -0700

    Forgot to revert macOS button test as well

--- apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowButtonTestSection.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowButtonTestSection.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react';
 import { Platform, View } from 'react-native';
-import { ButtonV1 as Button, FAB } from '@fluentui/react-native';
+import { FAB } from '@fluentui/react-native';
 import { commonTestStyles } from '../Common/styles';
 import { svgProps } from '../Common/iconExamples';
 
 export const ShadowButtonTestSection: React.FunctionComponent = () => {
-  if (Platform.OS === 'macos') {
-    return (
-      <View style={{ padding: 24, margin: 16 }}>
-        <Button style={commonTestStyles.vmargin}>Default Button: has shadow by default</Button>
-        <Button style={commonTestStyles.vmargin} appearance="primary">
-          Primary Button: does not have shadow by default
-        </Button>
-        <Button style={commonTestStyles.vmargin} appearance="subtle">
-          Subtle Button: does not have shadow by default
-        </Button>
-      </View>
-    );
-  } else if (Platform.OS === 'ios') {
+  if (Platform.OS === 'ios') {
     return (
       <View style={{ padding: 24, margin: 16 }}>
         <FAB style={commonTestStyles.vmargin} icon={{ svgSource: svgProps, width: 20, height: 20 }}>
