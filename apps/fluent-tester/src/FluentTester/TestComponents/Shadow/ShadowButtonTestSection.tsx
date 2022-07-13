395bd607262ff9d975c2cf6ddb11e00786bce2f6
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-07-13 11:55:34 -0700

    Move custom components outside render method

--- apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowButtonTestSection.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Shadow/ShadowButtonTestSection.tsx
@@ -3,11 +3,11 @@ import { Platform, View } from 'react-native';
 import { FAB, Text } from '@fluentui/react-native';
 import { commonTestStyles, stackStyle } from '../Common/styles';
 
+const CustomFABNoShadow = FAB.customize({ shadowDepth: undefined });
+const CustomFABShadow64 = FAB.customize({ shadowDepth: 'shadow64' });
+
 export const ShadowButtonTestSection: React.FunctionComponent = () => {
   if (Platform.OS === 'ios') {
-    const CustomFABNoShadow = FAB.customize({ shadowDepth: undefined });
-    const CustomFABShadow64 = FAB.customize({ shadowDepth: 'shadow64' });
-
     return (
       <View style={stackStyle}>
         <FAB style={commonTestStyles.vmargin}>FAB with default shadow</FAB>
