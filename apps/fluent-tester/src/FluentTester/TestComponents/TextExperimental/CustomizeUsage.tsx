3c2728e993014ee36272608ce23165aa8212bb31
chiuam <67026167+chiuam@users.noreply.github.com>
2022-07-14 15:46:21 -0400

    Revert "Add support for onAccessibilityTap on Text" (#1853)

--- apps/fluent-tester/src/FluentTester/TestComponents/TextExperimental/CustomizeUsage.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/TextExperimental/CustomizeUsage.tsx
@@ -50,7 +50,7 @@ export const CustomizeUsage: React.FunctionComponent = () => {
   const BlockText = () => {
     return (
       <>
-        <Text style={{ textTransform: 'uppercase' }} font="base" weight="medium" truncate>
+        <Text style={{ textTransform: 'uppercase' }} font="base" weight="medium">
           I am going to be truncated since I am a very very very very very very very very very very very very very very very very very very
           very very very very very very very very very very very very very very very very very very very very very very very very very very
           very very very very very very very very very very very very very very very very long block text.
