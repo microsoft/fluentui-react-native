4b7a03eafd0e4035d3170b5763d641833f0274c2
nabil-k <nkhalil942@gmail.com>
2022-07-18 17:15:06 -0700

    applied new picker changes to other platforms

--- apps/fluent-tester/src/FluentTester/TestComponents/Common/MenuPicker.desktop.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Common/MenuPicker.desktop.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Menu, MenuItem, MenuTrigger, MenuPopover, MenuList } from '@fluentui-react-native/menu';
+import { MenuPickerProps, collectionItem } from './MenuPicker';
 import { ButtonV1 as Button } from '@fluentui/react-native';
 import { SvgXml } from 'react-native-svg';
 
@@ -8,19 +9,6 @@ const chevronXml = `
 <svg width="12" height="16" viewBox="0 0 11 6" color="#000">
   <path fill='currentColor' d='M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L5.5 4.79289L9.64645 0.646447C9.84171 0.451185 10.1583 0.451185 10.3536 0.646447C10.5488 0.841709 10.5488 1.15829 10.3536 1.35355L5.85355 5.85355C5.65829 6.04882 5.34171 6.04882 5.14645 5.85355L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z' />
 </svg>`;
-
-interface collectionItem {
-  label: string;
-  value?: string;
-}
-
-interface MenuPickerProps {
-  prompt?: string;
-  selected?: string;
-  onChange?: (value: string, index?: number) => void;
-  collection?: collectionItem[];
-  style?: any;
-}
 
 export const MenuPicker: React.FunctionComponent<MenuPickerProps> = (props: MenuPickerProps) => {
   const { prompt, selected, onChange, collection } = props;
