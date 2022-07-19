a936147adabf55fc9eb6287e3439e769bee31cf0
ValentinaKozlova <v.kozova13@gmail.com>
2022-07-19 18:45:22 +0200

    removed comment

--- packages/experimental/Badge/src/Badge.tsx
+++ packages/experimental/Badge/src/Badge.tsx
@@ -1,6 +1,5 @@
 /** @jsx withSlots */
-// import { Children } from 'react';
-import * as React from 'react';
+import { Children, ReactNode } from 'react';
 import { View } from 'react-native';
 import { badgeName, BadgeType, BadgeProps } from './Badge.types';
 import { Text } from '@fluentui-react-native/experimental-text';
@@ -36,12 +35,12 @@ export const Badge = compose<BadgeType>({
 
     const Slots = useSlots(userProps, (layer) => badgeLookup(layer, userProps));
 
-    return (final: BadgeProps, ...children: React.ReactNode[]) => {
+    return (final: BadgeProps, ...children: ReactNode[]) => {
       const { icon, iconPosition = 'before', ...mergedProps } = mergeProps(badge, final);
       return (
         <Slots.root {...mergedProps}>
           {icon && iconPosition === 'before' && <Slots.icon {...iconProps} />}
-          {React.Children.map(children, (child) => (typeof child === 'string' ? <Slots.text key="text">{child}</Slots.text> : child))}
+          {Children.map(children, (child) => (typeof child === 'string' ? <Slots.text key="text">{child}</Slots.text> : child))}
           {icon && iconPosition === 'after' && <Slots.icon {...iconProps} />}
         </Slots.root>
       );
