cca025f8c214372e3a9cdc8b0e5cff3c4ef91c9d
Nabil Khalil <duomaster01@gmail.com>
2022-07-19 15:36:33 -0700

    MenuPicker can change Light/Dark Mode Theme (#1872)
    * added collection item structure
    
    * themes can be switched now
    
    * updated callout test's pickers
    
    * updated picker for persona
    
    * applied new picker changes to other platforms
    
    * changed onChange type
    
    * Change files
    
    * removed console log

--- apps/fluent-tester/src/FluentTester/TestComponents/Common/MenuPicker.desktop.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/Common/MenuPicker.desktop.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Menu, MenuItem, MenuTrigger, MenuPopover, MenuList } from '@fluentui-react-native/menu';
+import { MenuPickerProps, collectionItem } from './MenuPicker';
 import { ButtonV1 as Button } from '@fluentui/react-native';
 import { SvgXml } from 'react-native-svg';
 
@@ -9,23 +10,23 @@ const chevronXml = `
   <path fill='currentColor' d='M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L5.5 4.79289L9.64645 0.646447C9.84171 0.451185 10.1583 0.451185 10.3536 0.646447C10.5488 0.841709 10.5488 1.15829 10.3536 1.35355L5.85355 5.85355C5.65829 6.04882 5.34171 6.04882 5.14645 5.85355L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z' />
 </svg>`;
 
-interface MenuPickerProps {
-  prompt?: string;
-  selected?: any;
-  onChange?: (value: any, index?: number) => void;
-  collection?: any[];
-  style?: any;
-}
-
 export const MenuPicker: React.FunctionComponent<MenuPickerProps> = (props: MenuPickerProps) => {
   const { prompt, selected, onChange, collection } = props;
+
+  let label;
+  collection.forEach((item) => {
+    if (item.value == selected) {
+      label = item.label;
+    }
+  });
+
   return (
     <View style={menuPickerStyles.container}>
       <Text style={menuPickerStyles.prompt}>{prompt}</Text>
       <Menu>
         <MenuTrigger>
           <Button>
-            <Text>{selected}</Text>
+            <Text>{label}</Text>
             <View style={menuPickerStyles.chevronContainer}>
               <SvgXml xml={chevronXml} />
             </View>
@@ -33,9 +34,9 @@ export const MenuPicker: React.FunctionComponent<MenuPickerProps> = (props: Menu
         </MenuTrigger>
         <MenuPopover>
           <MenuList>
-            {collection.map((value, index) => (
-              <MenuItem onClick={() => onChange(value, index)} key={index}>
-                {value}
+            {collection.map((collectionItem: collectionItem, index: number) => (
+              <MenuItem onClick={() => onChange(collectionItem.value, index)} key={collectionItem.value ?? index}>
+                {collectionItem.label}
               </MenuItem>
             ))}
           </MenuList>
