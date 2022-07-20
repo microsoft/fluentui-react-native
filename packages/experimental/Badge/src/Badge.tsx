d5074e40bfe70296ceda91c4c4d8f2ee206239ac
Valentina <v.kozlova13@gmail.com>
2022-07-20 09:37:22 +0200

    [Badge] Changed API according to Web spec, removed text prop (#1873)
    * Changed API of Badge according to latest Web component

--- packages/experimental/Badge/src/Badge.tsx
+++ packages/experimental/Badge/src/Badge.tsx
@@ -1,20 +1,9 @@
 /** @jsx withSlots */
+import { Children, ReactNode } from 'react';
 import { View } from 'react-native';
 import { badgeName, BadgeType, BadgeProps } from './Badge.types';
 import { Text } from '@fluentui-react-native/experimental-text';
-import {
-  compose,
-  withSlots,
-  UseSlots,
-  mergeProps,
-  compressible,
-  UseTokens,
-  buildUseTokens,
-  useFluentTheme,
-  mergeStyles,
-  applyTokenLayers,
-} from '@fluentui-react-native/framework';
-import { BadgeTokens } from '.';
+import { compose, withSlots, UseSlots, mergeProps } from '@fluentui-react-native/framework';
 import { Icon } from '@fluentui-react-native/icon';
 import { createIconProps } from '@fluentui-react-native/interactive-hooks';
 import { stylingSettings } from './Badge.styling';
@@ -46,14 +35,13 @@ export const Badge = compose<BadgeType>({
 
     const Slots = useSlots(userProps, (layer) => badgeLookup(layer, userProps));
 
-    return (final: BadgeProps, ...children: React.ReactNode[]) => {
-      const { text, icon, iconPosition = 'before', ...mergedProps } = mergeProps(badge, final);
+    return (final: BadgeProps, ...children: ReactNode[]) => {
+      const { icon, iconPosition = 'before', ...mergedProps } = mergeProps(badge, final);
       return (
         <Slots.root {...mergedProps}>
           {icon && iconPosition === 'before' && <Slots.icon {...iconProps} />}
-          {text && <Slots.text key="text">{text}</Slots.text>}
+          {Children.map(children, (child) => (typeof child === 'string' ? <Slots.text key="text">{child}</Slots.text> : child))}
           {icon && iconPosition === 'after' && <Slots.icon {...iconProps} />}
-          {children}
         </Slots.root>
       );
     };
@@ -61,50 +49,3 @@ export const Badge = compose<BadgeType>({
 });
 
 export default Badge;
-
-const useBadgeTokens = buildUseTokens<BadgeTokens>(
-  (t) => ({
-    backgroundColor: t.colors.brandBackground,
-    borderRadius: 9999,
-    minWidth: 32,
-    height: 32,
-  }),
-  badgeName,
-);
-
-// In future it'll be good to use compessible for Badge because it will make the component more flexible
-export const CompressibleBadge = compressible<BadgeProps, BadgeTokens>((props: BadgeProps, useTokens: UseTokens<BadgeTokens>) => {
-  const { text, ...rest } = props;
-  const theme = useFluentTheme();
-  let [tokens, cache] = useTokens(theme);
-
-  [tokens, cache] = applyTokenLayers(tokens, ['outline, filled, circular, square, rounded'], cache, (state) => props[state]);
-
-  const [tokenStyle] = cache(
-    () => ({
-      display: 'flex',
-      alignItems: 'center',
-      flexDirection: 'row',
-      alignSelf: 'flex-start',
-      justifyContent: 'center',
-      minWidth: tokens.minWidth,
-      height: tokens.height,
-      margin: 0,
-      backgroundColor: tokens.backgroundColor,
-      borderRadius: tokens.borderRadius,
-      color: '#fff',
-      paddingHorizontal: 20,
-    }),
-    ['borderRadius', 'minWidth', 'height', 'backgroundColor'],
-  );
-
-  return (extra: BadgeProps, children: React.ReactNode) => {
-    const mergedProps = { text, tokenStyle, ...rest, ...extra, style: mergeStyles(tokenStyle) };
-    return (
-      <View {...mergedProps}>
-        {text && <Text {...tokenStyle}>{text}</Text>}
-        {children}
-      </View>
-    );
-  };
-}, useBadgeTokens);
