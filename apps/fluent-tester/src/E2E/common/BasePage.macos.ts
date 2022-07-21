66962ee6e10c998751f59212000bb71edb2f90c2
Samuel Freiberg <safreibe@microsoft.com>
2022-07-21 12:41:32 -0700

    Trying new selectors

--- apps/fluent-tester/src/E2E/common/BasePage.macos.ts
+++ apps/fluent-tester/src/E2E/common/BasePage.macos.ts
@@ -55,35 +55,55 @@ export class BasePage {
   /* Scrolls until the desired test page's button is displayed. We use the scroll viewer UI element as the point to start scrolling.
    * We use a negative number as the Y-coordinate because that enables us to scroll downwards */
   scrollToComponentButton(): void {
-    const scrollViewer = $('//*[@class="RCTCustomScrollView"]');
-    //scrollViewer = $('//*[@class="RCTCustomScrollView"]');
-    //const scrollViewer1 = $('.RCTCustomScrollView');
-    console.log('scrollviewer found: ' + (scrollViewer != null));
-    // const success1 = browser.waitUntil(
-    //   () => {
-    //     return By('SCROLLVIEW_TEST_ID').isDisplayed();
-    //   },
-    //   {
-    //     timeout: 10000,
-    //     timeoutMsg: 'Could not find the ScrollViewer.',
-    //     interval: 1000,
-    //   },
-    // );
-    // expect(success1).toBeTruthy();
-
     const success = browser.waitUntil(
       () => {
-        driver.touchScroll(COMPONENT_SCROLL_COORDINATES.x, COMPONENT_SCROLL_COORDINATES.y, scrollViewer.elementId);
-        return this.isButtonInView();
+        return $('RCTCustomScrollView').isDisplayed();
       },
       {
-        timeout: 15000,
-        timeoutMsg: 'Did not scroll to test page button before timeout.',
+        timeout: 7000,
+        timeoutMsg: 'Could not find the ScrollViewer. RCTCustomScrollView',
         interval: 1000,
       },
     );
-
     expect(success).toBeTruthy();
+
+    const success1 = browser.waitUntil(
+      () => {
+        return $('//*[@class="RCTCustomScrollView"]').isDisplayed();
+      },
+      {
+        timeout: 7000,
+        timeoutMsg: 'Could not find the ScrollViewer. /*[@class="RCTCustomScrollView"]',
+        interval: 1000,
+      },
+    );
+    expect(success1).toBeTruthy();
+
+    const success2 = browser.waitUntil(
+      () => {
+        return $('[name="RCTCustomScrollView"]').isDisplayed();
+      },
+      {
+        timeout: 7000,
+        timeoutMsg: 'Could not find the ScrollViewer. [name="RCTCustomScrollView"]',
+        interval: 1000,
+      },
+    );
+    expect(success2).toBeTruthy();
+
+    // const success3 = browser.waitUntil(
+    //   () => {
+    //     driver.touchScroll(COMPONENT_SCROLL_COORDINATES.x, COMPONENT_SCROLL_COORDINATES.y, scrollViewer.elementId);
+    //     return this.isButtonInView();
+    //   },
+    //   {
+    //     timeout: 15000,
+    //     timeoutMsg: 'Did not scroll to test page button before timeout.',
+    //     interval: 1000,
+    //   },
+    // );
+
+    // expect(success3).toBeTruthy();
   }
 
   /* Waits for the test page to load. If the test page doesn't load before the timeout, it causes the test to fail. */
