ae39f8367ff6c0bec0ae669d48c7696fc707d673
Samuel Freiberg <safreibe@microsoft.com>
2022-07-21 09:17:43 -0700

    Was using incorrect selector

--- apps/fluent-tester/src/E2E/common/BasePage.macos.ts
+++ apps/fluent-tester/src/E2E/common/BasePage.macos.ts
@@ -55,9 +55,9 @@ export class BasePage {
   /* Scrolls until the desired test page's button is displayed. We use the scroll viewer UI element as the point to start scrolling.
    * We use a negative number as the Y-coordinate because that enables us to scroll downwards */
   scrollToComponentButton(): void {
-    const scrollViewElement = $('~SCROLLVIEW_TEST_ID');
+    const scrollViewElement = By('SCROLLVIEW_TEST_ID');
 
-    browser.waitUntil(
+    const success = browser.waitUntil(
       () => {
         driver.touchScroll(COMPONENT_SCROLL_COORDINATES.x, COMPONENT_SCROLL_COORDINATES.y, scrollViewElement.elementId);
         return this.isButtonInView();
@@ -68,6 +68,8 @@ export class BasePage {
         interval: 1000,
       },
     );
+
+    expect(success).toBeTruthy();
   }
 
   /* Waits for the test page to load. If the test page doesn't load before the timeout, it causes the test to fail. */
