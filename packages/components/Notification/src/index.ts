e1bd1e98b5f246af63e89095d51cef2b59b12aea
Joanna Qu <joannaquu@gmail.com>
2022-07-25 15:46:04 -0700

    update index.ts

--- packages/components/Notification/src/index.ts
+++ packages/components/Notification/src/index.ts
@@ -1,2 +1,2 @@
 export { Notification } from './Notification';
-export type { NotificationTokens, NotificationProps, NotificationSlotProps, NotificationType } from './Notification.types';
+export * from './Notification.types';
