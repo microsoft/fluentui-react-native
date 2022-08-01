0cddc3ae312ebd30731cd794f57ab2c972afaabc
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-08-01 12:39:17 -0700

    updated tests

--- packages/experimental/RadioGroup/src/Radio.test.tsx
+++ packages/experimental/RadioGroup/src/Radio.test.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { Radio } from './Radio';
 import * as renderer from 'react-test-renderer';
 
-describe('RadioButton component tests', () => {
-  it('RadioButton default', () => {
+describe('Radio component tests', () => {
+  it('Radio default', () => {
     const tree = renderer.create(<Radio value="key1" label="Default Button" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
--- packages/experimental/RadioGroup/src/RadioGroup.test.tsx
+++ packages/experimental/RadioGroup/src/RadioGroup.test.tsx
@@ -3,24 +3,24 @@ import { View } from 'react-native';
 import { RadioGroup, Radio } from '.';
 import * as renderer from 'react-test-renderer';
 
-describe('RadioButton component tests', () => {
-  it('RadioButton default', () => {
+describe('RadioGroup component tests', () => {
+  it('RadioGroup default', () => {
     const tree = renderer.create(
       <RadioGroup label="Uncontrolled RadioGroup" defaultValue="key2">
-        <Radio value="key1" label="RadioButton1" />
-        <Radio value="key2" label="RadioButton2" />
+        <Radio value="key1" label="Radio1" />
+        <Radio value="key2" label="Radio2" />
       </RadioGroup>,
     );
 
     expect(tree).toMatchSnapshot();
   });
 
-  it('RadioButton not direct child of radio group', () => {
+  it('Radio not direct child of RadioGroup', () => {
     const tree = renderer.create(
       <RadioGroup label="Uncontrolled RadioGroup" defaultValue="key2">
         <View>
-          <Radio value="key1" label="RadioButton1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
-          <Radio value="key2" label="RadioButton2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
+          <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
+          <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
         </View>
       </RadioGroup>,
     );
