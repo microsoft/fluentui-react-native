b96a005d4df8e777e7684bb9d63a205b6a2fc35b
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-08-03 07:36:22 -0700

    moved tester files

--- apps/fluent-tester/src/TestComponents/RadioGroupExperimental/ExperimentalRadioGroup/consts.ts
+++
@@ -1,2 +0,0 @@
-export const HOMEPAGE_EXPERIMENTAL_RADIO-GROUP_BUTTON = 'Homepage_ExperimentalRadioGroup_Button';
-export const EXPERIMENTAL_RADIO-GROUP_TESTPAGE = 'ExperimentalRadioGroup_TestPage';
--- apps/fluent-tester/src/TestComponents/RadioGroupExperimental/ExperimentalRadioGroup/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ExperimentalRadioGroupTest';
-export * from './consts';
