65343abfe433871ba47f5172f03d9a82b5d5c23d
Valentina <v.kozlova13@gmail.com>
2022-08-03 06:43:12 -0700

    Updated paths for component-generator and documentation (#1951)

--- apps/component-generator/consts.js
+++ apps/component-generator/consts.js
@@ -3,7 +3,7 @@ const getRegExp = (name) => new RegExp(name, 'g');
 
 exports.COMPONENT_PATH = '../../packages/components/';
 exports.COMPONENT_TEMPLATE_PATH = './component-templates/ComponentTemplate/';
-exports.TESTER_PATH = '../../apps/fluent-tester/src/FluentTester/TestComponents/';
+exports.TESTER_PATH = '../../apps/fluent-tester/src/TestComponents/';
 exports.TESTER_TEMPLATE_PATH = './component-templates/TesterComponentTemplate/*';
 exports.NAME_CAMEL_CASE = NAME_CAMEL_CASE;
 exports.NAME_CAMEL_CASE_REGEXP = getRegExp(NAME_CAMEL_CASE);
@@ -11,7 +11,7 @@ exports.NAME_LOWER_CAMEL_CASE_REGEXP = getRegExp('componentName');
 exports.NAME_KEBAB_CASE_REGEXP = getRegExp('component-name');
 exports.NAME_COBOL_CASE_REGEXP = getRegExp('COMPONENT_NAME');
 
-exports.TEST_PAGES_WIN32_PATH = '../../apps/fluent-tester/src/FluentTester/';
+exports.TEST_PAGES_WIN32_PATH = '../../apps/fluent-tester/src/';
 exports.TEST_PAGE_IMPORT_TO_INSERT = `import { ComponentNameTest, HOMEPAGE_COMPONENT_NAME_BUTTON } from './TestComponents/ComponentName';\n// --> testPage import insert`;
 exports.TEST_PAGE_COMPONENT_TO_INSERT = `{
     name: 'ComponentName Test',
