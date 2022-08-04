06349f5cde26a7be2eda16de9cd11fedb71bd525
Nabil Khalil <nkhalil942@gmail.com>
2022-08-04 12:04:58 -0700

    Win32 Switch Control - Accessibility (#1953)
    * added toggle test page
    
    * created base of toggle
    
    * added toggle to tester
    
    * added rough animation of thumb
    
    * set thumb to animate within the track
    
    * added AnimationLayout test
    
    * added animation class animations
    
    * added animations from netui
    
    * saving toggle state
    
    * renamed toggle package to switch
    
    * renamed Toggle files to Switch
    
    * made switch have the same dimensions as redlines
    
    * removed some files not needed and added label
    
    * added basic tokens
    
    * made switch have the toggle role
    
    * moved switch test test to new test folder
    
    * removed t
    
    * changed layout animation and padding
    
    * added justify content tokens to move thumb
    
    * added padding to label and moved label
    
    * added defaultChecked prop and a few others
    
    * added proper tokens
    
    * set all proper token values
    
    * added label positioning
    
    * added on/off text
    
    * added new usages and switched to using animationClass for now
    
    * added switch test and removed animationCLass
    
    * Change files
    
    * removed button files
    
    * reset .vscode
    
    * removed Switch.tsx from root
    
    * reset theme folder
    
    * removed excessive testIDs
    
    * fixed grammar issue on package.json
    
    * removed some leftover button tokens
    
    * cleaned up code more
    
    * added e2e tests
    
    * added e2e switch test to tester
    
    * fixed typo
    
    * Revert "added e2e switch test to tester"
    
    This reverts commit 749feb5819a732fd6a5c0ae6202b30d35ee89ca6.
    
    * fixed e2e issue
    
    * Change files
    
    * made switch e2e win32 specific
    
    * got rid of defaultDisplaySquare
    
    * updated switch test description
    
    * removed comment in useSwitch
    
    * removed empty arrays in styles
    
    * implemented checked prop
    
    * fixed checked bug and updated styles
    
    * added accessibility
    
    * added types to onAccessibilityActionProp callback and updated snapshopts
    
    * setting border styles in tokens now
    
    * removed accessibilityLabel being passed in twice
    
    * added warning for defaultChecked and checked being used
    
    * added ControlSwitchValues test
    
    * removed accessibility additions for another PR
    
    * updated package.json
    
    * export Switch types like props and tokens
    
    * moved switch to experimental folder
    
    * Change files
    
    * updated snapshot
    
    * removed experimental text props
    
    * updated e2e to correctly test callback
    
    * updated e2e to checked value rather than the checked prop itself
    
    * exported useSwitch
    
    * moved defaultAccessibilityActions declaration outside of the control
    
    * togglOn and toggleOff tokens are now handled by the toggle state
    
    * resolved type error and downgraded appium
    
    * removed console log
    
    * animations are only triggered on state change
    
    * added label a11y testing
    
    * updated witch test label
    
    * updated snapshots
    
    * made use of useAsToggleWithEvent
    
    * warning now only appears in dev mode
    
    * disabled animation if user disables animations in os
    
    * using useLayOut effect for triggering animations
    
    * updated snapshot
    
    * added accessibility items
    
    * added part of e2e for accessibility
    
    * renamed package
    
    * added all e2e tests for accessibility
    
    * fixed e2e bugs
    
    * reverted wdio config
    
    * fixed typo on package.json
    
    * updated snapshot
    
    * made better use of the spread operator
    
    * updated snapshots
    
    * corrected snapshot and opted for using ...props instead of ...rest

--- apps/fluent-tester/src/TestComponents/Switch/E2ESwitchTest.tsx
+++ apps/fluent-tester/src/TestComponents/Switch/E2ESwitchTest.tsx
@@ -3,7 +3,13 @@ import { Stack } from '@fluentui-react-native/stack';
 import { Switch } from '@fluentui-react-native/switch';
 import { Text } from 'react-native';
 import { stackStyle } from '../Common/styles';
-import { SWITCH_TEST_COMPONENT, SWITCH_ON_PRESS } from './consts';
+import {
+  SWITCH_TEST_COMPONENT,
+  SWITCH_ACCESSIBILITY_LABEL,
+  SWITCH_NO_A11Y_LABEL_COMPONENT,
+  SWITCH_TEST_COMPONENT_LABEL,
+  SWITCH_ON_PRESS,
+} from './consts';
 
 export const E2ESwitchTest: React.FunctionComponent = () => {
   const [switchPressed, setSwitchSwitchPressed] = React.useState(false);
@@ -17,8 +23,9 @@ export const E2ESwitchTest: React.FunctionComponent = () => {
 
   return (
     <Stack style={stackStyle}>
-      <Switch testID={SWITCH_TEST_COMPONENT} label={'Switch Test'} onChange={onToggle} />
+      <Switch testID={SWITCH_TEST_COMPONENT} label={'Switch Test'} onChange={onToggle} accessibilityLabel={SWITCH_ACCESSIBILITY_LABEL} />
       {switchPressed ? <Text testID={SWITCH_ON_PRESS}>Switch Toggled On</Text> : null}
+      <Switch label={SWITCH_TEST_COMPONENT_LABEL} testID={SWITCH_NO_A11Y_LABEL_COMPONENT} />
     </Stack>
   );
 };
--- apps/fluent-tester/src/TestComponents/Switch/consts.ts
+++ apps/fluent-tester/src/TestComponents/Switch/consts.ts
@@ -1,8 +1,12 @@
 export const HOMEPAGE_SWITCH_BUTTON = 'Homepage_Switch_Button';
 export const SWITCH_TESTPAGE = 'Switch_TestPage';
 
-/* Text Switch 1 */
-export const SWITCH_TEST_COMPONENT = 'Switch_Test_Component';
+/* Test Switch 1 */
+export const SWITCH_TEST_COMPONENT = 'Switch_Test_Component'; // A component on each specific test page
 export const SWITCH_ACCESSIBILITY_LABEL = 'E2E testing Switch accessibility label';
 
-export const SWITCH_ON_PRESS = 'Switch_On_Press';
+/* Test Switch 2 */
+export const SWITCH_NO_A11Y_LABEL_COMPONENT = 'Switch_No_A11y_label_Component'; // A component on each specific test page
+export const SWITCH_TEST_COMPONENT_LABEL = 'Test Switch2 - No Accessibility Label';
+
+export const SWITCH_ON_PRESS = 'Switch_On_Press'; // For testing the press functionality on a Switch
