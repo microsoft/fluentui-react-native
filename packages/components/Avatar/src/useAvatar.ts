176f41a3591652147f826862d7c2e169c1a88395
ValentinaKozlova <v.kozova13@gmail.com>
2022-08-10 13:13:26 +0200

    PR fix

--- packages/components/Avatar/src/useAvatar.ts
+++ packages/components/Avatar/src/useAvatar.ts
@@ -136,7 +136,8 @@ export const removeRedundantCharacters = (name: string): string[] => {
     return [];
   }
 
-  const _name = name.length > 100 ? name.trim().substring(0, 100) : name;
+  const MAX_NAME_LENGTH = 100;
+  const _name = name.length > MAX_NAME_LENGTH ? name.trim().substring(0, MAX_NAME_LENGTH) : name;
 
   const WORDS_IN_BRACES_REGEXP = new RegExp('[(\\[\\{][^\\)\\]\\}]*[\\)\\]\\}]', 'g');
   const PHONE_NUMBER_REGEXP = new RegExp('(\\+|(\\d|\\s))', 'g');
