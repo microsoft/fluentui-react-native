130d98d686fe63b1eeff950b96c251517e8e3d06
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-08-11 09:54:47 -0700

    fixed comment

--- packages/experimental/RadioGroup/src/RadioGroup/useRadioGroup.ts
+++ packages/experimental/RadioGroup/src/RadioGroup/useRadioGroup.ts
@@ -6,7 +6,7 @@ import { View } from 'react-native';
 export const useRadioGroup = (props: RadioGroupProps): RadioGroupInfo => {
   const { value, defaultValue, onChange } = props;
 
-  // This hook updates the Selected Button and calls the customer's onClick function. This gets called after a button is pressed.
+  // This hook updates the selected Radio and calls the customer's onClick function. This gets called after a button is pressed.
   const data = useValue(value || defaultValue || null, onChange);
 
   const [selectedButtonRef, setSelectedButtonRef] = React.useState(React.useRef<View>(null));
