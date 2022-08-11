25f9f2b38dc274bb43b5231c4a6ee4d88a555ce9
Nabil Khalil <nkhalil942@gmail.com>
2022-08-11 15:32:57 -0700

    Win32 Switch Updated Animations (#1960)
    * replaced layout animation with animation class
    
    * replaced ViewWin32 w/ View
    
    * updated snapshots
    
    * moved up View import statement
    
    * removed track animationClass since it wasnt working
    
    * updated snapshots
    
    * Change files
    
    * stubbed other platforms
    
    * updated test

--- packages/experimental/Switch/src/useSwitch.ts
+++ packages/experimental/Switch/src/useSwitch.ts
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { useAsPressable, useKeyProps, useOnPressWithFocus, useViewCommandFocus } from '@fluentui-react-native/interactive-hooks';
 import { SwitchProps, SwitchInfo } from './Switch.types';
-import { LayoutAnimation, AccessibilityState, AccessibilityInfo, AccessibilityActionEvent } from 'react-native';
+import { AccessibilityState, AccessibilityActionEvent } from 'react-native';
 import { memoize } from '@fluentui-react-native/framework';
 import { useAsToggleWithEvent } from '@fluentui-react-native/interactive-hooks';
 
 const defaultAccessibilityActions = [{ name: 'Toggle' }];
-const toggleAnimation = LayoutAnimation.create(200, LayoutAnimation.Types.linear, LayoutAnimation.Properties.scaleX);
 
 export const useSwitch = (props: SwitchProps): SwitchInfo => {
   const defaultComponentRef = React.useRef(null);
@@ -30,7 +29,6 @@ export const useSwitch = (props: SwitchProps): SwitchInfo => {
   const isDisabled = !!disabled;
   const [checkedState, toggleCallback] = useAsToggleWithEvent(defaultChecked, checked, onChange);
   const focusRef = isDisabled ? null : componentRef;
-  const isFirstRender = React.useRef(true);
 
   if (__DEV__ && defaultChecked !== undefined && checked !== undefined) {
     console.warn('The props defaultChecked and checked are mutually exclusive. Use only one of the props, do not use both.');
@@ -55,16 +53,6 @@ export const useSwitch = (props: SwitchProps): SwitchInfo => {
     },
     [toggleCallback, onAccessibilityAction],
   );
-
-  // Triggers animation only when the checked state changes
-  React.useLayoutEffect(() => {
-    // The `isFirstRender` value lets us apply the animation effect after the control mounts
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-    } else if (AccessibilityInfo.isReduceMotionEnabled) {
-      LayoutAnimation.configureNext(toggleAnimation);
-    }
-  }, [checkedState]);
 
   return {
     props: {
