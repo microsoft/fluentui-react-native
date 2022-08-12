581ebd6bf2ab95e9b64580a62c0cd30b1f0ad8b2
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-08-11 17:43:08 -0700

    Pass key shadow props into child instead of a new view

--- packages/experimental/Shadow/src/Shadow.tsx
+++ packages/experimental/Shadow/src/Shadow.tsx
@@ -7,13 +7,13 @@ import { getShadowTokenStyleSet } from './shadowStyle';
 export const Shadow = stagedComponent((props: ShadowProps) => {
   return (final: ShadowProps, children: React.ReactNode) => {
     const shadowTokenStyleSet = getShadowTokenStyleSet(props.shadowToken);
-    const mergedProps = mergeProps(final, { style: shadowTokenStyleSet.key });
+    const mergedProps = mergeProps(final, { style: shadowTokenStyleSet.ambient });
 
-    return (
-      <View style={shadowTokenStyleSet.ambient}>
-        <View {...mergedProps}>{children}</View>
-      </View>
-    );
+    const childrenArray = React.Children.toArray(children) as React.ReactElement[];
+    const child = childrenArray[0];
+    const childWithKeyShadow = React.cloneElement(child, mergeProps(child.props, { style: shadowTokenStyleSet.key }));
+
+    return <View {...mergedProps}>{childWithKeyShadow}</View>;
   };
 });
 
