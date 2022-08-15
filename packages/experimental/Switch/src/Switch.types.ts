68283275792b53fbd169951771f57d242074123b
nabil-k <nkhalil942@gmail.com>
2022-08-15 12:42:43 -0700

    removed unused types

--- packages/experimental/Switch/src/Switch.types.ts
+++ packages/experimental/Switch/src/Switch.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ViewProps, ViewStyle, ColorValue, View } from 'react-native';
+import { ViewProps, ViewStyle, ColorValue } from 'react-native';
 import { TextProps } from '@fluentui-react-native/text';
 import { FontTokens, IBorderTokens, IColorTokens, LayoutTokens } from '@fluentui-react-native/tokens';
 import { IFocusable, IWithPressableOptions, InteractionEvent } from '@fluentui-react-native/interactive-hooks';
@@ -167,8 +167,6 @@ export interface SwitchProps extends Omit<IWithPressableOptions<ViewProps>, 'onP
 }
 
 export type SwitchState = IPressableState & { toggled?: boolean };
-
-export type textBeingTestedStates = 'init' | 'onText' | 'offText' | 'done';
 
 export interface SwitchInfo {
   props: IWithPressableEvents<SwitchProps & React.ComponentPropsWithRef<any>>;
