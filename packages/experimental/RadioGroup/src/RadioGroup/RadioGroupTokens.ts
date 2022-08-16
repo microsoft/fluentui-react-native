709125e0543ed5e1375bac4875c147dbef8af206
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-08-16 10:10:46 -0700

    added variant

--- packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
@@ -1,12 +1,9 @@
 import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
 import { RadioGroupTokens } from './RadioGroup.types';
-import { globalTokens } from '@fluentui-react-native/theme-tokens';
 
 export const defaultRadioGroupTokens: TokenSettings<RadioGroupTokens, Theme> = (t: Theme) =>
   ({
     color: t.colors.menuItemText,
-    fontFamily: t.typography.families.primary,
-    fontSize: globalTokens.font.size[400],
-    fontWeight: globalTokens.font.weight.semibold,
+    variant: 'subtitle2Strong',
   } as RadioGroupTokens);
