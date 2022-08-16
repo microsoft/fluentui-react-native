06c3b245e467b741c93038be270a26018b3acd62
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-08-16 08:55:39 -0700

    added alias tokens

--- packages/experimental/RadioGroup/src/RadioGroup/RadioGroup.styling.ts
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroup.styling.ts
@@ -1,14 +1,12 @@
 import { radioGroupName, RadioGroupTokens, RadioGroupSlotProps, RadioGroupProps } from './RadioGroup.types';
-import { UseStylingOptions, buildProps } from '@fluentui-react-native/framework';
+import { Theme, UseStylingOptions, buildProps } from '@fluentui-react-native/framework';
 import { defaultRadioGroupTokens } from './RadioGroupTokens';
-// import { fontStyles } from '@fluentui-react-native/tokens';
+import { fontStyles } from '@fluentui-react-native/tokens';
 
 export const stylingSettings: UseStylingOptions<RadioGroupProps, RadioGroupSlotProps, RadioGroupTokens> = {
   tokens: [defaultRadioGroupTokens, radioGroupName],
   slotProps: {
     root: buildProps(() => ({
-      // accessible: true,
-      // accessibilityRole: 'radio',
       style: {
         display: 'flex',
         alignItems: 'flex-start',
@@ -16,15 +14,12 @@ export const stylingSettings: UseStylingOptions<RadioGroupProps, RadioGroupSlotP
       },
     })),
     label: buildProps(
-      () => ({
-        // tokens: RadioGroupTokens, theme: Theme
+      (tokens: RadioGroupTokens, theme: Theme) => ({
         style: {
-          fontSize: 16,
-          fontWeight: '600',
-          // ...fontStyles.from(tokens, theme),
+          ...fontStyles.from(tokens, theme),
         },
       }),
-      // [...fontStyles.keys],
+      [...fontStyles.keys],
     ),
   },
 };
--- packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
@@ -1,13 +1,12 @@
 import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
 import { RadioGroupTokens } from './RadioGroup.types';
-// import { globalTokens } from '@fluentui-react-native/theme-tokens';
+import { globalTokens } from '@fluentui-react-native/theme-tokens';
 
 export const defaultRadioGroupTokens: TokenSettings<RadioGroupTokens, Theme> = (t: Theme) =>
   ({
     color: t.colors.menuItemText,
-    // fontFamily: t.typography.families.primary,
-    // fontSize: globalTokens.font.size[400],
-    // fontWeight: globalTokens.font.weight.semibold,
-    // variant: t.typography.variants.subtitle2,
+    fontFamily: t.typography.families.primary,
+    fontSize: globalTokens.font.size[400],
+    fontWeight: globalTokens.font.weight.semibold,
   } as RadioGroupTokens);
