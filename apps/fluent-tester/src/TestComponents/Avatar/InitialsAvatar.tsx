e1c19bacf2220b24b0d03fa03628d635db548e01
Joanna Qu <joannaquu@gmail.com>
2022-08-18 11:05:34 -0700

    update name

--- apps/fluent-tester/src/TestComponents/Avatar/InitialsAvatar.tsx
+++ apps/fluent-tester/src/TestComponents/Avatar/InitialsAvatar.tsx
@@ -4,13 +4,13 @@ import { TextInput, View } from 'react-native';
 import { commonTestStyles as commonStyles } from '../Common/styles';
 
 export const InitialsAvatar: FunctionComponent = () => {
-  const [name, setName] = React.useState('Kat Larrson');
+  const [name, setName] = React.useState('Kat Larsson');
   const color = AvatarColors[getHashCodeWeb(name) % AvatarColors.length];
 
   return (
     <View style={commonStyles.root}>
       <TextInput value={name} onChangeText={setName} style={commonStyles.textBox} />
-      <Avatar name={name} size={120} avatarColor={color} />
+      <Avatar name={name} size={120} avatarColor={color} style={{ marginLeft: 20 }} />
     </View>
   );
 };
