87029244fd0d487f1130262963398b2c8702a741
Gulnaz Sayed <30990970+gulnazsayed@users.noreply.github.com>
2022-08-18 11:21:03 -0700

    V1 Basic RadioGroup & Tester Page (#1983)
    * copied RadioGroup to experimental
    
    * renamed RadioButton to Radio in all instances
    
    * aligned RadioGroup api to web naming
    
    * updated tests
    
    * created new component with correct framework
    
    * changed component package name
    
    * moved tester files
    
    * fixed tester pages
    
    * fixed component file names and updated radiogroup types
    
    * updated radiogroup
    
    * radio types
    
    * fixed suggestions and integrated tokens
    
    * radio tokens
    
    * use radiogroup
    
    * fixed radiogroup
    
    * removed radiogroup lookup function and embedded
    
    * removed unused imports:
    
    * testing new radiogroup
    
    * remove duplicate dependencies
    
    * fixed dependency again
    
    * Commented not working lines
    
    * updated radio implementation and added tokens
    
    * removed old framework experimental code
    
    * renamed correct experimental folder
    
    * changed package directory
    
    * added description to package
    
    * fixed index.ts for imports
    
    * added old test for new radiogroup old radio
    
    * reverted test
    
    * fixed buttonKeys issue and fixed context
    
    * code cleanup
    
    * more code cleanup
    
    * removed accessibilityRole
    
    * fixed comment
    
    * code cleanup
    
    * removed duplicated props in RadioGroup
    
    * checked isCircularNavigation
    
    * fixed accessibilityLabel check
    
    * removed unused packages
    
    * Delete CHANGELOG.json
    
    * Delete CHANGELOG.md
    
    * removed packages
    
    * added test page
    
    * fixed context
    
    * added alias tokens
    
    * Change files
    
    * Update @fluentui-react-native-experimental-radio-group-3303e2fd-9462-4d0a-8094-e0f96cbaf35c.json
    
    * Update @fluentui-react-native-tester-5d0b64d0-cf4a-4183-bd1f-9d39458eb3b9.json
    
    * revised setSelectedKey
    
    * added variant
    
    * fixed package
    
    * removed accessibility label
    
    * fixed index.ts
    
    * add dependency
    
    * alphabetical change
    
    Co-authored-by: Valentyna Kozlova <vkozlova@microsoft.com>

---
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
@@ -0,0 +1,9 @@
+import { Theme } from '@fluentui-react-native/framework';
+import { TokenSettings } from '@fluentui-react-native/use-styling';
+import { RadioGroupTokens } from './RadioGroup.types';
+
+export const defaultRadioGroupTokens: TokenSettings<RadioGroupTokens, Theme> = (t: Theme) =>
+  ({
+    color: t.colors.menuItemText,
+    variant: 'subtitle2Strong',
+  } as RadioGroupTokens);
