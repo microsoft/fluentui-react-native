f189a4704aa18a6db8847ea2a39766410c0a2fc7
Valentina <v.kozlova13@gmail.com>
2022-08-18 11:03:41 -0700

    Added more tests for Badge (#1981)
    * Added more tests for Badge

--- apps/fluent-tester/src/TestComponents/Badge/E2EBadgeTest.tsx
+++ apps/fluent-tester/src/TestComponents/Badge/E2EBadgeTest.tsx
@@ -1,4 +1,4 @@
-import { Badge } from '@fluentui-react-native/badge';
+import { Badge, PresenceBadge } from '@fluentui-react-native/badge';
 import * as React from 'react';
 import { View } from 'react-native';
 import { BADGE_TEST_COMPONENT, BADGE_SECONDARY_TEST_COMPONENT } from './consts';
@@ -9,7 +9,7 @@ export const E2EBadgeTest: React.FunctionComponent = () => {
       <Badge testID={BADGE_TEST_COMPONENT} appearance="outline" size="extraLarge" color="success" shape="square">
         Basic Badge
       </Badge>
-      <Badge testID={BADGE_SECONDARY_TEST_COMPONENT}>Default Badge</Badge>
+      <PresenceBadge testID={BADGE_SECONDARY_TEST_COMPONENT} status="available" outOfOffice={true} />
     </View>
   );
 };
