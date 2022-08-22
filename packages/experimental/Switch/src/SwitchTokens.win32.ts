9aef9dcb15a017eb183ae14a352d7d6be2e1386f
nabil-k <nkhalil942@gmail.com>
2022-08-22 10:35:10 -0700

    removed comment

--- packages/experimental/Switch/src/SwitchTokens.win32.ts
+++ packages/experimental/Switch/src/SwitchTokens.win32.ts
@@ -2,47 +2,6 @@ import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
 import { SwitchTokens } from './Switch.types';
 
-// toggleOn: {
-//   trackColor: t.colors.compoundBrandBackground1,
-//   thumbColor: t.colors.neutralForegroundOnBrandHover,
-//   borderColor: t.colors.compoundBrandBackground1,
-//   hovered: {
-//     trackColor: t.colors.neutralForegroundOnBrandHover,
-//     thumbColor: t.colors.compoundBrandBackground1Hover,
-//     borderColor: t.colors.compoundBrandBackground1Hover,
-//   },
-//   pressed: {
-//     trackColor: t.colors.compoundBrandBackground1Pressed,
-//     thumbColor: t.colors.neutralForegroundOnBrandHoverPressed,
-//     borderColor: t.colors.compoundBrandBackground1Pressed,
-//   },
-//   disabled: {
-//     trackColor: t.colors.neutralForegroundDisabled,
-//     thumbColor: t.colors.brandBackground,
-//     borderColor: t.colors.neutralForegroundDisabled,
-//   },
-// },
-// toggleOff: {
-//   trackColor: t.colors.brandBackground,
-//   thumbColor: t.colors.neutralForegroundOnBrand,
-//   borderColor: t.colors.neutralForegroundOnBrand,
-//   hovered: {
-//     trackColor: t.colors.brandBackgroundHover,
-//     thumbColor: t.colors.neutralForegroundOnBrandHover,
-//     borderColor: t.colors.neutralForegroundOnBrandHover,
-//   },
-//   pressed: {
-//     trackColor: t.colors.neutralForegroundOnBrandPressed,
-//     thumbColor: t.colors.neutralStrokeAccessiblePressed,
-//     borderColor: t.colors.neutralStrokeAccessiblePressed,
-//   },
-//   disabled: {
-//     trackColor: t.colors.brandBackground,
-//     thumbColor: t.colors.neutralStrokeDisabled,
-//     borderColor: t.colors.neutralStrokeDisabled,
-//   },
-// }
-
 export const defaultSwitchTokens: TokenSettings<SwitchTokens, Theme> = (t: Theme) => ({
   borderWidth: 1,
   borderRadius: 10,
