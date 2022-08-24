c6d04b01c0999ee9dbff37a9f3a70dbd85fc028e
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-08-24 13:54:13 -0700

    early return

--- packages/experimental/Shadow/src/Shadow.tsx
+++ packages/experimental/Shadow/src/Shadow.tsx
@@ -8,23 +8,23 @@ export const Shadow = stagedComponent((props: ShadowProps) => {
   return (final: ShadowProps, children: React.ReactNode) => {
     if (!props.shadowToken) {
       return <>{children}</>;
-    } else {
-      const shadowTokenStyleSet = getShadowTokenStyleSet(props.shadowToken);
-      const mergedProps = mergeProps(final, { style: shadowTokenStyleSet.ambient });
-
-      const childrenArray = React.Children.toArray(children) as React.ReactElement[];
-      const child = childrenArray[0];
-
-      if (__DEV__) {
-        if (childrenArray.length !== 1) {
-          console.warn('Shadow must only have one child');
-        }
-      }
-
-      const childWithKeyShadow = React.cloneElement(child, mergeProps(child.props, { style: shadowTokenStyleSet.key }));
-
-      return <View {...mergedProps}>{childWithKeyShadow}</View>;
     }
+
+    const shadowTokenStyleSet = getShadowTokenStyleSet(props.shadowToken);
+    const mergedProps = mergeProps(final, { style: shadowTokenStyleSet.ambient });
+
+    const childrenArray = React.Children.toArray(children) as React.ReactElement[];
+    const child = childrenArray[0];
+
+    if (__DEV__) {
+      if (childrenArray.length !== 1) {
+        console.warn('Shadow must only have one child');
+      }
+    }
+
+    const childWithKeyShadow = React.cloneElement(child, mergeProps(child.props, { style: shadowTokenStyleSet.key }));
+
+    return <View {...mergedProps}>{childWithKeyShadow}</View>;
   };
 });
 
