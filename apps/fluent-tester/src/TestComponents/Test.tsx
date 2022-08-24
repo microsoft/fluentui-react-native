042cf9fb3f9be539528a882e1fa74c16270c032a
Joanna Qu <joannaquu@gmail.com>
2022-08-24 10:20:48 -0700

    change to font icon

--- apps/fluent-tester/src/TestComponents/Test.tsx
+++ apps/fluent-tester/src/TestComponents/Test.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
-import { Text, ToggleButton, Separator, createIconProps } from '@fluentui/react-native';
+import { Text, ToggleButton, Separator } from '@fluentui/react-native';
 import { Stack } from '@fluentui-react-native/stack';
 import { stackStyle } from './Common/styles';
-import Svg, { G, Path, SvgProps } from 'react-native-svg';
-import { Icon, SvgIconProps } from '@fluentui-react-native/icon';
 
 export type TestSection = {
   name: string;
@@ -65,22 +63,6 @@ const isMobile = Platform.OS == 'android' || (Platform.OS == 'ios' && !Platform.
 
 export const Test = (props: TestProps): React.ReactElement<Record<string, never>> => {
   const [showStatus, setShowStatus] = React.useState(false);
-  const toggleSvg: React.FunctionComponent<SvgProps> = () => {
-    const path = showStatus
-      ? 'M2.75 5.25h6.5s0.75 0 0.75 0.75v0s0 0.75 -0.75 0.75h-6.5s-0.75 0 -0.75 -0.75v0s0 -0.75 0.75 -0.75'
-      : 'M6.5 1.75C6.5 1.33579 6.16421 1 5.75 1C5.33579 1 5 1.33579 5 1.75V5H1.75C1.33579 5 1 5.33579 1 5.75C1 6.16421 1.33579 6.5 1.75 6.5H5V9.75C5 10.1642 5.33579 10.5 5.75 10.5C6.16421 10.5 6.5 10.1642 6.5 9.75V6.5H9.75C10.1642 6.5 10.5 6.16421 10.5 5.75C10.5 5.33579 10.1642 5 9.75 5H6.5V1.75Z';
-    return (
-      <Svg>
-        <G>
-          <Path d={path} fill="black" />
-        </G>
-      </Svg>
-    );
-  };
-  const svgProps: SvgIconProps = {
-    src: toggleSvg,
-  };
-  const toggleIconProps = createIconProps({ svgSource: svgProps, width: 12, height: 12 });
 
   return (
     <View testID="ScrollViewAreaForComponents">
@@ -92,7 +74,7 @@ export const Test = (props: TestProps): React.ReactElement<Record<string, never>
         <Text style={styles.description}>{props.description}</Text>
       </Stack>
       <ToggleButton onClick={() => setShowStatus(!showStatus)} style={[styles.statusLabel]}>
-        Platform Status <Icon {...toggleIconProps} />
+        Platform Status {showStatus ? '-' : '+'}
       </ToggleButton>
       {!isMobile && showStatus && (
         <Stack style={stackStyle}>
