a5988a4d5aeaef0ea1172468243ea94cd7ce25b8
Ruriko Araki <ruaraki@microsoft.com>
2022-08-29 16:19:48 -0700

    Fix build

--- packages/experimental/Dropdown/src/Dropdown/Dropdown.styling.ts
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.styling.ts
@@ -1,5 +1,5 @@
 import { UseStylingOptions } from '@fluentui-react-native/framework';
-import { dropdownName, DropdownProps, DropdownSlotProps } from './Dropdown.types';
+import { dropdownName, DropdownProps, DropdownSlotProps, DropdownTokens } from './Dropdown.types';
 
 export const stylingSettings: UseStylingOptions<DropdownProps, DropdownSlotProps, DropdownTokens> = {
   tokens: [dropdownName],
