3cceeddd92d6bd8b23471c9589304c848e190fc5
Ruriko Araki <ruaraki@microsoft.com>
2022-08-29 16:10:24 -0700

    Skeletal styling file

---
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.styling.ts
@@ -0,0 +1,6 @@
+import { UseStylingOptions } from '@fluentui-react-native/framework';
+import { dropdownName, DropdownProps, DropdownSlotProps } from './Dropdown.types';
+
+export const stylingSettings: UseStylingOptions<DropdownProps, DropdownSlotProps, DropdownTokens> = {
+  tokens: [dropdownName],
+};
