5dded69ee6eedd34063117ac9d7efb3ba91ca207
Ruriko Araki <ruaraki@microsoft.com>
2022-08-30 17:08:47 -0700

    Implement compose framework for Dropdown (#2062)
    * New files
    
    * Initial types
    
    * Skeletal render
    
    * Skeletal useDropdown
    
    * Skeletal styling file
    
    * Change files
    
    * Fix build
    
    * Update packages

---
+++ packages/experimental/Dropdown/src/Dropdown/useDropdown.ts
@@ -0,0 +1,8 @@
+import { DropdownProps, DropdownState } from './Dropdown.types';
+
+export const useDropdown = (_props: DropdownProps): DropdownState => {
+  return {
+    props: {},
+    state: {},
+  };
+};
