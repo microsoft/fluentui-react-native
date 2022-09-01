f8322fb6819d50060fc1fc8e2c934e323531edf3
Ruriko Araki <ruaraki@microsoft.com>
2022-09-01 14:50:53 -0700

    Slots for option

--- packages/experimental/Dropdown/src/Option/Option.tsx
+++ packages/experimental/Dropdown/src/Option/Option.tsx
@@ -1,14 +1,18 @@
 import { compose, UseSlots } from '@fluentui-react-native/framework';
+import { TextV1 as Text } from '@fluentui-react-native/text';
 import React from 'react';
 import { View } from 'react-native';
+import { Svg } from 'react-native-svg';
 import { optionName, OptionProps, OptionType } from './Option.types';
 
 export const Option = compose<OptionType>({
   displayName: optionName,
   slots: {
     root: View,
+    checkIcon: Svg,
+    label: Text,
   },
-  useRender: (_userProps: OptionProps, _useSLots: UseSlots<OptionType>) => {
+  useRender: (_userProps: OptionProps, _useSlots: UseSlots<OptionType>) => {
     return (_final: OptionProps, ..._children: React.ReactNode[]) => {
       return null;
     };
