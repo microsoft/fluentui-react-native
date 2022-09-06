6278145d6d8cd2ea55e2a1b3f1cccb4d91356308
Ruriko Araki <ruaraki@microsoft.com>
2022-09-06 15:33:33 -0700

    Add new tokens

--- packages/experimental/Dropdown/src/Option/Option.types.ts
+++ packages/experimental/Dropdown/src/Option/Option.types.ts
@@ -1,15 +1,26 @@
 import type { IViewProps } from '@fluentui-react-native/adapters';
 import { IPressableHooks, IWithPressableOptions } from '@fluentui-react-native/interactive-hooks';
 import { TextProps } from '@fluentui-react-native/text';
+import { FontTokens, IBorderTokens, IColorTokens, LayoutTokens } from '@fluentui-react-native/tokens';
+import { ColorValue } from 'react-native';
 import { SvgProps } from 'react-native-svg';
 
 export const optionName = 'Option';
 
-export interface OptionTokens {}
+export interface OptionTokens extends FontTokens, IBorderTokens, IColorTokens, LayoutTokens {
+  /**
+   * Color of the checkmark icon on the Option indicating Option is selected
+   */
+  checkIconColor?: ColorValue;
 
-export interface OptionProps extends IWithPressableOptions<IViewProps> {}
+  /**
+   * Height and width of the checkmark icon on the Option indicating Option is selected
+   */
+  checkIconSize?: number;
+}
 
-export interface OptionState extends IPressableHooks<OptionProps & React.ComponentPropsWithRef<any>> {}
+export type OptionProps = IWithPressableOptions<IViewProps>;
+export type OptionState = IPressableHooks<OptionProps & React.ComponentPropsWithRef<any>>;
 
 export interface OptionSlotProps {
   root: IViewProps;
