0eb2b9f47d0a104412998fc88f69b16e1f7c2154
Ruriko Araki <ruaraki@microsoft.com>
2022-09-06 16:01:58 -0700

    Implement styling

--- packages/experimental/Dropdown/src/Option/Option.styling.ts
+++ packages/experimental/Dropdown/src/Option/Option.styling.ts
@@ -1,29 +1,40 @@
-import { UseStylingOptions } from '@fluentui-react-native/framework';
+import { borderStyles, buildProps, fontStyles, layoutStyles, Theme, UseStylingOptions } from '@fluentui-react-native/framework';
 import { optionName, OptionProps, OptionSlotProps, OptionTokens } from './Option.types';
 
 export const stylingSettings: UseStylingOptions<OptionProps, OptionSlotProps, OptionTokens> = {
   tokens: [optionName],
   slotProps: {
-    root: {
-      style: {
-        alignItems: 'center',
-        alignSelf: 'flex-start',
-        borderColor: 'black',
-        borderRadius: 2,
-        borderWidth: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        minHeight: 24,
-        minWidth: 160,
-      },
-    },
-    checkIcon: {
-      height: 12,
-      width: 12,
-      viewBox: '0 0 12 12',
-    },
-    label: {
-      color: 'black',
-    },
+    root: buildProps(
+      (tokens: OptionTokens, theme: Theme) => ({
+        style: {
+          alignItems: 'center',
+          alignSelf: 'flex-start',
+          display: 'flex',
+          flexDirection: 'row',
+          ...borderStyles.from(tokens, theme),
+          ...layoutStyles.from(tokens, theme),
+        },
+      }),
+      [...borderStyles.keys, ...layoutStyles.keys],
+    ),
+    checkIcon: buildProps(
+      (tokens: OptionTokens) => ({
+        color: tokens.checkmarkColor,
+        height: tokens.checkmarkSize,
+        viewBox: '0 0 ' + tokens.checkmarkSize + ' ' + tokens.checkmarkSize,
+        width: tokens.checkmarkSize,
+      }),
+      ['checkmarkColor', 'checkmarkSize'],
+    ),
+    label: buildProps(
+      (tokens: OptionTokens, theme: Theme) => ({
+        color: tokens.color,
+        style: {
+          paddingStart: tokens.spacingContentIcon,
+          ...fontStyles.from(tokens, theme),
+        },
+      }),
+      ['color', 'spacingContentIcon', ...fontStyles.keys],
+    ),
   },
 };
