6ab3e95df9790fabc280600d4c4150a1852e7701
Samuel Freiberg <32719011+samuelfreiberg@users.noreply.github.com>
2022-09-08 15:15:35 -0700

    iOS E2E Testing + Improving E2E Code-Reuse (#2097)
    * Yarn
    
    * Merge
    
    * Fixing repo
    
    * Update package
    
    * Merge
    
    * Removing change file
    
    * Moving all test assets to the same folder in /Common/
    
    * Revert "Moving all test assets to the same folder in /Common/"
    
    This reverts commit cb3ab143254551f81d3080c2a7d42acc6b0f71dd.
    
    * Adding initial ios testing
    
    * Merging to upstream main
    
    * Reverting back to upstream/main
    
    * ios working
    
    * Removed MacOS BasePage because we can use the same selector as Win32. No need for it anymore. All macOS tests running correctly
    
    * Removing platform-specific NavigateAppPage. Now that the selectors are the same, we don't need separate ones
    
    * Trimming down all component's PageObject to just ONE file. No need for platform-specific page objects anymore, since they all pull from the same selectors
    
    * iOS and macOS both fully working
    
    * Adding iOS to CI Pipeline
    
    * Change files
    
    * Reverting previous mistakes
    
    * Reverting unwanted change
    
    * Removing unwanted change
    
    * Reverting podfile.lock
    
    * Reverting podfile changes
    
    * Adding additional comments
    
    * Fix prettier
    
    * Increasing iOS retry timeout. App takes longer to load because of emulator
    
    * Increasing app connection timeout across the board
    
    * Change files
    
    * Improve display name's in .ado pipeline
    
    * Improving ADO E2E logic to remove duplicate steps
    
    * Fixing parameter in .ado
    
    Co-authored-by: dev <dev@oe-mac-C07-C60ES-JYVY-checkout.selfhost.corp.microsoft.com>

--- apps/fluent-tester/src/E2E/Avatar/pages/AvatarPageObject.ts
+++ apps/fluent-tester/src/E2E/Avatar/pages/AvatarPageObject.ts
@@ -4,7 +4,7 @@ import {
   AVATAR_TEST_COMPONENT,
   AVATAR_SECONDARY_TEST_COMPONENT,
 } from '../../../TestComponents/Avatar/consts';
-import { BasePage, By } from '../../common/BasePage.win';
+import { BasePage, By } from '../../common/BasePage';
 
 export const enum AvatarComponentSelector {
   PrimaryComponent, //this._primaryComponent
