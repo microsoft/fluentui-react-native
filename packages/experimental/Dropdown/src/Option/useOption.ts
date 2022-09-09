2dbd5524e4fb87aca5466a596ee50bed13d9e349
Ruriko Araki <ruaraki@microsoft.com>
2022-09-09 16:32:24 -0700

    Implement interaction handling

--- packages/experimental/Dropdown/src/Option/useOption.ts
+++ packages/experimental/Dropdown/src/Option/useOption.ts
@@ -1,8 +1,18 @@
+import { useAsPressable } from '@fluentui-react-native/interactive-hooks';
+import * as React from 'react';
 import { OptionProps, OptionState } from './Option.types';
 
-export const useOption = (_props: OptionProps): OptionState => {
+export const useOption = (props: OptionProps): OptionState => {
+  // attach the pressable state handlers
+  const defaultComponentRef = React.useRef(null);
+  const { onPress, componentRef = defaultComponentRef, disabled, ...rest } = props;
+
+  const pressable = useAsPressable({ ...rest, disabled, onPress });
+
   return {
-    props: {},
-    state: {},
+    props: {
+      ...pressable.props,
+    },
+    state: pressable.state,
   };
 };
