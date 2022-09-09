581f9ee8f979deac543c1e6fe10c601223e411ed
Ruriko Araki <ruaraki@microsoft.com>
2022-09-09 16:49:53 -0700

    Simplify code

--- packages/experimental/Dropdown/src/Option/useOption.ts
+++ packages/experimental/Dropdown/src/Option/useOption.ts
@@ -1,18 +1,8 @@
 import { useAsPressable } from '@fluentui-react-native/interactive-hooks';
-import * as React from 'react';
 import { OptionProps, OptionState } from './Option.types';
 
 export const useOption = (props: OptionProps): OptionState => {
-  // attach the pressable state handlers
-  const defaultComponentRef = React.useRef(null);
-  const { onPress, componentRef = defaultComponentRef, disabled, ...rest } = props;
+  const pressable = useAsPressable(props);
 
-  const pressable = useAsPressable({ ...rest, disabled, onPress });
-
-  return {
-    props: {
-      ...pressable.props,
-    },
-    state: pressable.state,
-  };
+  return pressable;
 };
