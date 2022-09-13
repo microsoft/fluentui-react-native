8c8f1617662e513aebbcc5160102c9a103b2269f
Larry Win <winlarry@microsoft.com>
2022-09-13 09:43:55 -0700

    Remove thick checkmark logic in Checkbox

--- packages/components/Checkbox/src/Checkbox.tsx
+++ packages/components/Checkbox/src/Checkbox.tsx
@@ -4,10 +4,9 @@ import { View } from 'react-native';
 import { checkboxName, CheckboxType, CheckboxProps } from './Checkbox.types';
 import { TextV1 as Text } from '@fluentui-react-native/text';
 import { stylingSettings, getDefaultSize } from './Checkbox.styling';
-import { compose, mergeProps, withSlots, UseSlots, useFluentTheme } from '@fluentui-react-native/framework';
+import { compose, mergeProps, withSlots, UseSlots } from '@fluentui-react-native/framework';
 import { useCheckbox } from './useCheckbox';
 import { Svg, Path } from 'react-native-svg';
-import { shouldUseThickCheckmark } from './shouldUseThickCheckmark';
 
 export const Checkbox = compose<CheckboxType>({
   displayName: checkboxName,
@@ -32,7 +31,6 @@ export const Checkbox = compose<CheckboxType>({
         layer === userProps['size'] ||
         (!userProps['size'] && layer === getDefaultSize()),
     );
-    const thickCheckmark = shouldUseThickCheckmark(useFluentTheme());
 
     // now return the handler for finishing render
     return (final: CheckboxProps) => {
@@ -45,14 +43,7 @@ export const Checkbox = compose<CheckboxType>({
       );
 
       // We want a thicker checkmark in HC to make the checkmark stand out more.
-      const checkmarkPath = thickCheckmark ? (
-        <Path
-          fill="currentColor"
-          d="M9.76497 3.20474C10.0661 3.48915 10.0797 3.96383 9.79526 4.26497L5.54526 8.76497C5.40613 8.91228 5.21332 8.99703 5.01071 8.99993C4.8081 9.00282 4.61295 8.92361 4.46967 8.78033L2.21967 6.53033C1.92678 6.23744 1.92678 5.76257 2.21967 5.46967C2.51256 5.17678 2.98744 5.17678 3.28033 5.46967L4.98463 7.17397L8.70474 3.23503C8.98915 2.9339 9.46383 2.92033 9.76497 3.20474Z"
-          stroke="currentColor"
-          strokeWidth="1"
-        />
-      ) : (
+      const checkmarkPath = (
         <Path
           fill="currentColor"
           d="M9.76497 3.20474C10.0661 3.48915 10.0797 3.96383 9.79526 4.26497L5.54526 8.76497C5.40613 8.91228 5.21332 8.99703 5.01071 8.99993C4.8081 9.00282 4.61295 8.92361 4.46967 8.78033L2.21967 6.53033C1.92678 6.23744 1.92678 5.76257 2.21967 5.46967C2.51256 5.17678 2.98744 5.17678 3.28033 5.46967L4.98463 7.17397L8.70474 3.23503C8.98915 2.9339 9.46383 2.92033 9.76497 3.20474Z"
