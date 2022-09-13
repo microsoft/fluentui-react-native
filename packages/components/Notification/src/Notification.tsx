d71771c919102121235502db5705747355ca77ff
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-09-12 19:21:02 -0700

    Fix Notification rootStyle

--- packages/components/Notification/src/Notification.tsx
+++ packages/components/Notification/src/Notification.tsx
@@ -81,7 +81,7 @@ export const Notification = compose<NotificationType>({
 
     return (final: NotificationProps, ...children: React.ReactNode[]) => {
       const { variant, icon, title, action, onActionPress, ...rest } = mergeProps(userProps, final);
-      const mergedProps = mergeProps<PressableProps>(rest, rootStyle);
+      const mergedProps = mergeProps<PressableProps>(rest, { style: rootStyle });
       const iconProps = createIconProps(icon);
       const notificationButtonProps = createNotificationButtonProps(userProps);
 
