3627344077cce3455ed2666498a100698ef12d6a
Ruriko Araki <ruaraki@microsoft.com>
2022-09-20 12:52:20 -0700

    Make test file ts

--- transforms/jest.config.js
+++ transforms/jest.config.js
@@ -1,2 +1,2 @@
 const { configureJest } = require('@fluentui-react-native/scripts');
-module.exports = configureJest({ testRegex: '/__tests__/.*-test\\.js$' });
+module.exports = configureJest({ testRegex: '/__tests__/.*-test\\.ts$' });
