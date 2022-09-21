48852cbdbd500b88403dd012f45c18569638cba3
Ruriko Araki <ruaraki@microsoft.com>
2022-09-20 17:20:33 -0700

    Mark everything deprecated

--- packages/deprecated/theming-react-native/src/Theme.types.ts
+++ packages/deprecated/theming-react-native/src/Theme.types.ts
@@ -1,6 +1,15 @@
 import { IThemeRegistry, IProcessTheme } from '@uifabricshared/theme-registry';
 import { ITheme, IPartialTheme } from '@uifabricshared/theming-ramp';
 
+/**
+ * @deprecated
+ */
 export { ITheme, IPartialTheme } from '@uifabricshared/theming-ramp';
+/**
+ * @deprecated
+ */
 export type IThemeDefinition = IPartialTheme | IProcessTheme<ITheme, IPartialTheme>;
+/**
+ * @deprecated
+ */
 export type ThemeRegistry = IThemeRegistry<ITheme, IPartialTheme>;
