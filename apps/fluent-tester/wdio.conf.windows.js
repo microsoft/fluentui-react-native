b6fadd0f0c31d3fcbbfc86480fb9e5317911dc6f
Samuel Freiberg <safreibe@microsoft.com>
2022-09-20 20:25:29 -0700

    Making iOS/macOS, and windows e2e tests run sequentially

--- apps/fluent-tester/wdio.conf.windows.js
+++ apps/fluent-tester/wdio.conf.windows.js
@@ -9,19 +9,21 @@ exports.config = {
   /* UWP controls are a subset of the Win32 controls. Only some work on our UWP test app,
   so we must specify which ones we want to test here. */
   specs: [
-    'src/E2E/ActivityIndicator/specs/*.win.ts',
-    'src/E2E/Button/specs/*.win.ts',
-    'src/E2E/Callout/specs/*.win.ts',
-    'src/E2E/Checkbox/specs/*.windows.ts', // See spec file for more information
-    'src/E2E/Link/specs/*.win.ts',
-    'src/E2E/PersonaCoin/specs/*.win.ts',
-    'src/E2E/Pressable/specs/*.win.ts',
-    'src/E2E/Separator/specs/*.win.ts',
-    'src/E2E/Tabs/specs/*.windows.ts', // See spec file for more information
-    'src/E2E/Text/specs/*.win.ts',
-    'src/E2E/TextExperimental/specs/*.win.ts',
-    'src/E2E/Theme/specs/*.win.ts',
-    'src/E2E/Tokens/specs/*.win.ts',
+    [
+      'src/E2E/ActivityIndicator/specs/*.win.ts',
+      'src/E2E/Button/specs/*.win.ts',
+      'src/E2E/Callout/specs/*.win.ts',
+      'src/E2E/Checkbox/specs/*.windows.ts', // See spec file for more information
+      'src/E2E/Link/specs/*.win.ts',
+      'src/E2E/PersonaCoin/specs/*.win.ts',
+      'src/E2E/Pressable/specs/*.win.ts',
+      'src/E2E/Separator/specs/*.win.ts',
+      'src/E2E/Tabs/specs/*.windows.ts', // See spec file for more information
+      'src/E2E/Text/specs/*.win.ts',
+      'src/E2E/TextExperimental/specs/*.win.ts',
+      'src/E2E/Theme/specs/*.win.ts',
+      'src/E2E/Tokens/specs/*.win.ts',
+    ],
   ],
   exclude: [
     /* 'path/to/excluded/files' */
