9bf91fbab4fa916716097692f4a0047ff2df50e0
Samuel Freiberg <safreibe@microsoft.com>
2022-09-22 10:16:50 -0700

    Testing different logLevel for WDIO

--- apps/win32/wdio.conf.js
+++ apps/win32/wdio.conf.js
@@ -36,7 +36,7 @@ exports.config = {
    ** ===============================================================================================
    */
 
-  logLevel: 'info', // Level of logging verbosity: trace | debug | info | warn | error | silent
+  logLevel: 'debug', // Level of logging verbosity: trace | debug | info | warn | error | silent
   bail: 0, // If you only want to run your tests until a specific amount of tests have failed use bail (default is 0 - don't bail, run all tests).
   waitforTimeout: defaultWaitForTimeout, // Default timeout for all waitForXXX commands.
   connectionRetryTimeout: defaultConnectionRetryTimeout, // Timeout for any WebDriver request to a driver or grid.
