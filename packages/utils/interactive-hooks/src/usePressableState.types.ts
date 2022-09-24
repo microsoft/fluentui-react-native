24afc42279d3c4d7a118a84059d7b984115bea07
Saad Najmi <sanajmi@microsoft.com>
2022-09-24 21:19:57 +0300

    Just use any for the MouseEvent type

--- packages/utils/interactive-hooks/src/usePressableState.types.ts
+++ packages/utils/interactive-hooks/src/usePressableState.types.ts
@@ -1,91 +1,5 @@
 import { IViewProps } from '@fluentui-react-native/adapters';
-import { HostComponent, NativeSyntheticEvent, PressableProps } from 'react-native';
-
-// GH #1035: Temporarily copy the types needed for "MouseEvent"
-// while we wait for the new DefinitelyTyped package to land.
-// [
-export interface NativeUIEvent {
-  /**
-   * Returns a long with details about the event, depending on the event type.
-   */
-  readonly detail: number;
-}
-
-export interface NativeMouseEvent extends NativeUIEvent {
-  /**
-   * The X coordinate of the mouse pointer in global (screen) coordinates.
-   */
-  readonly screenX: number;
-  /**
-   * The Y coordinate of the mouse pointer in global (screen) coordinates.
-   */
-  readonly screenY: number;
-  /**
-   * The X coordinate of the mouse pointer relative to the whole document.
-   */
-  readonly pageX: number;
-  /**
-   * The Y coordinate of the mouse pointer relative to the whole document.
-   */
-  readonly pageY: number;
-  /**
-   * The X coordinate of the mouse pointer in local (DOM content) coordinates.
-   */
-  readonly clientX: number;
-  /**
-   * The Y coordinate of the mouse pointer in local (DOM content) coordinates.
-   */
-  readonly clientY: number;
-  /**
-   * Alias for NativeMouseEvent.clientX
-   */
-  readonly x: number;
-  /**
-   * Alias for NativeMouseEvent.clientY
-   */
-  readonly y: number;
-  /**
-   * Returns true if the control key was down when the mouse event was fired.
-   */
-  readonly ctrlKey: boolean;
-  /**
-   * Returns true if the shift key was down when the mouse event was fired.
-   */
-  readonly shiftKey: boolean;
-  /**
-   * Returns true if the alt key was down when the mouse event was fired.
-   */
-  readonly altKey: boolean;
-  /**
-   * Returns true if the meta key was down when the mouse event was fired.
-   */
-  readonly metaKey: boolean;
-  /**
-   * The button number that was pressed (if applicable) when the mouse event was fired.
-   */
-  readonly button: number;
-  /**
-   * The buttons being depressed (if any) when the mouse event was fired.
-   */
-  readonly buttons: number;
-  /**
-   * The secondary target for the event, if there is one.
-   */
-  readonly relatedTarget: null | number | React.ElementRef<HostComponent<unknown>>;
-  // offset is proposed: https://drafts.csswg.org/cssom-view/#extensions-to-the-mouseevent-interface
-  /**
-   * The X coordinate of the mouse pointer between that event and the padding edge of the target node
-   */
-  readonly offsetX: number;
-  /**
-   * The Y coordinate of the mouse pointer between that event and the padding edge of the target node
-   */
-  readonly offsetY: number;
-}
-
-export interface MouseEvent extends NativeSyntheticEvent<NativeMouseEvent> {}
-
-// ]
+import { PressableProps } from 'react-native';
 
 export type PressState = {
   pressed?: boolean;
@@ -154,12 +68,12 @@ export type PressableHoverProps = {
   /**
    * Called when the hover is activated to provide visual feedback.
    */
-  onHoverIn?: (event: MouseEvent) => any;
+  onHoverIn?: (event: any) => any;
 
   /**
    * Called when the hover is deactivated to undo visual feedback.
    */
-  onHoverOut?: (event: MouseEvent) => any;
+  onHoverOut?: (event: any) => any;
 };
 
 /**
