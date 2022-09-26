db03501224be9a35673a60cb27da1f999cc57628
Saad Najmi <sanajmi@microsoft.com>
2022-09-26 05:21:56 +0300

    Refactor PressablePropsExtended (#2157)
    * Introduce new adapter type IPressableProps
    
    * Undo legacy tabs change
    
    * Change files
    
    * Change name and add hover props
    
    * Refactor to PressablePropsExtended
    
    * fix types
    
    * Change files
    
    * Just use any for the MouseEvent type
    
    * Undo changes to adapters
    
    * Remove extra change file
    
    * Update package.json
    
    * fix depcheck

---
+++ packages/utils/interactive-hooks/src/usePressableState.types.ts
@@ -0,0 +1,86 @@
+import { IViewProps } from '@fluentui-react-native/adapters';
+import { PressableProps } from 'react-native';
+
+export type PressState = {
+  pressed?: boolean;
+};
+
+export type FocusState = {
+  focused?: boolean;
+};
+
+export type HoverState = {
+  hovered?: boolean;
+};
+
+export type PressableState = PressState & FocusState & HoverState;
+
+export type PressablePressProps = {
+  /**
+   * Duration (in milliseconds) from `onPressIn` before `onLongPress` is called.
+   */
+  delayLongPress?: PressableProps['delayLongPress'];
+
+  /**
+   * Called when a press gestute has been triggered.
+   */
+  onPress?: PressableProps['onPress'];
+
+  /**
+   * Called when the press is activated to provide visual feedback.
+   */
+  onPressIn?: PressableProps['onPressIn'];
+
+  /**
+   * Called when the press is deactivated to undo visual feedback.
+   */
+  onPressOut?: PressableProps['onPressOut'];
+
+  /**
+   * Called when a long press gesture has been triggered.
+   */
+  onLongPress?: PressableProps['onLongPress'];
+};
+
+export type PressableFocusProps = {
+  /**
+   * Called after the element loses focus.
+   */
+  onBlur?: (event: any) => any;
+
+  /**
+   * Called after the element is focused.
+   */
+  onFocus?: (event: any) => any;
+};
+
+export type PressableHoverProps = {
+  /**
+   * Duration to wait after hover in before calling `onHoverIn`.
+   */
+  delayHoverIn?: number;
+
+  /**
+   * Duration to wait after hover out before calling `onHoverOut`.
+   */
+  delayHoverOut?: number;
+
+  /**
+   * Called when the hover is activated to provide visual feedback.
+   */
+  onHoverIn?: (event: any) => any;
+
+  /**
+   * Called when the hover is deactivated to undo visual feedback.
+   */
+  onHoverOut?: (event: any) => any;
+};
+
+/**
+ * PressablePropsExtended includes all the desktop specific types unique to IViewProps, along with the Pressable event handlers
+ */
+export type PressablePropsExtended = Omit<IViewProps, keyof PressableProps> &
+  PressableProps &
+  PressableHoverProps &
+  PressableFocusProps &
+  PressablePressProps;
