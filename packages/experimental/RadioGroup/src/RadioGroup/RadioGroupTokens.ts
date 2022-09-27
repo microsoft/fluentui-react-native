9668c94d5dfe0f79c9b5f36a8d0cfc6aa0e320c2
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-09-26 18:05:33 -0700

    removed individual fonts

--- packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroupTokens.ts
@@ -1,7 +1,6 @@
 import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
 import { RadioGroupTokens } from './RadioGroup.types';
-import { globalTokens } from '@fluentui-react-native/theme-tokens';
 
 export const defaultRadioGroupTokens: TokenSettings<RadioGroupTokens, Theme> = (t: Theme) =>
   ({
@@ -9,8 +8,5 @@ export const defaultRadioGroupTokens: TokenSettings<RadioGroupTokens, Theme> = (
     variant: 'subtitle2Strong',
     disabled: {
       color: t.colors.neutralForegroundDisabled,
-      fontFamily: globalTokens.font.family.default,
-      fontSize: globalTokens.font.size[400],
-      fontWeight: globalTokens.font.weight.semibold,
     },
   } as RadioGroupTokens);
