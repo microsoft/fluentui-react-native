d4eb81adba363e0df9d19c7de68f31dce6dee20c
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-09-27 20:40:35 -0700

    reverted wdio

--- apps/win32/wdio.conf.js
+++ apps/win32/wdio.conf.js
@@ -11,7 +11,7 @@ const jasmineDefaultTimeout = 60000; // 60 seconds for Jasmine test timeout
 
 exports.config = {
   runner: 'local', // Where should your test be launched
-  specs: ['../fluent-tester/src/E2E/RadioGroupExperimental/specs/*.win.ts'],
+  specs: ['../fluent-tester/src/E2E/**/specs/*.win.ts'],
   exclude: ['../fluent-tester/src/E2E/Shimmer/specs/*.win.ts'],
 
   capabilities: [
