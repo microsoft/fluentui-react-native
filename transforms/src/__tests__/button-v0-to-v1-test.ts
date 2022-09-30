8be90c1ea8576ecc577856e5ba09c055ee6661f2
Ruriko Araki <ruaraki@microsoft.com>
2022-09-29 17:33:15 -0700

    Move the files

--- transforms/src/__tests__/button-v0-to-v1-test.ts
+++
@@ -1,7 +0,0 @@
-import { defineTest } from 'jscodeshift/dist/testUtils';
-
-jest.autoMockOff();
-
-describe('button-v0-to-v1', () => {
-  defineTest(__dirname, 'button-v0-to-v1', null, 'button-v0-to-v1', { parser: 'tsx' });
-});
