9bd71ee5319c7c638ed753d829914df9caae8b79
Ruriko Araki <ruaraki@microsoft.com>
2022-10-04 16:22:02 -0700

    Skeleton Listbox

---
+++ packages/experimental/Dropdown/src/Listbox/Listbox.styling.ts
@@ -0,0 +1,6 @@
+import { UseStylingOptions } from '@fluentui-react-native/framework';
+import { listboxName, ListboxProps, ListboxSlotProps, ListboxTokens } from './Listbox.types';
+
+export const stylingSettings: UseStylingOptions<ListboxProps, ListboxSlotProps, ListboxTokens> = {
+  tokens: [listboxName],
+};
