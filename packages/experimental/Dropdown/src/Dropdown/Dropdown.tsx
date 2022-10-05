0fa3fba5ce65450cce6a37ca9832dc23f142d48f
Ruriko Araki <ruaraki@microsoft.com>
2022-10-05 16:01:07 -0700

    Add dropdown slots

--- packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
@@ -1,4 +1,6 @@
+import { ButtonV1 as Button } from '@fluentui-react-native/button';
 import { compose, UseSlots } from '@fluentui-react-native/framework';
+import { Icon } from '@fluentui-react-native/icon';
 import React from 'react';
 import { View } from 'react-native';
 import { dropdownName, DropdownProps, DropdownType } from './Dropdown.types';
@@ -7,6 +9,8 @@ export const Dropdown = compose<DropdownType>({
   displayName: dropdownName,
   slots: {
     root: View,
+    button: Button,
+    expandIcon: Icon,
   },
   useRender: (_userProps: DropdownProps, _useSLots: UseSlots<DropdownType>) => {
     return (_final: DropdownProps, ..._children: React.ReactNode[]) => {
--- packages/experimental/Dropdown/src/Dropdown/Dropdown.types.ts
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.types.ts
@@ -1,4 +1,6 @@
 import type { IViewProps } from '@fluentui-react-native/adapters';
+import { ButtonProps } from '@fluentui-react-native/button';
+import { IconProps } from '@fluentui-react-native/icon';
 import { IPressableHooks, IWithPressableOptions } from '@fluentui-react-native/interactive-hooks';
 
 export const dropdownName = 'Dropdown';
@@ -9,7 +11,11 @@ export interface DropdownProps extends IWithPressableOptions<IViewProps> {}
 
 export interface DropdownState extends IPressableHooks<DropdownProps & React.ComponentPropsWithRef<any>> {}
 
-export interface DropdownSlotProps {}
+export interface DropdownSlotProps {
+  root: IViewProps;
+  button: ButtonProps;
+  expandIcon: IconProps;
+}
 
 export interface DropdownType {
   props: DropdownProps;
