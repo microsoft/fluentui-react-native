808521ff9f9f9d7ccb6cb32530003148008c8f90
Ruriko Araki <ruaraki@microsoft.com>
2022-10-06 16:01:28 -0700

    Initial slots

--- packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
@@ -1,9 +1,24 @@
-import { buildUseTokens, compressible, UseTokens } from '@fluentui-react-native/framework';
+import { IViewProps } from '@fluentui-react-native/adapters';
+import { ButtonV1 as Button, ButtonProps } from '@fluentui-react-native/button';
+import { buildUseTokens, compressible, useSlot, UseTokens } from '@fluentui-react-native/framework';
 import React from 'react';
+import { View } from 'react-native';
+import { Svg, SvgProps } from 'react-native-svg';
 import { DropdownProps, DropdownTokens } from './Dropdown.types';
 
-export const Dropdown = compressible<DropdownProps, DropdownTokens>((_props: DropdownProps, _useTokens: UseTokens<DropdownTokens>) => {
+export const Dropdown = compressible<DropdownProps, DropdownTokens>((props: DropdownProps, _useTokens: UseTokens<DropdownTokens>) => {
+  const RootSlot = useSlot<IViewProps>(View, props);
+  const ButtonSlot = useSlot<ButtonProps>(Button, {});
+  const ExpandIconSlot = useSlot<SvgProps>(Svg, {});
+
   return (_final: DropdownProps, ..._children: React.ReactNode[]) => {
-    return null;
+    return (
+      <RootSlot>
+        <ButtonSlot>
+          {'Test'}
+          <ExpandIconSlot />
+        </ButtonSlot>
+      </RootSlot>
+    );
   };
 }, buildUseTokens<DropdownTokens>({}));
