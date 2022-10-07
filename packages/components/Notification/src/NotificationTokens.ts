dd25e87eab7fc40c7a83f7b8d03199390ba0a2ad
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-10-07 13:55:20 -0700

    Update notification shadow to use the shadow16 token value (#2211)
    * Update Notification to use shadow16
    
    * Change files
    
    * Update snapshot tests

--- packages/components/Notification/src/NotificationTokens.ts
+++ packages/components/Notification/src/NotificationTokens.ts
@@ -2,11 +2,6 @@ import { Theme } from '@fluentui-react-native/framework';
 import { TokenSettings } from '@fluentui-react-native/use-styling';
 import { NotificationTokens } from './Notification.types';
 
-const notificationShadowStyle = {
-  ambient: { x: 0, y: 8, blur: 8, color: '#00000024' },
-  key: { x: 0, y: 0, blur: 1, color: '#0000001f' },
-};
-
 export const defaultNotificationTokens: TokenSettings<NotificationTokens, Theme> = (t: Theme) =>
   ({
     backgroundColor: t.colors.background,
@@ -21,7 +16,7 @@ export const defaultNotificationTokens: TokenSettings<NotificationTokens, Theme>
     minHeight: 52,
     padding: 16,
     paddingVertical: 12,
-    shadowToken: notificationShadowStyle,
+    shadowToken: t.shadows.shadow16,
     isBar: {
       borderRadius: 0,
       fontWeight: '400',
