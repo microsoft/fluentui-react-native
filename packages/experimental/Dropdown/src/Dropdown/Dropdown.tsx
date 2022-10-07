4ac0aa1ab4cce60719dca64a15528eb5a17e6cfa
Ruriko Araki <ruaraki@microsoft.com>
2022-10-07 12:26:18 -0700

    Slots for Dropdown trigger

--- packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
+++ packages/experimental/Dropdown/src/Dropdown/Dropdown.tsx
@@ -1,6 +1,7 @@
+/** @jsx withSlots */
 import { IViewProps } from '@fluentui-react-native/adapters';
 import { ButtonV1 as Button, ButtonProps } from '@fluentui-react-native/button';
-import { buildUseTokens, compressible, useSlot, UseTokens } from '@fluentui-react-native/framework';
+import { buildUseTokens, compressible, useSlot, UseTokens, withSlots } from '@fluentui-react-native/framework';
 import React from 'react';
 import { View } from 'react-native';
 import { Path, Svg, SvgProps } from 'react-native-svg';
@@ -17,12 +18,15 @@ export const Dropdown = compressible<DropdownProps, DropdownTokens>((props: Drop
 
   const expandIconProps: SvgProps = React.useMemo(
     () => ({
-      width: '20',
-      height: '20',
-      viewBox: '0 0 20 20',
+      width: '16',
+      height: '16',
+      viewBox: '0 0 16 16',
       fill: 'none',
     }),
     [],
+  );
+  const expandIconPath = (
+    <Path d="M3.14645 5.64645C3.34171 5.45118 3.65829 5.45118 3.85355 5.64645L8 9.79289L12.1464 5.64645C12.3417 5.45118 12.6583 5.45118 12.8536 5.64645C13.0488 5.84171 13.0488 6.15829 12.8536 6.35355L8.35355 10.8536C8.15829 11.0488 7.84171 11.0488 7.64645 10.8536L3.14645 6.35355C2.95118 6.15829 2.95118 5.84171 3.14645 5.64645Z" />
   );
 
   const RootSlot = useSlot<IViewProps>(View, props);
@@ -33,10 +37,8 @@ export const Dropdown = compressible<DropdownProps, DropdownTokens>((props: Drop
     return (
       <RootSlot>
         <ButtonSlot>
-          {'Test'}
-          <ExpandIconSlot>
-            <Path d="M15.8537 7.64582C16.0493 7.84073 16.0499 8.15731 15.855 8.35292L10.39 13.8374C10.1751 14.0531 9.82574 14.0531 9.6108 13.8374L4.14582 8.35292C3.9509 8.15731 3.95147 7.84073 4.14708 7.64582C4.34269 7.4509 4.65927 7.45147 4.85418 7.64708L10.0004 12.8117L15.1466 7.64708C15.3415 7.45147 15.6581 7.4509 15.8537 7.64582Z" />
-          </ExpandIconSlot>
+          Test
+          <ExpandIconSlot>{expandIconPath}</ExpandIconSlot>
         </ButtonSlot>
       </RootSlot>
     );
