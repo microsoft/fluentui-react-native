995a61a5bf89c42faa427c54b1501e49d5ff48b3
Samuel Freiberg <safreibe@microsoft.com>
2022-10-07 12:30:20 -0700

    Changing struct of menu E2E test component. I think the menu was blocking the text needed for E2E testing

--- apps/fluent-tester/src/TestComponents/Menu/E2EMenuTest.tsx
+++ apps/fluent-tester/src/TestComponents/Menu/E2EMenuTest.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { View } from 'react-native';
 import { Stack } from '@fluentui-react-native/stack';
 import { stackStyle } from '../Common/styles';
 import { ButtonV1 as Button } from '@fluentui-react-native/button';
@@ -27,22 +28,24 @@ export const E2EMenuTest: React.FunctionComponent = () => {
 
   return (
     <Stack style={stackStyle}>
-      <Menu open={menuOpened} onOpenChange={onOpenChange}>
-        <MenuTrigger>
-          <Button testID={MENUTRIGGER_TEST_COMPONENT}>Test</Button>
-        </MenuTrigger>
-        <MenuPopover testID={MENUPOPOVER_TEST_COMPONENT}>
-          <MenuList>
-            <MenuItem testID={MENUITEM_TEST_COMPONENT} accessibilityLabel={MENUITEM_ACCESSIBILITY_LABEL}>
-              A plain MenuItem
-            </MenuItem>
-            <MenuItem disabled>A second disabled plain MenuItem</MenuItem>
-            <MenuItem testID={MENUITEM_NO_A11Y_LABEL_COMPONENT}>{MENUITEM_TEST_LABEL}</MenuItem>
-          </MenuList>
-        </MenuPopover>
-      </Menu>
+      <View style={{ flexDirection: 'row'}}>
+        {menuOpened ? <Text testID={MENU_ON_OPEN}>Menu opened</Text> : <Text testID={MENU_ON_CLOSE}>Menu closed</Text>}
 
-      {menuOpened ? <Text testID={MENU_ON_OPEN}>Menu opened</Text> : <Text testID={MENU_ON_CLOSE}>Menu closed</Text>}
+        <Menu open={menuOpened} onOpenChange={onOpenChange}>
+          <MenuTrigger>
+            <Button testID={MENUTRIGGER_TEST_COMPONENT}>Test</Button>
+          </MenuTrigger>
+          <MenuPopover testID={MENUPOPOVER_TEST_COMPONENT}>
+            <MenuList>
+              <MenuItem testID={MENUITEM_TEST_COMPONENT} accessibilityLabel={MENUITEM_ACCESSIBILITY_LABEL}>
+                A plain MenuItem
+              </MenuItem>
+              <MenuItem disabled>A second disabled plain MenuItem</MenuItem>
+              <MenuItem testID={MENUITEM_NO_A11Y_LABEL_COMPONENT}>{MENUITEM_TEST_LABEL}</MenuItem>
+            </MenuList>
+          </MenuPopover>
+        </Menu>
+      </View>
     </Stack>
   );
 };
