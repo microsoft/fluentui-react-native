b30b37a3adf1d0c3fbd0bb520cd34b2fd276510b
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-10-13 09:32:46 -0700

    react callback

--- apps/fluent-tester/src/TestComponents/RadioGroupExperimental/HorizontalRadioGroup.tsx
+++ apps/fluent-tester/src/TestComponents/RadioGroupExperimental/HorizontalRadioGroup.tsx
@@ -5,9 +5,9 @@ import { View } from 'react-native';
 
 export const HorizontalRadioGroup: React.FunctionComponent = () => {
   // Client's example onChange function
-  const onChange = (key: string) => {
+  const onChange = React.useCallback((key: string) => {
     console.log(key);
-  };
+  }, []);
 
   return (
     <View>
