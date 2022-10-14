f8e877c141ad3023259612c954287f6b7e6e08bd
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-10-14 16:12:13 -0700

    Add tokens-global.ios.ts

---
+++ packages/theming/theme-tokens/src/tokens-global.ios.ts
@@ -0,0 +1,5 @@
+import globalTokens from '@fluentui-react-native/design-tokens-ios/light/tokens-global.json';
+
+console.log('tokens-global.ios.ts');
+
+export default globalTokens;
--- packages/theming/theme-tokens/src/tokens-global.ts
+++ packages/theming/theme-tokens/src/tokens-global.ts
@@ -1,3 +1,5 @@
 import globalTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-global.json';
 
+console.log('tokens-global');
+
 export default globalTokens;
