d1c4083d4b9a6c8d4b1b4ca4f88d71c660d7a7cc
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-10-14 09:33:43 -0700

    Revert "Add stroke width to iOS theme"
    This reverts commit 6f9e4d18c63c76d534b6c2c4c50692c5b423e4bc.

--- packages/theming/apple-theme/src/appleTheme.ios.ts
+++ packages/theming/apple-theme/src/appleTheme.ios.ts
@@ -1,6 +1,6 @@
 import globalTokens from '@fluentui-react-native/design-tokens-ios/light/tokens-global.json';
 
-import { Theme, Spacing, StrokeWidth } from '@fluentui-react-native/theme-types';
+import { Theme, Spacing } from '@fluentui-react-native/theme-types';
 import { paletteFromAppleColors } from './appleColors.ios';
 import { appleTypography } from './appleTypography.ios';
 import { iOSShadows } from './appleShadows.ios';
@@ -13,12 +13,6 @@ function appleSpacing(): Spacing {
     l2: '32px',
     // v2 types
     ...globalTokens.spacing,
-  };
-}
-
-function iOSStrokeWidth(): StrokeWidth {
-  return {
-    ...globalTokens.stroke.width,
   };
 }
 
@@ -143,7 +137,6 @@ export const BaseAppleLightThemeIOS: Theme = {
   shadows: iOSShadows(),
   spacing: appleSpacing(),
   components: appleComponents,
-  stroke: { width: iOSStrokeWidth() },
   host: { appearance: 'light' },
 };
 
@@ -153,6 +146,5 @@ export const BaseAppleDarkThemeIOS: Theme = {
   shadows: iOSShadows(),
   spacing: appleSpacing(),
   components: appleComponents,
-  stroke: { width: iOSStrokeWidth() },
   host: { appearance: 'dark' },
 };
