dd828941ca06a8e97d1359b0abee3903648641af
Ruriko Araki <ruaraki@microsoft.com>
2022-10-20 11:28:54 -0700

    Address comments

--- packages/components/Menu/src/MenuTrigger/getRevisedProps.ts
+++ packages/components/Menu/src/MenuTrigger/getRevisedProps.ts
@@ -3,6 +3,12 @@ import { memoize } from '@fluentui-react-native/framework';
 import { InteractionEvent, isMouseEvent } from '@fluentui-react-native/interactive-hooks';
 import { MenuTriggerChildProps, MenuTriggerState } from './MenuTrigger.types';
 
+/**
+ * Creates a merged set of props between the MenuTrigger's child component's props
+ * and the props that the MenuTrigger wants its child to have.
+ *
+ * Because children are not accessible in the outer render, we deal with the merge in this function.
+ */
 export const getRevisedProps = memoize(getRevisedPropsWorker);
 function getRevisedPropsWorker(state: MenuTriggerState, props: any): MenuTriggerChildProps {
   const revisedProps = { ...state.props };
@@ -16,31 +22,31 @@ function getRevisedPropsWorker(state: MenuTriggerState, props: any): MenuTrigger
 
   if (props.onAccessibilityAction) {
     revisedProps.onAccessibilityAction = (e: AccessibilityActionEvent) => {
-      props.onAccessibilityAction(e);
       state.props.onAccessibilityAction(e);
+      props.onAccessibilityAction(e);
     };
   }
 
   if (props.onClick) {
     revisedProps.onClick = (e: InteractionEvent) => {
-      props.onClick(e);
       state.props.onClick(e);
+      props.onClick(e);
     };
   }
 
   let onHoverIn = undefined;
   if (props.onHoverIn) {
     onHoverIn = (e: InteractionEvent) => {
-      props.onHoverIn(e);
       state.props.onHoverIn(isMouseEvent(e) && e);
+      props.onHoverIn(e);
     };
   }
 
   let onHoverOut = undefined;
   if (props.onHoverOut) {
     onHoverOut = (e: InteractionEvent) => {
-      props.onHoverOut(e);
       state.props.onHoverOut(isMouseEvent(e) && e);
+      props.onHoverOut(e);
     };
   }
 
