7e615c499281b757a7d668ede25ef1e033de02c2
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2022-10-25 10:35:01 -0700

    Remove console.log statements

--- packages/theming/theme-tokens/src/tokens-global.ios.ts
+++ packages/theming/theme-tokens/src/tokens-global.ios.ts
@@ -1,5 +1,3 @@
 import globalTokens from '@fluentui-react-native/design-tokens-ios/light/tokens-global.json';
 
-console.log('tokens-global.ios.ts');
-
 export default globalTokens;
--- packages/theming/theme-tokens/src/tokens-global.ts
+++ packages/theming/theme-tokens/src/tokens-global.ts
@@ -1,5 +1,3 @@
 import globalTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-global.json';
 
-console.log('tokens-global');
-
 export default globalTokens;
