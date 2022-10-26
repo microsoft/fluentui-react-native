70c90407167d042176352e7d29a0dcae2aa67a5b
Adam Gleitman <adgleitm@microsoft.com>
2022-10-25 19:49:53 -0700

    Another bump to iOS 16 (pls work now)

--- apps/fluent-tester/wdio.conf.ios.js
+++ apps/fluent-tester/wdio.conf.ios.js
@@ -14,8 +14,8 @@ exports.config = {
     {
       maxInstances: 1, // Maximum number of total parallel running workers.
       platformName: 'iOS',
-      'appium:platformVersion': '15.5',
-      'appium:deviceName': 'iPhone 13',
+      'appium:platformVersion': '16.0',
+      'appium:deviceName': 'iPhone 14',
       'appium:automationName': 'XCUITest',
       'appium:bundleId': 'com.microsoft.ReactTestApp',
     },
