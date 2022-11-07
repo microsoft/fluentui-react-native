1c37d1859d1ed1009e1efe0e894ada950483a9bf
Adam Gleitman <adam.gleitman@gmail.com>
2022-11-07 13:01:28 -0800

    Add font metrics package to FURN (#2298)
    * Add NativeFontMetrics package
    
    * Remove unneeded API
    
    * Fix spacing
    
    * Change files
    
    * Add @types/use-subscription dependency
    
    * Update yarn.lock
    
    * Add use-subscription as a dependency
    
    * Update yarn.lock again
    
    * NativeFontMetrics.tsx -> NativeFontMetrics.ts
    
    * Make NativeFontMetrics iOS only, part 1
    
    * Stub out NativeFontMetrics for non-iOS platforms
    
    * Keep use-subscription version consistent with react-native and react-native-macos
    
    * Remove core-android capability
    
    * id -> UIFontTextStyle
    
    * hasListeners -> _hasListeners
    
    * Revert "Remove core-android capability"
    
    This reverts commit 0c97508cd9169a0b917aa7d0154d79e74909abdb.
    
    * Revert "Revert "Remove core-android capability""
    
    This reverts commit 794c41cd493817ca775b462ed6a17cc98391d4bb.
    
    Co-authored-by: Adam Gleitman <adgleitm@microsoft.com>

---
+++ packages/experimental/NativeFontMetrics/src/NativeFontMetrics.ios.ts
@@ -0,0 +1,10 @@
+import { NativeModules } from 'react-native';
+import { ScaleFactors, TextStyle } from './NativeFontMetrics.types';
+
+export const NativeFontMetrics = NativeModules.FRNFontMetrics;
+
+interface NativeFontMetricsInterface {
+  allScaleFactors(): ScaleFactors;
+  scaleFactorForStyle(style: TextStyle): number;
+}
+export default NativeFontMetrics as NativeFontMetricsInterface;
