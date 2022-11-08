d79d0012043b62dab1471feb53aa9061a299674d
Valentyna Kozlova <vkozlova@microsoft.com>
2022-11-08 04:09:34 -0800

    Added Icon component

---
+++ packages/experimental/ExperimentalIcon/src/FontIcon/FontIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mergeProps, stagedComponent } from '@fluentui-react-native/framework';
+import { Text } from 'react-native';
+import { fontIconName, FontIconProps } from './FontIcon.types';
+import { useFontIcon } from './useFontIcon';
+
+export const FontIcon = stagedComponent((props: FontIconProps) => {
+  const fontIconProps = useFontIcon(props);
+  return (final: FontIconProps) => {
+    const newProps = mergeProps<FontIconProps>(fontIconProps, final);
+    const { codepoint, style, ...rest } = newProps;
+
+    const char = String.fromCharCode(codepoint);
+    return (
+      <Text style={style} {...rest}>
+        {char}
+      </Text>
+    );
+  };
+});
+
+FontIcon.displayName = fontIconName;
+
+export default FontIcon;
---
+++ packages/experimental/ExperimentalIcon/src/FontIcon/useFontIcon.ts
@@ -0,0 +1,33 @@
+import { TextStyle, Image, Platform } from 'react-native';
+import { getMemoCache } from '@fluentui-react-native/framework';
+import { FontIconProps } from './FontIcon.types';
+
+export const useFontIcon = (props: FontIconProps): FontIconProps => {
+  const { color, fontSrcFile, fontFamily, fontSize, ...rest } = props;
+
+  const style: TextStyle = fontStyleMemoCache(
+    {
+      fontSrcFile,
+      fontFamily: fontSrcFile != undefined ? fontFamilyFromFontSrcFile(fontSrcFile, fontFamily) : fontFamily,
+      fontSize,
+      color,
+    },
+    [color, fontSrcFile, fontFamily],
+  )[0];
+  return {
+    style,
+    ...rest,
+  };
+};
+
+function fontFamilyFromFontSrcFile(fontSrcFile: string, fontFamily: string): string {
+  if (Platform.OS == 'windows') {
+    // This `${family}#${path}` notation is specific to WPF
+    const asset = Image.resolveAssetSource(+fontSrcFile);
+    return `${fontFamily}#${asset.uri}`;
+  } else {
+    return fontFamily;
+  }
+}
+
+const fontStyleMemoCache = getMemoCache();
