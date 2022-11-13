42685ad6d78fdec7de899153a20b40863bad12f3
lenahong <lehon@microsoft.com>
2022-11-13 00:57:51 -0800

    Fix double press behavior for Link (#2308)
    * fix double clicking on inline links
    
    * remove icon hook from interactive-hooks
    
    * add textpackage dep
    
    * run prettier
    
    * Change files
    
    * add documentation
    
    * pull out platform check as a variable

--- packages/components/Button/src/FAB/FABCore.tsx
+++ packages/components/Button/src/FAB/FABCore.tsx
@@ -6,8 +6,8 @@ import { TextV1 as Text } from '@fluentui-react-native/text';
 import { stylingSettings } from './FAB.styling';
 import { compose, mergeProps, withSlots, UseSlots } from '@fluentui-react-native/framework';
 import { useButton } from '../useButton';
-import { Icon } from '@fluentui-react-native/icon';
-import { createIconProps, IPressableState } from '@fluentui-react-native/interactive-hooks';
+import { Icon, createIconProps } from '@fluentui-react-native/icon';
+import { IPressableState } from '@fluentui-react-native/interactive-hooks';
 import { Shadow } from '@fluentui-react-native/experimental-shadow';
 import { extractOuterStylePropsAndroid } from '../ExtractStyle.android';
 
