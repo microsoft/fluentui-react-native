c658475d1522ededc441d01c40188fb159571601
Lawrence Win <winlarry@microsoft.com>
2022-11-14 12:18:59 -0800

    Image sources are even better now

--- apps/fluent-tester/src/TestComponents/Persona/styles.ts
+++ apps/fluent-tester/src/TestComponents/Persona/styles.ts
@@ -1,6 +1,6 @@
 export * from '../PersonaCoin/styles';
 
-export const rajeshImageUrl =
-  'https://static.seattletimes.com/wp-content/uploads/2016/09/a9c139d2-8665-11e6-9a5f-c893d49e0984-780x1091.jpg';
+export const satyaImageUrl =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/MS-Exec-Nadella-Satya-2017-08-31-22_%28cropped%29.jpg/330px-MS-Exec-Nadella-Satya-2017-08-31-22_%28cropped%29.jpg';
 
 export const michaelImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/31/Michael_Jackson_in_1988.jpg';
