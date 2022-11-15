7b1ecbb5e58a75ac0ebefac4cdf3d281453c5719
Ayush Singh <ayushsinghs@yahoo.in>
2022-11-15 15:36:06 +0530

    Updated snapshots

--- packages/components/Button/src/FAB/FAB.test.tsx
+++ packages/components/Button/src/FAB/FAB.test.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { FAB } from './FAB';
 import { checkRenderConsistency, checkReRender } from '@fluentui-react-native/test-tools';
 import * as renderer from 'react-test-renderer';
-import { SvgIconProps } from '@fluentui-react-native/icon';
 
-const TestSvg = require('../../../../../apps/fluent-tester/src/FluentTester/test-data/test.svg');
-const svgProps: SvgIconProps = {
-  src: TestSvg,
-  viewBox: '0 0 500 500',
+const fontBuiltInProps = {
+  fontFamily: 'Arial',
+  codepoint: 0x2663,
+  fontSize: 16,
 };
+const iconProps = { fontSource: { ...fontBuiltInProps }, color: '#fff' };
 
 beforeAll(() => {
   jest.mock('react-native/Libraries/Utilities/Platform', () => ({
@@ -18,31 +18,29 @@ beforeAll(() => {
 });
 
 it('Default FAB (iOS)', () => {
-  const tree = renderer.create(<FAB icon={{ svgSource: svgProps }}>Default FAB (iOS)</FAB>).toJSON();
+  const tree = renderer.create(<FAB icon={iconProps}>Default FAB (iOS)</FAB>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it('Custom FAB with no shadow(iOS)', () => {
   const CustomFABNoShadow = FAB.customize({ shadowToken: undefined });
-  const tree = renderer
-    .create(<CustomFABNoShadow icon={{ svgSource: svgProps }}>Custom FAB with no shadow(iOS)</CustomFABNoShadow>)
-    .toJSON();
+  const tree = renderer.create(<CustomFABNoShadow icon={iconProps}>Custom FAB with no shadow(iOS)</CustomFABNoShadow>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it('Button simple rendering does not invalidate styling', () => {
-  checkRenderConsistency(() => <FAB icon={{ svgSource: svgProps }}>Default FAB</FAB>, 2);
+  checkRenderConsistency(() => <FAB icon={iconProps}>Default FAB</FAB>, 2);
 });
 
 it('FAB re-renders correctly', () => {
-  checkReRender(() => <FAB icon={{ svgSource: svgProps }}>Render twice</FAB>, 2);
+  checkReRender(() => <FAB icon={iconProps}>Render twice</FAB>, 2);
 });
 
 it('FAB shares produced styles across multiple renders', () => {
   const style = { backgroundColor: 'black' };
   checkRenderConsistency(
     () => (
-      <FAB icon={{ svgSource: svgProps }} style={style}>
+      <FAB icon={iconProps} style={style}>
         Shared styles
       </FAB>
     ),
@@ -54,7 +52,7 @@ it('FAB re-renders correctly with style', () => {
   const style = { borderColor: 'blue' };
   checkReRender(
     () => (
-      <FAB icon={{ svgSource: svgProps }} style={style}>
+      <FAB icon={iconProps} style={style}>
         Shared Style Render
       </FAB>
     ),
