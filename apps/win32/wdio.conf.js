a8dbd6106c978bfb2e950e74cb3987913d5bb6b6
Samuel Freiberg <safreibe@microsoft.com>
2022-11-22 18:04:25 -0700

    Fixing Win32 wdio.conf.ts specs path

--- apps/win32/wdio.conf.js
+++ apps/win32/wdio.conf.js
@@ -11,7 +11,7 @@ const jasmineDefaultTimeout = 60000; // 60 seconds for Jasmine test timeout
 
 exports.config = {
   runner: 'local', // Where should your test be launched
-  specs: ['../E2E/dist/win32_specs.bundle.js'],
+  specs: ['../E2E/src/**/specs/*.win.ts'],
   exclude: [],
 
   capabilities: [
