1e60075776e9c4f8520b59b1017b055e7beec12c
Valentyna Kozlova <vkozlova@microsoft.com>
2022-12-06 08:35:27 -0800

    Added information to the Icon spec.

--- packages/experimental/ExperimentalIcon/src/FontIcon/FontIcon.types.ts
+++ packages/experimental/ExperimentalIcon/src/FontIcon/FontIcon.types.ts
@@ -3,10 +3,28 @@ import { ColorValue, TextStyle, StyleProp } from 'react-native';
 export const fontIconName = 'FontIcon';
 
 export interface FontIconProps {
+  /**
+   * Unicode codepoint.
+   */
   codepoint: number;
+  /**
+   * Icon color.
+   */
   color?: ColorValue;
+  /**
+   * Font name.
+   */
   fontFamily?: string;
+  /**
+   * Font size.
+   */
   fontSize?: number;
+  /**
+   * Path to Font file.
+   */
   fontSrcFile?: string;
+  /**
+   * Style object which contains TextStyle props.
+   */
   style?: StyleProp<TextStyle>;
 }
