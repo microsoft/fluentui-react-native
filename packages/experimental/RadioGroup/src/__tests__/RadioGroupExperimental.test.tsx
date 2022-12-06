f088ac561a37bb03f429ebe35cbe2756abed0f65
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-12-06 10:15:38 -0800

    uncomment tests

--- packages/experimental/RadioGroup/src/__tests__/RadioGroupExperimental.test.tsx
+++ packages/experimental/RadioGroup/src/__tests__/RadioGroupExperimental.test.tsx
@@ -33,53 +33,53 @@ describe('RadioGroup component tests', () => {
 
   /* Uncomment these tests when Issue #2349 is fixed. */
 
-  // it('RadioGroup disabled', () => {
-  //   const tree = renderer
-  //     .create(
-  //       <RadioGroup disabled label="Disabled RadioGroup">
-  //         <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
-  //         <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
-  //       </RadioGroup>,
-  //     )
-  //     .toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('RadioGroup disabled', () => {
+    const tree = renderer
+      .create(
+        <RadioGroup disabled label="Disabled RadioGroup">
+          <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
+          <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
+        </RadioGroup>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-  // it('RadioGroup required', () => {
-  //   const tree = renderer
-  //     .create(
-  //       <RadioGroup required label="Required RadioGroup" defaultValue="key2">
-  //         <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
-  //         <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
-  //       </RadioGroup>,
-  //     )
-  //     .toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('RadioGroup required', () => {
+    const tree = renderer
+      .create(
+        <RadioGroup required label="Required RadioGroup" defaultValue="key2">
+          <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
+          <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
+        </RadioGroup>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-  // it('RadioGroup horizontal', () => {
-  //   const tree = renderer
-  //     .create(
-  //       <RadioGroup layout="horizontal" label="Horizontal RadioGroup" defaultValue="key2">
-  //         <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
-  //         <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
-  //       </RadioGroup>,
-  //     )
-  //     .toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('RadioGroup horizontal', () => {
+    const tree = renderer
+      .create(
+        <RadioGroup layout="horizontal" label="Horizontal RadioGroup" defaultValue="key2">
+          <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
+          <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
+        </RadioGroup>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-  // it('RadioGroup horizontal-stacked', () => {
-  //   const tree = renderer
-  //     .create(
-  //       <RadioGroup layout="horizontal-stacked" label="Horizontal-Stacked RadioGroup" defaultValue="key2">
-  //         <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
-  //         <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
-  //       </RadioGroup>,
-  //     )
-  //     .toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('RadioGroup horizontal-stacked', () => {
+    const tree = renderer
+      .create(
+        <RadioGroup layout="horizontal-stacked" label="Horizontal-Stacked RadioGroup" defaultValue="key2">
+          <Radio value="key1" label="Radio1" accessibilityPositionInSet={1} accessibilitySetSize={2} />
+          <Radio value="key2" label="Radio2" accessibilityPositionInSet={2} accessibilitySetSize={2} />
+        </RadioGroup>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   it('RadioGroup simple rendering does not invalidate styling', () => {
     checkRenderConsistency(() => <RadioGroup>Default RadioGroup</RadioGroup>, 2);
