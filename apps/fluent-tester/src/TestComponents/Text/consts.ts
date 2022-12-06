b95dbd5e242762c038f4d6cdf86bc4e71b7511e0
Krystal Kramer <krsiler@microsoft.com>
2022-12-06 12:42:40 -0500

    change accessibility label

--- apps/fluent-tester/src/TestComponents/Text/consts.ts
+++ apps/fluent-tester/src/TestComponents/Text/consts.ts
@@ -3,7 +3,7 @@ export const TEXT_TESTPAGE = 'Text_TestPage';
 
 /* E2E Testing Text 1 */
 export const FIRST_TEXT_COMPONENT = 'Text_Test_Component';
-export const TEXT_ACCESSIBILITY_LABEL = 'Text - Accessibility Label';
+export const TEXT_ACCESSIBILITY_LABEL = 'E2E testing Text_Test_Component accessibility label';
 
 /* E2E Testing Text 2 */
 export const SECOND_TEXT_COMPONENT = 'Text_No_A11y_Label_Component';
@@ -11,7 +11,7 @@ export const TEXT_COMPONENT_CONTENT = 'Text - No Accessibility Label Set';
 
 /* E2E Testing Deprecated Text 1 */
 export const THIRD_TEXT_COMPONENT = 'DeprecatedText_Test_Component';
-export const DEPRECATED_TEXT_ACCESSIBILITY_LABEL = 'Deprecated Text - Accessibility Label';
+export const DEPRECATED_TEXT_ACCESSIBILITY_LABEL = 'E2E testing DeprecatedText_Test_Component accessibility label';
 
 /* E2E Testing Deprecated Text 2 */
 export const FOURTH_TEXT_COMPONENT = 'DeprecatedText_No_A11y_Label_Component';
