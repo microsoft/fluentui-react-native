dacf2e019b8db793e9fff310fc65ba89ff7cc278
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2022-12-09 15:59:35 -0500

    removed defaults

--- packages/experimental/RadioGroup/src/index.ts
+++ packages/experimental/RadioGroup/src/index.ts
@@ -13,5 +13,4 @@ export { useRadioGroup } from './RadioGroup/useRadioGroup';
 export { useRadioGroupContextValue } from './RadioGroup/useRadioGroupContextValue';
 export { Radio, radioLookup } from './Radio/Radio';
 export { radioName, RadioProps, RadioSlotProps, RadioInfo, RadioTokens, RadioType } from './Radio/Radio.types';
-export { defaultRadioTokens } from './Radio/RadioTokens';
 export { useRadio } from './Radio/useRadio';
