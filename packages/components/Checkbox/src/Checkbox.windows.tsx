9472c7d08f7c8698e7ca5aacfdb665e6140726d3
Ruriko Araki <ruaraki@microsoft.com>
2022-12-09 14:44:56 -0800

    Windows checkbox override

---
+++ packages/components/Checkbox/src/Checkbox.windows.tsx
@@ -0,0 +1,18 @@
+/** @jsx withSlots */
+import { checkboxName, CheckboxProps, CheckboxType } from './Checkbox.types';
+import { compose, UseSlots } from '@fluentui-react-native/framework';
+import { View } from 'react-native';
+import { Svg } from 'react-native-svg';
+import { Text } from '@fluentui-react-native/text';
+
+export const Checkbox = compose<CheckboxType>({
+  displayName: checkboxName,
+  slots: { root: View, checkbox: View, checkmark: Svg, label: Text, required: Text },
+  useRender: (_props: CheckboxProps, _useSlots: UseSlots<CheckboxType>) => {
+    console.warn('Use legacy checkbox for Windows');
+    return (_rest: CheckboxProps) => {
+      return null;
+    };
+  },
+});
+export default Checkbox;
