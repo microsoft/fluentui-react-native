f1c9726bc2961c6bdff3d29697e69f694564cac3
Adam Gleitman <adgleitm@microsoft.com>
2022-12-14 17:15:01 -0800

    Add documentation for custom TextTokens

--- packages/components/text/src/Text.types.ts
+++ packages/components/text/src/Text.types.ts
@@ -12,7 +12,16 @@ export type TextTokens = Omit<FontTokens, 'fontFamily'> &
   IForegroundColorTokens &
   Omit<TextStyle, 'fontSize' | 'fontWeight' | 'color'> & {
     // TODO(#2268): Remove these once RN Core properly supports Dynamic Type scaling
+    /**
+     * (iOS only) The Dynamic Type ramp that a Text element should follow as the user changes their
+     * preferred content size.
+     */
     dynamicTypeRamp?: string;
+
+    /**
+     * (iOS only) The maximum font size that a Text element will grow to as the user changes their
+     * preferred content size.
+     */
     maximumFontSize?: number;
   };
 
