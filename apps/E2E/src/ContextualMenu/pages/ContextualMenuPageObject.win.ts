e16f202f22b0eea80ae6f28546c6d6c4915efaed
Lawrence Win <winlarry@microsoft.com>
2022-12-15 11:00:40 -0800

    Overhaul ContextualMenu E2E tests

--- apps/E2E/src/ContextualMenu/pages/ContextualMenuPageObject.win.ts
+++ apps/E2E/src/ContextualMenu/pages/ContextualMenuPageObject.win.ts
@@ -6,44 +6,17 @@ import {
 } from '../../../../fluent-tester/src/TestComponents/ContextualMenu/consts';
 import { BasePage, By } from '../../common/BasePage';
 
-/* This enum gives the spec file an EASY way to interact with SPECIFIC UI elements on the page.
- * The spec file should import this enum and use it when wanting to interact with different elements on the page. */
-export const enum ContextualMenuSelector {
-  ContextualMenu = 0, //this._primaryComponent
-  ContextualMenuItem, //this._contextualMenuItem
-}
-
 class ContextualMenuPageObject extends BasePage {
   /******************************************************************/
   /**************** UI Element Interaction Methods ******************/
   /******************************************************************/
   async waitForContextualMenuItemsToOpen(timeout?: number): Promise<void> {
-    await browser.waitUntil(async () => await this.contextualMenuItemDisplayed(), {
-      timeout: timeout ?? this.waitForUiEvent,
-      timeoutMsg: 'The Contextual Menu Items did not open.',
-      interval: 1000,
-    });
+    await this.waitForCondition(async () => this.contextualMenuItemDisplayed(), 'The Contextual Menu Items did not open.', timeout);
   }
 
   /* Whether the contextual menu item is displayed or not. It should be displayed after clicking on the MenuButton */
   async contextualMenuItemDisplayed(): Promise<boolean> {
     return await (await this._contextualMenuItem).isDisplayed();
-  }
-
-  /* Sends a Keyboarding command on a specific UI element */
-  async sendKey(contextualMenuSelector: ContextualMenuSelector, key: string): Promise<void> {
-    await (await this.getContextualMenuSelector(contextualMenuSelector)).addValue(key);
-  }
-
-  /* Returns the correct WebDriverIO element from the ContextualMenuSelector string */
-  async getContextualMenuSelector(contextualMenuSelector?: ContextualMenuSelector): Promise<WebdriverIO.Element> {
-    if (contextualMenuSelector == ContextualMenuSelector.ContextualMenu) {
-      return await this._primaryComponent;
-    } else if (contextualMenuSelector == ContextualMenuSelector.ContextualMenuItem) {
-      return await this._contextualMenuItem;
-    } else {
-      return await this._primaryComponent;
-    }
   }
 
   /*****************************************/
@@ -57,7 +30,7 @@ class ContextualMenuPageObject extends BasePage {
     return CONTEXTUALMENU_TESTPAGE;
   }
 
-  get _primaryComponent() {
+  get _contextualMenu() {
     return By(CONTEXTUALMENU_TEST_COMPONENT);
   }
 
