521434be94311d3904d514218ef9b0605aacc740
Lawrence Win <winlarry@microsoft.com>
2022-12-16 13:49:07 -0800

    More changes to ContextualMenu E2E

--- apps/E2E/src/ContextualMenu/pages/ContextualMenuPageObject.win.ts
+++ apps/E2E/src/ContextualMenu/pages/ContextualMenuPageObject.win.ts
@@ -5,18 +5,28 @@ import {
   CONTEXTUALMENUITEM_TEST_COMPONENT,
 } from '../../../../fluent-tester/src/TestComponents/ContextualMenu/consts';
 import { BasePage, By } from '../../common/BasePage';
+import { Keys } from '../../common/consts';
 
 class ContextualMenuPageObject extends BasePage {
   /******************************************************************/
   /**************** UI Element Interaction Methods ******************/
   /******************************************************************/
-  async waitForContextualMenuItemsToOpen(timeout?: number): Promise<void> {
+  async waitForContextualMenuItemsToOpen(timeout?: number): Promise<boolean> {
     await this.waitForCondition(async () => this.contextualMenuItemDisplayed(), 'The Contextual Menu Items did not open.', timeout);
+    return await (await this._contextualMenuItem).isDisplayed();
   }
 
   /* Whether the contextual menu item is displayed or not. It should be displayed after clicking on the MenuButton */
   async contextualMenuItemDisplayed(): Promise<boolean> {
     return await (await this._contextualMenuItem).isDisplayed();
+  }
+
+  async closeContextualMenu(): Promise<void> {
+    await this.sendKeys(this._contextualMenu, [Keys.ESCAPE]);
+    await this.waitForCondition(
+      async () => !(await this.contextualMenuItemDisplayed()),
+      "Pressed 'ESC' on the ContextualMenu, but it failed to close.",
+    );
   }
 
   /*****************************************/
--- apps/E2E/src/ContextualMenu/specs/ContextualMenu.spec.win.ts
+++ apps/E2E/src/ContextualMenu/specs/ContextualMenu.spec.win.ts
@@ -24,33 +24,33 @@ describe('ContextualMenu Functional Tests', async () => {
   beforeEach(async () => {
     await ContextualMenuPageObject.scrollToTestElement(await ContextualMenuPageObject._contextualMenu);
 
-    await ContextualMenuPageObject.sendKeys(ContextualMenuPageObject._contextualMenu, [Keys.ESCAPE]); // Reset ContextualMenu state for next test
+    await ContextualMenuPageObject.closeContextualMenu(); // Reset ContextualMenu state for next test
   });
 
-  it('Click on ContextualMenu Button and validate that the list of ContextualMenu Items open', async () => {
+  it('Click on ContextualMenu Button -> Validate that the menu opens by checking if its items are displayed', async () => {
     /* Click on the ContextualMenu */
     await ContextualMenuPageObject.click(ContextualMenuPageObject._contextualMenu);
-    await ContextualMenuPageObject.waitForContextualMenuItemsToOpen(PAGE_TIMEOUT);
 
-    await expect(await ContextualMenuPageObject.contextualMenuItemDisplayed()).toBeTruthy(
-      'Contextual Menu Items failed to display via clicking the Contextual Menu.',
+    await expect(ContextualMenuPageObject.waitForContextualMenuItemsToOpen(PAGE_TIMEOUT)).toBeTruthy(
+      'Clicked the ContextualMenu button, but the ContextualMenu items failed to open and display.',
     );
+
     await expect(await ContextualMenuPageObject.didAssertPopup()).toBeFalsy(ContextualMenuPageObject.ERRORMESSAGE_ASSERT); // Ensure no asserts popped up
   });
 
-  it('Type "SPACE" to select the ContextualMenu and validate that the list of ContextualMenu Items open', async () => {
+  it("Type 'SPACE' on ContextualMenu Button -> Validate that the mneu opens by checking if its items are displayed", async () => {
     /* Type a space on the ContextualMenu */
     await ContextualMenuPageObject.sendKeys(ContextualMenuPageObject._contextualMenu, [Keys.SPACE]);
-    await ContextualMenuPageObject.waitForContextualMenuItemsToOpen(PAGE_TIMEOUT);
 
-    await expect(await ContextualMenuPageObject.contextualMenuItemDisplayed()).toBeTruthy(
-      "Contextual Menu Items failed to display via pressing 'Space' on the Contextual Menu.",
+    await expect(ContextualMenuPageObject.waitForContextualMenuItemsToOpen(PAGE_TIMEOUT)).toBeTruthy(
+      "Pressed 'SPACE' on the ContextualMenu button, but the ContextualMenu items failed to open and display.",
     );
+
     await expect(await ContextualMenuPageObject.didAssertPopup()).toBeFalsy(ContextualMenuPageObject.ERRORMESSAGE_ASSERT); // Ensure no asserts popped up
   });
 
   /* Runs after all tests. This ensures the ContextualMenu closes. If it stays open, the test driver won't be able to close the test app */
   afterAll(async () => {
-    await ContextualMenuPageObject.sendKeys(ContextualMenuPageObject._contextualMenu, [Keys.ESCAPE]); // Reset ContextualMenu state for next test
+    await ContextualMenuPageObject.closeContextualMenu(); // Reset ContextualMenu state for next test
   });
 });
