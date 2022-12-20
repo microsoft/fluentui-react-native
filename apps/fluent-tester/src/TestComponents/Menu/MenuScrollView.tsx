c4c19ecaf89cea829d7df3acacd9930a4e71ab35
chiuam <amchiu@microsoft.com>
2022-12-20 11:20:24 -0500

    Add mutable menu test

--- apps/fluent-tester/src/TestComponents/Menu/MenuScrollView.tsx
+++ apps/fluent-tester/src/TestComponents/Menu/MenuScrollView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ButtonV1 as Button } from '@fluentui/react-native';
+import { ButtonV1 as Button, Text } from '@fluentui/react-native';
 import { Menu, MenuItem, MenuTrigger, MenuPopover, MenuList } from '@fluentui-react-native/menu';
 import { Stack } from '@fluentui-react-native/stack';
 import { stackStyle } from '../Common/styles';
@@ -10,9 +10,30 @@ export const MenuScrollView: React.FunctionComponent = () => {
   const [maxHeight, setMaxHeight] = useState<number>(800);
   const ScrollViewMenuPopover = MenuPopover.customize({ maxHeight: maxHeight });
   const menuItems = [];
+  const [data, setData] = useState([
+    <MenuItem key={1}>MenuItem 1</MenuItem>,
+    <MenuItem key={2}>MenuItem 2</MenuItem>,
+    <MenuItem key={3}>MenuItem 3</MenuItem>,
+    <MenuItem key={4}>MenuItem 4</MenuItem>,
+    <MenuItem key={5}>MenuItem 5</MenuItem>,
+  ]);
+
   for (let i = 0; i < 400; i++) {
-    menuItems.push(<MenuItem key={i}>MenuItem</MenuItem>);
+    menuItems.push(<MenuItem key={i}>MenuItem {i}</MenuItem>);
   }
+  const insertOnClick = React.useCallback(() => {
+    data.push(<MenuItem key={Math.random()}>MenuItem</MenuItem>);
+    const newList = [...data];
+    setData(newList);
+  }, [data]);
+
+  const popOnClick = React.useCallback(() => {
+    if (data.length > 0) {
+      data.pop();
+      const newList = [...data];
+      setData(newList);
+    }
+  }, [data]);
 
   return (
     <Stack style={stackStyle}>
@@ -35,6 +56,19 @@ export const MenuScrollView: React.FunctionComponent = () => {
           <MenuList>{menuItems}</MenuList>
         </ScrollViewMenuPopover>
       </Menu>
+      <Text>Add/Remove menu item</Text>
+      <Button onClick={insertOnClick}>Add new menu item</Button>
+      <Button onClick={popOnClick}>Remove last menu item</Button>
+      <View>
+        <Menu>
+          <MenuTrigger>
+            <Button>Mutable Menu</Button>
+          </MenuTrigger>
+          <ScrollViewMenuPopover>
+            <MenuList>{data}</MenuList>
+          </ScrollViewMenuPopover>
+        </Menu>
+      </View>
     </Stack>
   );
 };
