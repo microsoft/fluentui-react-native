d2c0af8c0097dad7f88e830141bd700e2f6b7036
Ruriko Araki <ruaraki@microsoft.com>
2022-12-20 18:12:52 -0800

    Clean up index files

--- packages/components/Link/src/index.ts
+++ packages/components/Link/src/index.ts
@@ -1,4 +1,21 @@
-export { linkName } from './Link.types';
+export { linkName as linkNameV1 } from './Link.types';
 export type { LinkTokens, LinkProps, LinkAppearance, LinkState, LinkSlotProps, LinkType } from './Link.types';
+export { defaultLinkTokens } from './LinkTokens';
+export { linkStates, stylingSettings as linkStylingSettings } from './Link.styling';
 export { useLink } from './useLink';
-export { Link } from './Link';
+export { Link as LinkV1, linkLookup } from './Link';
+
+export { linkName } from './legacy/Link.types';
+export type {
+  ILinkInfo,
+  ILinkOptions,
+  ILinkProps,
+  ILinkRenderData,
+  ILinkSlotProps,
+  ILinkState,
+  ILinkTokens,
+  ILinkType,
+  IWithLinkOptions,
+} from './legacy/Link.types';
+export { Link, useAsLink } from './legacy/Link';
+export type { ILinkHooks } from './legacy/Link';
