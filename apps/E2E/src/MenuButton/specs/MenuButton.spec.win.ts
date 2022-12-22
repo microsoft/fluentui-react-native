a55fbc39ba60e784091fb05dacd129cae4ceae18
Lawrence Win <winlarry@microsoft.com>
2022-12-22 17:19:16 -0600

    Refactor MenuButton test

--- apps/E2E/src/MenuButton/specs/MenuButton.spec.win.ts
+++ apps/E2E/src/MenuButton/specs/MenuButton.spec.win.ts
@@ -1,11 +1,10 @@
 import NavigateAppPage from '../../common/NavigateAppPage';
 import MenuButtonPageObject, { MenuButtonSelector } from '../pages/MenuButtonPageObject.win';
-import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT, MENUBUTTON_A11Y_ROLE, Keys } from '../../common/consts';
+import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT, MENUBUTTON_A11Y_ROLE, Keys, Attribute } from '../../common/consts';
 import {
   MENU_BUTTON_ACCESSIBILITY_LABEL,
   MENU_BUTTON_TEST_COMPONENT_LABEL,
 } from '../../../../fluent-tester/src/TestComponents/MenuButton/consts';
-import { ComponentSelector } from '../../common/BasePage';
 
 // Before testing begins, allow up to 60 seconds for app to open
 describe('MenuButton Testing Initialization', function () {
@@ -24,25 +23,41 @@ describe('MenuButton Testing Initialization', function () {
   });
 });
 
-/* This will be re-enabled with a MenuButton Bug is fixed. Currently in PR - "Integrating accessibilityLabel functionality for MenuButton #1117" */
 describe('MenuButton Accessibility Testing', () => {
   /* Scrolls and waits for the MenuButton to be visible on the Test Page */
   beforeEach(async () => {
-    await MenuButtonPageObject.scrollToTestElement();
+    await MenuButtonPageObject.scrollToTestElement(await MenuButtonPageObject._firstMenuButton);
   });
 
-  it('MenuButton - Validate accessibilityRole is correct', async () => {
-    await expect(await MenuButtonPageObject.getAccessibilityRole()).toEqual(MENUBUTTON_A11Y_ROLE);
+  it('Validate accessibilityRole is correct', async () => {
+    await expect(
+      await MenuButtonPageObject.compareAttribute(MenuButtonPageObject._firstMenuButton, Attribute.AccessibilityRole, MENUBUTTON_A11Y_ROLE),
+    ).toBeTrue();
+
     await expect(await MenuButtonPageObject.didAssertPopup()).toBeFalsy(MenuButtonPageObject.ERRORMESSAGE_ASSERT);
   });
 
-  it('MenuButton - Set accessibilityLabel', async () => {
-    await expect(await MenuButtonPageObject.getAccessibilityLabel(ComponentSelector.Primary)).toEqual(MENU_BUTTON_ACCESSIBILITY_LABEL);
+  it('Set accessibilityLabel -> Validate accessibilityLabel is correct', async () => {
+    await expect(
+      await MenuButtonPageObject.compareAttribute(
+        MenuButtonPageObject._firstMenuButton,
+        Attribute.AccessibilityLabel,
+        MENU_BUTTON_ACCESSIBILITY_LABEL,
+      ),
+    ).toBeTrue();
+
     await expect(await MenuButtonPageObject.didAssertPopup()).toBeFalsy(MenuButtonPageObject.ERRORMESSAGE_ASSERT);
   });
 
-  it('Do not set accessibilityLabel -> Default to MenuButton label', async () => {
-    await expect(await MenuButtonPageObject.getAccessibilityLabel(ComponentSelector.Secondary)).toEqual(MENU_BUTTON_TEST_COMPONENT_LABEL);
+  it('Do not set accessibilityLabel -> Validate accessibilityLabel defaults to MenuButton label', async () => {
+    await expect(
+      await MenuButtonPageObject.compareAttribute(
+        MenuButtonPageObject._secondMenuButton,
+        Attribute.AccessibilityLabel,
+        MENU_BUTTON_TEST_COMPONENT_LABEL,
+      ),
+    ).toBeTrue();
+
     await expect(await MenuButtonPageObject.didAssertPopup()).toBeFalsy(MenuButtonPageObject.ERRORMESSAGE_ASSERT);
   });
 });
@@ -50,31 +65,31 @@ describe('MenuButton Accessibility Testing', () => {
 describe('MenuButton Functional Testing', () => {
   /* Scrolls and waits for the MenuButton to be visible on the Test Page */
   beforeEach(async () => {
-    await MenuButtonPageObject.scrollToTestElement();
+    await MenuButtonPageObject.scrollToTestElement(await MenuButtonPageObject._firstMenuButton);
 
-    await MenuButtonPageObject.sendKey(MenuButtonSelector.MenuButton, Keys.ESCAPE); // Reset MenuButton state for next test
+    await MenuButtonPageObject.sendKeys(MenuButtonPageObject._firstMenuButton, [Keys.ESCAPE]); // Reset MenuButton state for next test
   });
 
-  it('Click on MenuButton and validate that the list of Menu Items open', async () => {
+  it('Click on first MenuButton -> Validate that its list of Menu Items display', async () => {
     /* Click on the MenuButton */
-    await MenuButtonPageObject.clickComponent();
-    await MenuButtonPageObject.waitForMenuItemsToOpen(PAGE_TIMEOUT);
+    await MenuButtonPageObject.click(MenuButtonPageObject._firstMenuButton);
 
-    await expect(await MenuButtonPageObject.menuItemDisplayed()).toBeTruthy();
+    await expect(await MenuButtonPageObject.waitForMenuItemToDisplay()).toBeTruthy();
+
     await expect(await MenuButtonPageObject.didAssertPopup()).toBeFalsy(MenuButtonPageObject.ERRORMESSAGE_ASSERT);
   });
 
-  it('Type "SPACE" to select the MenuButton and validate that the list of Menu Items open', async () => {
+  it('Type "SPACE" on first MenuButton -> Validate that its list of Menu Items display', async () => {
     /* Type a space on the MenuButton */
-    await MenuButtonPageObject.sendKey(MenuButtonSelector.MenuButton, Keys.SPACE);
-    await MenuButtonPageObject.waitForMenuItemsToOpen(PAGE_TIMEOUT);
+    await MenuButtonPageObject.sendKeys(MenuButtonPageObject._firstMenuButton, [Keys.SPACE]);
 
-    await expect(await MenuButtonPageObject.menuItemDisplayed()).toBeTruthy();
+    await expect(await MenuButtonPageObject.waitForMenuItemToDisplay()).toBeTruthy();
+
     await expect(await MenuButtonPageObject.didAssertPopup()).toBeFalsy(MenuButtonPageObject.ERRORMESSAGE_ASSERT);
   });
 
   /* Runs after all tests. This ensures the MenuButton closes. If it stays open, the test driver won't be able to close the test app */
   afterAll(async () => {
-    await MenuButtonPageObject.sendKey(MenuButtonSelector.MenuButton, Keys.ESCAPE);
+    await MenuButtonPageObject.sendKeys(MenuButtonPageObject._firstMenuButton, [Keys.ESCAPE]);
   });
 });
