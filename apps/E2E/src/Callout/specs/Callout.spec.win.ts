df72004819a003ac0a1aa5552a5e50d153b594a9
Lawrence Win <winlarry@microsoft.com>
2022-12-27 14:53:38 -0600

    Updating change message

--- apps/E2E/src/Callout/specs/Callout.spec.win.ts
+++ apps/E2E/src/Callout/specs/Callout.spec.win.ts
@@ -1,7 +1,7 @@
 import NavigateAppPage from '../../common/NavigateAppPage';
 import CalloutPageObject from '../pages/CalloutPageObject.win';
 import { CALLOUT_ACCESSIBILITY_LABEL } from '../consts';
-import { PAGE_TIMEOUT, BOOT_APP_TIMEOUT, CALLOUT_A11Y_ROLE } from '../../common/consts';
+import { Attribute, PAGE_TIMEOUT, BOOT_APP_TIMEOUT, CALLOUT_A11Y_ROLE } from '../../common/consts';
 
 // Before testing begins, allow up to 60 seconds for app to open
 describe('Callout Testing Initialization', function () {
