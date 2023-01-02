7bfe6e5118937132d626d6d84dbf4360678885f5
Rohan Prasad <rohanpd.work@gmail.com>
2023-01-02 13:03:18 +0530

    revert to prev impl

--- packages/experimental/Switch/src/useSwitch.ts
+++ packages/experimental/Switch/src/useSwitch.ts
@@ -6,15 +6,19 @@ import {
   useViewCommandFocus,
   InteractionEvent,
 } from '@fluentui-react-native/interactive-hooks';
-import { SwitchProps, SwitchInfo } from './Switch.types';
-import { AccessibilityState, AccessibilityActionEvent } from 'react-native';
+import { SwitchProps, SwitchInfo, IAnimationConfig } from './Switch.types';
+import { AccessibilityState, AccessibilityActionEvent, Animated, Platform } from 'react-native';
 import { memoize } from '@fluentui-react-native/framework';
 import { useAsToggleWithEvent } from '@fluentui-react-native/interactive-hooks';
 
 const defaultAccessibilityActions = [{ name: 'Toggle' }];
 
-export const useSwitch = (props: SwitchProps): SwitchInfo => {
+const isMobile = Platform.OS === 'android' || Platform.OS == 'ios';
+
+export const useSwitch = (props: SwitchProps, animationConfig?: IAnimationConfig): SwitchInfo => {
   const defaultComponentRef = React.useRef(null);
+  const [animation] = React.useState(new Animated.Value(0));
+  const [trackBackgroundAnimation] = React.useState(new Animated.Value(0));
 
   const {
     onChange,
@@ -33,14 +37,80 @@ export const useSwitch = (props: SwitchProps): SwitchInfo => {
     ...rest
   } = props;
 
+  const [checkedState, toggleCallback] = useAsToggleWithEvent(defaultChecked, checked, onChangeWithAnimation);
+
+  // Function to change background slowly over time when switch is toggled.
+  const startTrackBackgroundAnimation = React.useCallback((checked: boolean, animation: Animated.Value) => {
+    const toValue = checked ? 0 : 1;
+    Animated.timing(animation, {
+      toValue,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
+  // Function to transform the knob over track in animated way when switch is toggled.
+  const startTrackAnimation = React.useCallback(
+    (onInit = false, animationConfig: IAnimationConfig, animation: Animated.Value, toggled: boolean) => {
+      Animated.timing(animation, {
+        toValue: toggled
+          ? animationConfig.trackWidth - (animationConfig.thumbWidth + animationConfig.thumbMargin * 2)
+          : onInit
+          ? 0
+          : -(animationConfig.trackWidth + animationConfig.thumbWidth),
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    },
+    [],
+  );
+
   const onChangeWithAnimation = React.useCallback(
     (e: InteractionEvent, checked?: boolean) => {
       onChange && onChange(e, checked);
+      if (isMobile) {
+        if (checked) {
+          startTrackBackgroundAnimation(checked, trackBackgroundAnimation);
+          startTrackAnimation(false, animationConfig, animation, checked);
+        } else {
+          startTrackBackgroundAnimation(checked, trackBackgroundAnimation);
+          startTrackAnimation(false, animationConfig, animation, checked);
+        }
+      }
     },
-    [onChange],
+    [onChange, trackBackgroundAnimation, animationConfig, animation],
   );
 
-  const [checkedState, toggleCallback] = useAsToggleWithEvent(defaultChecked, checked, onChangeWithAnimation);
+  const switchAnimationStyles = {
+    // transform over toggled on position to toggled off position and vice versa
+    thumbAnimatedStyle: {
+      transform: [
+        {
+          translateX: animation,
+        },
+      ],
+    },
+    // Interpolate over toggled on color to toggled off color and vice versa
+    trackBackgroundStyle: {
+      backgroundColor: trackBackgroundAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: checkedState
+          ? [animationConfig.toggleOnBgColor, animationConfig.toggleOnBgColor]
+          : [animationConfig.toggleOffBgColor, animationConfig.toggleOffBgColor],
+      }),
+    },
+  };
+
+  //Setting the initial position of the know on track when page loads.
+  if (isMobile) {
+    if (checkedState) {
+      startTrackAnimation(false, animationConfig, animation, checkedState);
+      startTrackBackgroundAnimation(true, trackBackgroundAnimation);
+    } else {
+      startTrackAnimation(true, animationConfig, animation, checkedState);
+      startTrackBackgroundAnimation(false, trackBackgroundAnimation);
+    }
+  }
 
   const focusRef = disabled ? null : componentRef;
 
@@ -87,6 +157,7 @@ export const useSwitch = (props: SwitchProps): SwitchInfo => {
       onAccessibilityAction: onAccessibilityActionProp,
       accessibilityState: getAccessibilityState(checkedState, disabled, accessibilityState),
       disabled,
+      switchAnimationStyles: switchAnimationStyles,
       focusable: !disabled,
       ref: useViewCommandFocus(componentRef),
       checked: checkedState,
