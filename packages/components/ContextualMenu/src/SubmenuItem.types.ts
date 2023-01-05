0c4b7017d6872297523d4016d910c2a53696f587
Krystal Kramer <krsiler@microsoft.com>
2023-01-05 15:02:37 -0500

    Remove Pressable from ContextualMenu and use useAsPressable (#2488)
    * Revert win32 submenuitem back to useAsPressable
    
    * Change files
    
    * Update types in PressabilityConfig to match PressableProps
    
    * Change files
    
    * Revert "Change files"
    
    This reverts commit b30154e6ba0348dbeb67ba75521c79335ba8ab0d.
    
    * Revert "Update types in PressabilityConfig to match PressableProps"
    
    This reverts commit 0e0b0e3f4f138426ddfff00a1f30ef629b9cd40c.
    
    * Remove Pressable from ContextualMenuItem
    
    * update change file

--- packages/components/ContextualMenu/src/SubmenuItem.types.ts
+++ packages/components/ContextualMenu/src/SubmenuItem.types.ts
@@ -4,7 +4,6 @@ import { IRenderData } from '@uifabricshared/foundation-composable';
 import { ITextProps } from '@fluentui-react-native/text';
 import { ContextualMenuItemProps, ContextualMenuItemTokens, ContextualMenuItemState } from './ContextualMenuItem.types';
 import { IconProps } from '@fluentui-react-native/icon';
-import { PressablePropsExtended } from '@fluentui-react-native/interactive-hooks';
 
 export const submenuItemName = 'SubmenuItem';
 export interface SubmenuItemTokens extends ContextualMenuItemTokens {
@@ -15,7 +14,7 @@ export type SubmenuItemProps = ContextualMenuItemProps;
 export type SubmenuItemState = ContextualMenuItemState;
 
 export interface SubmenuItemSlotProps {
-  root: React.PropsWithRef<PressablePropsExtended>;
+  root: React.PropsWithRef<IViewProps>;
   startstack: IViewProps;
   icon: IconProps;
   content: ITextProps;
