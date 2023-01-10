33d522b91a22b6983f3212b3982c6df05d34fd98
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2023-01-10 08:44:11 -0800

    prettier

--- packages/experimental/RadioGroup/src/RadioGroup/RadioGroup.tsx
+++ packages/experimental/RadioGroup/src/RadioGroup/RadioGroup.tsx
@@ -64,7 +64,7 @@ export const RadioGroup = compose<RadioGroupType>({
           {radioGroupContent}
         </Slots.container>
       );
-      
+
       return (
         <RadioGroupProvider value={contextValue}>
           <Slots.root {...mergedProps}>
