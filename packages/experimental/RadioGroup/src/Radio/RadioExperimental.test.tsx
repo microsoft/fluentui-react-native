f038ccbdfaf6b8d59580a59001846af2cedfde1e
Gulnaz Sayed <30990970+gulnazsayed@users.noreply.github.com>
2023-01-12 11:14:00 -0800

    RadioGroup Adding Re-Render Depth 2 Jest Test (#2503)
    * checking depth 2 test
    
    * Change files
    
    * remove old test and comment

--- packages/experimental/RadioGroup/src/Radio/RadioExperimental.test.tsx
+++ packages/experimental/RadioGroup/src/Radio/RadioExperimental.test.tsx
@@ -18,9 +18,7 @@ describe('Radio component tests', () => {
     checkRenderConsistency(() => <Radio value="key1" label="Default Radio" />, 2);
   });
 
-  /* Re-render test for depth 2 is blocked and will need to be added after issue is resolved.
-  The test is blocked due to an upstream issue where the blur function in Pressable is a new instance between renders.  */
   it('Radio re-renders correctly', () => {
-    checkReRender(() => <Radio value="key1" label="Render twice" />);
+    checkReRender(() => <Radio value="key1" label="Render twice" />, 2);
   });
 });
