c60feb5e01f8917daaa76a790faaeb4032e69c9c
Ayush Singh <ayushsinghs@yahoo.in>
2023-01-12 18:05:45 +0530

    Add android tokens

---
+++ packages/experimental/Shimmer/src/ShimmerTokens.android.ts
@@ -0,0 +1,14 @@
+import { Theme } from '@fluentui-react-native/framework';
+import { ShimmerTokens } from './Shimmer.types';
+import { TokenSettings } from '@fluentui-react-native/use-styling';
+
+export const defaultShimmerTokens: TokenSettings<ShimmerTokens, Theme> = (theme: Theme) =>
+  ({
+    angle: 45,
+    delay: 0,
+    duration: 7000,
+    shimmerColor: theme.colors.neutralStencil1,
+    shimmerColorOpacity: 1,
+    shimmerWaveColor: theme.colors.neutralStencil2,
+    shimmerWaveColorOpacity: 1,
+  } as ShimmerTokens);
