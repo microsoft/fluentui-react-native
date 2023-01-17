b90d5aac09d379b7a29a7cf99d2773a8171064db
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2023-01-17 11:24:25 -0800

    check for RTL

--- packages/experimental/RadioGroup/src/Radio/useRadio.win32.ts
+++ packages/experimental/RadioGroup/src/Radio/useRadio.win32.ts
@@ -9,7 +9,7 @@ import {
   useKeyDownProps,
 } from '@fluentui-react-native/interactive-hooks';
 import { memoize } from '@fluentui-react-native/framework';
-import { AccessibilityState } from 'react-native';
+import { AccessibilityState, I18nManager } from 'react-native';
 
 const defaultAccessibilityActions = [{ name: 'Select' }];
 
@@ -78,17 +78,25 @@ export const useRadio = (props: RadioProps): RadioInfo => {
     };
   }, []);
 
+  const isRTL = I18nManager.isRTL;
+
   const onInvoke = React.useCallback(
     (e: KeyPressEvent) => {
       if (e.nativeEvent.key in DirectionalArrowKeys) {
         const length = radioGroupContext.enabledValues.length;
+        const previous =
+          (!isRTL && (e.nativeEvent.key === DirectionalArrowKeys.ArrowUp || e.nativeEvent.key === DirectionalArrowKeys.ArrowLeft)) ||
+          (isRTL && (e.nativeEvent.key === DirectionalArrowKeys.ArrowDown || e.nativeEvent.key === DirectionalArrowKeys.ArrowRight));
+        const next =
+          (isRTL && (e.nativeEvent.key === DirectionalArrowKeys.ArrowUp || e.nativeEvent.key === DirectionalArrowKeys.ArrowLeft)) ||
+          (!isRTL && (e.nativeEvent.key === DirectionalArrowKeys.ArrowDown || e.nativeEvent.key === DirectionalArrowKeys.ArrowRight));
         const currRadioIndex = radioGroupContext.enabledValues.indexOf(radioGroupContext.value);
         let newCurrRadioIndex;
-        if (e.nativeEvent.key === DirectionalArrowKeys.ArrowDown || e.nativeEvent.key === DirectionalArrowKeys.ArrowRight) {
+        if (next) {
           newCurrRadioIndex = (currRadioIndex + 1) % length;
           radioGroupContext.onChange && radioGroupContext.onChange(radioGroupContext.enabledValues[newCurrRadioIndex]);
           radioGroupContext.updateInvoked && radioGroupContext.updateInvoked(true);
-        } else if (e.nativeEvent.key === DirectionalArrowKeys.ArrowUp || e.nativeEvent.key === DirectionalArrowKeys.ArrowLeft) {
+        } else if (previous) {
           newCurrRadioIndex = (currRadioIndex - 1 + length) % length;
           radioGroupContext.onChange && radioGroupContext.onChange(radioGroupContext.enabledValues[newCurrRadioIndex]);
           radioGroupContext.updateInvoked && radioGroupContext.updateInvoked(true);
