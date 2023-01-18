25b56560a8d0f68222da35d4c7483655b5e9e140
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2023-01-18 12:50:22 -0800

    Update ColorTokens e2e test

--- apps/E2E/src/ColorTokens/pages/ColorTokensPageObject.win.ts
+++ apps/E2E/src/ColorTokens/pages/ColorTokensPageObject.win.ts
@@ -1,24 +1,17 @@
 import { HOMEPAGE_COLORTOKEN_BUTTON, COLORTOKEN_TESTPAGE, COLORTOKENS_TEST_COMPONENT } from '../consts';
-import { BasePage, By } from '../../common/BasePage';
+import { BasePage } from '../../common/BasePage';
 
 class ColorTokenPageObject extends BasePage {
   /*****************************************/
   /**************** Getters ****************/
   /*****************************************/
-  get _testPage() {
-    return By(COLORTOKEN_TESTPAGE);
-  }
 
   get _pageName() {
     return COLORTOKEN_TESTPAGE;
   }
 
-  get _primaryComponent() {
-    return By(COLORTOKENS_TEST_COMPONENT);
-  }
-
-  get _pageButton() {
-    return By(HOMEPAGE_COLORTOKEN_BUTTON);
+  get _pageButtonName() {
+    return HOMEPAGE_COLORTOKEN_BUTTON;
   }
 }
 
