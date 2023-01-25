75caa75cb862dfb708ca6622e1fc09dd5ed04f7c
Samuel Freiberg <safreibe@microsoft.com>
2023-01-25 11:54:06 -0700

    Testing CI Failure

--- apps/E2E/src/ColorTokens/pages/ColorTokensPageObject.win.ts
+++ apps/E2E/src/ColorTokens/pages/ColorTokensPageObject.win.ts
@@ -1,4 +1,4 @@
-import { HOMEPAGE_COLORTOKEN_BUTTON, COLORTOKEN_TESTPAGE } from '../consts';
+import { HOMEPAGE_COLORTOKEN_BUTTON } from '../consts';
 import { BasePage } from '../../common/BasePage';
 
 class ColorTokenPageObject extends BasePage {
@@ -7,7 +7,7 @@ class ColorTokenPageObject extends BasePage {
   /*****************************************/
 
   get _pageName() {
-    return COLORTOKEN_TESTPAGE;
+    return 'Hi';
   }
 
   get _pageButtonName() {
