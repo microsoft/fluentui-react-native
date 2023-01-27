17128d4d9682424b6c3df2725c7dbf4f209264d9
Lawrence Win <winlarry@microsoft.com>
2023-01-26 17:07:58 -0800

    Improve divider test page

--- apps/fluent-tester/src/TestComponents/Divider/DividerTest.tsx
+++ apps/fluent-tester/src/TestComponents/Divider/DividerTest.tsx
@@ -1,13 +1,98 @@
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Divider } from '@fluentui-react-native/divider';
+import { Stack } from '@fluentui-react-native/stack';
+import { TextV1 as Text } from '@fluentui-react-native/text';
 import { DIVIDER_TESTPAGE } from './consts';
-import { DividerDefault } from './DividerDefault';
 import { Test, TestSection, PlatformStatus } from '../Test';
+import { commonTestStyles } from '../Common/styles';
+import TestSvg from '../../../assets/test.svg';
+
+const CustomDivider = Divider.customize({ thickness: 3, paddingVertical: 4 });
+const CustomText = Text.customize({ margin: 8 });
+
+const dividerTestStyles = StyleSheet.create({
+  verticalDividerContainer: {
+    marginVertical: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
+
+export const HorizontalDividers: React.FunctionComponent = () => (
+  <Stack style={commonTestStyles.stack}>
+    <CustomText>Regular divider with no content below.</CustomText>
+    <Divider />
+    <CustomText>Regular divider with no content above.</CustomText>
+    <Divider>Divider with text content.</Divider>
+    <Divider insetSize={16} color="purple">
+      Colored divider with text + inset.
+    </Divider>
+    <CustomDivider alignContent="start">I am a start-aligned divider with a thickness = 3</CustomDivider>
+    <CustomDivider alignContent="end">I am a end-aligned divider with a thickness = 3</CustomDivider>
+  </Stack>
+);
+
+export const VerticalDividers: React.FunctionComponent = () => (
+  <Stack style={commonTestStyles.stack}>
+    <View style={dividerTestStyles.verticalDividerContainer}>
+      <CustomText>Regular divider {'->'}</CustomText>
+      <Divider vertical />
+      <CustomText>Divider with an inset {'->'}</CustomText>
+      <Divider vertical insetSize={16} />
+      <CustomDivider vertical>Thick divider with text</CustomDivider>
+      <Divider color="red" vertical alignContent="start">
+        Start-aligned divider
+      </Divider>
+      <Divider color="blue" vertical alignContent="end">
+        End-aligned divider
+      </Divider>
+    </View>
+    <Divider />
+    <View style={dividerTestStyles.verticalDividerContainer}>
+      <CustomText>The divider to the right of me should have a min height of 24px</CustomText>
+      <Divider vertical />
+      <CustomText>The divider to the left of me should have a min height of 24px</CustomText>
+    </View>
+  </Stack>
+);
+
+export const DividersWithAppearance: React.FunctionComponent = () => (
+  <Stack style={commonTestStyles.stack}>
+    <CustomDivider appearance="default">Default divider</CustomDivider>
+    <CustomDivider appearance="subtle">Subtle divider</CustomDivider>
+    <CustomDivider appearance="brand">Branded divider</CustomDivider>
+    <CustomDivider appearance="strong">Strong divider</CustomDivider>
+  </Stack>
+);
+
+export const DividersWithIcons: React.FunctionComponent = () => (
+  <Stack style={commonTestStyles.stack}>
+    <Divider appearance="brand" icon={{ fontSource: { fontFamily: 'Arial', codepoint: 0x2663, fontSize: 32 } }} />
+    <CustomText align="center">
+      Above is a branded Divider with a font icon as content. Below me is a branded divider with an svg icon as content.
+    </CustomText>
+    <Divider appearance="brand" icon={{ svgSource: { viewBox: '0 0 500 500', src: TestSvg, width: 32, height: 32 } }} />
+  </Stack>
+);
 
 const dividerSections: TestSection[] = [
   {
-    name: 'Divider Page',
+    name: 'Horizontal Dividers',
     testID: DIVIDER_TESTPAGE,
-    component: DividerDefault,
+    component: HorizontalDividers,
+  },
+  {
+    name: 'Vertical Dividers',
+    component: VerticalDividers,
+  },
+  {
+    name: 'Divider Appearances',
+    component: DividersWithAppearance,
+  },
+  {
+    name: 'Dividers with Icons',
+    component: DividersWithIcons,
   },
 ];
 
