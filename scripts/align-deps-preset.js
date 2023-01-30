4de4c2b37c835ebd9af015ff767f9dd68452f786
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-01-30 15:44:56 +0100

    chore: migrate to `@rnx-kit/align-deps` (#2564)

---
+++ scripts/align-deps-preset.js
@@ -0,0 +1,14 @@
+module.exports = {
+  0.68: {
+    jest: {
+      name: 'jest',
+      version: '^27.0.0',
+      devOnly: true,
+    },
+    'test-app': {
+      name: 'react-native-test-app',
+      version: '^2.0.2',
+      devOnly: true,
+    },
+  },
+};
