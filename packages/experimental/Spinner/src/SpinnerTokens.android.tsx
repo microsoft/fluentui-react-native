e4e1c503940e61e6606a809c6ffa3fcca08ace31
Rohan Prasad <rohanpd.work@gmail.com>
2023-02-02 18:53:35 +0530

    fix import

--- packages/experimental/Spinner/src/SpinnerTokens.android.tsx
+++ packages/experimental/Spinner/src/SpinnerTokens.android.tsx
@@ -1,6 +1,6 @@
 import type { Theme } from '@fluentui-react-native/framework';
 import type { TokenSettings } from '@fluentui-react-native/use-styling';
-import type { SpinnerTokens } from '../Spinner.types';
+import type { SpinnerTokens } from './Spinner.types';
 import { globalTokens } from '@fluentui-react-native/theme-tokens';
 import { Appearance } from 'react-native';
 export const defaultSpinnerTokens: TokenSettings<SpinnerTokens, Theme> = () =>
