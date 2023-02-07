ecd6c25993e613e6768b396e56c98c903fbf07b1
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2023-02-07 12:23:41 -0800

    Some clean up

--- packages/experimental/AppearanceAdditions/src/NativeAppearanceAdditions.ios.ts
+++ packages/experimental/AppearanceAdditions/src/NativeAppearanceAdditions.ios.ts
@@ -5,5 +5,4 @@ export const NativeAppearanceAdditions = NativeModules.FRNAppearanceAdditions;
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface NativeAppearanceAdditionsInterface {}
 
-// export default NativeFontMetrics as NativeFontMetricsInterface;
 export default NativeAppearanceAdditions as NativeAppearanceAdditionsInterface;
