d057d491160efae2f3ef2c6973d2fee70465ebde
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2023-02-09 18:11:07 -0500

    made win32 specific tokens

---
+++ packages/experimental/RadioGroup/src/Radio/RadioTokens.win32.ts
@@ -0,0 +1,97 @@
+import { Theme } from '@fluentui-react-native/framework';
+import { TokenSettings } from '@fluentui-react-native/use-styling';
+import { globalTokens } from '@fluentui-react-native/theme-tokens';
+import { RadioTokens } from './Radio.types';
+
+export const defaultRadioTokens: TokenSettings<RadioTokens, Theme> = (t: Theme) =>
+  ({
+    rootHorizontalPadding: globalTokens.size40,
+    borderColor: t.colors.background,
+    borderStyle: 'solid',
+    borderWidth: globalTokens.stroke.width10,
+    borderRadius: globalTokens.corner.radius40,
+    radioBorderWidth: globalTokens.stroke.width10,
+    radioVisibility: 0,
+    variant: 'subheaderStandard',
+    radioBorderStyle: 'solid',
+    radioBorder: t.colors.neutralStrokeAccessible,
+    color: t.colors.neutralForeground3,
+    radioOuterCircleSize: 20,
+    radioInnerCircleSize: 10,
+    labelMarginVertical: globalTokens.size40,
+    labelMarginRight: globalTokens.size20,
+    labelMarginLeft: globalTokens.sizeNone,
+    subtextVariant: 'caption1',
+    subtextMarginTop: globalTokens.sizeNone,
+    subtextMarginBottom: globalTokens.size40 + 1,
+    marginTop: globalTokens.size60,
+    marginRight: globalTokens.size60,
+    marginBottom: globalTokens.size60,
+    marginLeft: globalTokens.size40,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    labelAlignItems: 'flex-start',
+
+    labelPositionBelow: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      labelAlignItems: 'center',
+      labelMarginLeft: globalTokens.size20,
+      marginLeft: globalTokens.size60,
+    },
+
+    disabled: {
+      // Unchecked, Disabled
+      radioBorder: t.colors.neutralForegroundDisabled,
+      color: t.colors.neutralForegroundDisabled,
+      radioVisibility: 0,
+    },
+
+    hovered: {
+      // Unchecked, Hover
+      radioBorder: t.colors.neutralStrokeAccessibleHover,
+      color: t.colors.neutralForeground2,
+
+      selected: {
+        // Checked, Hover
+        radioBorder: t.colors.compoundBrandStroke1Hover,
+        radioFill: t.colors.compoundBrandBackground1Hover,
+        color: t.colors.neutralForeground2,
+        radioVisibility: 1,
+      },
+    },
+
+    pressed: {
+      // Unchecked, Pressed
+      radioBorder: t.colors.neutralStrokeAccessiblePressed,
+      color: t.colors.neutralForeground1,
+
+      selected: {
+        // Checked, Pressed
+        radioBorder: t.colors.compoundBrandStroke1Pressed,
+        radioFill: t.colors.compoundBrandBackground1Pressed,
+        color: t.colors.neutralForeground1,
+        radioVisibility: 1,
+      },
+    },
+
+    focused: {
+      selected: {
+        // Checked, Focused
+        radioVisibility: 1,
+      },
+    },
+
+    selected: {
+      // Checked, Rest
+      radioBorder: t.colors.compoundBrandStroke1,
+      radioFill: t.colors.compoundBrandStroke1,
+      color: t.colors.neutralForeground3,
+      radioVisibility: 1,
+
+      disabled: {
+        // Checked, Disabled
+        radioFill: t.colors.neutralForegroundDisabled,
+      },
+    },
+  } as RadioTokens);
