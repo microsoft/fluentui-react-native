46a37c15fe28440a0ddbc2783e4f327fc1654d34
Lawrence Win <winlarry@microsoft.com>
2023-02-15 14:40:48 -0800

    Fix copy-pasted test names

--- apps/E2E/src/Divider/specs/Divider.spec.win.ts
+++ apps/E2E/src/Divider/specs/Divider.spec.win.ts
@@ -7,7 +7,7 @@ describe('Divider Testing Initialization', () => {
     expect(await DividerPageObject.isInitialPageDisplayed()).toBeTruthy(DividerPageObject.ERRORMESSAGE_APPLOAD);
   });
 
-  it('Click and navigate to FocusTrapZone test page', async () => {
+  it('Click and navigate to Divider test page', async () => {
     /* Click on component button to navigate to test page */
     await DividerPageObject.navigateToPageAndLoadTests();
     expect(await DividerPageObject.isPageLoaded()).toBeTruthy(DividerPageObject.ERRORMESSAGE_PAGELOAD);
