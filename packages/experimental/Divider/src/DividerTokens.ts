4ea4d97ea0fa8cea2a682c43823c0e2c850e5066
Lawrence Win <lawrence.j.win@gmail.com>
2023-02-14 16:14:43 -0800

    Divider `appearance` prop now correctly applies color (#2625)
    * Appearance prop correctly affects color
    
    * Change files

--- packages/experimental/Divider/src/DividerTokens.ts
+++ packages/experimental/Divider/src/DividerTokens.ts
@@ -1,16 +1,13 @@
 import { buildUseTokens } from '@fluentui-react-native/framework';
-import type { Theme } from '@fluentui-react-native/framework';
 import type { DividerTokens } from './Divider.types';
 import { globalTokens } from '@fluentui-react-native/theme-tokens';
 
-export const useDividerTokens = buildUseTokens<DividerTokens>((theme: Theme) => ({
+export const useDividerTokens = buildUseTokens<DividerTokens>(() => ({
   // base tokens
   contentPadding: globalTokens.size120,
   flexAfter: 1,
   flexBefore: 1,
   insetSize: 0,
-  lineColor: theme.colors.neutralStroke2,
-  contentColor: theme.colors.neutralForeground2,
   minLineSize: globalTokens.size80,
   minWidth: 0,
   minHeight: 0,
