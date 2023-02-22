18266cd983563b14c55e1e145d97ab5b9b21c913
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2023-02-21 17:38:36 -0800

    Add test in shadow test page for adding a shadow to button

--- apps/fluent-tester/src/TestComponents/Shadow/ShadowButtonTestSection.tsx
+++ apps/fluent-tester/src/TestComponents/Shadow/ShadowButtonTestSection.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { Platform, View } from 'react-native';
-import { FAB, Text } from '@fluentui/react-native';
-import { commonTestStyles, stackStyle } from '../Common/styles';
+import { Platform, View, StyleSheet } from 'react-native';
+import { FAB } from '@fluentui/react-native';
+import { stackStyle } from '../Common/styles';
 import { shadowTestPageStyles } from './ShadowTestPageStyles';
 import { useFluentTheme } from '@fluentui-react-native/framework';
 import { iconProps } from '../Common/iconExamples';
+import { Shadow } from '@fluentui-react-native/experimental-shadow';
+import { ButtonV1 as Button } from '@fluentui-react-native/button';
+
+const styles = StyleSheet.create({ marginBetweenComponentsWithShadow: { margin: 10 } });
 
 const CustomFABNoShadow = FAB.customize({ shadowToken: undefined });
 const CustomFABShadow64 = FAB.customize({
@@ -14,25 +18,31 @@ const CustomFABShadow64 = FAB.customize({
 export const ShadowButtonTestSection: React.FunctionComponent = () => {
   const t = useFluentTheme();
 
-  if (Platform.OS === 'ios') {
-    return (
-      <View style={shadowTestPageStyles(t).backgroundColor}>
-        <FAB icon={iconProps} style={commonTestStyles.vmargin}>
-          FAB with default shadow
-        </FAB>
-        <CustomFABShadow64 icon={iconProps} style={commonTestStyles.vmargin}>
-          Custom FAB with shadow64
-        </CustomFABShadow64>
-        <CustomFABNoShadow icon={iconProps} style={commonTestStyles.vmargin}>
-          Custom FAB with no shadow
-        </CustomFABNoShadow>
-      </View>
-    );
-  } else {
-    return (
-      <View style={stackStyle}>
-        <Text>Shadows only implemented for iOS at this time</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={shadowTestPageStyles(t).backgroundColor}>
+      {Platform.OS === 'ios' && (
+        <View style={stackStyle}>
+          <FAB icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+            FAB with default shadow
+          </FAB>
+          <CustomFABShadow64 icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+            Custom FAB with shadow64
+          </CustomFABShadow64>
+          <CustomFABNoShadow icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+            Custom FAB with no shadow
+          </CustomFABNoShadow>
+        </View>
+      )}
+      {Platform.OS !== 'android' && (
+        <View style={stackStyle}>
+          <Shadow shadowToken={t.shadows['shadow16']}>
+            <Button style={styles.marginBetweenComponentsWithShadow}>Button with shadow16</Button>
+          </Shadow>
+          <View>
+            <Button style={styles.marginBetweenComponentsWithShadow}>Button without shadow</Button>
+          </View>
+        </View>
+      )}
+    </View>
+  );
 };
