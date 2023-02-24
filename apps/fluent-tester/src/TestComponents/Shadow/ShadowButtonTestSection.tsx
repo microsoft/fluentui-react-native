dd8aa729e05f0572a53eaff27ac6254d71805491
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2023-02-24 11:51:05 -0800

    Address comment: put platform checks into their own vars

--- apps/fluent-tester/src/TestComponents/Shadow/ShadowButtonTestSection.tsx
+++ apps/fluent-tester/src/TestComponents/Shadow/ShadowButtonTestSection.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Platform, View, StyleSheet } from 'react-native';
-import { FAB } from '@fluentui/react-native';
+import { FAB, Text } from '@fluentui/react-native';
 import { stackStyle } from '../Common/styles';
 import { shadowTestPageStyles } from './ShadowTestPageStyles';
 import { useFluentTheme } from '@fluentui-react-native/framework';
@@ -15,34 +15,45 @@ const CustomFABShadow64 = FAB.customize({
   shadowToken: { ambient: { x: 0, y: 0, blur: 8, color: '#00000033' }, key: { x: 0, y: 32, blur: 64, color: '#0000003d' } },
 });
 
+const platformSupportsShadow = Platform.OS !== 'android';
+const platformSupportsFAB = Platform.OS === 'ios' || Platform.OS === 'android';
+
 export const ShadowButtonTestSection: React.FunctionComponent = () => {
   const t = useFluentTheme();
 
-  return (
-    <View style={shadowTestPageStyles(t).backgroundColor}>
-      {Platform.OS === 'ios' && (
-        <View style={stackStyle}>
-          <FAB icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
-            FAB with default shadow
-          </FAB>
-          <CustomFABShadow64 icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
-            Custom FAB with shadow64
-          </CustomFABShadow64>
-          <CustomFABNoShadow icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
-            Custom FAB with no shadow
-          </CustomFABNoShadow>
-        </View>
-      )}
-      {Platform.OS !== 'android' && (
-        <View style={stackStyle}>
-          <Shadow shadowToken={t.shadows['shadow16']}>
-            <Button style={styles.marginBetweenComponentsWithShadow}>Button with shadow16</Button>
-          </Shadow>
-          <View>
-            <Button style={styles.marginBetweenComponentsWithShadow}>Button without shadow</Button>
+  if (platformSupportsShadow) {
+    return (
+      <View style={shadowTestPageStyles(t).backgroundColor}>
+        {platformSupportsShadow && platformSupportsFAB && (
+          <View style={stackStyle}>
+            <FAB icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+              FAB with default shadow
+            </FAB>
+            <CustomFABShadow64 icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+              Custom FAB with shadow64
+            </CustomFABShadow64>
+            <CustomFABNoShadow icon={iconProps} style={styles.marginBetweenComponentsWithShadow}>
+              Custom FAB with no shadow
+            </CustomFABNoShadow>
           </View>
-        </View>
-      )}
-    </View>
-  );
+        )}
+        {platformSupportsShadow && (
+          <View style={stackStyle}>
+            <Shadow shadowToken={t.shadows['shadow16']}>
+              <Button style={styles.marginBetweenComponentsWithShadow}>Button with shadow16</Button>
+            </Shadow>
+            <View>
+              <Button style={styles.marginBetweenComponentsWithShadow}>Button without shadow</Button>
+            </View>
+          </View>
+        )}
+      </View>
+    );
+  } else {
+    return (
+      <View style={stackStyle}>
+        <Text>Shadows not implemented on this platform at this time</Text>
+      </View>
+    );
+  }
 };
