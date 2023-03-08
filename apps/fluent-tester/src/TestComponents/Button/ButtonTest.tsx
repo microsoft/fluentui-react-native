83fcafcf521d9c1ed4aa86cd4ddd66e6758edf62
Ruriko Araki <ruaraki@microsoft.com>
2023-03-08 11:52:20 -0800

    Revert tests

--- apps/fluent-tester/src/TestComponents/Button/ButtonTest.tsx
+++ apps/fluent-tester/src/TestComponents/Button/ButtonTest.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { Platform } from 'react-native';
 
+import { ButtonShapeTest } from './ButtonShapeTestSection';
+import { ButtonSizeTest } from './ButtonSizeTestSection';
 import { ButtonVariantTest } from './ButtonVariantTestSection';
+import { ButtonFocusTest_deprecated } from './deprecated/ButtonFocusTest';
+import { ButtonIconTest_deprecated } from './deprecated/ButtonIconTest';
+import { E2EButtonTest_deprecated } from './deprecated/E2EButtonTest';
+import { E2EButtonTest } from './E2EButtonTest';
+import { ToggleButtonTest } from './ToggleButtonTestSection';
 import { BUTTON_TESTPAGE } from '../../../../E2E/src/ButtonLegacy/consts';
+import { ButtonHOCTest } from '../Button/ButtonHOCTestSection';
+import { ButtonIconTest } from '../Button/ButtonIconTestSection';
 import type { TestSection, PlatformStatus } from '../Test';
 import { Test } from '../Test';
 
@@ -10,6 +20,64 @@ const buttonSections: TestSection[] = [
     name: 'Button Variants',
     testID: BUTTON_TESTPAGE,
     component: ButtonVariantTest,
+  },
+  {
+    name: 'Icon Button',
+    component: ButtonIconTest,
+  },
+  ...Platform.select({
+    // The following sections are not supported for iOS or Android
+    ios: [],
+    android: [],
+    default: [
+      {
+        name: 'Toggle Button',
+        component: ToggleButtonTest,
+      },
+      {
+        name: 'Button Shape',
+        component: ButtonShapeTest,
+      },
+    ],
+  }),
+  {
+    name: 'Sizes',
+    component: ButtonSizeTest,
+  },
+  Platform.select({
+    android: null,
+    default: {
+      name: 'Customize, Compose, and Ref',
+      component: ButtonHOCTest,
+    },
+  }),
+  ...Platform.select({
+    android: [], // Following sections are not supported from Fluent Android
+    default: [
+      {
+        name: 'Deprecated Basic Button',
+        component: ButtonFocusTest_deprecated,
+      },
+      {
+        name: 'Deprecated Icon Button',
+        component: ButtonIconTest_deprecated,
+      },
+    ],
+  }),
+  {
+    name: 'E2E Button Testing',
+    component: E2EButtonTest,
+  },
+];
+
+const e2eSections: TestSection[] = [
+  {
+    name: 'E2E Button Testing',
+    component: E2EButtonTest,
+  },
+  {
+    name: 'Deprecated E2E Button Testing',
+    component: E2EButtonTest_deprecated,
   },
 ];
 
@@ -27,5 +95,7 @@ export const ButtonTest: React.FunctionComponent = () => {
 
   const spec = 'https://github.com/microsoft/fluentui-react-native/blob/main/packages/components/Button/SPEC.md';
 
-  return <Test name="Button Test" description={description} spec={spec} sections={buttonSections} status={status} />;
+  return (
+    <Test name="Button Test" description={description} spec={spec} sections={buttonSections} status={status} e2eSections={e2eSections} />
+  );
 };
--- apps/fluent-tester/src/TestComponents/Button/ButtonVariantTestSection.tsx
+++ apps/fluent-tester/src/TestComponents/Button/ButtonVariantTestSection.tsx
@@ -1,14 +1,50 @@
-import { ButtonV1 } from '@fluentui-react-native/button';
 import * as React from 'react';
 import { View } from 'react-native';
 
-import { testContentRootViewStyle } from '../Common/styles';
+import { Button, CompoundButton, FAB } from '@fluentui-react-native/experimental-button';
+
+import { iconProps } from '../Common/iconExamples';
+import { commonTestStyles, testContentRootViewStyle } from '../Common/styles';
 
 // Test also pulls button from deprecated package to ensure it's still working
 export const ButtonVariantTest: React.FunctionComponent = () => {
   return (
     <View style={testContentRootViewStyle}>
-      <ButtonV1 appearance="primary">A test</ButtonV1>
+      <Button style={commonTestStyles.vmargin}>Default</Button>
+      <Button disabled style={commonTestStyles.vmargin}>
+        Default Disabled
+      </Button>
+      <Button appearance="primary" style={commonTestStyles.vmargin}>
+        Primary
+      </Button>
+      <Button disabled appearance="primary" style={commonTestStyles.vmargin}>
+        Primary Disabled
+      </Button>
+      <Button appearance="subtle" style={commonTestStyles.vmargin}>
+        Subtle
+      </Button>
+      <Button loading>Loading Button</Button>
+      <Button block style={commonTestStyles.vmargin}>
+        Block
+      </Button>
+      <Button appearance="primary" block style={commonTestStyles.vmargin}>
+        Block Primary
+      </Button>
+      <Button appearance="subtle" block style={commonTestStyles.vmargin}>
+        Block Subtle
+      </Button>
+      <CompoundButton secondaryContent="Compound" style={commonTestStyles.vmargin}>
+        Default
+      </CompoundButton>
+      <CompoundButton appearance="primary" secondaryContent="Compound" style={commonTestStyles.vmargin}>
+        Primary
+      </CompoundButton>
+      <CompoundButton appearance="subtle" secondaryContent="Compound" style={commonTestStyles.vmargin}>
+        Subtle
+      </CompoundButton>
+      <FAB icon={iconProps} style={commonTestStyles.vmargin}>
+        FAB
+      </FAB>
     </View>
   );
 };
