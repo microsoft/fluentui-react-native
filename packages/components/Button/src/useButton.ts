0d5d6bf4724dc45e3be73cf840bc1b83b26082d0
Ruriko Araki <ruaraki@microsoft.com>
2023-03-14 09:30:31 -0700

    Fix customizeable accessible

--- packages/components/Button/src/useButton.ts
+++ packages/components/Button/src/useButton.ts
@@ -6,7 +6,17 @@ import type { ButtonProps, ButtonInfo } from './Button.types';
 
 export const useButton = (props: ButtonProps): ButtonInfo => {
   const defaultComponentRef = React.useRef(null);
-  const { onClick, accessibilityRole, componentRef = defaultComponentRef, disabled, loading, enableFocusRing, focusable, ...rest } = props;
+  const {
+    onClick,
+    accessible,
+    accessibilityRole,
+    componentRef = defaultComponentRef,
+    disabled,
+    loading,
+    enableFocusRing,
+    focusable,
+    ...rest
+  } = props;
   const isDisabled = !!disabled || !!loading;
   // GH #1336: Set focusRef to null if button is disabled to prevent getting keyboard focus.
   const focusRef = isDisabled ? null : componentRef;
@@ -25,7 +35,7 @@ export const useButton = (props: ButtonProps): ButtonInfo => {
        * Therefore, let's only pass it in if it's defined to limit this issue.
        */
       ...(isDisabled && { disabled: isDisabled }),
-      accessible: true,
+      accessible: accessible ?? true,
       accessibilityRole: accessibilityRole || 'button',
       onAccessibilityTap: props.onAccessibilityTap || (!hasTogglePattern ? props.onClick : undefined),
       accessibilityLabel: props.accessibilityLabel,
