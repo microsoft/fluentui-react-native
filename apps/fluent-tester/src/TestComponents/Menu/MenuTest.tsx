18b7eafb57c6ddb8b6d441682d79eef665c989d5
Rohan Prasad <rohanpd.work@gmail.com>
2023-04-17 11:52:00 +0530

    add blackbox example and remove unused tokens

--- apps/fluent-tester/src/TestComponents/Menu/MenuTest.tsx
+++ apps/fluent-tester/src/TestComponents/Menu/MenuTest.tsx
@@ -14,6 +14,7 @@ import {
   MenuDivider,
 } from '@fluentui-react-native/menu';
 import { Stack } from '@fluentui-react-native/stack';
+import { Switch } from '@fluentui-react-native/switch';
 import { TextV1 as Text } from '@fluentui-react-native/text';
 
 import { E2EMenuTest } from './E2EMenuTest';
@@ -315,6 +316,39 @@ const MenuWithCustomMenuTrigger: React.FunctionComponent = () => {
   );
 };
 
+const menuAsABlackboxStyles = StyleSheet.create({ actionButoon: { alignSelf: 'center', justifyContent: 'center' } });
+
+const MenuAsABlackbox: React.FunctionComponent = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [dnd, setDnD] = React.useState<boolean>(false);
+
+  const onOpenChange = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Stack style={stackStyle}>
+      <Menu onOpenChange={onOpenChange} open={open}>
+        <MenuTrigger>
+          <Button onClick={() => setOpen(!open)}>Blockbox Menu</Button>
+        </MenuTrigger>
+
+        <MenuPopover minWidth={200}>
+          <MenuList>
+            <Switch checked={dnd} onChange={() => setDnD(!dnd)} style={{ marginHorizontal: 10 }} label="Don't disturb" />
+            <MenuItem>{dnd ? 'DND is on' : 'DND is off'}</MenuItem>
+            <View style={menuAsABlackboxStyles.actionButoon}>
+              <Button appearance="subtle" onClick={onOpenChange}>
+                Action
+              </Button>
+            </View>
+          </MenuList>
+        </MenuPopover>
+      </Menu>
+    </Stack>
+  );
+};
+
 const menuSections: TestSection[] = [
   {
     name: 'Menu Default',
@@ -362,6 +396,10 @@ const menuSections: TestSection[] = [
     name: 'Menu with ScrollView',
     component: MenuScrollView,
   },
+  Platform.select({
+    android: { name: 'Menu as blackbox', component: MenuAsABlackbox },
+    default: null,
+  }),
   {
     name: 'Menu Trigger onClick Override',
     component: MenuTriggerOnClickCallback,
