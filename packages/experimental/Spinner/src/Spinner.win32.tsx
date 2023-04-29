cddb8eb9dcb4c047c658f6133ecb8c59770babcf
Lena Hong <lehon@microsoft.com>
2023-04-28 18:47:38 -0700

    add spinner tail svg without native animation

--- packages/experimental/Spinner/src/Spinner.win32.tsx
+++ packages/experimental/Spinner/src/Spinner.win32.tsx
@@ -8,23 +8,22 @@ import { TextV1 as Text } from '@fluentui-react-native/text';
 import { Path, Svg } from 'react-native-svg';
 import type { SvgProps } from 'react-native-svg';
 
-import { RCTNativeAnimatedSpinner } from './consts.win32';
+// import { RCTNativeAnimatedSpinner } from './consts.win32';
 import { stylingSettings } from './Spinner.styling.win32';
 import { spinnerName } from './Spinner.types';
 import type { SpinnerProps, SpinnerType, SpinnerSvgProps } from './Spinner.types.win32';
 import { diameterSizeMap, lineThicknessSizeMap, getDefaultSize } from './SpinnerTokens.win32';
 import { useSpinner } from './useSpinner';
-// TODO: getTailPath, tailSvg
 
 const getTrackPath = (diameter: number, width: number, color: ColorValue) => {
   const start = {
     x: width / 2,
-    y: diameter / 2 + width / 2,
+    y: diameter / 2,
   };
-
-  const path = `M${start.x} ${start.y} a${diameter / 2} ${diameter / 2} 0 1 0 ${diameter} 0 a${diameter / 2} ${
-    diameter / 2
-  } 0 1 0 -${diameter} 0}`;
+  const innerRadius = diameter / 2 - width / 2;
+  const path = `M${start.x} ${start.y} a${innerRadius} ${innerRadius} 0 1 0 ${innerRadius * 2} 0 a${innerRadius} ${innerRadius} 0 1 0 -${
+    innerRadius * 2
+  } 0}`;
   return <Path d={path} stroke={color} strokeWidth={width} strokeLinecap="round" fillOpacity={0} />;
 };
 
@@ -37,7 +36,31 @@ const trackSvg: React.FunctionComponent<SpinnerSvgProps> = (props: SpinnerSvgPro
       width: diameterSizeMap[size],
     },
   };
-  const path = getTrackPath(diameterSizeMap[size] - lineThicknessSizeMap[size], lineThicknessSizeMap[size], trackColor);
+  const path = getTrackPath(diameterSizeMap[size], lineThicknessSizeMap[size], trackColor);
+
+  return <Svg {...svgProps}>{path}</Svg>;
+};
+
+const getTailPath = (diameter: number, width: number, color: ColorValue) => {
+  const start = {
+    x: diameter - width / 2,
+    y: diameter / 2,
+  };
+  const innerRadius = diameter / 2 - width / 2;
+  const path = `M${start.x} ${start.y} a${innerRadius} ${innerRadius} 0 0 1 -${innerRadius} ${innerRadius}`;
+  return <Path d={path} stroke={color} strokeWidth={width} strokeLinecap="round" fillOpacity={0} />;
+};
+
+/* Track is a full circle with a transparent fill */
+const tailSvg: React.FunctionComponent<SpinnerSvgProps> = (props: SpinnerSvgProps) => {
+  const { size, tailColor } = props;
+  const svgProps: SvgProps = {
+    style: {
+      height: diameterSizeMap[size],
+      width: diameterSizeMap[size],
+    },
+  };
+  const path = getTailPath(diameterSizeMap[size], lineThicknessSizeMap[size], tailColor);
 
   return <Svg {...svgProps}>{path}</Svg>;
 };
@@ -51,14 +74,26 @@ export const spinnerLookup = (layer: string, userProps: SpinnerProps): boolean =
   );
 };
 
+const spinnerTailContainer: React.FunctionComponent<SpinnerProps> = (props: SpinnerProps) => {
+  const { size } = props;
+  /* TODO: Add back in when native animated spinner is ready
+   *return <RCTNativeAnimatedSpinner {...{ ...props, style: { position: 'absolute', height: diameterSizeMap[size], width: diameterSizeMap[size], overflow: 'hidden' } }} />;
+   */
+  return (
+    <View
+      {...{ ...props, style: { position: 'absolute', height: diameterSizeMap[size], width: diameterSizeMap[size], overflow: 'hidden' } }}
+    />
+  );
+};
+
 export const Spinner = compose<SpinnerType>({
   displayName: spinnerName,
   ...stylingSettings,
   slots: {
     root: View,
     track: trackSvg,
-    tail: Svg,
-    tailContainer: RCTNativeAnimatedSpinner,
+    tail: tailSvg,
+    tailContainer: spinnerTailContainer,
     label: Text,
   },
   useRender: (props: SpinnerProps, useSlots: UseSlots<SpinnerType>) => {
@@ -70,6 +105,9 @@ export const Spinner = compose<SpinnerType>({
       return (
         <Slots.root {...mergedProps}>
           <Slots.track />
+          <Slots.tailContainer>
+            <Slots.tail />
+          </Slots.tailContainer>
         </Slots.root>
       );
     };
