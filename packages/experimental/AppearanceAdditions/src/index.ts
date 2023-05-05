b88e0da456905842f32f2b832afb057063560f37
lyzhan7 <78454019+lyzhan7@users.noreply.github.com>
2023-05-04 19:10:31 -0700

    Rename getSizeClass file to useAppearanceAdditions

--- packages/experimental/AppearanceAdditions/src/index.ts
+++ packages/experimental/AppearanceAdditions/src/index.ts
@@ -2,4 +2,4 @@ export { NativeAppearanceAdditions } from './NativeAppearanceAdditions';
 export { appearanceAdditions } from './appearanceAdditions';
 export type { AppearanceAdditions, SizeClass as SizeClassIOS } from './NativeAppearanceAdditions.types';
 
-export { useHorizontalSizeClass, useAccessibilityContrastOption, useUserInterfaceLevel } from './getSizeClass';
+export { useHorizontalSizeClass, useAccessibilityContrastOption, useUserInterfaceLevel } from './useAppearanceAdditions';
