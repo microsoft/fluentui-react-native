5003996936f3e4be08e06894281088849a1102a3
Rohan Prasad <rohanpd.work@gmail.com>
2023-05-09 00:40:55 +0530

    adding by drawer working without pan responder

---
+++ packages/experimental/DrawerV1/src/Drawer.tsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useRef } from 'react';
+import { Animated, Dimensions, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+
+const Drawer = ({ children, isVisible, onClose, position }) => {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (isVisible) {
+      Animated.spring(animatedValue, {
+        toValue: 1,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.spring(animatedValue, {
+        toValue: 0,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [animatedValue, isVisible]);
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleBackdropPress = () => {
+    onClose();
+  };
+
+  const animatedTranslateX = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: position === 'left' ? [-width * 0.8, 0] : position === 'right' ? [width * 0.8, 0] : [0, 0],
+  });
+
+  const animatedTranslateY = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: position === 'top' ? [-height * 0.5, 0] : position === 'bottom' ? [height * 0.5, 0] : [0, 0],
+  });
+
+  const animatedOpacity = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 0.5],
+  });
+
+  const animatedElevation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 5],
+  });
+
+  const animatedWidth = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: position === 'bottom' ? ['100%', '100%'] : position === 'left' || position === 'right' ? ['80%', '80%'] : ['100%', '100%'],
+  });
+
+  const animatedHeight = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: position === 'bottom' ? ['50%', '100%'] : ['100%', '100%'],
+  });
+
+  const animatedStyle = {
+    transform: position === 'left' || position === 'right' ? [{ translateX: animatedTranslateX }] : [{ translateY: animatedTranslateY }],
+    width: animatedWidth,
+    height: animatedHeight,
+    elevation: animatedElevation,
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <TouchableWithoutFeedback onPress={handleBackdropPress}>
+          <Animated.View style={[styles.backdrop, { opacity: animatedOpacity }]} />
+        </TouchableWithoutFeedback>
+      )}
+      <Animated.View style={[styles.container, styles[position], animatedStyle]}>
+        <View style={styles.dragger} />
+        {children}
+      </Animated.View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    zIndex: 1000,
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    borderRadius: 10,
+  },
+  left: {
+    left: 0,
+  },
+  right: {
+    right: 0,
+  },
+  top: {
+    top: 0,
+  },
+  bottom: {
+    bottom: 0,
+  },
+  dragger: {
+    width: 40,
+    height: 5,
+    borderRadius: 10,
+    backgroundColor: '#CCC',
+    alignSelf: 'center',
+    marginVertical: 10,
+  },
+  backdrop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 999,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#000',
+  },
+});
+
+export default Drawer;
--- packages/experimental/DrawerV1/src/DrawerV1.tsx
+++ packages/experimental/DrawerV1/src/DrawerV1.tsx
@@ -1,10 +1,12 @@
 /** @jsx withSlots */
-import { ScrollView } from 'react-native';
+import { useState } from 'react';
+import { Button, Dimensions, ScrollView, StyleSheet, View } from 'react-native';
 
 import type { UseSlots } from '@fluentui-react-native/framework';
 import { compose, mergeProps, withSlots } from '@fluentui-react-native/framework';
 import { Text } from '@fluentui-react-native/text';
 
+import Drawer from './Drawer';
 import { stylingSettings } from './DrawerV1.styling';
 import type { DrawerV1Type, DrawerV1Props } from './DrawerV1.types';
 import { DrawerV1Name } from './DrawerV1.types';
@@ -20,14 +22,60 @@ export const DrawerV1 = compose<DrawerV1Type>({
     const DrawerV1 = useDrawerV1(userProps);
     const Slots = useSlots(userProps);
 
+    const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+    const [drawerPosition, setDrawerPosition] = useState('left');
+
+    const handleDrawerClose = () => {
+      setIsDrawerVisible(false);
+    };
+
+    const handleOpenDrawer = (position) => {
+      setDrawerPosition(position);
+      setIsDrawerVisible(true);
+    };
     return (final: DrawerV1Props) => {
       const { ...mergedProps } = mergeProps(DrawerV1.props, final);
 
       return (
         <Slots.root {...mergedProps}>
-          <Text>Hello from Drawer</Text>
+          <View style={styles.container}>
+            <View style={styles.content}>
+              <Text style={styles.text}>This is the main content of the screen.</Text>
+              <Button title="Open Drawer (Left)" onPress={() => handleOpenDrawer('left')} />
+              <Button title="Open Drawer (Right)" onPress={() => handleOpenDrawer('right')} />
+              <Button title="Open Drawer (Bottom)" onPress={() => handleOpenDrawer('bottom')} />
+            </View>
+            <Drawer isVisible={isDrawerVisible} onClose={handleDrawerClose} position={drawerPosition}>
+              <View style={styles.drawerContent}>
+                <Text style={styles.text}>This is the content of the drawer.</Text>
+                <Button title="Close Drawer" onPress={handleDrawerClose} />
+              </View>
+            </Drawer>
+          </View>
         </Slots.root>
       );
     };
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  drawerContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
