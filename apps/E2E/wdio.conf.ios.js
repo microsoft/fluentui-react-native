09b0824cd67fc5e909c8bc7898dba9b46bab7fa2
Saad Najmi <sanajmi@microsoft.com>
2023-06-02 12:52:15 -0700

    Use iOS 16.2

--- apps/E2E/wdio.conf.ios.js
+++ apps/E2E/wdio.conf.ios.js
@@ -14,8 +14,8 @@ exports.config = {
     {
       maxInstances: 1, // Maximum number of total parallel running workers.
       platformName: 'iOS',
-      'appium:platformVersion': '16.4',
-      'appium:deviceName': 'iPhone 14 Pro',
+      'appium:platformVersion': '16.2',
+      'appium:deviceName': 'iPhone 14',
       'appium:automationName': 'XCUITest',
       'appium:bundleId': 'com.microsoft.ReactTestApp',
     },
