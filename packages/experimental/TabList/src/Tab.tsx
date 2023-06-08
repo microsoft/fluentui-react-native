58e60f6d412ce4bb7e5e80411d9c3ef66c36d2cb
Lawrence Win <winlarry@microsoft.com>
2023-06-08 16:43:41 -0700

    More fixes

--- packages/experimental/TabList/src/Tab.tsx
+++ packages/experimental/TabList/src/Tab.tsx
@@ -1,10 +1,10 @@
+/** @jsxRuntime classic */
 /** @jsx withSlots */
-import * as React from 'react';
 import { Pressable, View } from 'react-native';
 
 import type { UseSlots } from '@fluentui-react-native/framework';
-import { compose, mergeProps, withSlots } from '@fluentui-react-native/framework';
-import { Icon, createIconProps } from '@fluentui-react-native/icon';
+import { compose, withSlots } from '@fluentui-react-native/framework';
+import { Icon } from '@fluentui-react-native/icon';
 import { TextV1 as Text } from '@fluentui-react-native/text';
 
 import { stylingSettings } from './Tab.styling';
@@ -25,35 +25,12 @@ export const Tab = compose<TabType>({
   useRender: (userProps: TabProps, useSlots: UseSlots<TabType>) => {
     const tabsItem = useTab(userProps);
 
-    const iconProps = createIconProps(userProps.icon);
-
     // Grab the styled slots.
     const Slots = useSlots(userProps, (layer) => tabsItem.state[layer] || userProps[layer]);
 
     // Return the handler to finish render.
-    return (final: TabProps, ...children: React.ReactNode[]) => {
-      if (!tabsItem.state) {
-        return null;
-      }
-
-      const { icon, itemKey, itemCount, headerText, ...mergedProps } = mergeProps(tabsItem.props, final);
-
-      let containerText: string | null = null;
-      React.Children.forEach(children, (child) => {
-        if (typeof child === 'string') {
-          containerText = child;
-        }
-      });
-
-      return (
-        <Slots.root {...mergedProps}>
-          <Slots.stack>
-            {icon && <Slots.icon {...iconProps} />}
-            <Slots.content key="content">{containerText}</Slots.content>
-          </Slots.stack>
-          <Slots.indicator />
-        </Slots.root>
-      );
+    return () => {
+      return <Slots.root></Slots.root>;
     };
   },
 });
