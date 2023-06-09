464e93945e2fc7cff5cb7f8257dea371322e9d01
Saad Najmi <sanajmi@microsoft.com>
2023-06-09 07:41:48 -0700

    Update useMenuPopover.ts

--- packages/components/Menu/src/MenuPopover/useMenuPopover.ts
+++ packages/components/Menu/src/MenuPopover/useMenuPopover.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { I18nManager, Platform } from 'react-native';
+import { I18nManager } from 'react-native';
 
 import type { DirectionalHint, DismissBehaviors } from '@fluentui-react-native/callout';
 
