d03cf8004555adafeea32f25c59aebd5a4c5633a
Ayush Singh <ayushsinghs@yahoo.in>
2023-06-12 14:16:16 +0530

    Remove extra export

--- packages/components/Tabs/src/TabsItem.settings.windows.ts
+++ packages/components/Tabs/src/TabsItem.settings.windows.ts
@@ -4,8 +4,6 @@ import type { IComposeSettings } from '@uifabricshared/foundation-compose';
 import type { TabsItemType } from './TabsItem.types';
 import { tabsItemName } from './TabsItem.types';
 
-export const tabsItemSelectActionLabel = 'Select a TabsItem';
-
 export const settings: IComposeSettings<TabsItemType> = [
   {
     tokens: {
