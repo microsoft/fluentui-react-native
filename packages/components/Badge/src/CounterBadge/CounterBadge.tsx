24f85a893e81204d910ed1275368e0ebbe321236
Ayush Singh <ayushsinghs@yahoo.in>
2023-06-13 12:37:20 +0530

    Counter badge changes for Android

--- packages/components/Badge/src/CounterBadge/CounterBadge.tsx
+++ packages/components/Badge/src/CounterBadge/CounterBadge.tsx
@@ -2,7 +2,7 @@
 /** @jsx withSlots */
 import type { ReactNode } from 'react';
 import React, { Children } from 'react';
-import { View } from 'react-native';
+import { View, I18nManager } from 'react-native';
 
 import { Shadow } from '@fluentui-react-native/experimental-shadow';
 import type { UseSlots } from '@fluentui-react-native/framework';
@@ -14,7 +14,20 @@ import { stylingSettings } from './CounterBadge.styling';
 import type { CounterBadgeType, CounterBadgeProps } from './CounterBadge.types';
 import { counterBadgeName } from './CounterBadge.types';
 import { useCounterBadge } from './useCounterBadge';
-import { badgeLookup } from '../Badge';
+
+export const counterBadgeLookup = (layer: string, userProps: CounterBadgeProps): boolean => {
+  return (
+    userProps[layer] ||
+    layer === userProps['appearance'] ||
+    layer === userProps['size'] ||
+    (layer === 'large' && !userProps['size']) ||
+    layer === userProps['shape'] ||
+    (layer === 'circular' && !userProps['shape']) ||
+    layer === userProps['badgeColor'] ||
+    (layer === 'rtl' && I18nManager.isRTL) ||
+    (layer === 'list' && userProps.list)
+  );
+};
 
 export const CounterBadge = compose<CounterBadgeType>({
   displayName: counterBadgeName,
@@ -28,8 +41,7 @@ export const CounterBadge = compose<CounterBadgeType>({
   useRender: (userProps: CounterBadgeProps, useSlots: UseSlots<CounterBadgeType>) => {
     const iconProps = createIconProps(userProps.icon);
     const badge = useCounterBadge(userProps);
-
-    const Slots = useSlots(badge.props, (layer) => badgeLookup(layer, badge.props));
+    const Slots = useSlots(badge.props, (layer) => counterBadgeLookup(layer, badge.props));
 
     return (final: CounterBadgeProps, ...children: ReactNode[]) => {
       const { count, icon, iconPosition = 'before', overflowCount, dot, ...mergedProps } = mergeProps(badge.props, final);
@@ -37,7 +49,7 @@ export const CounterBadge = compose<CounterBadgeType>({
       const displayCount = count && count > overflowCount ? `${overflowCount}+` : `${count}`;
       const hasChildren = Children.toArray(children)[0];
 
-      return showBadge ? (
+      return showBadge || hasChildren ? (
         <Slots.shadow>
           <Slots.root {...mergedProps}>
             {!dot && (
