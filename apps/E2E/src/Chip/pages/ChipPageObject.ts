41138e61350d3f5b5db3f3cff17254a3736d4ecf
Ayush Singh <ayushsinghs@yahoo.in>
2023-06-24 12:44:07 +0530

    Removing unused func from pageobj

--- apps/E2E/src/Chip/pages/ChipPageObject.ts
+++ apps/E2E/src/Chip/pages/ChipPageObject.ts
@@ -3,19 +3,10 @@ import { AndroidAttribute } from '../../common/consts';
 import { CHIP_TESTPAGE, CHIP_TEST_COMPONENT, CHIP_TEXT, HOMEPAGE_CHIP_BUTTON } from '../consts';
 
 class ChipPageObject extends BasePage {
-  async getPrimaryComponentAttribute(attribute: string): Promise<string> {
-    return await (await this._primaryComponent).getAttribute(attribute);
-  }
-
-  async getSecondaryComponentAttribute(attribute: string): Promise<string> {
-    return await (await this._secondaryComponent).getAttribute(attribute);
-  }
-
   /******************************************************************/
   /**************** UI Element Interaction Methods ******************/
   /******************************************************************/
 
-  //Android only
   async verifyTextContent(text: string): Promise<boolean> {
     const callbackText = await this._callbackText;
     return (await callbackText.getAttribute(AndroidAttribute.Text)) == text;
