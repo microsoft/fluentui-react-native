eecc18dd5aa21331a9a16c55c958227d7c05962e
Lena Hong <lehon@microsoft.com>
2023-06-30 19:28:00 -0700

    only set alias tokens from palette when paletteName is defined

--- packages/theming/win32-theme/src/createOfficeTheme.ts
+++ packages/theming/win32-theme/src/createOfficeTheme.ts
@@ -66,7 +66,7 @@ export function createOfficeTheme(options: ThemeOptions = {}): ThemeReference {
       }
 
       return {
-        colors: createAliasesFromPalette(theme.host.palette, ref.themeName === 'HighContrast'),
+        ...(paletteName !== undefined && { colors: createAliasesFromPalette(theme.host.palette, ref.themeName === 'HighContrast') }),
         typography: win32Typography(),
       };
     },
