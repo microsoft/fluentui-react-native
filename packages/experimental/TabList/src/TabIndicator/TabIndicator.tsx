519cc621d769db8fc233c288307d9329dd835286
Lawrence Win <winlarry@microsoft.com>
2023-07-24 10:58:09 -0700

    Pass props of TabIndicator correctly

--- packages/experimental/TabList/src/TabIndicator/TabIndicator.tsx
+++ packages/experimental/TabList/src/TabIndicator/TabIndicator.tsx
@@ -3,7 +3,7 @@
 import { View } from 'react-native';
 
 import type { UseSlots } from '@fluentui-react-native/framework';
-import { compose, withSlots } from '@fluentui-react-native/framework';
+import { compose, mergeProps, withSlots } from '@fluentui-react-native/framework';
 
 import { stylingSettings } from './TabIndicator.styling';
 import type { TabIndicatorProps, TabIndicatorType } from './TabIndicator.types';
@@ -20,8 +20,9 @@ export const TabIndicator = compose<TabIndicatorType>({
     const Slots = useSlots(userProps);
 
     return (final: TabIndicatorProps) => {
+      const mergedProps = mergeProps(userProps, final);
       return (
-        <Slots.root {...final}>
+        <Slots.root {...mergedProps}>
           <Slots.indicator />
         </Slots.root>
       );
