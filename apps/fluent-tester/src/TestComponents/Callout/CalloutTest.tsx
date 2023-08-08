7337161f466e5e90076f6a053b7c9305f2f6b9c4
Saad Najmi <saadnajmi2@gmail.com>
2023-08-07 23:22:26 -0700

    one more line

--- apps/fluent-tester/src/TestComponents/Callout/CalloutTest.tsx
+++ apps/fluent-tester/src/TestComponents/Callout/CalloutTest.tsx
@@ -5,7 +5,6 @@ import { Text, View, Switch, ScrollView } from 'react-native';
 import { Button, Callout, Separator, Pressable, StealthButton } from '@fluentui/react-native';
 import type { IFocusable, RestoreFocusEvent, DismissBehaviors, ICalloutProps } from '@fluentui/react-native';
 import type { VisualEffectMaterial } from '@fluentui-react-native/callout';
-import { useTheme } from '@fluentui-react-native/framework';
 
 import { E2ECalloutTest } from './CalloutE2ETest';
 import { CALLOUT_TESTPAGE } from '../../../../E2E/src/Callout/consts';
