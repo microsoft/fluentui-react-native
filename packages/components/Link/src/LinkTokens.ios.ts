246b61693260867223d17ae6793af100a329e92e
Monica Rivera <44449640+mrivera-ms@users.noreply.github.com>
2023-08-22 12:12:04 -0700

    Add LinkTokens.ios.ts (#3053)
    * Add LinkTokens.ios.ts
    
    * Change files

---
+++ packages/components/Link/src/LinkTokens.ios.ts
@@ -0,0 +1,20 @@
+import type { Theme } from '@fluentui-react-native/framework';
+import type { TokenSettings } from '@fluentui-react-native/use-styling';
+
+import type { LinkTokens } from './Link.types';
+
+export const defaultLinkTokens: TokenSettings<LinkTokens, Theme> = (t: Theme) =>
+  ({
+    color: t.colors.brandForeground1,
+    alignSelf: 'flex-start',
+    inline: {
+      textDecorationLine: 'underline',
+    },
+    disabled: {
+      color: t.colors.brandForeground1Disabled,
+      textDecorationLine: 'none',
+    },
+    pressed: {
+      color: t.colors.brandForeground1Pressed,
+    },
+  } as LinkTokens);
