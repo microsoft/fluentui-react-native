e365039fb9ecff35d8e6d1a3a5b5851352b57e41
Samuel Freiberg <safreibe@microsoft.com>
2023-09-13 16:16:11 -0700

    Improving checkbox logic

--- apps/E2E/src/CheckboxV1/pages/CheckboxV1PageObject.ts
+++ apps/E2E/src/CheckboxV1/pages/CheckboxV1PageObject.ts
@@ -17,11 +17,8 @@ class CheckboxV1PageObject extends BasePage {
 
   /* Waits for the checkbox to be checked or unchecked if the new state is true or false. Returns true if the
    * checkbox toggled to the new state. */
-  async waitForCheckboxToggle(newState: boolean, errorMessage: string): Promise<boolean> {
-    if ((await this.isCheckboxChecked()) !== newState) {
-      await this.waitForCondition(async () => (await this.isCheckboxChecked()) === newState, errorMessage);
-    }
-    return (await this.isCheckboxChecked()) === newState;
+  async waitForCheckboxToggle(newState: boolean, errorMessage: string): Promise<boolean | void> {
+    return await this.waitForCondition(async () => (await this.isCheckboxChecked()) === newState, errorMessage);
   }
 
   /* Waits for the checkbox to be checked or unchecked if the new state is true or false. Returns true if the
@@ -51,10 +48,9 @@ class CheckboxV1PageObject extends BasePage {
   /* Unlike snapshot testing, we cannot spy on functions to determine if they get called or not. In order to determine if
    * the onChange() callback gets fired, we show / hide the a Text label as the callback gets fired. This way, we know that
    * the onChange() callback has fired by checking that the label element is currently displayed. */
-  async didOnChangeCallbackFire(errorMsg: string): Promise<boolean> {
+  async didOnChangeCallbackFire(errorMsg: string): Promise<boolean | void> {
     const callbackText = await this._callbackText;
-    await this.waitForCondition(async () => await callbackText.isDisplayed(), errorMsg);
-    return await callbackText.isDisplayed();
+    return await this.waitForCondition(async () => await callbackText.isDisplayed(), errorMsg);
   }
 
   /*****************************************/
