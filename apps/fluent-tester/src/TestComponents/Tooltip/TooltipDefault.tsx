85e28f22495b96d98bd0d40f504d1ea28b92a1aa
REDMOND\gulnazsayed <gulnazsayed@microsoft.com>
2023-09-26 09:56:38 -0700

    added more tooltip enabled components in test file

--- apps/fluent-tester/src/TestComponents/Tooltip/TooltipDefault.tsx
+++ apps/fluent-tester/src/TestComponents/Tooltip/TooltipDefault.tsx
@@ -4,10 +4,33 @@ import { View } from 'react-native';
 import { Button } from '@fluentui-react-native/experimental-button';
 
 export const TooltipDefault: React.FunctionComponent = () => {
+  // const [check, setCheck] = React.useState(false);
+
+  // const toggleCheck = React.useCallback(() => {
+  //   setCheck(!check);
+  // }, [setCheck]);
+
   return (
     <View>
+      {/* <Button onClick={toggleCheck}>Click</Button>
+      {check && (
+        <View>
+          <Tooltip content="Test">
+            <Button>Test</Button>
+          </Tooltip>
+        </View>
+      )} */}
       <Tooltip content="Test">
         <Button>Test</Button>
+      </Tooltip>
+      <Tooltip content="Test 2">
+        <Button>Test 2</Button>
+      </Tooltip>
+      <Tooltip content="Test 3">
+        <Button>Test 3</Button>
+      </Tooltip>
+      <Tooltip content="Test 4">
+        <Button>Test 4</Button>
       </Tooltip>
     </View>
   );
