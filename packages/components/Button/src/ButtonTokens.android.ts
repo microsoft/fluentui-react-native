64c7af679c98c8a6db5e3c0ebcfd3193c8dd5f7a
gremlin529 <56889386+gremlin529@users.noreply.github.com>
2023-10-24 11:30:15 -0700

    Fixing up button values for figma (Padding and sizes)

--- packages/components/Button/src/ButtonTokens.android.ts
+++ packages/components/Button/src/ButtonTokens.android.ts
@@ -34,7 +34,6 @@ export const defaultButtonTokens: TokenSettings<ButtonTokens, Theme> = () =>
     },
     large: {
       paddingHorizontal: globalTokens.size200,
-      paddingVertical: globalTokens.size120,
       borderRadius: globalTokens.corner.radius80,
       iconSize: 20,
       outline: {
@@ -44,11 +43,10 @@ export const defaultButtonTokens: TokenSettings<ButtonTokens, Theme> = () =>
       spacingIconContentBefore: globalTokens.size80,
       spacingIconContentAfter: globalTokens.size80,
       minHeight: 48,
-      minWidth: 48,
+      minWidth: 36,
     },
     medium: {
       paddingHorizontal: globalTokens.size120,
-      paddingVertical: globalTokens.size80,
       borderRadius: globalTokens.corner.radius40,
       iconSize: 20,
       outline: {
@@ -62,7 +60,6 @@ export const defaultButtonTokens: TokenSettings<ButtonTokens, Theme> = () =>
     },
     small: {
       paddingHorizontal: globalTokens.size80,
-      paddingVertical: globalTokens.size40,
       borderRadius: globalTokens.corner.radius40,
       iconSize: 16,
       outline: {
@@ -73,6 +70,9 @@ export const defaultButtonTokens: TokenSettings<ButtonTokens, Theme> = () =>
       spacingIconContentAfter: globalTokens.size40,
       minHeight: 28,
       minWidth: 28,
+    },
+    rounded: {
+      borderRadius: globalTokens.corner.radius40,
     },
     circular: {
       borderRadius: globalTokens.corner.radiusCircular,
