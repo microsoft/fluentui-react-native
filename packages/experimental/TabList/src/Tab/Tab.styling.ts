4475c4851d7d18ae063eebf7b54a69787718bd57
Lawrence Win <winlarry@microsoft.com>
2023-10-26 14:13:16 -0700

    Fix nit

--- packages/experimental/TabList/src/Tab/Tab.styling.ts
+++ packages/experimental/TabList/src/Tab/Tab.styling.ts
@@ -27,11 +27,11 @@ export const useTabSlotProps = (props: TabProps, tokens: TabTokens, theme: Theme
         justifyContent: 'center',
         padding: 1,
         backgroundColor: tokens.backgroundColor,
-        ...(!context.vertical ? { height: '100%' } : {}),
+        ...(!vertical ? { height: '100%' } : {}),
         ...borderStyles.from(tokens, theme),
       },
     }),
-    [tokens, theme, context.vertical],
+    [tokens, theme, vertical],
   );
 
   const contentContainer = React.useMemo<IViewProps>(
