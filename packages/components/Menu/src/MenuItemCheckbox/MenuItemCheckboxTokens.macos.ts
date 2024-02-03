2f346a09245e4fdea8ff76324e5307e02055ed66
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2024-02-02 17:59:28 -0800

    Use Correct checkmarkPadding for MenuItemCheckbox (#3407)
    * Use Correct checkmarkPadding for MenuItemCheckbox
    
    * Update tester/macOS pod file
    
    * Change files
    
    * Revert test color
    
    * Revert "Update tester/macOS pod file"
    
    This reverts commit 20ba101d0c9ee1ff5735c900f48186dfeb121807.

--- packages/components/Menu/src/MenuItemCheckbox/MenuItemCheckboxTokens.macos.ts
+++ packages/components/Menu/src/MenuItemCheckbox/MenuItemCheckboxTokens.macos.ts
@@ -7,7 +7,7 @@ import type { MenuItemCheckboxTokens } from './MenuItemCheckbox.types';
 export const defaultMenuItemCheckboxTokens: TokenSettings<MenuItemCheckboxTokens, Theme> = (t: Theme): MenuItemCheckboxTokens => ({
   backgroundColor: t.colors.transparentBackground,
   borderRadius: 5,
-  checkmarkPadding: globalTokens.sizeNone,
+  checkmarkPadding: globalTokens.size20,
   checkmarkSize: 16,
   checkmarkVisibility: 0,
   color: t.colors.neutralForeground2,
