43dde5da3c68516d6c10dabfcebde7ee11c43cf1
riosn-n <33673026+riosn-n@users.noreply.github.com>
2024-02-22 17:50:40 -0800

    Adding useControllableValue hook (#3434)
    * Adding useControllableValue hook.
    
    * Change files
    
    * Adopting useControllableValue in Input component.
    
    * Change files
    
    * Updating snapshot test.
    
    * Returning View instead of div.
    
    * Removing HTMLElement restriction.
    
    * Specifying callback type.
    
    * Using React.Fragment instead of View.
    
    * Removing DIVs from tests.

--- packages/utils/test-tools/src/index.ts
+++ packages/utils/test-tools/src/index.ts
@@ -1,3 +1,3 @@
-export { checkReRender, checkRenderConsistency, compareTrees, snapshotPropTree } from './enzymeTests';
+export { checkReRender, checkRenderConsistency, compareTrees, snapshotPropTree, validateHookValueNotChanged } from './enzymeTests';
 export type { JSXProducer, PropTreeFilter, PropTreeSnapshot } from './enzymeTests';
 export { mockTheme } from './mockTheme';
