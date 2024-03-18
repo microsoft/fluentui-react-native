ee8ff9d8c7e673c15ca5aae77b02a54a08989fd7
Saad Najmi <sanajmi@microsoft.com>
2024-03-18 10:31:35 -0700

    Update wdio.conf.ios.js

--- apps/E2E/wdio.conf.ios.js
+++ apps/E2E/wdio.conf.ios.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // Booting the iOS simulator os very slow, most likely due to slower CI hardware. 
 // Arbitrarily increase the timeout by a set multiplier
-const timeoutMultiplier = 5
+const timeoutMultiplier = 10
 const defaultWaitForTimeout = 20000 * timeoutMultiplier;
 const defaultConnectionRetryTimeout =  60000 * timeoutMultiplier;
 const jasmineDefaultTimeout = 60000 * timeoutMultiplier ; // 60 seconds for Jasmine test timeout
