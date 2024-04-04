afb9025d6617d6d0c07bad01488b97cb5a1928d3
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-04-04 10:07:52 -0700

    fix

--- packages/framework/immutable-merge/jest.config.js
+++ packages/framework/immutable-merge/jest.config.js
@@ -1,2 +1,8 @@
 const { configureJest } = require('@fluentui-react-native/scripts');
-module.exports = configureJest();
+module.exports = configureJest({
+  // Jest is picking up the hoisted version of lru-cache, which is incompatible from the version
+  // required by semver
+  moduleNameMapper: {
+    'lru-cache': require.resolve('lru-cache', {paths:[require.resolve('semver')]}),
+  },
+});
