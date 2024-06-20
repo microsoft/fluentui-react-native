de6e0c81f8e8bf4213650f77809ffd9f4369742c
Saad Najmi <sanajmi@microsoft.com>
2024-06-20 13:51:23 -0700

    fix(menu): Add High Contrast border on macOS

--- packages/components/Menu/src/MenuPopover/MenuPopoverTokens.macos.ts
+++ packages/components/Menu/src/MenuPopover/MenuPopoverTokens.macos.ts
@@ -4,10 +4,13 @@ import { buildUseTokens } from '@fluentui-react-native/framework';
 import type { MenuPopoverTokens } from './MenuPopover.types';
 import { menuPopoverName } from './MenuPopover.types';
 
+import { isHighContrast } from '@fluentui-react-native/theming-utils';
+
 export const useMenuPopoverTokens = buildUseTokens<MenuPopoverTokens>(
-  (_t: Theme) => ({
-    borderWidth: 0,
+  (t: Theme) => ({
+    borderWidth: 1,
     borderRadius: 5,
+    borderColor: isHighContrast() ? t.colors.neutralStroke1 : t.colors.transparentStroke,
   }),
   menuPopoverName,
 );
