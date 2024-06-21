4ebe8cf46f0ce827d2549cbd04f5267af2edd16f
Saad Najmi <sanajmi@microsoft.com>
2024-06-21 11:33:07 -0700

    fix lint

--- packages/components/Menu/src/MenuPopover/MenuPopoverTokens.macos.ts
+++ packages/components/Menu/src/MenuPopover/MenuPopoverTokens.macos.ts
@@ -1,10 +1,10 @@
 import type { Theme } from '@fluentui-react-native/framework';
 import { buildUseTokens } from '@fluentui-react-native/framework';
+import { isHighContrast } from '@fluentui-react-native/theming-utils';
 
 import type { MenuPopoverTokens } from './MenuPopover.types';
 import { menuPopoverName } from './MenuPopover.types';
 
-import { isHighContrast } from '@fluentui-react-native/theming-utils';
 
 export const useMenuPopoverTokens = buildUseTokens<MenuPopoverTokens>(
   (t: Theme) => ({
