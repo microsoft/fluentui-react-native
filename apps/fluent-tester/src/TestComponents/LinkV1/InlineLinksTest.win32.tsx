a8a6d9854f9fb1bb5739f67ac58752d26242f00b
Ruriko Araki <ruaraki@microsoft.com>
2024-07-12 13:10:02 -0700

    Remove win32 inline link test page fork

--- apps/fluent-tester/src/TestComponents/LinkV1/InlineLinksTest.win32.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import { TextV1 as Text } from '@fluentui/react-native';
-
-// Platform.select not available for win32
-// Forking test instead
-export const InlineLinks: React.FunctionComponent = () => {
-  return <Text>Inline links are not supported on win32.</Text>;
-};
