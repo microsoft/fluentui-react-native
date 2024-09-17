1a0e3d0d6150c5a23453d662e36bb230406d8319
Patrick Boyd <patboyd@microsoft.com>
2024-09-17 11:26:03 -0700

    Nearly-sensible version of focusWindow, blurWindow API

--- packages/components/Callout/src/Callout.tsx
+++ packages/components/Callout/src/Callout.tsx
@@ -8,14 +8,14 @@ import * as React from 'react';
 import { findNodeHandle, Platform } from 'react-native';
 import type { HostComponent } from 'react-native';
 
-import { useViewCommandFocus } from '@fluentui-react-native/interactive-hooks';
+import { setAndForwardRef } from '@fluentui-react-native/interactive-hooks';
 import { backgroundColorTokens, borderTokens } from '@fluentui-react-native/tokens';
 import type { IUseComposeStyling } from '@uifabricshared/foundation-compose';
 import { compose } from '@uifabricshared/foundation-compose';
 import { mergeSettings } from '@uifabricshared/foundation-settings';
 
 import { settings } from './Callout.settings';
-import type { ICalloutProps, ICalloutSlotProps, ICalloutType } from './Callout.types';
+import type { CalloutNativeMethods, ICalloutProps, ICalloutSlotProps, ICalloutType } from './Callout.types';
 import { calloutName } from './Callout.types';
 import type { NativeProps as CalloutNativeProps } from './CalloutNativeComponent';
 import CalloutNativeComponent from './CalloutNativeComponent';
@@ -27,11 +27,29 @@ const NativeCalloutView = Platform.select<HostComponent<MacOSCalloutNativeProps>
   default: CalloutNativeComponent, // win32
 });
 
+export function useWindowCommandFocus(
+  forwardedRef: React.Ref<CalloutNativeMethods | null> | undefined,
+  // initialValue?: React.Component
+): (ref: React.ElementRef<any>) => void {
+  /**
+   * Set up the forwarding ref to enable adding the focus method.
+   */
+  const focusRef = React.useRef<any>();
+
+  const _setNativeRef = setAndForwardRef({
+    getForwardedRef: () => forwardedRef,
+    setLocalRef: (localRef: any) => {
+      focusRef.current = localRef;
+    },
+  });
+  return _setNativeRef;
+}
+
 export const Callout = compose<ICalloutType>({
   displayName: calloutName,
   usePrepareProps: (props: ICalloutProps, useStyling: IUseComposeStyling<ICalloutType>) => {
     const { componentRef, target, ...rest } = props;
-    const calloutRef = useViewCommandFocus(componentRef);
+    const calloutRef = useWindowCommandFocus(componentRef);
     const [nativeTarget, setNativeTarget] = React.useState<number | string | null>(null);
 
     React.useLayoutEffect(() => {
