f645f4ec2302e613cc1e5823a9a5bd762e12e3d2
Saad Najmi <sanajmi@microsoft.com>
2024-10-08 15:01:19 -0500

    Exhaustive color pass

--- packages/components/Button/src/ButtonColorTokens.macos.ts
+++ packages/components/Button/src/ButtonColorTokens.macos.ts
@@ -8,65 +8,66 @@ import type { ButtonTokens } from './Button.types';
 // this discrepancy in the token packages, and then update these definitions as appropriate.
 
 // Github #X2: The macOS brand ramps from the token package use blue from Fluent V1, instead of communication blue.
-// Update the packages to use the newly defiened Fluent 2 brand ramp.
+// Update the packages to use the newly defined Fluent 2 brand ramp.
 
 export const defaultButtonColorTokens: TokenSettings<ButtonTokens, Theme> = (t: Theme) =>
   ({
     backgroundColor: t.colors.neutralBackground2,
     color: t.colors.neutralForeground2,
     borderColor: t.colors.neutralStroke2,
-    iconColor: t.colors.neutralForeground3,
+    iconColor: t.colors.neutralForeground2,
     disabled: {
       backgroundColor: t.colors.neutralBackground2,
       color: t.colors.neutralForegroundDisabled,
       borderColor: t.colors.neutralStrokeDisabled,
-      iconColor: t.colors.neutralForeground3,
+      iconColor: t.colors.neutralForegroundDisabled,
     },
     hovered: {
       backgroundColor: t.colors.neutralBackground2,
       color: t.colors.neutralForeground2,
       borderColor: t.colors.neutralStroke2,
-      iconColor: t.colors.neutralForeground3,
+      iconColor: t.colors.neutralForeground2,
     },
     pressed: {
       backgroundColor: t.colors.neutralBackground2Pressed,
       color: t.colors.neutralForeground2,
       borderColor: t.colors.neutralStroke2,
-      iconColor: t.colors.neutralForeground3,
+      iconColor: t.colors.neutralForeground2,
     },
     focused: {
       backgroundColor: t.colors.neutralBackground2,
       color: t.colors.neutralForeground2,
       borderColor: t.colors.neutralStroke2,
-      icon: t.colors.neutralForeground3,
+      icon: t.colors.neutralForeground2,
     },
     primary: {
       backgroundColor: t.colors.brandBackground,
       color: t.colors.neutralForegroundOnBrand,
-      borderColor: t.colors.transparentStroke,
+      borderColor: t.colors.brandStroke1,
       iconColor: t.colors.neutralForegroundOnBrand,
       disabled: {
         backgroundColor: t.colors.neutralBackgroundDisabled,
         color: t.colors.neutralForegroundDisabled,
-        borderColor: t.colors.transparentStroke,
+        borderColor: t.colors.neutralStrokeDisabled,
         iconColor: t.colors.neutralForegroundDisabled,
       },
       hovered: {
+        // Update to brandBackgroundHover, once the alias token matches brandBackground
         backgroundColor: t.colors.brandBackground,
-        color: t.colors.neutralForegroundOnBrand,
-        borderColor: t.colors.transparentStroke,
-        iconColor: t.colors.neutralForegroundOnBrand,
+        color: t.colors.neutralForegroundOnBrandHover,
+        borderColor: t.colors.brandBackground,
+        iconColor: t.colors.neutralForegroundOnBrandHover,
       },
       pressed: {
         backgroundColor: t.colors.brandBackgroundPressed,
         color: t.colors.neutralForegroundOnBrandPressed,
-        borderColor: t.colors.transparentStroke,
+        borderColor: t.colors.brandBackgroundPressed,
         iconColor: t.colors.neutralForegroundOnBrandPressed,
       },
       focused: {
         backgroundColor: t.colors.brandBackground,
         color: t.colors.neutralForegroundOnBrand,
-        borderColor: t.colors.transparentStroke,
+        borderColor: t.colors.brandBackground,
         iconColor: t.colors.neutralForegroundOnBrand,
       },
     },
@@ -76,28 +77,28 @@ export const defaultButtonColorTokens: TokenSettings<ButtonTokens, Theme> = (t: 
       borderColor: t.colors.transparentStroke,
       iconColor: t.colors.brandForeground1,
       disabled: {
+        backgroundColor: t.colors.subtleBackground,
         color: t.colors.brandForeground1Disabled,
         borderColor: t.colors.transparentStrokeDisabled,
-        backgroundColor: t.colors.ghostDisabledBackground,
-        iconColor: t.colors.ghostDisabledIcon,
+        iconColor: t.colors.brandForeground1Disabled,
       },
       hovered: {
         backgroundColor: t.colors.subtleBackgroundHover,
         color: t.colors.brandForeground1Hover,
-        borderColor: t.colors.transparentStroke,
-        iconColor: t.colors.brandForeground1,
+        borderColor: t.colors.subtleBackgroundHover,
+        iconColor: t.colors.brandForeground1Hover,
       },
       pressed: {
         backgroundColor: t.colors.subtleBackgroundPressed,
-        borderColor: t.colors.transparentStroke,
         color: t.colors.brandForeground1Pressed,
-        icon: t.colors.brandForeground1Pressed,
+        borderColor: t.colors.subtleBackgroundPressed,
+        iconColor: t.colors.brandForeground1Pressed,
       },
       focused: {
-        borderColor: t.colors.subtleBackground,
-        backgroundColor: t.colors.brandForeground1,
-        color: t.colors.transparentStroke,
-        icon: t.colors.brandForeground1,
+        backgroundColor: t.colors.subtleBackground,
+        color: t.colors.brandForeground1,
+        borderColor: t.colors.transparentStroke,
+        iconColor: t.colors.brandForeground1,
       },
     },
   } as ButtonTokens);
