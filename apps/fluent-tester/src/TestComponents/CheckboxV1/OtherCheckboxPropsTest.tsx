0ffa289481c62d91969be2a5c10da7883350e409
Ruriko Araki <ruaraki@microsoft.com>
2025-01-14 10:33:08 -0800

    Add new Checkbox test

--- apps/fluent-tester/src/TestComponents/CheckboxV1/OtherCheckboxPropsTest.tsx
+++ apps/fluent-tester/src/TestComponents/CheckboxV1/OtherCheckboxPropsTest.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Platform, View } from 'react-native';
+import { Platform, Pressable, View, type ViewStyle } from 'react-native';
 
 import { ButtonV1 as Button } from '@fluentui-react-native/button';
 import { Checkbox } from '@fluentui-react-native/experimental-checkbox';
+import { TextV1 } from '@fluentui-react-native/text';
 
 import { mobileStyles } from '../Common/styles';
+
+const pressableStyle: ViewStyle = { borderWidth: 1, padding: 4, gap: 4, flexDirection: 'row', alignItems: 'center' };
 
 export const OtherCheckbox: React.FunctionComponent = () => {
   const [isChecked, setisChecked] = React.useState(false);
@@ -17,24 +20,45 @@ export const OtherCheckbox: React.FunctionComponent = () => {
     setisChecked(false);
   }, []);
 
-  const memoizedStyles = React.useMemo(() => (Platform.OS === 'android' ? { ...mobileStyles.containerSpacedEvenly, height: 150 } : {}), []);
+  const onChange = React.useCallback((_e, checked: boolean) => {
+    console.log(checked);
+    setisChecked(checked);
+  }, []);
+
+  const onPress = React.useCallback(() => {
+    setisChecked(!isChecked);
+  }, [isChecked]);
+
+  const memoizedStyles = React.useMemo(
+    () => (Platform.OS === 'android' ? { ...mobileStyles.containerSpacedEvenly, height: 150 } : { gap: 8 }),
+    [],
+  );
 
   return (
     <View style={memoizedStyles}>
-      <Button onClick={setCheckedTrue} size="small">
-        Check controlled checkboxes below
-      </Button>
-      <Button onClick={setCheckedFalse} size="small">
-        Uncheck controlled checkboxes below
-      </Button>
+      <View>
+        <Button onClick={setCheckedTrue} size="small">
+          Check controlled checkboxes below
+        </Button>
+        <Button onClick={setCheckedFalse} size="small">
+          Uncheck controlled checkboxes below
+        </Button>
+      </View>
 
-      <Checkbox label="This is a controlled Checkbox" checked={isChecked} />
-      {Platform.OS !== 'android' && (
-        <>
-          <Checkbox label="Checkbox rendered with labelPosition 'before' (controlled)" labelPosition="before" checked={isChecked} />
-          <Checkbox label="A required checkbox with other required text" required="**" />
-        </>
-      )}
+      <View>
+        <Checkbox label="This is a controlled Checkbox" checked={isChecked} onChange={onChange} />
+        <Pressable style={pressableStyle} onPress={onPress}>
+          <Checkbox checked={isChecked} onChange={onChange} />
+          <TextV1>A controlled checkbox in a Pressable. Pressing Pressable also toggles Checkbox</TextV1>
+        </Pressable>
+
+        {Platform.OS !== 'android' && (
+          <>
+            <Checkbox label="Checkbox rendered with labelPosition 'before' (controlled)" labelPosition="before" checked={isChecked} />
+            <Checkbox label="A required checkbox with other required text" required="**" />
+          </>
+        )}
+      </View>
     </View>
   );
 };
