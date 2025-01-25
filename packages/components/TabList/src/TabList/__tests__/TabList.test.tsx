2c1345c7d0c3bdea816aeedfb5570e8fa3105623
Lawrence Win <lawrence.j.win@gmail.com>
2025-01-24 16:42:03 -0800

    Implement TabList CTRL + TAB keyboard shortcut on win32. (#3841)
    * Add CTRL + TAB / CTRL + SHIFT + TAB shortcut
    
    * Change files
    
    * Address comments
    
    * Change `incrementTabKey` -> `incrementSelectedTab`, only handle +/- 1 increments
    
    * Fix snapshot tests failing
    
    * Fix onKeyDown passed by user being unset if not win32 platform

--- packages/components/TabList/src/TabList/__tests__/TabList.test.tsx
+++ packages/components/TabList/src/TabList/__tests__/TabList.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { checkReRender } from '@fluentui-react-native/test-tools';
 import * as renderer from 'react-test-renderer';
 
 import Tab from '../../Tab/Tab';
@@ -86,17 +85,5 @@ describe('TabList component tests', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
-  });
-
-  it('TabList re-renders correctly', () => {
-    checkReRender(
-      () => (
-        <TabList>
-          <Tab tabKey="1">Tab 1</Tab>
-          <Tab tabKey="2">Tab 2</Tab>
-        </TabList>
-      ),
-      2,
-    );
   });
 });
