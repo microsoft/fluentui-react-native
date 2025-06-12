7c4322db4ff21666e4d8202f1aab69e05e1fd500
Gulnaz Sayed <30990970+gulnazsayed@users.noreply.github.com>
2025-06-12 17:08:19 -0400

    Remove Caterpillar Custom Theme (#3903)
    * remove caterpillar theme
    
    * Change files

--- apps/fluent-tester/src/theme/applyTheme.ts
+++ apps/fluent-tester/src/theme/applyTheme.ts
@@ -1,63 +1,20 @@
-import type { PartialTheme, Theme, ThemeOptions } from '@fluentui-react-native/framework';
+import type { PartialTheme, ThemeOptions } from '@fluentui-react-native/framework';
 import { createOfficeTheme, getThemingModule } from '@fluentui-react-native/win32-theme';
 
-export type ThemeNames = 'Default' | 'Office' | 'Caterpillar' | 'Apple';
+export type ThemeNames = 'Default' | 'Office' | 'Apple';
 
 export const themeChoices = [
   { label: 'Default', value: 'Default' },
   { label: 'Office', value: 'Office' },
-  { label: 'Caterpillar', value: 'Caterpillar' },
 ];
-
-function applyCaterpillarTheme(parent: Theme): PartialTheme {
-  return parent.host?.appearance === 'dark'
-    ? {
-        colors: {
-          buttonBackground: '#111111',
-          buttonBackgroundHovered: '#ffcd11',
-          buttonBackgroundPressed: '#eeeeee',
-          buttonText: '#ffffff',
-          buttonTextPressed: '#111111',
-          buttonTextHovered: '#000000',
-        },
-        components: {
-          Button: {
-            borderWidth: 0,
-            tokens: {
-              borderWidth: 0,
-            },
-          },
-        },
-      }
-    : {
-        colors: {
-          buttonBackground: '#ffcd11',
-          buttonBackgroundHovered: '#111111',
-          buttonBackgroundPressed: '#eeeeee',
-          buttonText: '#000000',
-          buttonTextPressed: '#111111',
-          buttonTextHovered: '#ffffff',
-        },
-        components: {
-          Button: {
-            borderWidth: 0,
-            tokens: {
-              borderWidth: 0,
-            },
-          },
-        },
-      };
-}
 
 const themingModule = getThemingModule()[0];
 
 /** apply the currently active theme layering */
-export function applyTheme(parent: Theme, name: ThemeNames, appearance: ThemeOptions['appearance']): PartialTheme {
+export function applyTheme(name: ThemeNames, appearance: ThemeOptions['appearance']): PartialTheme {
   switch (name) {
     case 'Office':
       return themingModule ? createOfficeTheme({ appearance, paletteName: 'Dialogs_FluentSV' }).theme : {};
-    case 'Caterpillar':
-      return applyCaterpillarTheme(parent);
     default:
       return {};
   }
