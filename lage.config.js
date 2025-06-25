622840d242c97375c46660388475f33026f5a145
Saad Najmi <sanajmi@microsoft.com>
2025-06-25 15:56:53 -0700

    ci: link lockfile (#3918)
    * ci: link lockfile
    
    * yarn dedupe
    
    * f

--- lage.config.js
+++ lage.config.js
@@ -6,7 +6,7 @@ module.exports = {
     buildci: ['build', 'test', 'depcheck'],
     bundle: ['build-tools', 'build'],
     clean: [],
-    depcheck: ['build-tools'],
+    depcheck: ['build-tools', 'align-deps'],
     lint: ['build-tools'],
     prettier: ['build-tools'],
     ['prettier-fix']: ['build-tools'],
