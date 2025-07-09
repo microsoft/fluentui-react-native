21c0373de9e627005ef720f226cb6c6781331ef0
Jason Morse <jasonmo@microsoft.com>
2025-07-08 23:09:40 -0700

    fix script execution using node

--- scripts/src/utils/runScript.js
+++ scripts/src/utils/runScript.js
@@ -1,6 +1,32 @@
 // @ts-check
 
 const { spawn } = require('child_process');
+const path = require('path');
+
+/** @type {Record<string, string>} */
+const cmdToModule = {
+  tsc: 'typescript',
+};
+
+/**
+ * Get the path to a bin entrypoint for a js package.
+ * @param {string} command
+ * @returns {string}
+ */
+function getBinPath(command) {
+  const cmdModule = cmdToModule[command] ?? command;
+  const pkgJsonPath = require.resolve(`${cmdModule}/package.json`, {
+    paths: [process.cwd()],
+  });
+  if (!pkgJsonPath) {
+    throw new Error(`Could not find package.json for command: ${command}`);
+  }
+  const pkgBinPath = require(pkgJsonPath).bin?.[command];
+  if (!pkgBinPath) {
+    throw new Error(`Command "${command}" not found in package.json of ${cmdModule}`);
+  }
+  return path.join(path.dirname(pkgJsonPath), pkgBinPath);
+}
 
 /**
  * @param {string} command
@@ -8,7 +34,7 @@ const { spawn } = require('child_process');
  * @returns {Promise<number>}
  */
 export async function runScript(command, ...args) {
-  const spawnArgs = [command, ...args];
+  const spawnArgs = [getBinPath(command), ...args];
 
   return new Promise((resolve) => {
     spawn(process.execPath, spawnArgs, {
