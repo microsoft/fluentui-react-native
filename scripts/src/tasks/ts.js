8468bfea6c514dbac69389654a4f59bb3fc23b39
Jason Morse <jasonmo@microsoft.com>
2025-07-08 17:04:47 -0700

    move codegen helpers to utils folder

--- scripts/src/tasks/ts.js
+++ scripts/src/tasks/ts.js
@@ -1,29 +1,17 @@
 // @ts-check
 
-const path = require('path');
-const { tscTask, argv } = require('just-scripts');
-const libPath = path.resolve(process.cwd(), 'lib');
-const srcPath = path.resolve(process.cwd(), 'src');
+const { runScript } = require('../utils/runScript.js');
 
-function getExtraTscParams(args) {
-  return { pretty: true, target: 'es5', ...(args.production && { inlineSources: true, sourceRoot: path.relative(libPath, srcPath) }) };
-}
+const TSC_COMMAND = 'tsc';
 
 module.exports.ts = {
   commonjs: () => {
-    const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib-commonjs', module: 'commonjs' });
+    return runScript(TSC_COMMAND, '--outDir', 'lib-commonjs');
   },
   esm: () => {
-    const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib', module: 'esnext' });
-  },
-  amd: () => {
-    const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib-amd', module: 'amd' });
+    return runScript(TSC_COMMAND, '--outDir', 'lib', '--module', 'esnext');
   },
   commonjsOnly: () => {
-    const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib', module: 'commonjs' });
-  }
+    return runScript(TSC_COMMAND, '--outDir', 'lib');
+  },
 };
