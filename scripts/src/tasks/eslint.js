8425961cbe65419ffdebcbf71b203b2d6343e6ac
Jason Morse <jasonmo@microsoft.com>
2025-07-10 17:06:55 -0700

    fix lint configuration with new format

--- scripts/src/tasks/eslint.js
+++ scripts/src/tasks/eslint.js
@@ -1,7 +1,19 @@
 // @ts-check
 
-import { argv, eslintTask } from 'just-scripts';
-export const eslint = eslintTask({
-  files: ['src/'],
-  ...argv(),
-});
+const { argv } = require('just-scripts');
+const { runScript } = require('../utils/runScript.js');
+
+/**
+ * @returns {import('just-scripts').TaskFunction}
+ */
+module.exports.eslint = () => {
+  return async (done) => {
+    const args = argv().fix ? ['--fix'] : [];
+    const result = await runScript('eslint', 'src/', ...args);
+    if (result !== 0) {
+      done(new Error(`ESLint failed with exit code ${result}`));
+    } else {
+      done();
+    }
+  };
+};
