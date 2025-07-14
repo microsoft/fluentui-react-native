f26615ce988786b1e5ebc9b66e476f2d5c628a5b
Jason Morse <jasonmo@microsoft.com>
2025-07-14 10:42:49 -0700

    fix align-deps (which requires .cjs) and remove 'react-dom' preset

--- scripts/configs/align-deps-preset.cjs
+++ scripts/configs/align-deps-preset.cjs
@@ -5,22 +5,12 @@ module.exports = {
       version: '^0.73.0',
       capabilities: ['core'],
     },
-    'react-dom': {
-      name: 'react-dom',
-      version: '~18.2.0',
-      capabilities: ['react'],
-    },
   },
   0.74: {
     'core-win32': {
       name: '@office-iss/react-native-win32',
       version: '^0.74.0',
       capabilities: ['core'],
-    },
-    'react-dom': {
-      name: 'react-dom',
-      version: '~18.2.0',
-      capabilities: ['react'],
     },
   },
 };
