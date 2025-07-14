5ecef3d7d01d33e1de615d074e5adf8e3a983a52
Jason Morse <jasonmo@microsoft.com>
2025-07-13 23:14:10 -0700

    finish script conversion to modern js with jsdoc syntax

--- scripts/src/tasks/jest.js
+++ scripts/src/tasks/jest.js
@@ -1,13 +1,20 @@
 // @ts-check
 
-import { argv } from 'just-scripts';
+import { argv, logger } from 'just-scripts';
 import { runScript } from '../utils/runScript.js';
+import fs from 'fs';
+import path from 'path';
 
 /**
  * @returns {import('just-scripts').TaskFunction}
  */
 export function jest() {
   return async (done) => {
+    if (!fs.existsSync(path.join(process.cwd(), './jest.config.js'))) {
+      logger.warn('No jest configuration found, skipping jest.');
+      done();
+      return;
+    }
     const args = ['--passWithNoTests'];
     if (process.env.TF_BUILD) {
       args.push('--runInBand');
