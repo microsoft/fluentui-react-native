98da8e8cd4b479774d05f451c49c4829fefb4de4
Jason Morse <jasonmo@microsoft.com>
2026-01-22 13:02:51 -0800

    split out tester implementation into a separate package

--- apps/fluent-tester/index.js
+++ apps/fluent-tester/index.js
@@ -1,13 +1,1 @@
-'use strict';
-
-import { AppRegistry, LogBox } from 'react-native';
-
-import { FluentTesterApp } from './src/FluentTesterApp';
-
-// When selecting a test page, the test page button might be under the yellowbox,
-//so we have to ensure that no yellowboxes are shown on boot that could affect test selection
-LogBox.ignoreLogs([/.*deprecated.*/, /.*Platform is not supported.*/])
-
-AppRegistry.registerComponent('FluentTester', () => FluentTesterApp);
-
-export default FluentTesterApp;
+require('./src/index');
