9729c0400c3288e03862dacdc6eb4229411c8894
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2026-01-23 14:19:23 -0800

    RN 0.81 - Step 2 - Split fluent-tester implementation into a new package (#3990)

---
+++ apps/fluent-tester/src/index.tsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import { AppRegistry, LogBox } from 'react-native';
+
+import { FluentTesterApp } from '@fluentui-react-native/tester-core';
+
+// When selecting a test page, the test page button might be under the yellowbox,
+//so we have to ensure that no yellowboxes are shown on boot that could affect test selection
+LogBox.ignoreLogs([/.*deprecated.*/, /.*Platform is not supported.*/]);
+
+AppRegistry.registerComponent('FluentTester', () => FluentTesterApp);
+
+export default FluentTesterApp;
--- apps/win32/src/index.tsx
+++ apps/win32/src/index.tsx
@@ -2,7 +2,7 @@
 
 import { AppRegistry } from 'react-native';
 
-import { FluentTesterApp } from '@fluentui-react-native/tester';
+import { FluentTesterApp } from '@fluentui-react-native/tester-core';
 
 AppRegistry.registerComponent('FluentTester', () => FluentTesterApp);
 
