ce3ff71f6f9c9f623bf5f3cc0c07cde14929542b
Jason Morse <jasonmo@microsoft.com>
2026-01-29 15:37:19 -0800

    add win32 test app for 0.81

---
+++ apps/win32-81/metro.config.js
@@ -0,0 +1,43 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+// @ts-check
+const { exclusionList, makeMetroConfig, resolveUniqueModule } = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+const { getDefaultConfig } = require('metro-config');
+
+const excludeMixins = [];
+const extraNodeModules = {};
+function ensureUniqueModule(moduleName, excludeList, nodeModules) {
+  const [nmEntry, excludePattern] = resolveUniqueModule(moduleName);
+  excludeMixins.push(excludePattern);
+  extraNodeModules[moduleName] = nmEntry;
+}
+
+// build up the added excludes and extraNodeModules
+['react-native-svg'].forEach((moduleName) => ensureUniqueModule(moduleName));
+
+module.exports = async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig(__dirname);
+
+  return makeMetroConfig({
+    resolver: {
+      assetExts: [...assetExts.filter((ext) => ext !== 'svg'), 'ttf', 'otf', 'png'],
+      blockList: exclusionList(excludeMixins),
+      extraNodeModules: {
+        ...extraNodeModules,
+      },
+      sourceExts: [...sourceExts, 'svg'],
+      resolveRequest: MetroSymlinksResolver(),
+    },
+    transformer: {
+      // This transformer selects between the regular transformer and svg transformer depending on the file type
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+  });
+};
