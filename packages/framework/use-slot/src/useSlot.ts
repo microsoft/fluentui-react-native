6da801f360fa544b96fdc49637e9ec16f84a5003
Jason Morse <jasonmo@microsoft.com>
2026-01-29 15:11:24 -0800

    fix useSlot implementation

--- packages/framework/use-slot/src/useSlot.ts
+++ packages/framework/use-slot/src/useSlot.ts
@@ -5,38 +5,58 @@ import type { PropsFilter, FunctionComponent } from '@fluentui-react-native/fram
 
 export type ComponentType<TProps> = React.ComponentType<TProps>;
 
+type SlotData<TProps> = {
+  innerComponent: React.ComponentType<TProps>;
+  propsToMerge?: TProps;
+};
+
 /**
  * useSlot hook function, allows authoring against pluggable slots as well as allowing components to be called as functions rather than
  * via createElement if they support it.
  *
  * @param component - any kind of component that can be rendered as part of the tree
- * @param initialProps - props, particularly the portion that includes styles, that should be passed to the component. These will be merged with what are specified in the JSX tree
+ * @param hookProps - props, particularly the portion that includes styles, that should be passed to the component. These will be merged with what are specified in the JSX tree
  * @param filter - optional filter that will prune the props before forwarding to the component
  * @returns
  */
 export function useSlot<TProps>(
   component: React.ComponentType<TProps>,
-  initialProps: Partial<TProps>,
+  hookProps?: Partial<TProps>,
   filter?: PropsFilter,
 ): FunctionComponent<TProps> {
-  // filter the initial props if a filter is specified
-  const filteredProps = filterProps(initialProps, filter);
+  // create this once for this hook instance to hold slot data between phases
+  const slotData = React.useMemo(() => {
+    return {} as SlotData<TProps>;
+  }, []);
+
+  // see if this component is a phased render component
+  const phasedRender = getPhasedRender<TProps>(component);
+  if (phasedRender) {
+    // if it is, run the first phase now with the hook props
+    slotData.innerComponent = phasedRender(hookProps as TProps);
+    slotData.propsToMerge = undefined;
+  } else {
+    // otherwise pass the hook props directly to the component
+    slotData.innerComponent = component;
+    slotData.propsToMerge = hookProps as TProps;
+  }
 
   // build the secondary processing function and the result holder, done via useMemo so the function identity stays the same. Rebuilding the closure every time would invalidate render
-  return React.useMemo<FunctionComponent<TProps>>(() => {
-    // extract the phased component function if that pattern is being used, will be undefined if it is a standard component
-    const phasedRender = getPhasedRender<TProps>(component as React.ComponentType<TProps>);
-
-    // do the first phase render with the initial props if we are using the staged pattern. This is typically getting
-    // styles and tokens in place a single time for the component.
-    const finalRender = phasedRender ? phasedRender(initialProps as TProps) : component;
-
-    // now return a direct component function that can be used in JSX/TSX, this pattern is safe since we won't be using
-    // hooks in this closure
-    return directComponent<TProps>((innerProps: TProps) => {
-      const finalInner = filterProps<TProps>(innerProps, filter);
-      const finalProps = phasedRender ? finalInner : mergeProps(filteredProps, finalInner);
-      return renderForJsxRuntime(finalRender as React.ComponentType<TProps>, finalProps);
-    });
-  }, [component, filter]);
+  return React.useMemo<FunctionComponent<TProps>>(
+    () =>
+      directComponent<TProps>((innerProps: TProps) => {
+        const { propsToMerge, innerComponent } = slotData;
+        if (propsToMerge) {
+          // merge in props from phase one if they haven't been captured in the phased render
+          innerProps = mergeProps<TProps>(propsToMerge, innerProps);
+        }
+        if (filter) {
+          // filter the final props if a filter is specified
+          innerProps = filterProps<TProps>(innerProps, filter);
+        }
+        // now render the component with the final props
+        return renderForJsxRuntime(innerComponent, innerProps);
+      }),
+    [component, filter, slotData],
+  );
 }
