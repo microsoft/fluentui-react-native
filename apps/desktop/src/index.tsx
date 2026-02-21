9234813748120fcb2da037992a7e59afdde6ebe1
Jason Morse <jasonmo@microsoft.com>
2026-02-20 16:02:11 -0800

    try to fix 81 testers

--- apps/desktop/src/index.tsx
+++ apps/desktop/src/index.tsx
@@ -1,11 +1,12 @@
 import { AppRegistry, LogBox } from 'react-native';
 
-import { FluentTesterApp as DesktopTester } from '@fluentui-react-native/tester-core';
+import { FluentTesterApp } from '@fluentui-react-native/tester-core';
 
 // When selecting a test page, the test page button might be under the yellowbox,
 //so we have to ensure that no yellowboxes are shown on boot that could affect test selection
 LogBox.ignoreLogs([/.*deprecated.*/, /.*Platform is not supported.*/]);
 
-AppRegistry.registerComponent('DesktopTester', () => DesktopTester);
+console.log('App is', FluentTesterApp);
+AppRegistry.registerComponent('DesktopTester', () => FluentTesterApp);
 
-export default DesktopTester;
+export default FluentTesterApp;
