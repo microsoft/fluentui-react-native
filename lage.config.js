92ccbabacb3e194f72556e3628e8094445438540
Jason Morse <jasonmo@microsoft.com>
2026-02-27 15:10:50 -0800

    update lage with formatting changes

--- lage.config.js
+++ lage.config.js
@@ -1,60 +1,48 @@
 /** @type {import('lage').ConfigOptions} */
 const config = {
-  npmClient: "yarn",
+  npmClient: 'yarn',
   pipeline: {
-    "build-cjs": {
+    'build-cjs': {
       // cjs builds need to wait for the esm builds to produce the type definitions
-      dependsOn: ["^build-core", "^build-cjs"],
-      inputs: ["*", "src/**/*", "assets/**/*"],
-      outputs: ["lib-commonjs/**/*"],
+      dependsOn: ['^build-core', '^build-cjs'],
+      inputs: ['*', 'src/**/*', 'assets/**/*'],
+      outputs: ['lib-commonjs/**/*'],
     },
-    "build-core": {
+    'build-core': {
       // the core build does esm builds (which produce type definitions used by both cjs and esm builds)
       // this also handles noEmit packages which should be run in sequence with other packages
-      dependsOn: ["^build-core"],
-      inputs: ["*", "src/**/*", "assets/**/*"],
-      outputs: ["lib/**/*"],
+      dependsOn: ['^build-core'],
+      inputs: ['*', 'src/**/*', 'assets/**/*'],
+      outputs: ['lib/**/*'],
     },
-    "build-all": {
-      dependsOn: ["build-core", "build-cjs"],
-      inputs: ["*", "src/**/*", "assets/**/*"],
-      outputs: ["lib/**/*", "lib-commonjs/**/*"],
+    'build-all': {
+      dependsOn: ['build-core', 'build-cjs'],
+      inputs: ['*', 'src/**/*', 'assets/**/*'],
+      outputs: ['lib/**/*', 'lib-commonjs/**/*'],
     },
-    buildci: ["build-all", "test", "lint", "lint-package"],
+    buildci: ['build-all', 'test', 'lint', 'lint-package'],
     bundle: {
-      inputs: [
-        "**/*",
-        "!node_modules/**/*",
-        "!dist/**/*",
-        "!lib/**/*",
-        "!lib-commonjs/**/*",
-      ],
-      outputs: ["dist/**/*"],
+      inputs: ['**/*', '!node_modules/**/*', '!dist/**/*', '!lib/**/*', '!lib-commonjs/**/*'],
+      outputs: ['dist/**/*'],
     },
     clean: {
       cache: false,
     },
     lint: {
-      inputs: ["*", "src/**/*"],
+      inputs: ['*', 'src/**/*'],
       outputs: [],
     },
-    "lint-package": {
-      inputs: [
-        "**/*",
-        "!node_modules/**/*",
-        "!dist/**/*",
-        "!lib/**/*",
-        "!lib-commonjs/**/*",
-      ],
+    'lint-package': {
+      inputs: ['**/*', '!node_modules/**/*', '!dist/**/*', '!lib/**/*', '!lib-commonjs/**/*'],
       outputs: [],
     },
     format: {
-      inputs: ["*", "src/**/*"],
+      inputs: ['*', 'src/**/*'],
       outputs: [],
     },
-    "pr-check": ["buildci", "lint-package", "lint-lockfile", "format:check"],
+    'pr-check': ['buildci', 'lint-package', 'lint-lockfile', 'format:check'],
     test: {
-      dependsOn: ["build-all"],
+      dependsOn: ['build-all'],
       inputs: [],
       outputs: [],
     },
