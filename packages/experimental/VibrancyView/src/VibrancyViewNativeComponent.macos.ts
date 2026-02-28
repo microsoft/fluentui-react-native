a3bb282aabf6b880c14242b276c6dc42bdda06fa
Jason Morse <jasonmo@microsoft.com>
2026-02-27 16:43:57 -0800

    fix menu button, tooltip and vibrancy view

--- packages/experimental/VibrancyView/src/VibrancyViewNativeComponent.macos.ts
+++ packages/experimental/VibrancyView/src/VibrancyViewNativeComponent.macos.ts
@@ -1,5 +1,27 @@
-import type { HostComponent } from 'react-native';
-import type { NativeProps } from './VibrancyView.types';
+import type { HostComponent, ViewProps } from 'react-native';
+import type { WithDefault } from 'react-native/Libraries/Types/CodegenTypes';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+
+export interface NativeProps extends ViewProps {
+  material?: WithDefault<
+    | 'titlebar'
+    | 'selection'
+    | 'menu'
+    | 'popover'
+    | 'sidebar'
+    | 'headerview'
+    | 'sheet'
+    | 'windowbackground'
+    | 'hudWindow'
+    | 'fullScreenUI'
+    | 'toolTip'
+    | 'contentBackground'
+    | 'underWindowBackground'
+    | 'underPageBackground',
+    'menu'
+  >;
+  blendingMode?: WithDefault<'behindWindow' | 'withinWindow', 'behindWindow'>;
+  state?: WithDefault<'followsWindowActiveState' | 'active' | 'inactive', 'followsWindowActiveState'>;
+}
 
 export default codegenNativeComponent<NativeProps>('FRNVibrancyView') as HostComponent<NativeProps>;
