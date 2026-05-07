971dd14dfe83238aa601f8e64a801bdde5957fe6
Jason Morse <jasonmo@microsoft.com>
2026-05-07 10:40:26 -0700

    some cleanup to fix pipeline breaks

--- apps/win32-81/src/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-import { AppRegistry } from 'react-native';
-import { FluentTesterApp } from '@fluentui-react-native/tester-core';
-AppRegistry.registerComponent('FluentTester', () => FluentTesterApp);
-export default FluentTesterApp;
-//# sourceMappingURL=index.js.map
--- apps/win32/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_native_1 = require("react-native");
-const tester_core_1 = require("@fluentui-react-native/tester-core");
-react_native_1.AppRegistry.registerComponent('FluentTester', () => tester_core_1.FluentTesterApp);
-exports.default = tester_core_1.FluentTesterApp;
-//# sourceMappingURL=index.js.map
