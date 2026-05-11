5378f5858ce5b77a0460e9ad1bf9a3adcb84a9a7
Jason Morse <jasonmo@microsoft.com>
2026-05-11 10:09:40 -0700

    add changeset and comments to constraints file

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -4,7 +4,9 @@ const { constrain } = require('./scripts/src/index.ts');
 module.exports = defineConfig({
   constraints: async ({ Yarn }) => {
     for (const workspace of Yarn.workspaces()) {
+      // skip the root workspace, as it should be be evaluated as a standard package
       if (workspace.cwd !== '.') {
+        // call through to the scripts constrain implementation that will do validation of each workspace one by one
         constrain(workspace);
       }
     }
