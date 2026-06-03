ce37a6d22059c447e46525ac87f91935ee62906d
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2026-06-03 10:38:46 -0700

    Remove last config package and move to root level package linting (#4109)
    * move all rnx-kit packages to the catalog
    
    * add core configuration utilities
    
    * move kit config into scripts
    
    * add knip configuration
    
    * remove lint-repo command and add yarn constraints run
    
    * adjust lage runs to use the lint-repo commands
    
    * remove leading message for lint-repo
    
    * update lockfile to use catalogs for knip
    
    * add changeset and comments to constraints file

---
+++ knip.config.ts
@@ -0,0 +1,54 @@
+import type { KnipConfig } from 'knip';
+import { repoContext } from './scripts/src/repoContext.ts';
+import { PackageContext } from './scripts/src/pkgContext.ts';
+import path from 'node:path';
+
+const ignoredPaths = ['apps', 'scripts', 'packages/configs', 'packages/dependency-profiles'];
+
+const config = async (): Promise<KnipConfig> => {
+  const repoCtx = repoContext();
+  // filter the workspace dirs to not include the ignored paths
+  const workspaceDirs = repoCtx.workspaces.filter(
+    (dir: string) => !ignoredPaths.some((ignored) => dir.startsWith(path.join(repoCtx.root, ignored))),
+  );
+  // now fill in the details for those
+  const workspaceDetails = Object.fromEntries(
+    workspaceDirs.map((dir: string) => {
+      const relativePath = path.posix.normalize(path.relative(process.cwd(), dir));
+      if (relativePath === '.') {
+        // skip the root dir
+        return [relativePath, {}];
+      }
+      const ctx = PackageContext.init(dir);
+      const extraEntries = ctx.manifest.furn?.knip?.extraEntries;
+      const knip = ctx.manifest.furn?.knip || {};
+      const baseEntry = knip.entry ?? ['src/**/*.{ts,tsx}'];
+      const entry = extraEntries ? [...baseEntry, ...extraEntries] : baseEntry;
+      const baseProject = knip.project ?? ['src/**/*'];
+      const project = knip.extraProject ? [...baseProject, ...knip.extraProject] : baseProject;
+
+      const ignoreDependencies: string[] = [];
+
+      return [
+        relativePath,
+        {
+          entry,
+          project,
+          ignoreDependencies,
+        },
+      ];
+    }),
+  );
+
+  const base: KnipConfig = {
+    include: ['dependencies'],
+    exclude: ['optionalPeerDependencies', 'devDependencies', 'unlisted'],
+    ignore: ignoredPaths.map((path) => `${path}/**`),
+    metro: false,
+    workspaces: workspaceDetails,
+  };
+
+  return base;
+};
+
+export default config;
