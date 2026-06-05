eadc25815c4cde9065f8540b7dbf9ce401eaffed
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-06-05 10:02:04 -0700

    Launch packager for RNW E2E test

--- apps/E2E/launchE2EWindows.ps1
+++ apps/E2E/launchE2EWindows.ps1
@@ -4,10 +4,21 @@
 
 #Start-Process -FilePath "$Env:ComSpec" -ArgumentList "/C", "start", "shell:AppsFolder\$pkgName!App"
 
-#Give app chance to create window
-#Start-Sleep -Seconds 1
+for ($i = 1; $i -le 10; $i++) {
+
+  if (Get-Process ReactApp -ErrorAction Ignore)
+  {
+    Write-Host "ReactApp is running"
+    break;
+  }
+
+  Write-Host "Waiting for app to start..."
+
+  #Give app chance to create window
+  Start-Sleep -Seconds 1
+}
 
 #Set env var for wdio.conf.windows to use as appium:appTopLevelWindow
-$env:E2ETEST_APP_TOP_LEVEL_WINDOW = "0x" +(Get-Process ReactApp).MainWindowHandle.ToString("x")
+$env:E2ETEST_APP_TOP_LEVEL_WINDOW = "0x" +(Get-Process ReactApp -ErrorAction Stop).MainWindowHandle.ToString("x")
 
 yarn "e2etest:windows"
