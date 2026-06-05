e71defd374d290371d8d80bb0a3dec0a956487db
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-06-05 15:20:00 -0700

    Maybe get dump files for e2e windows crashes

--- apps/E2E/launchE2EWindows.ps1
+++ apps/E2E/launchE2EWindows.ps1
@@ -1,8 +1,19 @@
+param ([switch]$enableDumpFiles = $false)
 
 #Find package name of the react test app
 $pkgName = (Get-AppxPackage -Name "40411fc5-8e92-4d46-b68d-b62df44b1366").PackageFamilyName
 
 Start-Process -FilePath "$Env:ComSpec" -ArgumentList "/C", "start", "shell:AppsFolder\$pkgName!App"
+
+
+if ($enableDumpFiles) {
+  sudo reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" /f
+  sudo reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps\ReactApp.exe" /v DumpType /t REG_DWORD /d 2 /f
+  sudo reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps\ReactApp.exe" /v DumpFolder /t REG_EXPAND_SZ /d "$PSScriptRoot\errorShots" /f
+
+  # Give time for regkeys to be set
+  Start-Sleep -Seconds 5
+}
 
 for ($i = 1; $i -le 10; $i++) {
 
