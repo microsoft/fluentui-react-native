5b5afea1ec42f26fbb74c445422e98aaef195273
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2026-07-08 09:48:13 -0700

    feat(scripts): Allow jest tests to run multiple platforms, scripts cleanup (#4136)
    * add jest multiplexing and remove deprecated/unused scripts
    
    * fix Stack test (that wasn't running before)
    
    * merge in changes and fix break
    
    * add changeset

--- scripts/src/cli.ts
+++ scripts/src/cli.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
 import { Builtins, Cli } from 'clipanion';
-import { BuildCommand } from './tasks/build.ts';
 import { CleanCommand } from './tasks/clean.ts';
 import { FormatCommand } from './tasks/format.ts';
 import { LintCommand } from './tasks/lint.ts';
 import { JestCommand } from './tasks/jest.ts';
 import { CheckPublishingCommand } from './tasks/checkPublishingTask.ts';
-import { DepcheckCommand } from './tasks/depcheck.ts';
 
 const cli = new Cli({
   binaryLabel: 'fluentui-scripts CLI',
@@ -15,13 +13,11 @@ const cli = new Cli({
   binaryVersion: '0.0.0',
 });
 
-cli.register(BuildCommand);
 cli.register(CleanCommand);
 cli.register(FormatCommand);
 cli.register(LintCommand);
 cli.register(JestCommand);
 cli.register(CheckPublishingCommand);
-cli.register(DepcheckCommand);
 
 cli.register(Builtins.DefinitionsCommand);
 cli.register(Builtins.HelpCommand);
