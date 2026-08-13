c50d267ee9ddac056ec731f3859d2b515a9c69e9
Jason Morse <jasonmo@microsoft.com>
2026-08-13 08:45:39 -0700

    index updates for components package

--- packages/agentic-components/src/index.ts
+++ packages/agentic-components/src/index.ts
@@ -1,4 +1,6 @@
 export { Button } from './components/button/button';
 export type { ButtonAppearance, ButtonIconPosition, ButtonProps, ButtonShape, ButtonSize } from './components/button/button.types';
+export { Switch } from './components/switch/switch';
+export type { SwitchLayout, SwitchProps, SwitchSlots } from './components/switch/switch.types';
 export { Icon } from './primitives/icon/icon';
 export type { FontIconSource, IconElementProps, IconProps, SvgIconSource } from './primitives/icon/icon.types';
