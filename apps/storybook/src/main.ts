ccbf1bc9991dc299c418a0d04704d406771f3243
Jason Morse <jasonmo@microsoft.com>
2026-08-19 14:46:02 -0700

    fix merge

--- apps/storybook/src/main.ts
+++ apps/storybook/src/main.ts
@@ -8,19 +8,10 @@ import type { StorybookConfig } from '@storybook/react-native';
  * (`apps/storybook/src`).
  */
 const main: StorybookConfig = {
-<<<<<<< HEAD
-  stories: [
-    '../../../packages/agentic-components/src/**/*.stories.?(ts|tsx)',
-    '../../../packages/native/Callout/src/**/*.stories.?(ts|tsx)',
-  ],
-||||||| 853f1fd1d
-  stories: ['../../src/**/*.stories.?(ts|tsx)', '../../../native/Callout/src/**/*.stories.?(ts|tsx)'],
-=======
   stories: [
     '../../../packages/agentic-components/src/**/*.stories.?(ts|tsx)',
     '../../../packages/native/callout/src/**/*.stories.?(ts|tsx)',
   ],
->>>>>>> 29f00b3223cfa916b30552f546b32c45308d0c04
   addons: [],
   deviceAddons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
 };
